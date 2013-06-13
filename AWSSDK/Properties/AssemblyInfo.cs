50d85ea6443e83820c7381b5a1dc7ed7bc3d2b96
amazonwebservices <aws-dr-tools@amazon.com>
2013-06-13 14:13:12 -0700

    Version 1.5.23.1 of the AWS .NET SDK This release re-enables Travis CI build support

--- AWSSDK/Properties/AssemblyInfo.cs
+++ AWSSDK/Properties/AssemblyInfo.cs
@@ -54,8 +54,8 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.23.0")]
-[assembly: AssemblyFileVersion("1.5.23.0")]
+[assembly: AssemblyVersion("1.5.23.1")]
+[assembly: AssemblyFileVersion("1.5.23.1")]
 
 [assembly: System.CLSCompliant(true)]
 [assembly: System.Security.AllowPartiallyTrustedCallers]
