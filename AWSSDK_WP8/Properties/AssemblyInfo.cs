8a50d0525f44659eedc925b34d9845f2c00d01f8
PavelSafronov <pavel@amazon.com>
2013-09-23 12:37:37 -0700

    Version 2.0.0.5 of the AWS SDK for .NET (Preview)

--- AWSSDK_WP8/Properties/AssemblyInfo.cs
+++ AWSSDK_WP8/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.0.0.4")]
-[assembly: AssemblyFileVersion("2.0.0.4")]
+[assembly: AssemblyVersion("2.0.0.5")]
+[assembly: AssemblyFileVersion("2.0.0.5")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
