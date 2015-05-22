307e0348842b85dd881372d10583b66428beb873
Norm Johanson <Norm Johanson>
2015-05-21 18:26:18 -0700

    Commit version number update changes

--- sdk/src/Services/Kinesis/Generated/AmazonKinesisConfig.cs
+++ sdk/src/Services/Kinesis/Generated/AmazonKinesisConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.Kinesis
     public partial class AmazonKinesisConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.0.0.2");
+            InternalSDKUtils.BuildUserAgentString("3.0.1.0");
 
         private string _userAgent = UserAgentString;
 
