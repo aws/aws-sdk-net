5b38c1cb7597370269468bef1758eb08cb067ab4
Karthik Saligrama <saligram@amazon.com>
2015-06-08 15:01:08 -0700

    application settings on ios and android

--- sdk/src/PCLCommon/PCLCommon.Android/Implementations/ApplicationSettings.cs
+++ sdk/src/PCLCommon/PCLCommon.Android/Implementations/ApplicationSettings.cs
@@ -1,27 +1,28 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 using Android.App;
 using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 
 namespace Amazon.Common
 {
     public class ApplicationSettings : IApplicationSettings
     {
+        private const string SETTINGS_STORE_NAME = "AWSSDK_SETTINGS";
+
+        //application settings mode are curently not supported on android
         public void SetValue(string key, string value, ApplicationSettingsMode mode)
         {
+            var sharedPreferences = Application.Context.GetSharedPreferences(SETTINGS_STORE_NAME, FileCreationMode.Private);
+            var sharedPreferencesEditor = sharedPreferences.Edit();
+            sharedPreferencesEditor.PutString(key, value);
+            sharedPreferencesEditor.Commit();
             throw new NotImplementedException();
         }
 
         public string GetValue(string key, ApplicationSettingsMode mode)
         {
-            throw new NotImplementedException();
+            var sharedPreferences = Application.Context.GetSharedPreferences(SETTINGS_STORE_NAME, FileCreationMode.Private);
+            return sharedPreferences.GetString(key, string.Empty);;
         }
     }
 }
--- sdk/src/PCLCommon/PCLCommon.iOS/Implementations/ApplicationSettings.cs
+++ sdk/src/PCLCommon/PCLCommon.iOS/Implementations/ApplicationSettings.cs
@@ -1,10 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 using Foundation;
-using UIKit;
+using Security;
+using System;
+
 
 namespace Amazon.Common
 {
@@ -12,12 +9,50 @@ namespace Amazon.Common
     {
         public void SetValue(string key, string value, ApplicationSettingsMode mode)
         {
-            throw new NotImplementedException();
+            var rec = new SecRecord(SecKind.GenericPassword)
+            {
+                Generic = NSData.FromString(key)
+            };
+
+            //if the record already existings we will delete them, so that we dont get duplicate error, alternatively you can use the update api as well.
+            SecStatusCode res;
+            var match = SecKeyChain.QueryAsRecord(rec, out res);
+            if (res == SecStatusCode.Success)
+            {
+                SecKeyChain.Remove(match);
+            }
+
+            rec = new SecRecord(SecKind.GenericPassword)
+            {
+                Generic = NSData.FromString(key, NSStringEncoding.UTF8),
+                ValueData = NSData.FromString(value, NSStringEncoding.UTF8)
+            };
+
+            var err = SecKeyChain.Add(rec);
+
+            if (err != SecStatusCode.Success)
+            {
+                throw new Exception(string.Format("Application Settings exception, Unable to store key {0}, got error: {1}", key, err.ToString()));
+            }
         }
 
         public string GetValue(string key, ApplicationSettingsMode mode)
         {
-            throw new NotImplementedException();
+            var rec = new SecRecord(SecKind.GenericPassword)
+            {
+                Generic = NSData.FromString(key)
+            };
+
+            SecStatusCode res;
+            var match = SecKeyChain.QueryAsRecord(rec, out res);
+            if (res != SecStatusCode.Success)
+            {
+                return string.Empty;
+            }
+            else
+            {
+                return NSString.FromData(match.ValueData, NSStringEncoding.UTF8);
+            }
         }
     }
 }
