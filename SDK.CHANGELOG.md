3b177e7b911f4acd3588fe24286054849bb9c38b
Norm Johanson <Norm Johanson>
2015-06-19 16:02:32 -0700

    Commit version number update changes

---
+++ SDK.CHANGELOG.md
@@ -0,0 +1,6 @@
+### 3.0.4.0 (2015-06-19 22:54 UTC)
+* AutoScaling (3.0.1.0)
+* EC2 (3.0.4.0)
+	* Regen EC2 for latest API changes. The update contains the new VPC Flow Logs feature.
+* ECS (3.0.1.0)
+	* Regen ECS with latest service API updates.
--- sdk/src/Services/EC2/Generated/AmazonEC2Config.cs
+++ sdk/src/Services/EC2/Generated/AmazonEC2Config.cs
@@ -32,7 +32,7 @@ namespace Amazon.EC2
     public partial class AmazonEC2Config : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.0.3.0");
+            InternalSDKUtils.BuildUserAgentString("3.0.4.0");
 
         private string _userAgent = UserAgentString;
 
