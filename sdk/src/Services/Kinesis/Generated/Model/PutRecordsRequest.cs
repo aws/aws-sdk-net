007a612b9d0c8ca2a88315cea8848ad5ccea92ce
Jim Flanagan <jimfl@amazon.com>
2015-09-10 10:43:55 -0700

    Add timestamp field to Amazon.Kinesis.Model.Record type.

--- sdk/src/Services/Kinesis/Generated/Model/PutRecordsRequest.cs
+++ sdk/src/Services/Kinesis/Generated/Model/PutRecordsRequest.cs
@@ -29,17 +29,24 @@ namespace Amazon.Kinesis.Model
 {
     /// <summary>
     /// Container for the parameters to the PutRecords operation.
-    /// Puts (writes) multiple data records from a producer into an Amazon Kinesis stream
-    /// in a single call (also referred to as a <code>PutRecords</code> request). Use this
-    /// operation to send data from a data producer into the Amazon Kinesis stream for real-time
-    /// ingestion and processing. Each shard can support up to 1000 records written per second,
-    /// up to a maximum total of 1 MB data written per second.
+    /// Writes multiple data records from a producer into an Amazon Kinesis stream in a single
+    /// call (also referred to as a <code>PutRecords</code> request). Use this operation to
+    /// send data from a data producer into the Amazon Kinesis stream for data ingestion and
+    /// processing. 
     /// 
+    ///  
+    /// <para>
+    /// Each <code>PutRecords</code> request can support up to 500 records. Each record in
+    /// the request can be as large as 1 MB, up to a limit of 5 MB for the entire request,
+    /// including partition keys. Each shard can support writes up to 1,000 records per second,
+    /// up to a maximum data write total of 1 MB per second.
+    /// </para>
     ///  
     /// <para>
     /// You must specify the name of the stream that captures, stores, and transports the
     /// data; and an array of request <code>Records</code>, with each record in the array
-    /// requiring a partition key and data blob. 
+    /// requiring a partition key and data blob. The record size limit applies to the total
+    /// size of the partition key and data blob.
     /// </para>
     ///  
     /// <para>
@@ -52,7 +59,7 @@ namespace Amazon.Kinesis.Model
     /// the partition key and associated data to a specific shard. An MD5 hash function is
     /// used to map partition keys to 128-bit integer values and to map associated data records
     /// to shards. As a result of this hashing mechanism, all data records with the same partition
-    /// key map to the same shard within the stream. For more information, see <a href="http://docs.aws.amazon.com/kinesis/latest/dev/kinesis-using-sdk-java-add-data-to-stream.html">Adding
+    /// key map to the same shard within the stream. For more information, see <a href="http://docs.aws.amazon.com/kinesis/latest/dev/developing-producers-with-sdk.html#kinesis-using-sdk-java-add-data-to-stream">Adding
     /// Data to a Stream</a> in the <i>Amazon Kinesis Developer Guide</i>.
     /// </para>
     ///  
@@ -60,7 +67,7 @@ namespace Amazon.Kinesis.Model
     /// Each record in the <code>Records</code> array may include an optional parameter, <code>ExplicitHashKey</code>,
     /// which overrides the partition key to shard mapping. This parameter allows a data producer
     /// to determine explicitly the shard where the record is stored. For more information,
-    /// see <a href="http://docs.aws.amazon.com/kinesis/latest/dev/kinesis-using-sdk-java-add-data-to-stream.html#kinesis-using-sdk-java-putrecords">Adding
+    /// see <a href="http://docs.aws.amazon.com/kinesis/latest/dev/developing-producers-with-sdk.html#kinesis-using-sdk-java-putrecords">Adding
     /// Multiple Records with PutRecords</a> in the <i>Amazon Kinesis Developer Guide</i>.
     /// </para>
     ///  
