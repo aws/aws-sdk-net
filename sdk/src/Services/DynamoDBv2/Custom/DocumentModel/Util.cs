aws/aws-sdk-net

commit 444d735c2468fdf1977cd5fedcea70c6b041087c

Add PaginationToken support to DocumentModel Query and Scan operations.

sdk/src/Services/DynamoDBv2/Custom/DocumentModel/Util.cs
@@ -16,10 +16,12 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Text;
 
 using Amazon.DynamoDBv2.Model;
-using Amazon.Util;
+using Amazon.DynamoDBv2.Model.Internal.MarshallTransformations;
+using Amazon.Runtime.Internal.Transform;
 using Amazon.Util.Internal;
 
 #pragma warning disable 1591
@@ -119,6 +121,12 @@ namespace Amazon.DynamoDBv2.DocumentModel
     {
         None,
         All
+    }
+
+    internal enum SearchType
+    {
+        Scan,
+        Query
     }
 
     public class KeyDescription
@@ -454,11 +462,65 @@ namespace Amazon.DynamoDBv2.DocumentModel
 
             return result;
         }
+
+        public static string ToPaginationToken(Dictionary<string, AttributeValue> nextKey)
+        {
+            if (nextKey == null)
+                return null;
+
+            var clearToken = SerializeClearString(nextKey);
+            return clearToken;
+        }
+        private static string SerializeClearString(Dictionary<string, AttributeValue> nextKey)
+        {
+            using (var writer = new StringWriter())
+            {
+                WriteNextKey(nextKey, writer);
+
+                return writer.ToString();
+            }
+        }
+
+        public static Dictionary<string, AttributeValue> FromPaginationToken(string token)
+        {
+            if (token == null)
+                return null;
+
+            return DeserializeClearString(token);
+        }
+        private static Dictionary<string, AttributeValue> DeserializeClearString(string token)
+        {
+            var bytes = Encoding.UTF8.GetBytes(token);
+            using (var stream = new MemoryStream(bytes))
+            {
+                return ReadNextKey(stream);
+            }
+        }
+
+        private static Dictionary<string, AttributeValue> ReadNextKey(Stream stream)
+        {
+            JsonUnmarshallerContext context = new JsonUnmarshallerContext(stream, false, null);
+            var unmarshaller = new DictionaryUnmarshaller<string, AttributeValue, StringUnmarshaller, AttributeValueUnmarshaller>(StringUnmarshaller.Instance, AttributeValueUnmarshaller.Instance);
+            var nextKey = unmarshaller.Unmarshall(context);
+            return nextKey;
+        }
+        private static void WriteNextKey(Dictionary<string, AttributeValue> nextKey, TextWriter writer)
+        {
+            var marshaller = Amazon.DynamoDBv2.Model.Internal.MarshallTransformations.AttributeValueMarshaller.Instance;
+            var jsonWriter = new ThirdParty.Json.LitJson.JsonWriter(writer);
+            var context = new Runtime.Internal.Transform.JsonMarshallerContext(null, jsonWriter);
+
+            context.Writer.WriteObjectStart();
+            foreach (var kvp in nextKey)
+            {
+                context.Writer.WritePropertyName(kvp.Key);
+                context.Writer.WriteObjectStart();
+                {
+                    marshaller.Marshall(kvp.Value, context);
+                }
+                context.Writer.WriteObjectEnd();
+            }
+            context.Writer.WriteObjectEnd();
+        }
     }
- 
-    internal enum SearchType
-    {
-        Scan,
-        Query
-    }
-}
+ }

sdk/src/Services/DynamoDBv2/Custom/DocumentModel/Search.cs
@@ -115,6 +115,23 @@ namespace Amazon.DynamoDBv2.DocumentModel
         public Dictionary<string, AttributeValue> NextKey { get; private set; }
 
         /// <summary>
+        /// Pagination token corresponding to the item where the search operation stopped,
+        /// inclusive of the previous result set. Use this value to start a new
+        /// operation to resume search from the next item.
+        /// </summary>
+        public string PaginationToken
+        {
+            get
+            {
+                return Common.ToPaginationToken(NextKey);
+            }
+            internal set
+            {
+                NextKey = Common.FromPaginationToken(value);
+            }
+        }
+
+        /// <summary>
         /// List of currently found items
         /// </summary>
         public List<Document> Matches { get; private set; }

sdk/src/Services/DynamoDBv2/Custom/DocumentModel/Table.cs
@@ -747,7 +747,8 @@ namespace Amazon.DynamoDBv2.DocumentModel
                 Select = currentConfig.Select,
                 CollectResults = currentConfig.CollectResults,
                 IndexName = currentConfig.IndexName,
-                IsConsistentRead = currentConfig.ConsistentRead
+                IsConsistentRead = currentConfig.ConsistentRead,
+                PaginationToken = currentConfig.PaginationToken
             };
 
             if (currentConfig.TotalSegments != 0)
@@ -848,7 +849,8 @@ namespace Amazon.DynamoDBv2.DocumentModel
                 IsBackwardSearch = config.BackwardSearch,
                 IndexName = config.IndexName,
                 Select = config.Select,
-                CollectResults = config.CollectResults
+                CollectResults = config.CollectResults,
+                PaginationToken = config.PaginationToken
             };
 
             return ret;

sdk/src/Services/DynamoDBv2/Custom/DocumentModel/TableOperationConfigs.cs
@@ -253,6 +253,14 @@ namespace Amazon.DynamoDBv2.DocumentModel
         /// Default value is AND.
         /// </summary>
         public ConditionalOperatorValues ConditionalOperator { get; set; }
+
+        /// <summary>
+        /// Pagination token corresponding to the item where the last Scan operation
+        /// stopped, inclusive of the previous result set. Set this value to resume
+        /// Scan operation from the next item.
+        /// This token should be retrieved from a Search object.
+        /// </summary>
+        public string PaginationToken { get; set; }
     }
 
     /// <summary>
@@ -341,6 +349,14 @@ namespace Amazon.DynamoDBv2.DocumentModel
         /// Default value is AND.
         /// </summary>
         public ConditionalOperatorValues ConditionalOperator { get; set; }
+
+        /// <summary>
+        /// Pagination token corresponding to the item where the last Query operation
+        /// stopped, inclusive of the previous result set. Set this value to resume
+        /// Query operation from the next item.
+        /// This token should be retrieved from a Search object.
+        /// </summary>
+        public string PaginationToken { get; set; }
     }
 
     /// <summary>

sdk/test/IntegrationTests/Tests/DynamoDB/DocumentTests.cs
@@ -59,7 +59,102 @@ namespace AWSSDK_DotNet.IntegrationTests.Tests.DynamoDB
 
                 // Test expressions for scan
                 TestExpressionsOnScan(hashRangeTable);
+
+                // Test Query and Scan manual pagination
+                TestPagination(hashRangeTable);
             }
+        }
+
+        private void TestPagination(Table hashRangeTable)
+        {
+            var itemCount = 10;
+            var batchWrite = hashRangeTable.CreateBatchWrite();
+            var name = "Borg";
+            // Put items
+            for (int i = 0; i < itemCount; i++)
+            {
+                Document doc = new Document();
+                doc["Name"] = name;
+                doc["Age"] = 1 + i;
+                doc["Company"] = "Big River";
+                doc["Score"] = 120 + i;
+                doc["IsTester"] = true;
+                doc["Manager"] = "Kirk";
+                batchWrite.AddDocumentToPut(doc);
+            }
+            batchWrite.Execute();
+
+            // Paginated scan
+            {
+                var request = new ScanOperationConfig
+                {
+                    Limit = 1
+                };
+                var search = hashRangeTable.Scan(request);
+                var tokens = new List<string>();
+                var retrievedCount = VerifyPagination(search, tokens);
+                Assert.AreEqual(retrievedCount, itemCount);
+                Assert.AreEqual(itemCount, tokens.Count);
+
+                var token4 = tokens[4];
+                var token5 = tokens[5];
+                search = hashRangeTable.Scan(new ScanOperationConfig
+                {
+                    Limit = 1,
+                    PaginationToken = token4
+                });
+                search.GetNextSet();
+                Assert.AreNotEqual(token4, search.PaginationToken);
+                Assert.AreEqual(token5, search.PaginationToken);
+            }
+
+            // Paginated query
+            {
+                var filter = new QueryFilter("Name", QueryOperator.Equal, name);
+                var request = new QueryOperationConfig
+                {
+                    Limit = 1,
+                    Filter = filter
+                };
+                var search = hashRangeTable.Query(request);
+
+                var tokens = new List<string>();
+                var retrievedCount = VerifyPagination(search, tokens);
+                Assert.AreEqual(retrievedCount, itemCount);
+                Assert.AreEqual(itemCount, tokens.Count);
+
+                var token4 = tokens[4];
+                var token5 = tokens[5];
+                search = hashRangeTable.Query(new QueryOperationConfig
+                {
+                    Limit = 1,
+                    Filter = filter,
+                    PaginationToken = token4
+                });
+                search.GetNextSet();
+                Assert.AreNotEqual(token4, search.PaginationToken);
+                Assert.AreEqual(token5, search.PaginationToken);
+            }
+        }
+
+        private static int VerifyPagination(Search search, List<string> tokens)
+        {
+            int count = 0;
+            do
+            {
+                var items = search.GetNextSet();
+                var token = search.PaginationToken;
+                count += items.Count;
+
+                if (!search.IsDone)
+                {
+                    tokens.Add(token);
+                    Assert.AreNotEqual(0, items.Count);
+                    Assert.IsFalse(string.IsNullOrEmpty(token));
+                }
+            } while (!search.IsDone);
+
+            return count;
         }
 
         private void TestEmptyCollections(Table hashTable)

commit 31adcbcf474ddf694dbefbd64da0cfad653b0705

Fix FxCop issue introduced in commit 444d735c

sdk/src/Services/DynamoDBv2/Custom/DocumentModel/Util.cs
@@ -473,7 +473,7 @@ namespace Amazon.DynamoDBv2.DocumentModel
         }
         private static string SerializeClearString(Dictionary<string, AttributeValue> nextKey)
         {
-            using (var writer = new StringWriter())
+            using (var writer = new StringWriter(CultureInfo.InvariantCulture))
             {
                 WriteNextKey(nextKey, writer);
 