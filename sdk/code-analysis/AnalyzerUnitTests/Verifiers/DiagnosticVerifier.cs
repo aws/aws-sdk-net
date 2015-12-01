789990b62fe8266d3070c340db9bfe7fa40314ca
Steve Roberts <strobe@amazon.com>
2015-12-01 14:24:19 -0800

    Fixed missing )

--- sdk/code-analysis/AnalyzerUnitTests/Verifiers/DiagnosticVerifier.cs
+++ sdk/code-analysis/AnalyzerUnitTests/Verifiers/DiagnosticVerifier.cs
@@ -241,7 +241,7 @@ namespace TestHelper
                         {
                             Assert.IsTrue(location.IsInSource,
                                 string.Format("Test base does not currently handle diagnostics in metadata locations. Diagnostic in metadata: {0}\r\n",
-								              diagnostics[i]);
+								              diagnostics[i]));
 
                             string resultMethodName = diagnostics[i].Location.SourceTree.FilePath.EndsWith(".cs") ? "GetCSharpResultAt" : "GetBasicResultAt";
                             var linePosition = diagnostics[i].Location.GetLineSpan().StartLinePosition;
