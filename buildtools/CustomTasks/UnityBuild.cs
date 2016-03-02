a774b49bfa3df51381a9c1cb4181642f9c1fa6bd
Johnny Hoffman <jonahoff@amazon.com>
2016-03-01 16:46:13 -0800

    On device testing generalized and ported to C#

--- buildtools/CustomTasks/UnityBuild.cs
+++ buildtools/CustomTasks/UnityBuild.cs
@@ -10,6 +10,15 @@ namespace CustomTasks
         public string UnityExe { get; set; }
         public string DeploymentPath { get; set; }
         public string ProjectPath { get; set; }
+        // Unity command may intermittently fail with inability to access files
+        // it need, and still exit with code 0. Allow retrying if the
+        // application was not created.
+        public bool RetryOnApplicationMissing { get; set; }
+
+        private const int MaxTries = 5;
+        private string APKLocation;
+        private string IOS64BuildLocation;
+        private string IOSMonoBuildLocation;
 
         public override bool Execute()
         {
@@ -28,40 +37,72 @@ namespace CustomTasks
             var buildPath = String.Format(@"{0}{1}{2}{1}{3}", DeploymentPath, Path.DirectorySeparatorChar, @"unity", @"build");
             Environment.SetEnvironmentVariable("UNITYBUILDPATH", buildPath);
 
+            APKLocation = Path.Combine(buildPath, "Android", "UnityTestRunner.apk");
+            IOS64BuildLocation = Path.Combine(buildPath, "iOS64");
+            IOSMonoBuildLocation = Path.Combine(buildPath, "iOSMono");
+
             if (Platform.Equals("android", StringComparison.InvariantCultureIgnoreCase))
             {
-                return ExecuteAndroid();
+                return ExecuteAndroid(RetryOnApplicationMissing);
             }
             else if (Platform.Equals("ios64", StringComparison.InvariantCultureIgnoreCase))
             {
-                return ExecuteIos64();
+                return ExecuteIos64(RetryOnApplicationMissing);
             }
             else if (Platform.Equals("ios32", StringComparison.InvariantCultureIgnoreCase))
             {
-                return ExecuteIos32();
+                return ExecuteIos32(RetryOnApplicationMissing);
             }
             throw new ArgumentException("Invalid Platform");
         }
 
-        private bool ExecuteAndroid()
+        private bool ExecuteAndroid(bool retry = false)
         {
-            var command = string.Format(@"-quit -batchmode -projectPath {0} -executeMethod BuildTools.Android -logFile {1}", ProjectPath, string.Format(@"{0}{1}{2}{1}{3}{1}log-android.txt", DeploymentPath, Path.DirectorySeparatorChar, @"unity", @"build"));
-            UnityPackage.ExecuteUnityCommand(UnityExe, command);
-            return true;
+            int tryCount = 0;
+            bool success;
+            do
+            {
+                var command = string.Format(@"-quit -batchmode -projectPath {0} -executeMethod BuildTools.Android -logFile {1}", ProjectPath, string.Format(@"{0}{1}{2}{1}{3}{1}log-android.txt", DeploymentPath, Path.DirectorySeparatorChar, @"unity", @"build"));
+                UnityPackage.ExecuteUnityCommand(UnityExe, command);
+                success = File.Exists(APKLocation);
+                tryCount++;
+            }
+            while (retry && !success && tryCount <= MaxTries);
+            return success;
         }
 
-        private bool ExecuteIos64()
+        private bool ExecuteIos64(bool retry = false)
         {
-            var command = string.Format(@"-quit -batchmode -projectPath {0} -executeMethod BuildTools.Ios64 -logFile {1}", ProjectPath, string.Format(@"{0}{1}{2}{1}{3}{1}log-ios64.txt", DeploymentPath, Path.DirectorySeparatorChar, @"unity", @"build"));
-            UnityPackage.ExecuteUnityCommand(UnityExe, command);
-            return true;
+            int tryCount = 0;
+            bool success;
+            do
+            {
+                var command = string.Format(@"-quit -batchmode -projectPath {0} -executeMethod BuildTools.Ios64 -logFile {1}", ProjectPath, string.Format(@"{0}{1}{2}{1}{3}{1}log-ios64.txt", DeploymentPath, Path.DirectorySeparatorChar, @"unity", @"build"));
+                UnityPackage.ExecuteUnityCommand(UnityExe, command);
+                // Approximation of knowing if the app was created is checking
+                // if the build directory exists and some subdirectories exist.
+                success = Directory.Exists(IOS64BuildLocation) && (Directory.GetDirectories(IOS64BuildLocation).Length == 0);
+                tryCount++;
+            }
+            while (retry && !success && tryCount <= MaxTries);
+            return success;
         }
 
-        private bool ExecuteIos32()
+        private bool ExecuteIos32(bool retry = false)
         {
-            var command = string.Format(@"-quit -batchmode -projectPath {0} -executeMethod BuildTools.IosMono -logFile {1}", ProjectPath, string.Format(@"{0}{1}{2}{1}{3}{1}log-ios-mono.txt", DeploymentPath, Path.DirectorySeparatorChar, @"unity", @"build"));
-            UnityPackage.ExecuteUnityCommand(UnityExe, command);
-            return true;
+            int tryCount = 0;
+            bool success;
+            do
+            {
+                var command = string.Format(@"-quit -batchmode -projectPath {0} -executeMethod BuildTools.IosMono -logFile {1}", ProjectPath, string.Format(@"{0}{1}{2}{1}{3}{1}log-ios-mono.txt", DeploymentPath, Path.DirectorySeparatorChar, @"unity", @"build"));
+                UnityPackage.ExecuteUnityCommand(UnityExe, command);
+                // Approximation of knowing if the app was created is checking
+                // if the build directory exists and some subdirectories exist.
+                success = Directory.Exists(IOSMonoBuildLocation) && (Directory.GetDirectories(IOSMonoBuildLocation).Length > 0);
+                tryCount++;
+            }
+            while (retry && !success && tryCount <= MaxTries);
+            return success;
         }
     }
 }
---
+++ buildtools/OnDeviceTestingTasks/SetupTestSettings.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.Build.Utilities;
+using Newtonsoft.Json;
+using System.IO;
+using Amazon.Runtime;
+
+namespace OnDeviceTestingTasks
+{
+    public class SetupTestSettings : Task
+    {
+        private bool _forceCreate = true;
+        private string _region = "us-east-1";
+        public string SettingsRelativePath { get; set; }
+        public bool ForceCreate { get { return _forceCreate; } set { _forceCreate = value; } }
+        public string Region { get { return _region; } set { _region = value; } }
+
+        public override bool Execute()
+        {
+            if (string.IsNullOrEmpty(SettingsRelativePath))
+            {
+                throw new ArgumentNullException("SettingsRelativePath");
+            }
+
+            Log.LogMessage("Settings location '{0}' and ForceCreate '{1}'", SettingsRelativePath, ForceCreate);
+
+            bool alreadyExists = File.Exists(SettingsRelativePath);
+            if (alreadyExists && !ForceCreate)
+            {
+                Log.LogMessage("Settings file '{0}' already exists. Exiting.", SettingsRelativePath);
+                return true;
+            }
+            if (alreadyExists)
+            {
+                Log.LogMessage("Removing existing settings file '{0}'.", SettingsRelativePath);
+                File.Delete(SettingsRelativePath);
+            }
+            else
+            {
+                Log.LogMessage("No pre-existing settings file to remove.");
+            }
+
+            var credentials = FallbackCredentialsFactory.GetCredentials().GetCredentials();
+            var settings = new Settings()
+            {
+                AccessKeyId = credentials.AccessKey,
+                SecretAccessKey = credentials.SecretKey,
+                AccountId = "",
+                RegionEndpoint = Region
+            };
+
+            string json = JsonConvert.SerializeObject(settings);
+
+            using (StreamWriter writer = File.CreateText(SettingsRelativePath))
+            {
+                Log.LogMessage("Writing settings to '{0}'.", SettingsRelativePath);
+                writer.Write(json);
+                writer.Flush();
+            }
+            return true;
+        }
+    }
+
+    public class Settings
+    {
+        public string AccessKeyId;
+        public string SecretAccessKey;
+        public string RegionEndpoint;
+        public string AccountId;
+    }
+}
