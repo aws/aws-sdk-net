79ed93574cb479e4b32d2d1e3327e67bca999ceb
Steve Roberts <strobe@amazon.com>
2016-06-02 13:10:45 -0700

    Minor version updates due to build tool fixes

--- sdk/src/Services/ApplicationDiscoveryService/Generated/AmazonApplicationDiscoveryServiceConfig.cs
+++ sdk/src/Services/ApplicationDiscoveryService/Generated/AmazonApplicationDiscoveryServiceConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.ApplicationDiscoveryService
     public partial class AmazonApplicationDiscoveryServiceConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.1.0.0");
+            InternalSDKUtils.BuildUserAgentString("3.1.1.1");
 
         private string _userAgent = UserAgentString;
 
