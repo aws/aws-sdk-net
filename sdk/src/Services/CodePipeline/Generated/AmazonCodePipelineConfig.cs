b5864e305f07d0795b28fea3933aaecbe5f68022
Norm Johanson <normj@amazon.com>
2016-06-21 17:06:48 -0700

    Commit version number update changes

--- sdk/src/Services/CodePipeline/Generated/AmazonCodePipelineConfig.cs
+++ sdk/src/Services/CodePipeline/Generated/AmazonCodePipelineConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.CodePipeline
     public partial class AmazonCodePipelineConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.1.1.7");
+            InternalSDKUtils.BuildUserAgentString("3.1.2.0");
 
         private string _userAgent = UserAgentString;
 
