785e1e202d351814922307e6ccec8f327c786de7
Norm Johanson <normj@amazon.com>
2016-06-30 14:35:38 -0700

    Commit version number update changes

--- sdk/src/Core/Amazon.Util/Internal/InternalSDKUtils.generated.cs
+++ sdk/src/Core/Amazon.Util/Internal/InternalSDKUtils.generated.cs
@@ -29,6 +29,6 @@ namespace Amazon.Util.Internal
 {
     public static partial class InternalSDKUtils
     {
-        internal const string CoreVersionNumber = "3.1.8.0";
+        internal const string CoreVersionNumber = "3.1.8.1";
     }
 }
