610fa6bf08305952a0b3f07e11c4874346dd67d3
Steve Roberts <strobe@amazon.com>
2016-07-05 12:32:02 -0700

    Documentation update.

--- sdk/src/Services/IdentityManagement/Generated/Model/GetPolicyVersionRequest.cs
+++ sdk/src/Services/IdentityManagement/Generated/Model/GetPolicyVersionRequest.cs
@@ -32,7 +32,15 @@ namespace Amazon.IdentityManagement.Model
     /// Retrieves information about the specified version of the specified managed policy,
     /// including the policy document.
     /// 
-    ///  
+    ///  <note> 
+    /// <para>
+    /// Policies returned by this API are URL-encoded compliant with <a href="https://tools.ietf.org/html/rfc3986">RFC
+    /// 3986</a>. You can use a URL decoding method to convert the policy back to plain JSON
+    /// text. For example, if you use Java, you can use the <code>decode</code> method of
+    /// the <code>java.net.URLDecoder</code> utility class in the Java SDK. Other languages
+    /// and SDKs provide similar functionality.
+    /// </para>
+    ///  </note> 
     /// <para>
     /// To list the available versions for a policy, use <a>ListPolicyVersions</a>.
     /// </para>
