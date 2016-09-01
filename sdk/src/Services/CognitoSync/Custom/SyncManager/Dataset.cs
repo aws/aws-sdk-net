07c3d31dd42a7af61378d1e2e20f64722cfd0195
Andrey Timofeev <andrey.timofeev@mobirate.com>
2016-09-01 07:09:16 +0400

    Added try catch block around GetIdentityId during sync.

--- sdk/src/Services/CognitoSync/Custom/SyncManager/Dataset.cs
+++ sdk/src/Services/CognitoSync/Custom/SyncManager/Dataset.cs
@@ -689,15 +689,14 @@ namespace Amazon.CognitoSync.SyncManager
         private async Task SynchornizeInternalAsync(CancellationToken cancellationToken)
 #endif
         {
-
-            //make sure we have the latest identity id
-#if BCL35 || UNITY
-            CognitoCredentials.GetIdentityId();
-#else
-            await CognitoCredentials.GetIdentityIdAsync().ConfigureAwait(false);
-#endif
             try
             {
+                //make sure we have the latest identity id
+#if BCL35 || UNITY
+                CognitoCredentials.GetIdentityId();
+#else
+                await CognitoCredentials.GetIdentityIdAsync().ConfigureAwait(false);
+#endif
                 bool resume = true;
                 List<string> mergedDatasets = LocalMergedDatasets;
                 if (mergedDatasets.Count > 0)
@@ -730,6 +729,7 @@ namespace Amazon.CognitoSync.SyncManager
             }
             catch (Exception e)
             {
+                EndSynchronizeAndCleanup();
 #if UNITY
                 FireSyncFailureEvent(e, options);
 #else
