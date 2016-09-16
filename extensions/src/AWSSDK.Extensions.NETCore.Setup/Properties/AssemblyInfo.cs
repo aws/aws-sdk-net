87ea28be9ed4772e090f173018797e08ce12f9a6
Norm Johanson <normj@amazon.com>
2016-09-15 23:24:22 -0700

    Update AWSSDK.Extensions.NETCore.Setup to version 3.3.0.0

--- extensions/src/AWSSDK.Extensions.NETCore.Setup/Properties/AssemblyInfo.cs
+++ extensions/src/AWSSDK.Extensions.NETCore.Setup/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 
-[assembly: AssemblyVersion("3.2")]
-[assembly: AssemblyFileVersion("3.2.8.0")]
+[assembly: AssemblyVersion("3.3")]
+[assembly: AssemblyFileVersion("3.3.0.0")]
