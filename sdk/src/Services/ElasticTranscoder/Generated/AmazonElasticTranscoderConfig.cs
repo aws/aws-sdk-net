0bc915a8cda02a8b19455c3b462bd93472024241
Norm Johanson <normj@amazon.com>
2016-10-11 13:06:54 -0700

    Commit version number update changes

--- sdk/src/Services/ElasticTranscoder/Generated/AmazonElasticTranscoderConfig.cs
+++ sdk/src/Services/ElasticTranscoder/Generated/AmazonElasticTranscoderConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.ElasticTranscoder
     public partial class AmazonElasticTranscoderConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.3.0.0");
+            InternalSDKUtils.BuildUserAgentString("3.3.0.1");
 
         private string _userAgent = UserAgentString;
 
