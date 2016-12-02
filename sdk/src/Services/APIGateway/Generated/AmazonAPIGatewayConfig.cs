437efad0d4934827979e50e313acea57b5983bd5
Norm Johanson <normj@amazon.com>
2016-12-01 16:51:25 -0800

    Commit version number update changes

--- sdk/src/Services/APIGateway/Generated/AmazonAPIGatewayConfig.cs
+++ sdk/src/Services/APIGateway/Generated/AmazonAPIGatewayConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.APIGateway
     public partial class AmazonAPIGatewayConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.3.2.0");
+            InternalSDKUtils.BuildUserAgentString("3.3.3.0");
 
         private string _userAgent = UserAgentString;
 
--- sdk/src/Services/DirectConnect/Generated/AmazonDirectConnectConfig.cs
+++ sdk/src/Services/DirectConnect/Generated/AmazonDirectConnectConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.DirectConnect
     public partial class AmazonDirectConnectConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.3.1.1");
+            InternalSDKUtils.BuildUserAgentString("3.3.2.0");
 
         private string _userAgent = UserAgentString;
 
