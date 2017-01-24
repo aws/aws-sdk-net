e86c2b4f0029fe34e485ab800d694d91d76c1e66
Pavel Safronov <pavel@amazon.com>
2017-01-24 14:12:11 -0800

    Add timing task and output elapsed time messages.

---
+++ buildtools/CustomTasks/TimingTask.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace CustomTasks
+{
+    public class TimingTask : Task
+    {
+        private readonly static TimeSpan SleepTime = TimeSpan.FromSeconds(30);
+        private static bool ShouldInit = true;
+        private static Thread MainThread = null;
+        private static DateTime StartTime;
+
+        public override bool Execute()
+        {
+            if (ShouldInit)
+            {
+                ShouldInit = false;
+                StartTime = DateTime.Now;
+                MainThread = new Thread(() =>
+                {
+                    try
+                    {
+                        while (true)
+                        {
+                            var elapsed = DateTime.Now - StartTime;
+                            Console.WriteLine(">>>>> ELAPSED TIME = " + elapsed.ToString("h'h 'm'm 's's'"));
+                            Thread.Sleep(SleepTime);
+                        }
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine("TimingTask encountered error: " + e);
+                    }
+                });
+                MainThread.IsBackground = true;
+                MainThread.Start();
+            }
+
+            return true;
+        }
+    }
+}
