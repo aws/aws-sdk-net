9827b37851db47bb791841c6619a465f0f338f3b
Vellozzi <vellozzi@amazon.com>
2017-03-15 10:20:44 -0700

    Fix missed commit when manually merging in sdk/src/Core/Amazon.Runtime/AWSCredentials.cs from master.

--- sdk/src/Core/Amazon.Runtime/Credentials/URIBasedRefreshingCredentialHelper.cs
+++ sdk/src/Core/Amazon.Runtime/Credentials/URIBasedRefreshingCredentialHelper.cs
@@ -74,6 +74,7 @@ namespace Amazon.Runtime
             public string SecretAccessKey { get; set; }
             public string Token { get; set; }
             public DateTime Expiration { get; set; }
+            public string RoleArn { get; set; }
         }
         #endregion
 
