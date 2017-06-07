ca9b063f2e9d6289b5b6954810d1f047b5250283
Norm Johanson <normj@amazon.com>
2017-06-07 12:35:20 -0700

    Commit version number update changes

--- sdk/src/Services/CodeBuild/Generated/AmazonCodeBuildConfig.cs
+++ sdk/src/Services/CodeBuild/Generated/AmazonCodeBuildConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.CodeBuild
     public partial class AmazonCodeBuildConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.3.0.17");
+            InternalSDKUtils.BuildUserAgentString("3.3.1.0");
 
         private string _userAgent = UserAgentString;
 
