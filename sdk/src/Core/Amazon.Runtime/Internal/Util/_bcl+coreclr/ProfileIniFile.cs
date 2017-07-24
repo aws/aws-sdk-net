a28ccdb7123a4a6938ffd8dc7c240adf9315f707
Sattwik Pati <sattwikp@amazon.com>
2017-07-24 11:42:30 -0700

    Removing System.Threading.Tasks from ProfileIniFile.cs

--- sdk/src/Core/Amazon.Runtime/Internal/Util/_bcl+coreclr/ProfileIniFile.cs
+++ sdk/src/Core/Amazon.Runtime/Internal/Util/_bcl+coreclr/ProfileIniFile.cs
@@ -17,7 +17,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace Amazon.Runtime.Internal.Util
 {
