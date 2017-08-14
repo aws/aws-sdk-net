899cd65dc7ba3cfae0d34e43f642aee23ed97082
Norm Johanson <normj@amazon.com>
2017-08-14 11:07:03 -0700

    Commit version number update changes

--- sdk/src/Services/Batch/Generated/AmazonBatchConfig.cs
+++ sdk/src/Services/Batch/Generated/AmazonBatchConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.Batch
     public partial class AmazonBatchConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.3.2.9");
+            InternalSDKUtils.BuildUserAgentString("3.3.3.0");
 
         private string _userAgent = UserAgentString;
 
--- sdk/src/Services/StorageGateway/Generated/AmazonStorageGatewayConfig.cs
+++ sdk/src/Services/StorageGateway/Generated/AmazonStorageGatewayConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.StorageGateway
     public partial class AmazonStorageGatewayConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.3.4.3");
+            InternalSDKUtils.BuildUserAgentString("3.3.5.0");
 
         private string _userAgent = UserAgentString;
 
