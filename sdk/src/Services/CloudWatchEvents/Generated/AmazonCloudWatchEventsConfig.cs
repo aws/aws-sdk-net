bb816aa0e11f6b2bad90623fa9c8fd922d6fdc9b
Norm Johanson <normj@amazon.com>
2017-09-13 15:13:43 -0700

    Commit version number update changes

--- sdk/src/Services/CloudWatchEvents/Generated/AmazonCloudWatchEventsConfig.cs
+++ sdk/src/Services/CloudWatchEvents/Generated/AmazonCloudWatchEventsConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.CloudWatchEvents
     public partial class AmazonCloudWatchEventsConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.3.2.2");
+            InternalSDKUtils.BuildUserAgentString("3.3.3.0");
 
         private string _userAgent = UserAgentString;
 
