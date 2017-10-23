28f9449555037bc2b012d9e9137a78334b18fb8b
Norm Johanson <normj@amazon.com>
2017-10-23 10:43:01 -0700

    Add unit test

--- extensions/test/NETCore.SetupTests/DependencyInjectionTests.cs
+++ extensions/test/NETCore.SetupTests/DependencyInjectionTests.cs
@@ -11,6 +11,8 @@ using Xunit;
 using Amazon;
 using Amazon.S3;
 using Amazon.Extensions.NETCore.Setup;
+using Amazon.Extensions.NETCore.Setup.Internal;
+using Amazon.Runtime;
 
 namespace DependencyInjectionTests
 {
@@ -52,6 +54,45 @@ namespace DependencyInjectionTests
             var controller = ActivatorUtilities.CreateInstance<TestController>(serviceProvider);
             Assert.NotNull(controller.S3Client);
             Assert.Equal(RegionEndpoint.EUCentral1, controller.S3Client.Config.RegionEndpoint);
+        }
+
+        [Fact]
+        public void HandlerTest()
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("./TestFiles/GetClientConfigSettingsTest.json");
+            IConfiguration config = builder.Build();
+
+            ServiceCollection services = new ServiceCollection();
+            services.AddDefaultAWSOptions(config.GetAWSOptions());
+            services.AddAWSService<IAmazonS3>(new AWSOptions { Region = RegionEndpoint.EUCentral1 });
+            services.AddAmazonServiceClientCreateEventHandle(new WrapperHandler());
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var client = serviceProvider.GetService<IAmazonS3>();
+            Assert.True(client is WrapperHandler.WrapperS3Client);
+        }
+
+        public class WrapperHandler : Amazon.Extensions.NETCore.Setup.Internal.IAmazonServiceClientEventHandler
+        {
+            public string Name => "FakeWrapper";
+
+            public IAmazonService Process(IAmazonService client)
+            {
+                return new WrapperS3Client(client as IAmazonS3);
+            }
+
+            public class WrapperS3Client : AmazonS3Client
+            {
+                IAmazonS3 _s3Client;
+
+                public WrapperS3Client(IAmazonS3 client)
+                    : base(client.Config.RegionEndpoint)
+                {
+                    this._s3Client = client;
+                }
+            }
         }
 
         public class TestController
