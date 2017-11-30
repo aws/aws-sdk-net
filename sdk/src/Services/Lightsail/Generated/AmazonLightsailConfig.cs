55c63c2e016a0d8a80a3822bf16cedcefe1781a0
Norm Johanson <normj@amazon.com>
2017-11-29 20:37:20 -0800

    Commit version number update changes

--- sdk/src/Services/Lightsail/Generated/AmazonLightsailConfig.cs
+++ sdk/src/Services/Lightsail/Generated/AmazonLightsailConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.Lightsail
     public partial class AmazonLightsailConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.3.4.0");
+            InternalSDKUtils.BuildUserAgentString("3.3.5.0");
 
         private string _userAgent = UserAgentString;
 
