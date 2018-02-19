ee323c2070bc350ce4bea88bb5f86ab123691036
Magic73 <magic73@gmail.com>
2018-02-19 14:30:51 +0100

    Update ClientConfig.cs
    Use custom http message handle to use with HttpClient. (example: NativeMessageHandler from ModernHttpClient)

--- sdk/src/Core/Amazon.Runtime/_mobile/ClientConfig.cs
+++ sdk/src/Core/Amazon.Runtime/_mobile/ClientConfig.cs
@@ -27,6 +27,7 @@ namespace Amazon.Runtime
     /// </summary>    
     public abstract partial class ClientConfig
     {
+        private HttpClientHandler _httpClientHandler = null;
         private IWebProxy proxy = null;
         private string proxyHost;
         private int proxyPort = -1;
@@ -35,7 +36,24 @@ namespace Amazon.Runtime
         {
             return FallbackRegionFactory.GetRegionEndpoint();
         }
-
+       
+        /// <summary>
+        /// Return a custom http message handle to use with HttpClient. (example: NativeMessageHandler from ModernHttpClient)
+        /// Default Http Message Handler used if null value returned
+        /// </summary>
+        public HttpClientHandler GetHttpMessageHandler()
+        {
+            return _httpClientHandler; // Use the default Http client handler
+        }
+        /// <summary>
+        /// Update the Http Client handler to use
+        /// </summary>
+        /// <param name="httpClientHandler">The http client handler to use</param>
+        public void SetHttpMessageHandler(HttpClientHandler httpClientHandler)
+        {
+            this._httpClientHandler = httpClientHandler;
+        }
+        
         /// <summary>
         /// Returns a WebProxy instance configured to match the proxy settings
         /// in the client configuration.
