6ce32a675711f0416042e22ec29eb3a2d5baffb1
Norm Johanson <normj@amazon.com>
2018-03-20 16:03:29 -0700

    Commit version number update changes

--- sdk/src/Services/CostExplorer/Generated/AmazonCostExplorerConfig.cs
+++ sdk/src/Services/CostExplorer/Generated/AmazonCostExplorerConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.CostExplorer
     public partial class AmazonCostExplorerConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.3.1.2");
+            InternalSDKUtils.BuildUserAgentString("3.3.2.0");
 
         private string _userAgent = UserAgentString;
 
