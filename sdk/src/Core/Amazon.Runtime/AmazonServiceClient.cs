f3cd1be39b6735207bc3e55f9c7274570bf24c00
Norm Johanson <normj@amazon.com>
2018-03-28 18:49:35 -0700

    Fix FxCop violations

--- sdk/src/Core/Amazon.Runtime/AmazonServiceClient.cs
+++ sdk/src/Core/Amazon.Runtime/AmazonServiceClient.cs
@@ -505,7 +505,7 @@ namespace Amazon.Runtime
                     "by System.Uri and thus cannot be handled by the .NET SDK.", resourcePath));
 
             var parameterizedPath = string.Concat(AWSSDKUtils.UrlEncode(resourcePath, true), sb);
-            var hasSlash = url.AbsoluteUri.EndsWith("/") || parameterizedPath.StartsWith("/");
+            var hasSlash = url.AbsoluteUri.EndsWith("/", StringComparison.Ordinal) || parameterizedPath.StartsWith("/", StringComparison.Ordinal);
             var uri = hasSlash
                 ? new Uri(url.AbsoluteUri + parameterizedPath)
                 : new Uri(url.AbsoluteUri + "/" + parameterizedPath);
