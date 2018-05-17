313c8cfdda9ff2f710035f4f2c21088b8245a6ac
Norm Johanson <normj@amazon.com>
2018-05-17 16:15:22 -0700

    Commit version number update changes

--- sdk/src/Services/CognitoIdentityProvider/Generated/AmazonCognitoIdentityProviderConfig.cs
+++ sdk/src/Services/CognitoIdentityProvider/Generated/AmazonCognitoIdentityProviderConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.CognitoIdentityProvider
     public partial class AmazonCognitoIdentityProviderConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.3.9.5");
+            InternalSDKUtils.BuildUserAgentString("3.3.10.0");
 
         private string _userAgent = UserAgentString;
 
