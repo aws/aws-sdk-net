2bb69e2778ef13889b00f8fbbb98f88209eb0440
Norm Johanson <normj@amazon.com>
2018-05-22 16:47:58 -0700

    Commit version number update changes

--- sdk/src/Services/ECS/Generated/AmazonECSConfig.cs
+++ sdk/src/Services/ECS/Generated/AmazonECSConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.ECS
     public partial class AmazonECSConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.3.13.3");
+            InternalSDKUtils.BuildUserAgentString("3.3.14.0");
 
         private string _userAgent = UserAgentString;
 
