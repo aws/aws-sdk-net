4fa11d9afbdd1d1b1401cecc94758cc467064d82
Steven Kang <stevkang@amazon.com>
2018-06-01 13:07:07 -0700

    Documentation updates for redshift

--- sdk/src/Services/Redshift/Generated/_bcl45/IAmazonRedshift.cs
+++ sdk/src/Services/Redshift/Generated/_bcl45/IAmazonRedshift.cs
@@ -822,7 +822,7 @@ namespace Amazon.Redshift
         /// 
         ///  
         /// <para>
-        /// A resource can have up to 10 tags. If you try to create more than 10 tags for a resource,
+        /// A resource can have up to 50 tags. If you try to create more than 50 tags for a resource,
         /// you will receive an error and the attempt will fail.
         /// </para>
         ///  
