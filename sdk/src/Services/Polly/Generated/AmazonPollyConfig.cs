0539db0dcf0080ad7bcc93fc26920d0076467f52
Norm Johanson <normj@amazon.com>
2018-07-17 09:56:42 -0700

    Commit version number update changes

--- sdk/src/Services/Polly/Generated/AmazonPollyConfig.cs
+++ sdk/src/Services/Polly/Generated/AmazonPollyConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.Polly
     public partial class AmazonPollyConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.3.5.3");
+            InternalSDKUtils.BuildUserAgentString("3.3.6.0");
 
         private string _userAgent = UserAgentString;
 
