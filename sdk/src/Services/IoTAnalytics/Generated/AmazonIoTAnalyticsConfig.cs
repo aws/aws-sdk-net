623351b6307adc9be3ace0a28690d652ca60f1ba
Norm Johanson <normj@amazon.com>
2018-08-23 15:15:48 -0700

    Commit version number update changes

--- sdk/src/Services/IoTAnalytics/Generated/AmazonIoTAnalyticsConfig.cs
+++ sdk/src/Services/IoTAnalytics/Generated/AmazonIoTAnalyticsConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.IoTAnalytics
     public partial class AmazonIoTAnalyticsConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.3.2.5");
+            InternalSDKUtils.BuildUserAgentString("3.3.3.0");
 
         private string _userAgent = UserAgentString;
 
