61e17383833e71525bf15763f33485525d392ac0
Norm Johanson <normj@amazon.com>
2018-08-24 19:11:54 -0700

    Commit version number update changes

--- sdk/src/Services/Glue/Generated/AmazonGlueConfig.cs
+++ sdk/src/Services/Glue/Generated/AmazonGlueConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.Glue
     public partial class AmazonGlueConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.3.10.4");
+            InternalSDKUtils.BuildUserAgentString("3.3.11.0");
 
         private string _userAgent = UserAgentString;
 
