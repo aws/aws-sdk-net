818654aa32249f577cfd1daeaf869a65b07cac65
Norm Johanson <normj@amazon.com>
2018-08-29 14:32:27 -0700

    Commit version number update changes

--- sdk/src/Services/SageMakerRuntime/Generated/AmazonSageMakerRuntimeConfig.cs
+++ sdk/src/Services/SageMakerRuntime/Generated/AmazonSageMakerRuntimeConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.SageMakerRuntime
     public partial class AmazonSageMakerRuntimeConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.3.0.15");
+            InternalSDKUtils.BuildUserAgentString("3.3.1.0");
 
         private string _userAgent = UserAgentString;
 
