cd243430b48aa9a8924f906a3ba05b0165b50177
Norm Johanson <normj@amazon.com>
2018-09-04 15:32:24 -0700

    Commit version number update changes

--- sdk/src/Services/RDS/Generated/AmazonRDSConfig.cs
+++ sdk/src/Services/RDS/Generated/AmazonRDSConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.RDS
     public partial class AmazonRDSConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.3.27.4");
+            InternalSDKUtils.BuildUserAgentString("3.3.27.5");
 
         private string _userAgent = UserAgentString;
 
