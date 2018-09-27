1d3fb7504d3e1fb904195880a5358c1af9175311
Norm Johanson <normj@amazon.com>
2018-09-27 14:13:39 -0700

    Commit version number update changes

--- sdk/src/Services/MQ/Generated/AmazonMQConfig.cs
+++ sdk/src/Services/MQ/Generated/AmazonMQConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.MQ
     public partial class AmazonMQConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.3.1.8");
+            InternalSDKUtils.BuildUserAgentString("3.3.2.0");
 
         private string _userAgent = UserAgentString;
 
