a236220347dbbbc7c1638d1bd2987eb42162f374
Norm Johanson <normj@amazon.com>
2018-11-29 11:54:05 -0800

    Commit version number update changes

--- sdk/src/Services/ServerlessApplicationRepository/Generated/AmazonServerlessApplicationRepositoryConfig.cs
+++ sdk/src/Services/ServerlessApplicationRepository/Generated/AmazonServerlessApplicationRepositoryConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.ServerlessApplicationRepository
     public partial class AmazonServerlessApplicationRepositoryConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.3.3.9");
+            InternalSDKUtils.BuildUserAgentString("3.3.4.0");
 
         private string _userAgent = UserAgentString;
 
--- sdk/src/Services/StepFunctions/Generated/AmazonStepFunctionsConfig.cs
+++ sdk/src/Services/StepFunctions/Generated/AmazonStepFunctionsConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.StepFunctions
     public partial class AmazonStepFunctionsConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.3.2.36");
+            InternalSDKUtils.BuildUserAgentString("3.3.3.0");
 
         private string _userAgent = UserAgentString;
 
