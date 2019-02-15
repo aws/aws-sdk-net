442547495ddd91b67ade3338a51e7a7c3e042cd3
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2019-02-14 20:30:55 -0800

    Commit version number update changes

--- sdk/src/Services/Chime/Generated/AmazonChimeConfig.cs
+++ sdk/src/Services/Chime/Generated/AmazonChimeConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.Chime
     public partial class AmazonChimeConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.3.1.21");
+            InternalSDKUtils.BuildUserAgentString("3.3.1.22");
 
         private string _userAgent = UserAgentString;
 
