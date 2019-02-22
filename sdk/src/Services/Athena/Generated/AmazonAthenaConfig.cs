2164c49d12db84fbc8b34611943361b4e513a171
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2019-02-22 12:09:55 -0800

    Commit version number update changes

--- sdk/src/Services/Athena/Generated/AmazonAthenaConfig.cs
+++ sdk/src/Services/Athena/Generated/AmazonAthenaConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.Athena
     public partial class AmazonAthenaConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.3.2.1");
+            InternalSDKUtils.BuildUserAgentString("3.3.3.0");
 
         private string _userAgent = UserAgentString;
 
--- sdk/src/Services/Cloud9/Generated/AmazonCloud9Config.cs
+++ sdk/src/Services/Cloud9/Generated/AmazonCloud9Config.cs
@@ -32,7 +32,7 @@ namespace Amazon.Cloud9
     public partial class AmazonCloud9Config : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.3.0.57");
+            InternalSDKUtils.BuildUserAgentString("3.3.1.0");
 
         private string _userAgent = UserAgentString;
 
