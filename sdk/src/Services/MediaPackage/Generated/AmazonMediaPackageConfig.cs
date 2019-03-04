08c86fce10f9d4ef520d3f3ad23d71500d049165
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2019-03-04 12:11:07 -0800

    Commit version number update changes

--- sdk/src/Services/MediaPackage/Generated/AmazonMediaPackageConfig.cs
+++ sdk/src/Services/MediaPackage/Generated/AmazonMediaPackageConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.MediaPackage
     public partial class AmazonMediaPackageConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.3.6.5");
+            InternalSDKUtils.BuildUserAgentString("3.3.7.0");
 
         private string _userAgent = UserAgentString;
 
--- sdk/src/Services/SimpleSystemsManagement/Generated/AmazonSimpleSystemsManagementConfig.cs
+++ sdk/src/Services/SimpleSystemsManagement/Generated/AmazonSimpleSystemsManagementConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.SimpleSystemsManagement
     public partial class AmazonSimpleSystemsManagementConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.3.40.0");
+            InternalSDKUtils.BuildUserAgentString("3.3.41.0");
 
         private string _userAgent = UserAgentString;
 
