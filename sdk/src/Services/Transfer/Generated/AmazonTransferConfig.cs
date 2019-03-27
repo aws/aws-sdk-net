2e33be399aedad459441db9f1e0f895c1611a822
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2019-03-27 14:29:37 -0700

    Commit version number update changes

--- sdk/src/Services/Transfer/Generated/AmazonTransferConfig.cs
+++ sdk/src/Services/Transfer/Generated/AmazonTransferConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.Transfer
     public partial class AmazonTransferConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.3.100.2");
+            InternalSDKUtils.BuildUserAgentString("3.3.101.0");
 
         private string _userAgent = UserAgentString;
 
