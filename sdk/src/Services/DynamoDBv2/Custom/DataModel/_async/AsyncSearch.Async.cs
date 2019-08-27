6d4c70acdba11cfbe475edc55b770cb685f5b7d6
Morten Brix Pedersen <mope@queue-it.com>
2019-08-27 13:48:02 +0200

    Add IAsyncSearch interface to allow mocking.

--- sdk/src/Services/DynamoDBv2/Custom/DataModel/_async/AsyncSearch.Async.cs
+++ sdk/src/Services/DynamoDBv2/Custom/DataModel/_async/AsyncSearch.Async.cs
@@ -24,6 +24,31 @@ using Amazon.DynamoDBv2.DataModel;
 
 namespace Amazon.DynamoDBv2.DataModel
 {
+    public partial interface IAsyncSearch<T>
+    {
+        /// <summary>
+        /// Initiates the asynchronous execution to get the next set of results from DynamoDB.
+        /// 
+        /// If there are more items in the Scan/Query, PaginationToken will be
+        /// set and can be consumed in a new Scan/Query operation to resume
+        /// retrieving items from this point.
+        /// </summary>
+        /// <param name="cancellationToken">Token which can be used to cancel the task.</param>
+        /// <returns>
+        /// A Task that can be used to poll or wait for results, or both.
+        /// Results will include the next set of result items from DynamoDB.
+        /// </returns>
+        Task<List<T>> GetNextSetAsync(CancellationToken cancellationToken = default(CancellationToken));
+        /// <summary>
+        /// Initiates the asynchronous execution to get all the remaining results from DynamoDB.
+        /// </summary>
+        /// <param name="cancellationToken">Token which can be used to cancel the task.</param>
+        /// <returns>
+        /// A Task that can be used to poll or wait for results, or both.
+        /// Results will include the remaining result items from DynamoDB.
+        /// </returns>
+        Task<List<T>> GetRemainingAsync(CancellationToken cancellationToken = default(CancellationToken));
+    }
     /// <summary>
     /// A strongly-typed object for retrieving search results (Query or Scan)
     /// from DynamoDB.
