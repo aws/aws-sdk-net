522025caffe0a02d1baac47b17f99129932ac23f
Jia, Bill <bill.jia@activenetwork.com>
2019-09-14 08:57:35 +0800

    Remove unused local variable.

--- sdk/src/Services/DynamoDBv2/Custom/DataModel/ContextInternal.cs
+++ sdk/src/Services/DynamoDBv2/Custom/DataModel/ContextInternal.cs
@@ -405,7 +405,6 @@ namespace Amazon.DynamoDBv2.DataModel
 
         private bool TryFromListToArray(Type targetType, DynamoDBList list, DynamoDBFlatConfig flatConfig, out object output)
         {
-            var targetTypeWrapper = TypeFactory.GetTypeInfo(targetType);
             if ((!Utils.ImplementsInterface(targetType, typeof(ICollection<>)) &&
                 !Utils.ImplementsInterface(targetType, typeof(IList))) ||
                 !Utils.CanInstantiateArray(targetType))
