01745f492a6ed3bc8a02b6184f998adde146f9df
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2019-10-07 13:21:43 -0700

    Commit version number update changes

--- sdk/src/Services/DirectConnect/Generated/AmazonDirectConnectConfig.cs
+++ sdk/src/Services/DirectConnect/Generated/AmazonDirectConnectConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.DirectConnect
     public partial class AmazonDirectConnectConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.3.103.41");
+            InternalSDKUtils.BuildUserAgentString("3.3.104.0");
 
         private string _userAgent = UserAgentString;
 
--- sdk/src/Services/KinesisFirehose/Generated/AmazonKinesisFirehoseConfig.cs
+++ sdk/src/Services/KinesisFirehose/Generated/AmazonKinesisFirehoseConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.KinesisFirehose
     public partial class AmazonKinesisFirehoseConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.3.100.81");
+            InternalSDKUtils.BuildUserAgentString("3.3.101.0");
 
         private string _userAgent = UserAgentString;
 
