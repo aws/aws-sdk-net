696c49aa2973311cf8f8f708c4d8be4f1048ec27
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2019-10-24 12:28:14 -0700

    Commit version number update changes

--- sdk/src/Services/AppMesh/Generated/AmazonAppMeshConfig.cs
+++ sdk/src/Services/AppMesh/Generated/AmazonAppMeshConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.AppMesh
     public partial class AmazonAppMeshConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.3.105.17");
+            InternalSDKUtils.BuildUserAgentString("3.3.106.0");
 
         private string _userAgent = UserAgentString;
 
--- sdk/src/Services/SageMaker/Generated/AmazonSageMakerConfig.cs
+++ sdk/src/Services/SageMaker/Generated/AmazonSageMakerConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.SageMaker
     public partial class AmazonSageMakerConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.3.105.10");
+            InternalSDKUtils.BuildUserAgentString("3.3.106.0");
 
         private string _userAgent = UserAgentString;
 
