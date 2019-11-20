d896ddb4180c1e4f841e769f4886872613f4a8da
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2019-11-20 11:23:42 -0800

    This release introduces support for Amazon S3 Replication Time Control, a new feature of S3 Replication that provides a predictable replication time backed by a Service Level Agreement. S3 Replication Time Control helps customers meet compliance or business requirements for data replication, and provides visibility into the replication process with new Amazon CloudWatch Metrics.

---
+++ sdk/src/Services/S3/Custom/Model/Metrics.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Amazon.S3.Model
+{
+    /// <summary>
+    /// A container specifying replication metrics-related information, including whether 
+    /// emitting metrics and Amazon S3 events for replication are enabled. In addition, contains 
+    /// configurations related to specific metrics or events. Must be specified together with a 
+    /// <code>ReplicationTime</code> block.
+    /// </summary>
+    public class Metrics
+    {
+        private MetricsStatus status;
+        private ReplicationTimeValue eventThreshold;
+
+        /// <summary>
+        /// Specifies whether the replication metrics are enabled.
+        /// </summary>
+        public MetricsStatus Status
+        {
+            get { return this.status; }
+            set { this.status = value; }
+        }
+
+        /// <summary>
+        /// A container specifying the time threshold for emitting the 
+        /// <code>s3:Replication:OperationMissedThreshold</code> event.
+        /// </summary>
+        public ReplicationTimeValue EventThreshold
+        {
+            get { return this.eventThreshold; }
+            set { this.eventThreshold = value; }
+        }
+
+        internal bool IsSetStatus()
+        {
+            return this.status != null;
+        }
+
+        internal bool IsSetEventThreshold()
+        {
+            return this.eventThreshold != null;
+        }
+    }
+}
---
+++ sdk/src/Services/S3/Custom/Model/ReplicationTimeValue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Amazon.S3.Model
+{
+    /// <summary>
+    /// A container specifying the time value.
+    /// </summary>
+    public class ReplicationTimeValue
+    {
+        private int? minutes;
+
+        /// <summary>
+        /// Contains an integer specifying time in minutes.
+        /// </summary>
+        public int Minutes
+        {
+            get { return this.minutes ?? default(int); }
+            set { this.minutes = value; }
+        }
+
+        internal bool IsSetMinutes()
+        {
+            return this.minutes.HasValue;
+        }
+    }
+}
