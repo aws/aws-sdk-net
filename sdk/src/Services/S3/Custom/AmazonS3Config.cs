c24dfcd94c1cd1a3b06b139f86d4f194faf1930d
Matteo Prosperi <41970398+matteo-prosperi@users.noreply.github.com>
2019-12-19 12:53:22 -0800

    Adding configuration of S3 timeouts to MaxTimeout for .NET Standard

--- sdk/src/Services/S3/Custom/AmazonS3Config.cs
+++ sdk/src/Services/S3/Custom/AmazonS3Config.cs
@@ -131,7 +131,7 @@ namespace Amazon.S3
         protected override void Initialize()
         {
             this.AllowAutoRedirect = false;
-#if BCL45
+#if BCL45 || NETSTANDARD
             // Set Timeout and ReadWriteTimeout for S3 to max timeout as per-request
             // timeouts are not supported.
             this.Timeout = ClientConfig.MaxTimeout;
