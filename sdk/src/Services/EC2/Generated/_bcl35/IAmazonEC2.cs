02c9962cd9eb401f460f8f28322c84548b552915
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2020-05-21 11:14:06 -0700

    From this release onwards ProvisionByoipCidr publicly supports IPv6. Updated ProvisionByoipCidr API to support tags for public IPv4 and IPv6 pools. Added NetworkBorderGroup to the DescribePublicIpv4Pools response.

--- sdk/src/Services/EC2/Generated/_bcl35/IAmazonEC2.cs
+++ sdk/src/Services/EC2/Generated/_bcl35/IAmazonEC2.cs
@@ -4002,9 +4002,10 @@ namespace Amazon.EC2
 
 
         /// <summary>
-        /// Adds or overwrites the specified tags for the specified Amazon EC2 resource or resources.
-        /// Each resource can have a maximum of 50 tags. Each tag consists of a key and optional
-        /// value. Tag keys must be unique per resource.
+        /// Adds or overwrites only the specified tags for the specified Amazon EC2 resource or
+        /// resources. When you specify an existing tag key, the value is overwritten with the
+        /// new value. Each resource can have a maximum of 50 tags. Each tag consists of a key
+        /// and optional value. Tag keys must be unique per resource.
         /// 
         ///  
         /// <para>
