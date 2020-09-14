a35d6309d0ea7ccb72c4af99fb8d6a3f88ce820b
Josh Hurley <53570882+joshongithub@users.noreply.github.com>
2020-09-14 18:57:05 -0400

    removed lazy load and made a static field instead

--- sdk/src/Services/S3/Custom/Util/AmazonS3Uri.cs
+++ sdk/src/Services/S3/Custom/Util/AmazonS3Uri.cs
@@ -32,9 +32,7 @@ namespace Amazon.S3.Util
     /// </summary>
     public class AmazonS3Uri
     {
-        private static Regex endpointRegexMatch;
-
-        private static Regex EndpointRegexMatch => endpointRegexMatch ?? (endpointRegexMatch = new Regex(@"^(.+\.)?s3[.-]([a-z0-9-]+)\.", RegexOptions.Compiled));
+        private static Regex EndpointRegexMatch = new Regex(@"^(.+\.)?s3[.-]([a-z0-9-]+)\.", RegexOptions.Compiled);
 
         /// <summary>
         /// True if the URI contains the bucket in the path, false if it contains the bucket in the authority.
