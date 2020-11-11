8ee24f499d3254031c084e556c16dd884e06abd1
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2020-11-11 11:15:49 -0800

    Adding support to remove a Provisioned Product launch role via UpdateProvisionedProductProperties

--- sdk/src/Services/ServiceCatalog/Generated/Model/UpdateProvisionedProductPropertiesRequest.cs
+++ sdk/src/Services/ServiceCatalog/Generated/Model/UpdateProvisionedProductPropertiesRequest.cs
@@ -116,15 +116,15 @@ namespace Amazon.ServiceCatalog.Model
         /// </para>
         ///  
         /// <para>
-        /// The <code>LAUNCH_ROLE</code> key accepts user ARNs and role ARNs. This key allows
-        /// an administrator to call <code>UpdateProvisionedProductProperties</code> to update
-        /// the launch role that is associated with a provisioned product. This role is used when
-        /// an end-user calls a provisioning operation such as <code>UpdateProvisionedProduct</code>,
-        /// <code>TerminateProvisionedProduct</code>, or <code>ExecuteProvisionedProductServiceAction</code>.
-        /// Only an ARN role or <code>null</code> is valid. A user ARN is invalid. For example,
-        /// if an admin user passes <code>null</code> as the value for the key <code>LAUNCH_ROLE</code>,
-        /// the admin removes the launch role that is associated with the provisioned product.
-        /// As a result, the end user operations use the credentials of the end user.
+        /// The <code>LAUNCH_ROLE</code> key accepts role ARNs. This key allows an administrator
+        /// to call <code>UpdateProvisionedProductProperties</code> to update the launch role
+        /// that is associated with a provisioned product. This role is used when an end user
+        /// calls a provisioning operation such as <code>UpdateProvisionedProduct</code>, <code>TerminateProvisionedProduct</code>,
+        /// or <code>ExecuteProvisionedProductServiceAction</code>. Only a role ARN or an empty
+        /// string <code>""</code> is valid. A user ARN is invalid. if an admin user passes an
+        /// empty string <code>""</code> as the value for the key <code>LAUNCH_ROLE</code>, the
+        /// admin removes the launch role that is associated with the provisioned product. As
+        /// a result, the end user operations use the credentials of the end user.
         /// </para>
         ///  
         /// <para>
