7e8eed2c09c1289c348daed654a19d16c5a7c6ba
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2020-11-12 12:48:24 -0800

    Commit version number update changes

--- sdk/src/Services/Polly/Generated/AmazonPollyConfig.cs
+++ sdk/src/Services/Polly/Generated/AmazonPollyConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.Polly
     public partial class AmazonPollyConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.5.0.36");
+            InternalSDKUtils.BuildUserAgentString("3.5.1.0");
 
         private string _userAgent = UserAgentString;
 
