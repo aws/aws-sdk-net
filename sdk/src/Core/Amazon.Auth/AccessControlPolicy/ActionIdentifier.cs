8af264b96dd942e0d0a53cb4e3f0d5b1ef8d8e12
Malhar Khimsaria <kmalhar@amazon.com>
2021-01-19 19:09:27 -0500

    Removed the Obsolete attribute from ActionIdentifier.cs

--- sdk/src/Core/Amazon.Auth/AccessControlPolicy/ActionIdentifier.cs
+++ sdk/src/Core/Amazon.Auth/AccessControlPolicy/ActionIdentifier.cs
@@ -39,9 +39,6 @@ namespace Amazon.Auth.AccessControlPolicy
     /// Constants for known actions can be found in the Amazon.Auth.AccessControlPolicy.ActionIdentifiers namespace.
     /// </para>
     /// </summary>
-    [Obsolete(
-            "The Amazon.Auth.AccessContolPolicy classes are out of date and are no longer being maintained.\n" +
-            "Please construct policy documents using the information found here: https://docs.aws.amazon.com/IAM/latest/UserGuide/access_policies.html#access_policies-json")]
     public class ActionIdentifier
     {
         private string actionName;
