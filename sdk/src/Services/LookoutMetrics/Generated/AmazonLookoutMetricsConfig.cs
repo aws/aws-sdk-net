563139bc3954755da0bc8eccf49ef179bbf576e4
Alex Shovlin <shovlia@amazon.com>
2021-03-25 21:12:41 -0400

    Fix v3.7 versioning for two recent services

--- sdk/src/Services/LookoutMetrics/Generated/AmazonLookoutMetricsConfig.cs
+++ sdk/src/Services/LookoutMetrics/Generated/AmazonLookoutMetricsConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.LookoutMetrics
     public partial class AmazonLookoutMetricsConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.3");
+            InternalSDKUtils.BuildUserAgentString("3.7.0.0");
 
         private string _userAgent = UserAgentString;
 
