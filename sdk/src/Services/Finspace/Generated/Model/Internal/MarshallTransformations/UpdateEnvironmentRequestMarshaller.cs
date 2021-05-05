54ab73906c69af1f5e66bbc32fca2525022f4953
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2021-05-04 19:45:36 -0700

    Documentation updates for FinSpace API.

--- sdk/src/Services/Finspace/Generated/Model/Internal/MarshallTransformations/UpdateEnvironmentRequestMarshaller.cs
+++ sdk/src/Services/Finspace/Generated/Model/Internal/MarshallTransformations/UpdateEnvironmentRequestMarshaller.cs
@@ -55,7 +55,7 @@ namespace Amazon.Finspace.Model.Internal.MarshallTransformations
         public IRequest Marshall(UpdateEnvironmentRequest publicRequest)
         {
             IRequest request = new DefaultRequest(publicRequest, "Amazon.Finspace");
-            request.Headers["Content-Type"] = "application/json";
+            request.Headers["Content-Type"] = "application/x-amz-json-1.1";
             request.Headers[Amazon.Util.HeaderKeys.XAmzApiVersion] = "2021-03-12";            
             request.HttpMethod = "PUT";
 
