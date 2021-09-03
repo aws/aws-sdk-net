7307d8964a2d3acf279a97cb28faaea1d6295888
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2021-09-03 11:10:00 -0700

    Documentation updates for Chime

--- sdk/src/Services/ChimeSDKIdentity/Generated/_netstandard/IAmazonChimeSDKIdentity.cs
+++ sdk/src/Services/ChimeSDKIdentity/Generated/_netstandard/IAmazonChimeSDKIdentity.cs
@@ -34,7 +34,8 @@ namespace Amazon.ChimeSDKIdentity
     /// The Amazon Chime SDK Identity APIs in this section allow software developers to create
     /// and manage unique instances of their messaging applications. These APIs provide the
     /// overarching framework for creating and sending messages. For more information about
-    /// the identity APIs, refer to .
+    /// the identity APIs, refer to <a href="https://docs.aws.amazon.com/chime/latest/APIReference/API_Operations_Amazon_Chime_SDK_Identity.html">Amazon
+    /// Chime SDK identity</a>.
     /// </summary>
     public partial interface IAmazonChimeSDKIdentity : IAmazonService, IDisposable
     {
