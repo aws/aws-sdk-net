2c35e0ba2755bcf90af817953eb05aa5692c57c9
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2021-09-03 11:12:28 -0700

    Documentation updates for Chime

--- sdk/src/Services/ChimeSDKMessaging/Generated/_bcl35/AmazonChimeSDKMessagingClient.cs
+++ sdk/src/Services/ChimeSDKMessaging/Generated/_bcl35/AmazonChimeSDKMessagingClient.cs
@@ -38,7 +38,8 @@ namespace Amazon.ChimeSDKMessaging
     /// The Amazon Chime SDK Messaging APIs in this section allow software developers to send
     /// and receive messages in custom messaging applications. These APIs depend on the frameworks
     /// provided by the Amazon Chime SDK Identity APIs. For more information about the messaging
-    /// APIs, see .
+    /// APIs, see <a href="https://docs.aws.amazon.com/chime/latest/APIReference/API_Operations_Amazon_Chime_SDK_Messaging">Amazon
+    /// Chime SDK messaging</a>
     /// </summary>
     public partial class AmazonChimeSDKMessagingClient : AmazonServiceClient, IAmazonChimeSDKMessaging
     {
