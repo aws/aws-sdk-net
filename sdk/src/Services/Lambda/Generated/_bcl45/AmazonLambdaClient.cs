b8125c027c1bea70eebea37e2170233178dfb800
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2022-02-23 11:12:54 -0800

    Lambda releases .NET 6 managed runtime to be available in all commercial regions.

--- sdk/src/Services/Lambda/Generated/_bcl45/AmazonLambdaClient.cs
+++ sdk/src/Services/Lambda/Generated/_bcl45/AmazonLambdaClient.cs
@@ -5648,6 +5648,25 @@ namespace Amazon.Lambda
         /// 
         ///  
         /// <para>
+        /// If the function's package type is <code>Image</code>, you must specify the code package
+        /// in <code>ImageUri</code> as the URI of a <a href="https://docs.aws.amazon.com/lambda/latest/dg/lambda-images.html">container
+        /// image</a> in the Amazon ECR registry. 
+        /// </para>
+        ///  
+        /// <para>
+        /// If the function's package type is <code>Zip</code>, you must specify the deployment
+        /// package as a <a href="https://docs.aws.amazon.com/lambda/latest/dg/gettingstarted-package.html#gettingstarted-package-zip">.zip
+        /// file archive</a>. Enter the Amazon S3 bucket and key of the code .zip file location.
+        /// You can also provide the function code inline using the <code>ZipFile</code> field.
+        /// 
+        /// </para>
+        ///  
+        /// <para>
+        /// The code in the deployment package must be compatible with the target instruction
+        /// set architecture of the function (<code>x86-64</code> or <code>arm64</code>). 
+        /// </para>
+        ///  
+        /// <para>
         /// The function's code is locked when you publish a version. You can't modify the code
         /// of a published version, only the unpublished version.
         /// </para>
@@ -5713,6 +5732,25 @@ namespace Amazon.Lambda
         /// code package must be signed by a trusted publisher. For more information, see <a href="https://docs.aws.amazon.com/lambda/latest/dg/configuration-trustedcode.html">Configuring
         /// code signing</a>.
         /// 
+        ///  
+        /// <para>
+        /// If the function's package type is <code>Image</code>, you must specify the code package
+        /// in <code>ImageUri</code> as the URI of a <a href="https://docs.aws.amazon.com/lambda/latest/dg/lambda-images.html">container
+        /// image</a> in the Amazon ECR registry. 
+        /// </para>
+        ///  
+        /// <para>
+        /// If the function's package type is <code>Zip</code>, you must specify the deployment
+        /// package as a <a href="https://docs.aws.amazon.com/lambda/latest/dg/gettingstarted-package.html#gettingstarted-package-zip">.zip
+        /// file archive</a>. Enter the Amazon S3 bucket and key of the code .zip file location.
+        /// You can also provide the function code inline using the <code>ZipFile</code> field.
+        /// 
+        /// </para>
+        ///  
+        /// <para>
+        /// The code in the deployment package must be compatible with the target instruction
+        /// set architecture of the function (<code>x86-64</code> or <code>arm64</code>). 
+        /// </para>
         ///  
         /// <para>
         /// The function's code is locked when you publish a version. You can't modify the code
