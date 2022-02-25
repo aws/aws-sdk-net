6c07458ef75fc58b85aaba4bee0a62c5bad9f314
Alex Shovlin <shovlia@amazon.com>
2022-02-25 11:59:33 -0500

    Use a long-lived S3 Multi-Region Access Point for integration tests

--- sdk/test/Services/S3/IntegrationTests/MultiRegionAccessPointsTests.cs
+++ sdk/test/Services/S3/IntegrationTests/MultiRegionAccessPointsTests.cs
@@ -3,65 +3,32 @@ using Amazon.S3;
 using Amazon.S3.Model;
 using Amazon.S3.Util;
 using Amazon.S3Control;
-using Amazon.S3Control.Model;
-using Amazon.SecurityToken;
-using Amazon.SecurityToken.Model;
-using AWSSDK_DotNet.IntegrationTests.Utils;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Collections.Generic;
 
 namespace AWSSDK_DotNet.IntegrationTests.Tests.S3
 {
     [TestClass]
     public class MultiRegionAccessPointsTests : TestBase<AmazonS3Client>
     {
-        private static string _accountId;
         private static string _bucketName;
-        private static string _mrapName;
         private static string _mrapArn;
 
         [ClassInitialize]
         public static void Setup(TestContext context)
         {
             _bucketName = S3TestUtils.CreateBucketWithWait(Client);
-            _mrapName = UtilityMethods.SDK_TEST_PREFIX + DateTime.Now.Ticks;
-
-            // Look up the account ID for the credentials being used to run the tests
-            _accountId = new AmazonSecurityTokenServiceClient().GetCallerIdentity(new GetCallerIdentityRequest()).Account;
-
-            var request = new CreateMultiRegionAccessPointRequest
-            {
-                AccountId = _accountId,
-                Details = new CreateMultiRegionAccessPointInput
-                {
-                    Name = _mrapName,
-                    Regions = new List<Region>
-                    {
-                        new Region
-                        {
-                            Bucket = _bucketName
-                        }
-                    }
-                }
-            };
-
-            // All MRAP control plane requests must go to us-west-2 per
-            // https://docs.aws.amazon.com/AmazonS3/latest/userguide/ManagingMultiRegionAccessPoints.html
-            var mrapAlias = S3TestUtils.CreateMRAPWithWait(new AmazonS3ControlClient(RegionEndpoint.USWest2), request);
-            _mrapArn = $"arn:aws:s3::{_accountId}:accesspoint/{mrapAlias}";
+            _mrapArn = S3TestUtils.GetOrCreateTestMRAP(new AmazonS3ControlClient(RegionEndpoint.USWest2), Client);
         }
 
         [ClassCleanup]
         public static void Cleanup()
         {
-            var deleteRequest = new DeleteMultiRegionAccessPointRequest
-            { 
-                AccountId = _accountId,
-                Details = new DeleteMultiRegionAccessPointInput { Name = _mrapName }
-            };
+            // Delete the objects in the MRAP bucket, but leave the
+            // MRAP and bucket for future test runs
+            S3TestUtils.DeleteObjects(Client, _mrapArn);
 
-            S3TestUtils.DeleteMRAPWithWait(new AmazonS3ControlClient(RegionEndpoint.USWest2), deleteRequest);
+            // Delete the entire bucket used for the SigV4 tests
             AmazonS3Util.DeleteS3BucketWithObjects(Client, _bucketName);
         }
 
