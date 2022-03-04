234c13e0edbffe22485148f175d5934d15b3e43e
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2022-03-04 11:27:19 -0800

    Allow custom handler function.

--- sdk/src/Services/Synthetics/Generated/Model/CanaryCodeInput.cs
+++ sdk/src/Services/Synthetics/Generated/Model/CanaryCodeInput.cs
@@ -46,12 +46,17 @@ namespace Amazon.Synthetics.Model
         /// <summary>
         /// Gets and sets the property Handler. 
         /// <para>
-        /// The entry point to use for the source code when running the canary. This value must
-        /// end with the string <code>.handler</code>. The string is limited to 29 characters
-        /// or fewer.
+        /// The entry point to use for the source code when running the canary. For canaries that
+        /// use the <code>syn-python-selenium-1.0</code> runtime or a <code>syn-nodejs.puppeteer</code>
+        /// runtime earlier than <code>syn-nodejs.puppeteer-3.4</code>, the handler must be specified
+        /// as <code> <i>fileName</i>.handler</code>. For <code>syn-python-selenium-1.1</code>,
+        /// <code>syn-nodejs.puppeteer-3.4</code>, and later runtimes, the handler can be specified
+        /// as <code> <i>fileName</i>.<i>functionName</i> </code>, or you can specify a folder
+        /// where canary scripts reside as <code> <i>folder</i>/<i>fileName</i>.<i>functionName</i>
+        /// </code>.
         /// </para>
         /// </summary>
-        [AWSProperty(Required=true, Min=1, Max=1024)]
+        [AWSProperty(Required=true, Min=1, Max=128)]
         public string Handler
         {
             get { return this._handler; }
