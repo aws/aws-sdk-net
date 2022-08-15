4b6f79303a5a3ebe06cbc3b767e5361c40b37503
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2022-08-15 11:42:35 -0700

    Documentation updates to reflect service rename - AWS IAM Identity Center (successor to AWS Single Sign-On)

--- sdk/src/Services/IdentityStore/Generated/_bcl35/AmazonIdentityStoreClient.cs
+++ sdk/src/Services/IdentityStore/Generated/_bcl35/AmazonIdentityStoreClient.cs
@@ -35,10 +35,23 @@ namespace Amazon.IdentityStore
     /// <summary>
     /// Implementation for accessing IdentityStore
     ///
-    /// The identity store service used by Amazon Web Services Single Sign On provides a single
+    /// The Identity Store service used by Amazon Web Services Single Sign On provides a single
     /// place to retrieve all of your identities (users and groups). For more information,
     /// see the <a href="https://docs.aws.amazon.com/singlesignon/latest/userguide/what-is.html">Amazon
     /// Web Services SSO User Guide</a>.
+    /// 
+    ///  <note> 
+    /// <para>
+    /// Although Amazon Web Services Single Sign-On was renamed, the <code>sso</code> and
+    /// <code>identitystore</code> API namespaces will continue to retain their original name
+    /// for backward compatibility purposes. For more information, see <a href="https://docs.aws.amazon.com/singlesignon/latest/userguide/what-is.html#renamed">Amazon
+    /// Web Services SSO rename</a>.
+    /// </para>
+    ///  </note> 
+    /// <para>
+    /// This reference guide describes the identity store operations that you can call programatically
+    /// and includes detailed information on data types and errors.
+    /// </para>
     /// </summary>
     public partial class AmazonIdentityStoreClient : AmazonServiceClient, IAmazonIdentityStore
     {
