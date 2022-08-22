09703f16bdb47f5b1154627a01c05acee9ab363c
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2022-08-22 11:28:17 -0700

    releasing What-If Analysis APIs

---
+++ sdk/src/Services/ForecastQueryService/Generated/Model/QueryWhatIfForecastRequest.cs
@@ -0,0 +1,152 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ * 
+ *  http://aws.amazon.com/apache2.0
+ * 
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/*
+ * Do not modify this file. This file is generated from the forecastquery-2018-06-26.normal.json service model.
+ */
+using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.Text;
+using System.IO;
+using System.Net;
+
+using Amazon.Runtime;
+using Amazon.Runtime.Internal;
+
+namespace Amazon.ForecastQueryService.Model
+{
+    /// <summary>
+    /// Container for the parameters to the QueryWhatIfForecast operation.
+    /// Retrieves a what-if forecast.
+    /// </summary>
+    public partial class QueryWhatIfForecastRequest : AmazonForecastQueryServiceRequest
+    {
+        private string _endDate;
+        private Dictionary<string, string> _filters = new Dictionary<string, string>();
+        private string _nextToken;
+        private string _startDate;
+        private string _whatIfForecastArn;
+
+        /// <summary>
+        /// Gets and sets the property EndDate. 
+        /// <para>
+        /// The end date for the what-if forecast. Specify the date using this format: yyyy-MM-dd'T'HH:mm:ss
+        /// (ISO 8601 format). For example, 2015-01-01T20:00:00. 
+        /// </para>
+        /// </summary>
+        public string EndDate
+        {
+            get { return this._endDate; }
+            set { this._endDate = value; }
+        }
+
+        // Check to see if EndDate property is set
+        internal bool IsSetEndDate()
+        {
+            return this._endDate != null;
+        }
+
+        /// <summary>
+        /// Gets and sets the property Filters. 
+        /// <para>
+        /// The filtering criteria to apply when retrieving the forecast. For example, to get
+        /// the forecast for <code>client_21</code> in the electricity usage dataset, specify
+        /// the following:
+        /// </para>
+        ///  
+        /// <para>
+        ///  <code>{"item_id" : "client_21"}</code> 
+        /// </para>
+        ///  
+        /// <para>
+        /// To get the full what-if forecast, use the <a href="https://docs.aws.amazon.com/en_us/forecast/latest/dg/API_CreateWhatIfForecastExport.html">CreateForecastExportJob</a>
+        /// operation.
+        /// </para>
+        /// </summary>
+        [AWSProperty(Required=true, Min=1, Max=50)]
+        public Dictionary<string, string> Filters
+        {
+            get { return this._filters; }
+            set { this._filters = value; }
+        }
+
+        // Check to see if Filters property is set
+        internal bool IsSetFilters()
+        {
+            return this._filters != null && this._filters.Count > 0; 
+        }
+
+        /// <summary>
+        /// Gets and sets the property NextToken. 
+        /// <para>
+        /// If the result of the previous request was truncated, the response includes a <code>NextToken</code>.
+        /// To retrieve the next set of results, use the token in the next request. Tokens expire
+        /// after 24 hours.
+        /// </para>
+        /// </summary>
+        [AWSProperty(Min=1, Max=3000)]
+        public string NextToken
+        {
+            get { return this._nextToken; }
+            set { this._nextToken = value; }
+        }
+
+        // Check to see if NextToken property is set
+        internal bool IsSetNextToken()
+        {
+            return this._nextToken != null;
+        }
+
+        /// <summary>
+        /// Gets and sets the property StartDate. 
+        /// <para>
+        /// The start date for the what-if forecast. Specify the date using this format: yyyy-MM-dd'T'HH:mm:ss
+        /// (ISO 8601 format). For example, 2015-01-01T08:00:00.
+        /// </para>
+        /// </summary>
+        public string StartDate
+        {
+            get { return this._startDate; }
+            set { this._startDate = value; }
+        }
+
+        // Check to see if StartDate property is set
+        internal bool IsSetStartDate()
+        {
+            return this._startDate != null;
+        }
+
+        /// <summary>
+        /// Gets and sets the property WhatIfForecastArn. 
+        /// <para>
+        /// The Amazon Resource Name (ARN) of the what-if forecast to query.
+        /// </para>
+        /// </summary>
+        [AWSProperty(Required=true, Max=300)]
+        public string WhatIfForecastArn
+        {
+            get { return this._whatIfForecastArn; }
+            set { this._whatIfForecastArn = value; }
+        }
+
+        // Check to see if WhatIfForecastArn property is set
+        internal bool IsSetWhatIfForecastArn()
+        {
+            return this._whatIfForecastArn != null;
+        }
+
+    }
+}
--- sdk/test/Services/ForecastQueryService/UnitTests/Generated/Marshalling/ForecastQueryServiceMarshallingTests.cs
+++ sdk/test/Services/ForecastQueryService/UnitTests/Generated/Marshalling/ForecastQueryServiceMarshallingTests.cs
@@ -226,5 +226,192 @@ namespace AWSSDK_DotNet35.UnitTests.Marshalling
 
             InstantiateClassGenerator.ValidateObjectFullyInstantiated(response);
         }
+
+        [TestMethod]
+        [TestCategory("UnitTest")]
+        [TestCategory("Json")]
+        [TestCategory("ForecastQueryService")]
+        public void QueryWhatIfForecastMarshallTest()
+        {
+            var request = InstantiateClassGenerator.Execute<QueryWhatIfForecastRequest>();
+            var marshaller = new QueryWhatIfForecastRequestMarshaller();
+
+            var internalRequest = marshaller.Marshall(request);
+            var jsonRequest = UTF8Encoding.UTF8.GetString(internalRequest.Content);
+            Comparer.CompareObjectToJson<QueryWhatIfForecastRequest>(request,jsonRequest);
+
+            var webResponse = new WebResponseData
+            {
+                Headers = {
+                    {"x-amzn-RequestId", Guid.NewGuid().ToString()},
+                    {"x-amz-crc32","0"}
+                }
+            };
+            var jsonResponse = new JsonSampleGenerator(service_model, service_model.FindOperation("QueryWhatIfForecast").ResponseStructure).Execute();
+            webResponse.Headers.Add("Content-Length", UTF8Encoding.UTF8.GetBytes(jsonResponse).Length.ToString());
+            UnmarshallerContext context = new JsonUnmarshallerContext(Utils.CreateStreamFromString(jsonResponse), false, webResponse);
+            var response = QueryWhatIfForecastResponseUnmarshaller.Instance.Unmarshall(context) as QueryWhatIfForecastResponse;
+            InstantiateClassGenerator.ValidateObjectFullyInstantiated(response);
+        }
+
+        [TestMethod]
+        [TestCategory("UnitTest")]
+        [TestCategory("Json")]
+        [TestCategory("ForecastQueryService")]
+        public void QueryWhatIfForecast_InvalidInputExceptionMarshallTest()
+        {
+            var operation =  service_model.FindOperation("QueryWhatIfForecast");
+
+            var request = InstantiateClassGenerator.Execute<QueryWhatIfForecastRequest>();
+            var marshaller = new QueryWhatIfForecastRequestMarshaller();
+            var internalRequest = marshaller.Marshall(request);
+            var jsonRequest = UTF8Encoding.UTF8.GetString(internalRequest.Content);
+
+            Comparer.CompareObjectToJson<QueryWhatIfForecastRequest>(request,jsonRequest);
+
+            var exception = operation.Exceptions.First(e => e.Name.Equals("InvalidInputException"));
+            var jsonResponse = new JsonSampleGenerator(service_model, exception).Execute();
+            var webResponse = new WebResponseData
+            {
+                Headers = {
+                    {"x-amzn-RequestId", Guid.NewGuid().ToString()},
+                    {"x-amz-crc32","0"},
+                    {"x-amzn-ErrorType","InvalidInputException"},
+                    {"Content-Length", UTF8Encoding.UTF8.GetBytes(jsonResponse).Length.ToString()}
+                }
+            };
+            var context = new JsonUnmarshallerContext(Utils.CreateStreamFromString(jsonResponse), true, webResponse, true);
+            var response = QueryWhatIfForecastResponseUnmarshaller.Instance.UnmarshallException(context, null, System.Net.HttpStatusCode.OK);
+
+            InstantiateClassGenerator.ValidateObjectFullyInstantiated(response);
+        }
+
+        [TestMethod]
+        [TestCategory("UnitTest")]
+        [TestCategory("Json")]
+        [TestCategory("ForecastQueryService")]
+        public void QueryWhatIfForecast_InvalidNextTokenExceptionMarshallTest()
+        {
+            var operation =  service_model.FindOperation("QueryWhatIfForecast");
+
+            var request = InstantiateClassGenerator.Execute<QueryWhatIfForecastRequest>();
+            var marshaller = new QueryWhatIfForecastRequestMarshaller();
+            var internalRequest = marshaller.Marshall(request);
+            var jsonRequest = UTF8Encoding.UTF8.GetString(internalRequest.Content);
+
+            Comparer.CompareObjectToJson<QueryWhatIfForecastRequest>(request,jsonRequest);
+
+            var exception = operation.Exceptions.First(e => e.Name.Equals("InvalidNextTokenException"));
+            var jsonResponse = new JsonSampleGenerator(service_model, exception).Execute();
+            var webResponse = new WebResponseData
+            {
+                Headers = {
+                    {"x-amzn-RequestId", Guid.NewGuid().ToString()},
+                    {"x-amz-crc32","0"},
+                    {"x-amzn-ErrorType","InvalidNextTokenException"},
+                    {"Content-Length", UTF8Encoding.UTF8.GetBytes(jsonResponse).Length.ToString()}
+                }
+            };
+            var context = new JsonUnmarshallerContext(Utils.CreateStreamFromString(jsonResponse), true, webResponse, true);
+            var response = QueryWhatIfForecastResponseUnmarshaller.Instance.UnmarshallException(context, null, System.Net.HttpStatusCode.OK);
+
+            InstantiateClassGenerator.ValidateObjectFullyInstantiated(response);
+        }
+
+        [TestMethod]
+        [TestCategory("UnitTest")]
+        [TestCategory("Json")]
+        [TestCategory("ForecastQueryService")]
+        public void QueryWhatIfForecast_LimitExceededExceptionMarshallTest()
+        {
+            var operation =  service_model.FindOperation("QueryWhatIfForecast");
+
+            var request = InstantiateClassGenerator.Execute<QueryWhatIfForecastRequest>();
+            var marshaller = new QueryWhatIfForecastRequestMarshaller();
+            var internalRequest = marshaller.Marshall(request);
+            var jsonRequest = UTF8Encoding.UTF8.GetString(internalRequest.Content);
+
+            Comparer.CompareObjectToJson<QueryWhatIfForecastRequest>(request,jsonRequest);
+
+            var exception = operation.Exceptions.First(e => e.Name.Equals("LimitExceededException"));
+            var jsonResponse = new JsonSampleGenerator(service_model, exception).Execute();
+            var webResponse = new WebResponseData
+            {
+                Headers = {
+                    {"x-amzn-RequestId", Guid.NewGuid().ToString()},
+                    {"x-amz-crc32","0"},
+                    {"x-amzn-ErrorType","LimitExceededException"},
+                    {"Content-Length", UTF8Encoding.UTF8.GetBytes(jsonResponse).Length.ToString()}
+                }
+            };
+            var context = new JsonUnmarshallerContext(Utils.CreateStreamFromString(jsonResponse), true, webResponse, true);
+            var response = QueryWhatIfForecastResponseUnmarshaller.Instance.UnmarshallException(context, null, System.Net.HttpStatusCode.OK);
+
+            InstantiateClassGenerator.ValidateObjectFullyInstantiated(response);
+        }
+
+        [TestMethod]
+        [TestCategory("UnitTest")]
+        [TestCategory("Json")]
+        [TestCategory("ForecastQueryService")]
+        public void QueryWhatIfForecast_ResourceInUseExceptionMarshallTest()
+        {
+            var operation =  service_model.FindOperation("QueryWhatIfForecast");
+
+            var request = InstantiateClassGenerator.Execute<QueryWhatIfForecastRequest>();
+            var marshaller = new QueryWhatIfForecastRequestMarshaller();
+            var internalRequest = marshaller.Marshall(request);
+            var jsonRequest = UTF8Encoding.UTF8.GetString(internalRequest.Content);
+
+            Comparer.CompareObjectToJson<QueryWhatIfForecastRequest>(request,jsonRequest);
+
+            var exception = operation.Exceptions.First(e => e.Name.Equals("ResourceInUseException"));
+            var jsonResponse = new JsonSampleGenerator(service_model, exception).Execute();
+            var webResponse = new WebResponseData
+            {
+                Headers = {
+                    {"x-amzn-RequestId", Guid.NewGuid().ToString()},
+                    {"x-amz-crc32","0"},
+                    {"x-amzn-ErrorType","ResourceInUseException"},
+                    {"Content-Length", UTF8Encoding.UTF8.GetBytes(jsonResponse).Length.ToString()}
+                }
+            };
+            var context = new JsonUnmarshallerContext(Utils.CreateStreamFromString(jsonResponse), true, webResponse, true);
+            var response = QueryWhatIfForecastResponseUnmarshaller.Instance.UnmarshallException(context, null, System.Net.HttpStatusCode.OK);
+
+            InstantiateClassGenerator.ValidateObjectFullyInstantiated(response);
+        }
+
+        [TestMethod]
+        [TestCategory("UnitTest")]
+        [TestCategory("Json")]
+        [TestCategory("ForecastQueryService")]
+        public void QueryWhatIfForecast_ResourceNotFoundExceptionMarshallTest()
+        {
+            var operation =  service_model.FindOperation("QueryWhatIfForecast");
+
+            var request = InstantiateClassGenerator.Execute<QueryWhatIfForecastRequest>();
+            var marshaller = new QueryWhatIfForecastRequestMarshaller();
+            var internalRequest = marshaller.Marshall(request);
+            var jsonRequest = UTF8Encoding.UTF8.GetString(internalRequest.Content);
+
+            Comparer.CompareObjectToJson<QueryWhatIfForecastRequest>(request,jsonRequest);
+
+            var exception = operation.Exceptions.First(e => e.Name.Equals("ResourceNotFoundException"));
+            var jsonResponse = new JsonSampleGenerator(service_model, exception).Execute();
+            var webResponse = new WebResponseData
+            {
+                Headers = {
+                    {"x-amzn-RequestId", Guid.NewGuid().ToString()},
+                    {"x-amz-crc32","0"},
+                    {"x-amzn-ErrorType","ResourceNotFoundException"},
+                    {"Content-Length", UTF8Encoding.UTF8.GetBytes(jsonResponse).Length.ToString()}
+                }
+            };
+            var context = new JsonUnmarshallerContext(Utils.CreateStreamFromString(jsonResponse), true, webResponse, true);
+            var response = QueryWhatIfForecastResponseUnmarshaller.Instance.UnmarshallException(context, null, System.Net.HttpStatusCode.OK);
+
+            InstantiateClassGenerator.ValidateObjectFullyInstantiated(response);
+        }
     }
 }
