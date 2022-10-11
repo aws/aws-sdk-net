c050704fd0b66b60ad9711dfc0a63205f5540a05
Daniel Pinheiro <dspin@amazon.com>
2022-10-11 18:42:57 -0400

    Update NuGet.CommandLine to 6.3.1

--- generator/ServiceClientGeneratorLib/Generators/CodeAnalysis/CodeAnalysisPackages.cs
+++ generator/ServiceClientGeneratorLib/Generators/CodeAnalysis/CodeAnalysisPackages.cs
@@ -18,7 +18,7 @@ namespace ServiceClientGenerator.Generators.CodeAnalysis
     /// Class to produce the template output
     /// </summary>
     
-    #line 1 "C:\Users\jangirg\source\repos\aws-sdk-net\generator\ServiceClientGeneratorLib\Generators\CodeAnalysis\CodeAnalysisPackages.tt"
+    #line 1 "C:\Dev\AWSDotNetPublic\generator\ServiceClientGeneratorLib\Generators\CodeAnalysis\CodeAnalysisPackages.tt"
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.TextTemplating", "17.0.0.0")]
     public partial class CodeAnalysisPackages : CodeAnalysisPackagesBase
     {
@@ -36,7 +36,7 @@ namespace ServiceClientGenerator.Generators.CodeAnalysis
   <package id=""Microsoft.CodeAnalysis.CSharp.Workspaces"" version=""1.0.0"" targetFramework=""portable45-net45+win8"" />
   <package id=""Microsoft.CodeAnalysis.Workspaces.Common"" version=""1.0.0"" targetFramework=""portable45-net45+win8"" />
   <package id=""Microsoft.Composition"" version=""1.0.27"" targetFramework=""portable45-net45+win8"" />
-  <package id=""NuGet.CommandLine"" version=""6.2.1"" targetFramework=""portable45-net45+win8"" />
+  <package id=""NuGet.CommandLine"" version=""6.3.1"" targetFramework=""portable45-net45+win8"" />
   <package id=""System.Collections.Immutable"" version=""1.1.36"" targetFramework=""portable45-net45+win8"" />
   <package id=""System.Reflection.Metadata"" version=""1.0.21"" targetFramework=""portable45-net45+win8"" />
 </packages>");
