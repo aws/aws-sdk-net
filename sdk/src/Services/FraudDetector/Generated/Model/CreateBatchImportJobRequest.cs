7ab7de2ab2c7e7750ae29b214552de4ea7ef0d6d
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2022-10-18 11:23:10 -0700

    Documentation Updates for Amazon Fraud Detector

--- sdk/src/Services/FraudDetector/Generated/Model/CreateBatchImportJobRequest.cs
+++ sdk/src/Services/FraudDetector/Generated/Model/CreateBatchImportJobRequest.cs
@@ -63,8 +63,13 @@ namespace Amazon.FraudDetector.Model
         /// <summary>
         /// Gets and sets the property IamRoleArn. 
         /// <para>
-        /// The ARN of the IAM role created for Amazon S3 bucket that holds your data file. The
-        /// IAM role must have read and write permissions to both input and output S3 buckets.
+        /// The ARN of the IAM role created for Amazon S3 bucket that holds your data file.
+        /// </para>
+        ///  
+        /// <para>
+        /// The IAM role must have read permissions to your input S3 bucket and write permissions
+        /// to your output S3 bucket. For more information about bucket permissions, see <a href="https://docs.aws.amazon.com/AmazonS3/latest/userguide/example-policies-s3.html">User
+        /// policy examples</a> in the <i>Amazon S3 User Guide</i>.
         /// </para>
         /// </summary>
         [AWSProperty(Required=true, Min=1, Max=256)]
