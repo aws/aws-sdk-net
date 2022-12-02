fdb55ba214a28bf9a5d13bc11972d3ba8c81535a
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2022-12-02 11:19:44 -0800

    Fixes the URI for the evaluatecode endpoint to include the /v1 prefix (ie. "/v1/dataplane-evaluatecode").

--- sdk/src/Services/AppSync/Generated/Model/AppSyncRuntime.cs
+++ sdk/src/Services/AppSync/Generated/Model/AppSyncRuntime.cs
@@ -29,9 +29,9 @@ using Amazon.Runtime.Internal;
 namespace Amazon.AppSync.Model
 {
     /// <summary>
-    /// Describes a runtime used by an AWS AppSync pipeline resolver or AWS AppSync function.
-    /// Specifies the name and version of the runtime to use. Note that if a runtime is specified,
-    /// code must also be specified.
+    /// Describes a runtime used by an Amazon Web Services AppSync pipeline resolver or Amazon
+    /// Web Services AppSync function. Specifies the name and version of the runtime to use.
+    /// Note that if a runtime is specified, code must also be specified.
     /// </summary>
     public partial class AppSyncRuntime
     {
