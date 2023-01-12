18ef731a1d3774f2487a8bc2a660a488e189bc00
Muhammad Othman <muhammmadothman@gmail.com>
2023-01-12 13:57:53 -0500

    update DynamoDBEntryConversion.TryFromEntry to make it more readable

--- sdk/src/Services/DynamoDBv2/Custom/Conversion/DynamoDBEntryConversion.cs
+++ sdk/src/Services/DynamoDBv2/Custom/Conversion/DynamoDBEntryConversion.cs
@@ -573,12 +573,15 @@ namespace Amazon.DynamoDBv2
             if (d != null && TryFrom(d, targetType, out value))
                 return true;
 
-            value = null;
-
             var n = entry as DynamoDBNull;
             if (n != null)
+            {
+                value = null;
                 return true;
+            }
 
+            // Could not convert entry to a known type, setting value to null as it's an out parameter
+            value = null;
             return false;
         }
 
