ec5cb0454c5e1c74c7a9e934613b0294c86ec1d5
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2023-03-08 11:24:24 -0800

    This release provides the date and time VOD resources were created.

--- sdk/src/Services/MediaPackageVod/Generated/Model/CreatePackagingGroupResponse.cs
+++ sdk/src/Services/MediaPackageVod/Generated/Model/CreatePackagingGroupResponse.cs
@@ -35,6 +35,7 @@ namespace Amazon.MediaPackageVod.Model
     {
         private string _arn;
         private Authorization _authorization;
+        private string _createdAt;
         private string _domainName;
         private EgressAccessLogs _egressAccessLogs;
         private string _id;
@@ -68,6 +69,21 @@ namespace Amazon.MediaPackageVod.Model
         internal bool IsSetAuthorization()
         {
             return this._authorization != null;
+        }
+
+        /// <summary>
+        /// Gets and sets the property CreatedAt. The time the PackagingGroup was created.
+        /// </summary>
+        public string CreatedAt
+        {
+            get { return this._createdAt; }
+            set { this._createdAt = value; }
+        }
+
+        // Check to see if CreatedAt property is set
+        internal bool IsSetCreatedAt()
+        {
+            return this._createdAt != null;
         }
 
         /// <summary>
