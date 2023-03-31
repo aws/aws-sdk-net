783c64684a3340a233589134d13d132eb600618e
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2023-03-31 11:25:03 -0700

    In this release, you can now chose between soft delete and hard delete when calling the DeleteRecord API, so you have more flexibility when it comes to managing online store data.

--- sdk/src/Services/SageMakerFeatureStoreRuntime/Generated/ServiceEnumerations.cs
+++ sdk/src/Services/SageMakerFeatureStoreRuntime/Generated/ServiceEnumerations.cs
@@ -25,6 +25,56 @@ namespace Amazon.SageMakerFeatureStoreRuntime
 {
 
     /// <summary>
+    /// Constants used for properties of type DeletionMode.
+    /// </summary>
+    public class DeletionMode : ConstantClass
+    {
+
+        /// <summary>
+        /// Constant HardDelete for DeletionMode
+        /// </summary>
+        public static readonly DeletionMode HardDelete = new DeletionMode("HardDelete");
+        /// <summary>
+        /// Constant SoftDelete for DeletionMode
+        /// </summary>
+        public static readonly DeletionMode SoftDelete = new DeletionMode("SoftDelete");
+
+        /// <summary>
+        /// This constant constructor does not need to be called if the constant
+        /// you are attempting to use is already defined as a static instance of 
+        /// this class.
+        /// This constructor should be used to construct constants that are not
+        /// defined as statics, for instance if attempting to use a feature that is
+        /// newer than the current version of the SDK.
+        /// </summary>
+        public DeletionMode(string value)
+            : base(value)
+        {
+        }
+
+        /// <summary>
+        /// Finds the constant for the unique value.
+        /// </summary>
+        /// <param name="value">The unique value for the constant</param>
+        /// <returns>The constant for the unique value</returns>
+        public static DeletionMode FindValue(string value)
+        {
+            return FindValue<DeletionMode>(value);
+        }
+
+        /// <summary>
+        /// Utility method to convert strings to the constant class.
+        /// </summary>
+        /// <param name="value">The string value to convert to the constant class.</param>
+        /// <returns></returns>
+        public static implicit operator DeletionMode(string value)
+        {
+            return FindValue(value);
+        }
+    }
+
+
+    /// <summary>
     /// Constants used for properties of type TargetStore.
     /// </summary>
     public class TargetStore : ConstantClass
