8fde7cc1cde5df7a1f9965b61c778c67262c9c24
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2023-04-14 11:23:53 -0700

    Doc only update for Refactor Spaces environments without network bridge feature.

--- sdk/src/Services/MigrationHubRefactorSpaces/Generated/Model/GetEnvironmentResponse.cs
+++ sdk/src/Services/MigrationHubRefactorSpaces/Generated/Model/GetEnvironmentResponse.cs
@@ -254,7 +254,7 @@ namespace Amazon.MigrationHubRefactorSpaces.Model
         /// <summary>
         /// Gets and sets the property TransitGatewayId. 
         /// <para>
-        /// The ID of the transit gateway set up by the environment. 
+        /// The ID of the Transit Gateway set up by the environment, if applicable.
         /// </para>
         /// </summary>
         [AWSProperty(Min=21, Max=21)]
