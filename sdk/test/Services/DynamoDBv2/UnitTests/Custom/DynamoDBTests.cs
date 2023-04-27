aa8d77d2d47414c3e732da9c161b6ab505a0ceda
Giho Park <gpark@bitflowtech.com>
2023-04-27 08:45:22 +0200

    Changed Utils to use the correct method

--- sdk/test/Services/DynamoDBv2/UnitTests/Custom/DynamoDBTests.cs
+++ sdk/test/Services/DynamoDBv2/UnitTests/Custom/DynamoDBTests.cs
@@ -17,6 +17,8 @@ using Moq;
 
 namespace AWSSDK_DotNet35.UnitTests
 {
+    using Amazon.DynamoDBv2;
+
     [TestClass]
     public class DynamoDBEntryTests
     {
@@ -284,6 +286,38 @@ namespace AWSSDK_DotNet35.UnitTests
 
             Assert.IsTrue(doc["Name"] is Primitive);
             Assert.IsNull(doc["Name"].AsPrimitive().Value);
+        }
+
+        [TestMethod]
+        [TestCategory("DynamoDBv2")]
+        public void TestPropertyAttributeInheritance()
+        {
+            // Use a mock client to skip credential checks.
+            var mockClient = new Mock<IAmazonDynamoDB>();
+            var context = new DynamoDBContext(mockClient.Object);
+
+            var parent = new Parent();
+            parent.Property1 = "Value";
+
+            var child = new Child();
+            child.Property1 = "Value";
+
+            var parentDocument = context.ToDocument(parent);
+            var childDocument = context.ToDocument(child);
+
+            Assert.AreEqual(parentDocument["actualPropertyName"].AsString(), parent.Property1);
+            Assert.AreEqual(childDocument["actualPropertyName"].AsString(), child.Property1);
+        }
+
+        public class Parent
+        {
+            [DynamoDBProperty("actualPropertyName")]
+            public virtual string Property1 { get; set; }
+        }
+
+        public class Child : Parent
+        {
+            public override string Property1 { get; set; }
         }
 
 #if ASYNC_AWAIT
