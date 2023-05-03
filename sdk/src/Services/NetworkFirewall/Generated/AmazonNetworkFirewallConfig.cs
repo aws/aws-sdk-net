b6b6a4d36a19596ec5517ce2be6f5bd5415a6203
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2023-05-03 13:46:26 -0700

    Commit version number update changes

--- sdk/src/Services/NetworkFirewall/Generated/AmazonNetworkFirewallConfig.cs
+++ sdk/src/Services/NetworkFirewall/Generated/AmazonNetworkFirewallConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.NetworkFirewall
     public partial class AmazonNetworkFirewallConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("3.7.105.18");
+            InternalSDKUtils.BuildUserAgentString("3.7.106.0");
 
         private string _userAgent = UserAgentString;
 
