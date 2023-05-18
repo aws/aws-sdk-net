0a795b41ffefd0214fcc7a91e315b3e77bc61e7a
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2023-05-18 11:25:09 -0700

    This release introduces a new MXF Profile for XDCAM which is strictly compliant with the SMPTE RDD 9 standard and improved handling of output name modifiers.

--- sdk/src/Services/MediaConvert/Generated/Model/AacSettings.cs
+++ sdk/src/Services/MediaConvert/Generated/Model/AacSettings.cs
@@ -115,7 +115,7 @@ namespace Amazon.MediaConvert.Model
         /// following list shows the number of audio channels and channel layout for each coding
         /// mode. * 1.0 Audio Description (Receiver Mix): One channel, C. Includes audio description
         /// data from your stereo input. For more information see ETSI TS 101 154 Annex E. * 1.0
-        /// Mono: One channel, C. * 2.0 Stereo: Two channels, L, R. * 5.1 Surround: Five channels,
+        /// Mono: One channel, C. * 2.0 Stereo: Two channels, L, R. * 5.1 Surround: Six channels,
         /// C, L, R, Ls, Rs, LFE.
         /// </summary>
         public AacCodingMode CodingMode
--- sdk/src/Services/MediaConvert/Generated/Model/InputTemplate.cs
+++ sdk/src/Services/MediaConvert/Generated/Model/InputTemplate.cs
@@ -63,7 +63,7 @@ namespace Amazon.MediaConvert.Model
         /// help increase the video quality of your output relative to its bitrate, since noisy
         /// inputs are more complex and require more bits to encode. To help restore loss of detail
         /// after applying the filter, you can optionally add texture or sharpening as an additional
-        /// step.Jobs that use this feature incur pro-tier pricing. To not apply advanced input
+        /// step. Jobs that use this feature incur pro-tier pricing. To not apply advanced input
         /// filtering: Choose Disabled. Note that you can still apply basic filtering with Deblock
         /// and Denoise.
         /// </summary>
