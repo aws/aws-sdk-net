436613f3112cc6e19f5e0e53f21880b31ab4fbc4
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2023-07-07 11:18:47 -0700

    This release enables customers to create new Apache Iceberg tables and associated metadata in Amazon S3 by using native AWS Glue CreateTable operation.

---
+++ sdk/src/Services/Glue/Generated/Model/IcebergInput.cs
@@ -0,0 +1,78 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ * 
+ *  http://aws.amazon.com/apache2.0
+ * 
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/*
+ * Do not modify this file. This file is generated from the glue-2017-03-31.normal.json service model.
+ */
+using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.Text;
+using System.IO;
+using System.Net;
+
+using Amazon.Runtime;
+using Amazon.Runtime.Internal;
+
+namespace Amazon.Glue.Model
+{
+    /// <summary>
+    /// A structure that defines an Apache Iceberg metadata table to create in the catalog.
+    /// </summary>
+    public partial class IcebergInput
+    {
+        private MetadataOperation _metadataOperation;
+        private string _version;
+
+        /// <summary>
+        /// Gets and sets the property MetadataOperation. 
+        /// <para>
+        /// A required metadata operation. Can only be set to <code>CREATE</code>.
+        /// </para>
+        /// </summary>
+        [AWSProperty(Required=true)]
+        public MetadataOperation MetadataOperation
+        {
+            get { return this._metadataOperation; }
+            set { this._metadataOperation = value; }
+        }
+
+        // Check to see if MetadataOperation property is set
+        internal bool IsSetMetadataOperation()
+        {
+            return this._metadataOperation != null;
+        }
+
+        /// <summary>
+        /// Gets and sets the property Version. 
+        /// <para>
+        /// The table version for the Iceberg table. Defaults to 2.
+        /// </para>
+        /// </summary>
+        [AWSProperty(Min=1, Max=255)]
+        public string Version
+        {
+            get { return this._version; }
+            set { this._version = value; }
+        }
+
+        // Check to see if Version property is set
+        internal bool IsSetVersion()
+        {
+            return this._version != null;
+        }
+
+    }
+}
