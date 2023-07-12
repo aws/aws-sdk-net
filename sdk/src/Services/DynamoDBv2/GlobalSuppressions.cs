e69fd9481c8312887b32790414424901ae6e1e54
Alex Shovlin <shovlia@amazon.com>
2023-07-12 17:17:24 -0400

    feat: Add DynamoDBContextConfig.DisableFetchingTableMetadata to skip the internal DescribeTable call if the index structure is provided statically

--- sdk/src/Services/DynamoDBv2/GlobalSuppressions.cs
+++ sdk/src/Services/DynamoDBv2/GlobalSuppressions.cs
@@ -4,9 +4,10 @@
 // Project-level suppressions either have no target or are given 
 // a specific target and scoped to a namespace, type, member, etc.
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Scope = "member", Target = "~M:Amazon.DynamoDBv2.DataModel.ItemStorageConfigCache.CreateStorageConfig(System.Type,System.String)~Amazon.DynamoDBv2.DataModel.ItemStorageConfig")]
+using System.Diagnostics.CodeAnalysis;
+
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Scope = "member", Target = "~M:Amazon.DynamoDBv2.DocumentModel.Document.DateTimeToEpochSeconds(Amazon.DynamoDBv2.DocumentModel.DynamoDBEntry,System.String)~Amazon.DynamoDBv2.DocumentModel.DynamoDBEntry")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Scope = "member", Target = "~M:Amazon.DynamoDBv2.DocumentModel.Document.EpochSecondsToDateTime(Amazon.DynamoDBv2.DocumentModel.DynamoDBEntry,System.String)~Amazon.DynamoDBv2.DocumentModel.DynamoDBEntry")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Scope = "member", Target = "~M:Amazon.DynamoDBv2.DocumentModel.Table.TryLoadTable(Amazon.DynamoDBv2.IAmazonDynamoDB,Amazon.DynamoDBv2.DocumentModel.TableConfig,Amazon.DynamoDBv2.DocumentModel.Table@)~System.Boolean")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1720:Identifier contains type name", Scope = "type", Target = "~T:Amazon.DynamoDBv2.DocumentModel.DynamoDBEntryType")]
-
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Scope = "member", Target = "~M:Amazon.DynamoDBv2.DataModel.ItemStorageConfigCache.CreateStorageConfig(System.Type,System.String,Amazon.DynamoDBv2.DataModel.DynamoDBFlatConfig)~Amazon.DynamoDBv2.DataModel.ItemStorageConfig")]
