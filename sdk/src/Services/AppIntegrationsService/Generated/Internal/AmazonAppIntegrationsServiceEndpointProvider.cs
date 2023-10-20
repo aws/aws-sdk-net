c1c1eaf5c6a2c0cdf37a502fa9bf7ae9b809c3b5
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2023-10-20 20:12:27 +0000

    Updated ScheduleConfig to be an optional input to CreateDataIntegration to support event driven downloading of files from sources such as Amazon s3 using Amazon Connect AppIntegrations.

--- sdk/src/Services/AppIntegrationsService/Generated/Internal/AmazonAppIntegrationsServiceEndpointProvider.cs
+++ sdk/src/Services/AppIntegrationsService/Generated/Internal/AmazonAppIntegrationsServiceEndpointProvider.cs
@@ -78,7 +78,7 @@ namespace Amazon.AppIntegrationsService.Internal
                     }
                     if (Equals(refs["UseFIPS"], true))
                     {
-                        if (Equals(true, GetAttr(refs["PartitionResult"], "supportsFIPS")))
+                        if (Equals(GetAttr(refs["PartitionResult"], "supportsFIPS"), true))
                         {
                             return new Endpoint(Interpolate(@"https://app-integrations-fips.{Region}.{PartitionResult#dnsSuffix}", refs), InterpolateJson(@"", refs), InterpolateJson(@"", refs));
                         }
