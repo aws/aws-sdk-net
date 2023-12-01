ddec2f27dd9a04d6a35f3da201ceb72403ad4fde
Olli Pottonen <olli.pottonen@silverrailtech.com>
2023-12-01 10:49:42 +1000

    Fix URI parsing
    Recently 374a36f3 added a new group to this regex. That means
    the later group indexes get shifted, the assumptions on lines
    100--107 no longer hold, and we fail to detect the region. The
    simplest fix is to make the new group into a noncapturing group, see
    https://learn.microsoft.com/en-us/dotnet/standard/base-types/grouping-constructs-in-regular-expressions#noncapturing-groups

--- sdk/src/Services/S3/Custom/Util/AmazonS3Uri.cs
+++ sdk/src/Services/S3/Custom/Util/AmazonS3Uri.cs
@@ -32,7 +32,7 @@ namespace Amazon.S3.Util
     /// </summary>
     public class AmazonS3Uri
     {
-        private static readonly Regex EndpointRegexMatch = new Regex(@"^(.+\.)?(s3|s3express)[.-]([a-z0-9-]+)\.", RegexOptions.Compiled);
+        private static readonly Regex EndpointRegexMatch = new Regex(@"^(.+\.)?(?:s3|s3express)[.-]([a-z0-9-]+)\.", RegexOptions.Compiled);
 
         /// <summary>
         /// True if the URI contains the bucket in the path, false if it contains the bucket in the authority.
