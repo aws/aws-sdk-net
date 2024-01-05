6b41bcb058f403c87b412627a1cabe8f600731e1
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2024-01-05 19:20:04 +0000

    use_fips_ssl and require_ssl parameter support for Workgroup, UpdateWorkgroup, and CreateWorkgroup

--- sdk/src/Services/RedshiftServerless/Generated/Model/Workgroup.cs
+++ sdk/src/Services/RedshiftServerless/Generated/Model/Workgroup.cs
@@ -79,8 +79,9 @@ namespace Amazon.RedshiftServerless.Model
         /// An array of parameters to set for advanced control over a database. The options are
         /// <code>auto_mv</code>, <code>datestyle</code>, <code>enable_case_sensitive_identifier</code>,
         /// <code>enable_user_activity_logging</code>, <code>query_group</code>, <code>search_path</code>,
-        /// and query monitoring metrics that let you define performance boundaries. For more
-        /// information about query monitoring rules and available metrics, see <a href="https://docs.aws.amazon.com/redshift/latest/dg/cm-c-wlm-query-monitoring-rules.html#cm-c-wlm-query-monitoring-metrics-serverless">
+        /// <code>require_ssl</code>, <code>use_fips_ssl</code>, and query monitoring metrics
+        /// that let you define performance boundaries. For more information about query monitoring
+        /// rules and available metrics, see <a href="https://docs.aws.amazon.com/redshift/latest/dg/cm-c-wlm-query-monitoring-rules.html#cm-c-wlm-query-monitoring-metrics-serverless">
         /// Query monitoring metrics for Amazon Redshift Serverless</a>.
         /// </para>
         /// </summary>
