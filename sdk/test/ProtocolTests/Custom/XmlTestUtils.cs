74a90009eeb620418e5631f461c78c05866a5551
Peter Song <petesong@amazon.com>
2024-03-20 18:17:53 -0700

    make util classes internal. add license headers

--- sdk/test/ProtocolTests/Custom/XmlTestUtils.cs
+++ sdk/test/ProtocolTests/Custom/XmlTestUtils.cs
@@ -1,4 +1,19 @@
-﻿using Amazon.Runtime.Internal;
+﻿/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ * 
+ *  http://aws.amazon.com/apache2.0
+ * 
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+using Amazon.Runtime.Internal;
 using Amazon.Runtime.Internal.Util;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
@@ -12,7 +27,7 @@ namespace AWSSDK.ProtocolTests.Utils
 {
     internal static class XmlTestUtils
     {
-        internal static void AssertBody(IRequest actualRequest, string expectedBody)
+        public static void AssertBody(IRequest actualRequest, string expectedBody)
         {
             string actualBody;
             if(actualRequest.ContentStream != null)
@@ -29,7 +44,7 @@ namespace AWSSDK.ProtocolTests.Utils
             }
         }
 
-        internal static bool AreDocumentsEqual(XDocument expectedDoc, XDocument actualDoc)
+        private static bool AreDocumentsEqual(XDocument expectedDoc, XDocument actualDoc)
         {
             return AreElementsEqual(expectedDoc.Root,actualDoc.Root);
         }
