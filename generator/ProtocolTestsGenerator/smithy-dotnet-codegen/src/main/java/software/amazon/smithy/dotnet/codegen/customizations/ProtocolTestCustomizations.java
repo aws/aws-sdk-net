e59a4c5a0ea91fe458abe9b1eb9a489b748bc8f0
Peter Song <petesong@amazon.com>
2024-03-29 09:37:37 -0700

    add more to the vnext list

--- generator/ProtocolTestsGenerator/smithy-dotnet-codegen/src/main/java/software/amazon/smithy/dotnet/codegen/customizations/ProtocolTestCustomizations.java
+++ generator/ProtocolTestsGenerator/smithy-dotnet-codegen/src/main/java/software/amazon/smithy/dotnet/codegen/customizations/ProtocolTestCustomizations.java
@@ -25,7 +25,12 @@ public final class ProtocolTestCustomizations
             // but the value changes every time. The test case asserts a static value for the token that we cannot guarantee
             "Ec2ProtocolIdempotencyTokenAutoFill",
             "RestJsonQueryIdempotencyTokenAutoFill",
+<<<<<<< HEAD
             "QueryIdempotencyTokenAutoFill"
+=======
+            "QueryIdempotencyTokenAutoFill",
+            "QueryProtocolIdempotencyTokenAutoFill"
+>>>>>>> 79f1dfd0267 (add more to the vnext list)
 
     );
     public static  final  List<String> VNextTests = Arrays.asList(
@@ -38,6 +43,18 @@ public final class ProtocolTestCustomizations
             "QueryXmlEmptySelfClosedBlobs",
             "QuerySimpleScalarProperties",
             "Ec2XmlEmptySelfClosedBlobs",
+<<<<<<< HEAD
             "Ec2SimpleScalarProperties"
+=======
+            "Ec2SimpleScalarProperties",
+            " SimpleScalarPropertiesPureWhiteSpace",
+            //The tests below require a change in how empty collections are handled. These will be handled in v3.9
+            "XmlEmptyLists",
+            "XmlEmptyMaps",
+            "Ec2EmptyQueryLists",
+            "EmptyQueryLists",
+            "serializes_empty_list_shapes",
+            "serializes_empty_map_shapes"
+>>>>>>> 79f1dfd0267 (add more to the vnext list)
     );
 }
