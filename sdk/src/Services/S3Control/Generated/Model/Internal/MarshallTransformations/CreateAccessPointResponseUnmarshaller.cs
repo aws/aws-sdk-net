9ac51a62e92dc4d5b386a705ba1ff49b3dccd130
Peter Song <petesong@amazon.com>
2024-04-03 13:06:38 -0700

    Generated changes for issue 18

--- sdk/src/Services/S3Control/Generated/Model/Internal/MarshallTransformations/CreateAccessPointResponseUnmarshaller.cs
+++ sdk/src/Services/S3Control/Generated/Model/Internal/MarshallTransformations/CreateAccessPointResponseUnmarshaller.cs
@@ -58,7 +58,10 @@ namespace Amazon.S3Control.Model.Internal.MarshallTransformations
             int targetDepth = originalDepth + 1;
             if (context.IsStartOfDocument) 
                    targetDepth += 1;
-
+            if(context.Stream.Length == 0)
+            {
+                return;
+            }
             while (context.Read())
             {
                 if (context.IsStartElement || context.IsAttribute)
