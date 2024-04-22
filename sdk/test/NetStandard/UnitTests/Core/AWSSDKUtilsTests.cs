9f52e1a88f0f24cc4dff3a47f4340c0d30929086
stevenaw <sweerdenburg@gmail.com>
2024-04-21 23:00:43 -0400

    Add tests which currently pass

---
+++ sdk/test/NetStandard/UnitTests/Core/AWSSDKUtilsTests.cs
@@ -0,0 +1,40 @@
+﻿using Amazon.Runtime;
+using Amazon.Util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UnitTests.NetStandard.Core
+{
+    [Trait("Category", "Core")]
+    public class AWSSDKUtilsTests
+    {
+        [Fact]
+        public void BytesToHexString()
+        {
+            var bytes = Encoding.UTF8.GetBytes("Hello World");
+            var hexString = AWSSDKUtils.BytesToHexString(bytes);
+
+            Assert.Equal("48656C6C6F20576F726C64", hexString);
+        }
+
+        [Fact]
+        public void ToHexUppercase()
+        {
+            var bytes = Encoding.UTF8.GetBytes("Hello World");
+            var hexString = AWSSDKUtils.ToHex(bytes, false);
+
+            Assert.Equal("48656C6C6F20576F726C64", hexString);
+        }
+
+        [Fact]
+        public void ToHexLowercase()
+        {
+            var bytes = Encoding.UTF8.GetBytes("Hello World");
+            var hexString = AWSSDKUtils.ToHex(bytes, true);
+
+            Assert.Equal("48656c6c6f20576f726c64", hexString);
+        }
+    }
+}
