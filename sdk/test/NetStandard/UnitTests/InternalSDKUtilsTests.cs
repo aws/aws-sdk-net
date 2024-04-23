031030db78ef35a8b0a448d0f8a7207025267973
Peter Song <petesong@amazon.com>
2024-04-23 15:56:09 -0700

    Update InternalSDKUTils.DetermineFramework to return standard string and additional metadata if version parsing fails

--- sdk/test/NetStandard/UnitTests/InternalSDKUtilsTests.cs
+++ sdk/test/NetStandard/UnitTests/InternalSDKUtilsTests.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-
 using Xunit;
 using Amazon.Util.Internal;
-using System.Runtime.InteropServices;
-
+using Moq;
 namespace AWSSDK_NetStandard.UnitTests
 {
     public class InternalSDKUtilsTests
@@ -23,7 +21,7 @@ namespace AWSSDK_NetStandard.UnitTests
             Assert.NotEqual("Unknown", platform);
             Assert.Contains("md/ARCH", platform);
         }
-                
+
         [Theory]
         [InlineData(@"c:\a\b\c\test.txt", @"c:\a\b\c", true)]
         [InlineData(@"c:\a\b\c\..\test.txt", @"c:\a\b\c", false)]
@@ -46,6 +44,70 @@ namespace AWSSDK_NetStandard.UnitTests
         {
             var result = InternalSDKUtils.IsFilePathRootedWithDirectoryPath(filePath, directoryPath);
             Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void DetermineFrameworkHandlesNonconventionalVersions()
+        {
+            // Arrange
+            var mockWrapper = new Mock<IRuntimeInformationWrapper>();
+            mockWrapper.Setup(w => w.FrameworkDescription).Returns("01-12-2024");
+            InternalSDKUtils.SetRuntimeInformationWrapper(mockWrapper.Object);
+
+            // Act
+            var framework = InternalSDKUtils.DetermineFramework();
+
+            // Assert
+            Assert.Equal("Unknown md/framework-raw-version#01-12-2024", framework);
+            InternalSDKUtils.ResetRuntimeInformationWrapper();
+        }
+
+        [Fact]
+        public void DetermineFrameworkHandlesNonconventionalVersions2()
+        {
+            // Arrange
+            var mockWrapper = new Mock<IRuntimeInformationWrapper>();
+            mockWrapper.Setup(w => w.FrameworkDescription).Returns("-EX25+1");
+            InternalSDKUtils.SetRuntimeInformationWrapper(mockWrapper.Object);
+
+            // Act
+            var framework = InternalSDKUtils.DetermineFramework();
+
+            // Assert
+            Assert.Equal("Unknown md/framework-raw-version#-EX25+1", framework);
+            InternalSDKUtils.ResetRuntimeInformationWrapper();
+        }
+
+        [Fact]
+        public void DetermineFrameworkHandlesConventionalVersion()
+        {
+            // Arrange
+            var mockWrapper = new Mock<IRuntimeInformationWrapper>();
+            mockWrapper.Setup(w => w.FrameworkDescription).Returns("8.0.0");
+            InternalSDKUtils.SetRuntimeInformationWrapper(mockWrapper.Object);
+
+            // Act
+            var framework = InternalSDKUtils.DetermineFramework();
+
+            // Assert
+            Assert.Equal(".NET_Core#8.0.0", framework);
+            InternalSDKUtils.ResetRuntimeInformationWrapper();
+        }
+
+        [Fact]
+        public void DetermineFrameworkReturnsUnknownIfExceptionIsThrown()
+        {
+            // Arrange
+            var mockWrapper = new Mock<IRuntimeInformationWrapper>();
+            mockWrapper.Setup(w => w.FrameworkDescription).Throws<ArgumentException>();
+            InternalSDKUtils.SetRuntimeInformationWrapper(mockWrapper.Object);
+
+            // Act
+            var framework = InternalSDKUtils.DetermineFramework();
+
+            // Assert
+            Assert.Equal("Unknown", framework);
+            InternalSDKUtils.ResetRuntimeInformationWrapper();
         }
     }
 }
