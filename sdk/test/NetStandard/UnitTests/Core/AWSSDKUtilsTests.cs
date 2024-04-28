8f3795dcb54c9358ab79d9b4d111a199351b69b3
Daniel Marbach <danielmarbach@users.noreply.github.com>
2024-04-28 20:47:40 +0200

    Add unit tests

--- sdk/test/NetStandard/UnitTests/Core/AWSSDKUtilsTests.cs
+++ sdk/test/NetStandard/UnitTests/Core/AWSSDKUtilsTests.cs
@@ -1,4 +1,4 @@
-﻿using Amazon.Util;
+using Amazon.Util;
 using System.Text;
 using Xunit;
 
@@ -31,6 +31,26 @@ namespace UnitTests.NetStandard.Core
             var data = "Hello,   World!";
             var compressed = AWSSDKUtils.CompressSpaces(data);
             Assert.Equal("Hello, World!", compressed);
+        }
+
+        [Theory]
+        [InlineData("value, with special chars!", "value%2C%20with%20special%20chars%21")]
+        [InlineData("value, with special chars and path {/+:}", "value%2C%20with%20special%20chars%20and%20path%20%7B%2F%2B%3A%7D")]
+        public void UrlEncodeWithoutPath(string input, string expected)
+        {
+            var encoded = AWSSDKUtils.UrlEncode(input, path: false);
+
+            Assert.Equal(expected, encoded);
+        }
+
+        [Theory]
+        [InlineData("value, with special chars!", "value%2C%20with%20special%20chars!")]
+        [InlineData("value, with special chars and path {/+:}", "value%2C%20with%20special%20chars%20and%20path%20%7B/%2B:%7D")]
+        public void UrlEncodeWithPath(string input, string expected)
+        {
+            var encoded = AWSSDKUtils.UrlEncode(input, path: true);
+
+            Assert.Equal(expected, encoded);
         }
     }
 }
