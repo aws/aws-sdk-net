d14ded3a4dc515d7e6c669ccae3e42674e2d9ad2
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2024-05-08 18:18:28 +0000

    Adding Precision Hardware Clock (PHC) to public API DescribeInstanceTypes

--- sdk/src/Services/EC2/Generated/ServiceEnumerations.cs
+++ sdk/src/Services/EC2/Generated/ServiceEnumerations.cs
@@ -15421,6 +15421,56 @@ namespace Amazon.EC2
 
 
     /// <summary>
+    /// Constants used for properties of type PhcSupport.
+    /// </summary>
+    public class PhcSupport : ConstantClass
+    {
+
+        /// <summary>
+        /// Constant Supported for PhcSupport
+        /// </summary>
+        public static readonly PhcSupport Supported = new PhcSupport("supported");
+        /// <summary>
+        /// Constant Unsupported for PhcSupport
+        /// </summary>
+        public static readonly PhcSupport Unsupported = new PhcSupport("unsupported");
+
+        /// <summary>
+        /// This constant constructor does not need to be called if the constant
+        /// you are attempting to use is already defined as a static instance of 
+        /// this class.
+        /// This constructor should be used to construct constants that are not
+        /// defined as statics, for instance if attempting to use a feature that is
+        /// newer than the current version of the SDK.
+        /// </summary>
+        public PhcSupport(string value)
+            : base(value)
+        {
+        }
+
+        /// <summary>
+        /// Finds the constant for the unique value.
+        /// </summary>
+        /// <param name="value">The unique value for the constant</param>
+        /// <returns>The constant for the unique value</returns>
+        public static PhcSupport FindValue(string value)
+        {
+            return FindValue<PhcSupport>(value);
+        }
+
+        /// <summary>
+        /// Utility method to convert strings to the constant class.
+        /// </summary>
+        /// <param name="value">The string value to convert to the constant class.</param>
+        /// <returns></returns>
+        public static implicit operator PhcSupport(string value)
+        {
+            return FindValue(value);
+        }
+    }
+
+
+    /// <summary>
     /// Constants used for properties of type PlacementGroupState.
     /// </summary>
     public class PlacementGroupState : ConstantClass
