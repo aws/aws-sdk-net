2320b3de8c95dea145f93260c88928df27ef7f2b
Bo Blodgett <boblod@amazon.com>
2024-06-10 15:28:43 -0700

    Change extension projects version strategy in V4 to set AssemblyVersion.

--- extensions/src/AWSSDK.Extensions.NETCore.Setup/Properties/AssemblyInfo.cs
+++ extensions/src/AWSSDK.Extensions.NETCore.Setup/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 
-[assembly: AssemblyVersion("4.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
 [assembly: AssemblyFileVersion("4.0.0.0")]
