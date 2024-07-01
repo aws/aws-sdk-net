c90361d208f649989a1d638eff4b8ae03fc48709
Daniel Pinheiro <dspin@amazon.com>
2024-07-01 07:03:16 -0400

    feat: Add interfaces for auth scheme resolver

---
+++ extensions/src/Smithy.Identity.Abstractions/Auth/IAuthSchemeParameters.cs
@@ -0,0 +1,23 @@
+﻿namespace Smithy.Identity.Abstractions
+{
+    /// <summary>
+    /// <para>
+    /// The input to the authentication scheme resolver. A code-generated interface for passing 
+    /// in the data required for determining the authentication scheme.
+    /// </para>
+    /// </summary>
+    /// <remarks>
+    /// Service specific implementations may include additional properties, for example "region" if the
+    /// service is configured to use SigV4.
+    /// </remarks>
+    public interface IAuthSchemeParameters
+    {
+        /// <summary>
+        /// The service operation being invoked by the SDK.
+        /// </summary>
+        /// <remarks>
+        /// This is included by default for all generated authentication scheme parameters.
+        /// </remarks>
+        string Operation { get; set; }
+    }
+}
