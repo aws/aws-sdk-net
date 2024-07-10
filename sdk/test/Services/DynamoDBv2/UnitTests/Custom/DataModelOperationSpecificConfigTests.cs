41f9ab8d5ef13e2aefd6b426bea21bfe35142681
Alex Shovlin <shovlia@amazon.com>
2024-07-10 15:42:27 -0400

    Add base class for upcoming object persistence operation-specific config objects (#3380)

---
+++ sdk/test/Services/DynamoDBv2/UnitTests/Custom/DataModelOperationSpecificConfigTests.cs
@@ -0,0 +1,22 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AWSSDK_DotNet.UnitTests
+{
+    /// <summary>
+    /// These tests serve as a reminder to developers and reviewers to 
+    /// ensure that new DynamoDB operation-specific properties are plumbed
+    /// into the internal code paths correctly
+    /// </summary>
+    [TestClass]
+    public class DataModelOperationSpecificConfigTests
+    {
+        [TestMethod]
+        public void BaseOperationConfig()
+        {
+            // If this fails because you've added a property, be sure to add it to
+            // `ToDynamoDBOperationConfig` before updating this unit test
+            Assert.AreEqual(6, typeof(BaseOperationConfig).GetProperties().Length);
+        }
+    }
+}
