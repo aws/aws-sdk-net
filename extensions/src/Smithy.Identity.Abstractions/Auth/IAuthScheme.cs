f2a6d44ae7437fb580d8a073c0ff251ffe55c079
Daniel Pinheiro <3842788+dscpinheiro@users.noreply.github.com>
2024-07-15 09:00:03 -0400

    Add interfaces for auth scheme resolver (#3382)

---
+++ extensions/src/Smithy.Identity.Abstractions/Auth/IAuthScheme.cs
@@ -0,0 +1,24 @@
+﻿namespace Smithy.Identity.Abstractions
+{
+    /// <summary>
+    /// <para>
+    /// Authentication schemes represent a way that the service will authenticate the customer's
+    /// identity. An authentication scheme consists of an ID (e.g. "aws.auth#sigv4"), logic to 
+    /// determine the identity resolver, and a signer matching the authentication type.
+    /// </para>
+    /// 
+    /// <para>
+    /// For example, the "aws.auth#sigv4" authentication scheme selects the identity resolver for 
+    /// AWS credentials and the AWS SigV4 signer.
+    /// </para>
+    /// </summary>
+    public interface IAuthScheme<T> where T : BaseIdentity
+    {
+        /// <summary>
+        /// Unique identifier for the authentication scheme (e.g. "smithy.api#noAuth" or "aws.auth#sigv4").
+        /// </summary>
+        string SchemeId { get; }
+
+        // TODO: Include methods to retrieve identity resolver and signer for the current scheme (their interfaces haven't been created yet).
+    }
+}
---
+++ sdk/src/Core/Amazon.Runtime/Credentials/Internal/AwsV4AuthScheme.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+using Smithy.Identity.Abstractions;
+
+namespace Amazon.Runtime.Credentials.Internal
+{
+    /// <summary>
+    /// The <a href="https://smithy.io/2.0/aws/aws-auth.html#aws-auth-sigv4-trait">AWS Signature Version 4 authentication scheme</a> (which 
+    /// uses AWS credentials and the <a href="https://docs.aws.amazon.com/AmazonS3/latest/API/sig-v4-authenticating-requests.html">SigV4 signer</a>).
+    /// </summary>
+    public class AwsV4AuthScheme : IAuthScheme<AWSCredentials>
+    {
+        /// <inheritdoc/>
+        public string SchemeId => "aws.auth#sigv4";
+    }
+}
