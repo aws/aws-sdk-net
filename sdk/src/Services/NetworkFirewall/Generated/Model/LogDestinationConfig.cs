ef4700fd35fff35b71b0f1edc0b8b6c7afc1d6dc
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2024-07-25 19:48:01 +0000

    You can now log events that are related to TLS inspection, in addition to the existing alert and flow logging.

--- sdk/src/Services/NetworkFirewall/Generated/Model/LogDestinationConfig.cs
+++ sdk/src/Services/NetworkFirewall/Generated/Model/LogDestinationConfig.cs
@@ -32,14 +32,12 @@ namespace Amazon.NetworkFirewall.Model
     /// <summary>
     /// Defines where Network Firewall sends logs for the firewall for one log type. This
     /// is used in <a>LoggingConfiguration</a>. You can send each type of log to an Amazon
-    /// S3 bucket, a CloudWatch log group, or a Kinesis Data Firehose delivery stream.
+    /// S3 bucket, a CloudWatch log group, or a Firehose delivery stream.
     /// 
     ///  
     /// <para>
-    /// Network Firewall generates logs for stateful rule groups. You can save alert and flow
-    /// log types. The stateful rules engine records flow logs for all network traffic that
-    /// it receives. It records alert logs for traffic that matches stateful rules that have
-    /// the rule action set to <c>DROP</c> or <c>ALERT</c>. 
+    /// Network Firewall generates logs for stateful rule groups. You can save alert, flow,
+    /// and TLS log types. 
     /// </para>
     /// </summary>
     public partial class LogDestinationConfig
@@ -57,9 +55,12 @@ namespace Amazon.NetworkFirewall.Model
         ///  <ul> <li> 
         /// <para>
         /// For an Amazon S3 bucket, provide the name of the bucket, with key <c>bucketName</c>,
-        /// and optionally provide a prefix, with key <c>prefix</c>. The following example specifies
-        /// an Amazon S3 bucket named <c>DOC-EXAMPLE-BUCKET</c> and the prefix <c>alerts</c>:
-        /// 
+        /// and optionally provide a prefix, with key <c>prefix</c>. 
+        /// </para>
+        ///  
+        /// <para>
+        /// The following example specifies an Amazon S3 bucket named <c>DOC-EXAMPLE-BUCKET</c>
+        /// and the prefix <c>alerts</c>: 
         /// </para>
         ///  
         /// <para>
@@ -78,9 +79,9 @@ namespace Amazon.NetworkFirewall.Model
         /// </para>
         ///  </li> <li> 
         /// <para>
-        /// For a Kinesis Data Firehose delivery stream, provide the name of the delivery stream,
-        /// with key <c>deliveryStream</c>. The following example specifies a delivery stream
-        /// named <c>alert-delivery-stream</c>: 
+        /// For a Firehose delivery stream, provide the name of the delivery stream, with key
+        /// <c>deliveryStream</c>. The following example specifies a delivery stream named <c>alert-delivery-stream</c>:
+        /// 
         /// </para>
         ///  
         /// <para>
@@ -105,7 +106,7 @@ namespace Amazon.NetworkFirewall.Model
         /// Gets and sets the property LogDestinationType. 
         /// <para>
         /// The type of storage destination to send these logs to. You can send logs to an Amazon
-        /// S3 bucket, a CloudWatch log group, or a Kinesis Data Firehose delivery stream.
+        /// S3 bucket, a CloudWatch log group, or a Firehose delivery stream.
         /// </para>
         /// </summary>
         [AWSProperty(Required=true, Min=2, Max=30)]
@@ -124,10 +125,29 @@ namespace Amazon.NetworkFirewall.Model
         /// <summary>
         /// Gets and sets the property LogType. 
         /// <para>
-        /// The type of log to send. Alert logs report traffic that matches a <a>StatefulRule</a>
-        /// with an action setting that sends an alert log message. Flow logs are standard network
-        /// traffic flow logs. 
+        /// The type of log to record. You can record the following types of logs from your Network
+        /// Firewall stateful engine.
         /// </para>
+        ///  <ul> <li> 
+        /// <para>
+        ///  <c>ALERT</c> - Logs for traffic that matches your stateful rules and that have an
+        /// action that sends an alert. A stateful rule sends alerts for the rule actions DROP,
+        /// ALERT, and REJECT. For more information, see <a>StatefulRule</a>.
+        /// </para>
+        ///  </li> <li> 
+        /// <para>
+        ///  <c>FLOW</c> - Standard network traffic flow logs. The stateful rules engine records
+        /// flow logs for all network traffic that it receives. Each flow log record captures
+        /// the network flow for a specific standard stateless rule group.
+        /// </para>
+        ///  </li> <li> 
+        /// <para>
+        ///  <c>TLS</c> - Logs for events that are related to TLS inspection. For more information,
+        /// see <a href="https://docs.aws.amazon.com/network-firewall/latest/developerguide/tls-inspection-configurations.html">Inspecting
+        /// SSL/TLS traffic with TLS inspection configurations</a> in the <i>Network Firewall
+        /// Developer Guide</i>.
+        /// </para>
+        ///  </li> </ul>
         /// </summary>
         [AWSProperty(Required=true)]
         public LogType LogType
