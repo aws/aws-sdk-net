508f04b841681c7f9fdb29c093adeb2b82f9953a
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2024-08-13 18:17:32 +0000

    Add AttributesToGet parameter support for Glue GetTables

--- sdk/src/Services/Glue/Generated/Model/GetTablesRequest.cs
+++ sdk/src/Services/Glue/Generated/Model/GetTablesRequest.cs
@@ -35,6 +35,7 @@ namespace Amazon.Glue.Model
     /// </summary>
     public partial class GetTablesRequest : AmazonGlueRequest
     {
+        private List<string> _attributesToGet = AWSConfigs.InitializeCollections ? new List<string>() : null;
         private string _catalogId;
         private string _databaseName;
         private string _expression;
@@ -43,6 +44,38 @@ namespace Amazon.Glue.Model
         private string _nextToken;
         private DateTime? _queryAsOfTime;
         private string _transactionId;
+
+        /// <summary>
+        /// Gets and sets the property AttributesToGet. 
+        /// <para>
+        ///  Specifies the table fields returned by the <c>GetTables</c> call. This parameter
+        /// doesn’t accept an empty list. The request must include <c>NAME</c>.
+        /// </para>
+        ///  
+        /// <para>
+        /// The following are the valid combinations of values:
+        /// </para>
+        ///  <ul> <li> 
+        /// <para>
+        ///  <c>NAME</c> - Names of all tables in the database.
+        /// </para>
+        ///  </li> <li> 
+        /// <para>
+        ///  <c>NAME</c>, <c>TABLE_TYPE</c> - Names of all tables and the table types.
+        /// </para>
+        ///  </li> </ul>
+        /// </summary>
+        public List<string> AttributesToGet
+        {
+            get { return this._attributesToGet; }
+            set { this._attributesToGet = value; }
+        }
+
+        // Check to see if AttributesToGet property is set
+        internal bool IsSetAttributesToGet()
+        {
+            return this._attributesToGet != null && (this._attributesToGet.Count > 0 || !AWSConfigs.InitializeCollections); 
+        }
 
         /// <summary>
         /// Gets and sets the property CatalogId. 
--- sdk/src/Services/Glue/Generated/ServiceEnumerations.cs
+++ sdk/src/Services/Glue/Generated/ServiceEnumerations.cs
@@ -5285,6 +5285,56 @@ namespace Amazon.Glue
 
 
     /// <summary>
+    /// Constants used for properties of type TableAttributes.
+    /// </summary>
+    public class TableAttributes : ConstantClass
+    {
+
+        /// <summary>
+        /// Constant NAME for TableAttributes
+        /// </summary>
+        public static readonly TableAttributes NAME = new TableAttributes("NAME");
+        /// <summary>
+        /// Constant TABLE_TYPE for TableAttributes
+        /// </summary>
+        public static readonly TableAttributes TABLE_TYPE = new TableAttributes("TABLE_TYPE");
+
+        /// <summary>
+        /// This constant constructor does not need to be called if the constant
+        /// you are attempting to use is already defined as a static instance of 
+        /// this class.
+        /// This constructor should be used to construct constants that are not
+        /// defined as statics, for instance if attempting to use a feature that is
+        /// newer than the current version of the SDK.
+        /// </summary>
+        public TableAttributes(string value)
+            : base(value)
+        {
+        }
+
+        /// <summary>
+        /// Finds the constant for the unique value.
+        /// </summary>
+        /// <param name="value">The unique value for the constant</param>
+        /// <returns>The constant for the unique value</returns>
+        public static TableAttributes FindValue(string value)
+        {
+            return FindValue<TableAttributes>(value);
+        }
+
+        /// <summary>
+        /// Utility method to convert strings to the constant class.
+        /// </summary>
+        /// <param name="value">The string value to convert to the constant class.</param>
+        /// <returns></returns>
+        public static implicit operator TableAttributes(string value)
+        {
+            return FindValue(value);
+        }
+    }
+
+
+    /// <summary>
     /// Constants used for properties of type TableOptimizerEventType.
     /// </summary>
     public class TableOptimizerEventType : ConstantClass
