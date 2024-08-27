cdabde4fb7a9d0cbd663049d166f7f3a205e692d
Daniel Pinheiro <dspin@amazon.com>
2024-08-27 11:51:46 -0400

    chore: Change preview label to preview2

--- extensions/src/AWSSDK.Extensions.EC2.DecryptPassword/Properties/AssemblyInfo.cs
+++ extensions/src/AWSSDK.Extensions.EC2.DecryptPassword/Properties/AssemblyInfo.cs
@@ -18,6 +18,6 @@ using System.Runtime.InteropServices;
 #if NETFRAMEWORK
 [assembly: AssemblyVersion("4.0")]
 #else
-[assembly: AssemblyVersion("4.0.1.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
 #endif
-[assembly: AssemblyFileVersion("4.0.1.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
