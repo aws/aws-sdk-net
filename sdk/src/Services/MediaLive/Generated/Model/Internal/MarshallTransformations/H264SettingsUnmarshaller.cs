d039cd285124c5fd1f13b7e28c5b01b8bdb90d3c
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2024-09-03 18:15:46 +0000

    Added MinQP as a Rate Control option for H264 and H265 encodes.

--- sdk/src/Services/MediaLive/Generated/Model/Internal/MarshallTransformations/H264SettingsUnmarshaller.cs
+++ sdk/src/Services/MediaLive/Generated/Model/Internal/MarshallTransformations/H264SettingsUnmarshaller.cs
@@ -210,6 +210,12 @@ namespace Amazon.MediaLive.Model.Internal.MarshallTransformations
                     unmarshalledObject.MinIInterval = unmarshaller.Unmarshall(context);
                     continue;
                 }
+                if (context.TestExpression("minQp", targetDepth))
+                {
+                    var unmarshaller = IntUnmarshaller.Instance;
+                    unmarshalledObject.MinQp = unmarshaller.Unmarshall(context);
+                    continue;
+                }
                 if (context.TestExpression("numRefFrames", targetDepth))
                 {
                     var unmarshaller = IntUnmarshaller.Instance;
