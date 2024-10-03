a50a390219fc558905d2239b4c82d881e1ac7b29
Bo Blodgett <boblod@amazon.com>
2024-10-03 11:52:04 -0700

    Updates to support building and running netstandard project core only based UnitTests from service solutions.

--- generator/ServiceClientGeneratorLib/Generators/SourceFiles/CoreAssemblyInfo.cs
+++ generator/ServiceClientGeneratorLib/Generators/SourceFiles/CoreAssemblyInfo.cs
@@ -15,7 +15,7 @@ namespace ServiceClientGenerator.Generators.SourceFiles
     /// Class to produce the template output
     /// </summary>
     
-    #line 1 "C:\Dev\Repos\aws-sdk-net-staging\generator\ServiceClientGeneratorLib\Generators\SourceFiles\CoreAssemblyInfo.tt"
+    #line 1 "C:\Dev\repos\aws-sdk-net-staging\generator\ServiceClientGeneratorLib\Generators\SourceFiles\CoreAssemblyInfo.tt"
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.TextTemplating", "17.0.0.0")]
     public partial class CoreAssemblyInfo : BaseGenerator
     {
@@ -57,34 +57,39 @@ namespace ServiceClientGenerator.Generators.SourceFiles
                     "940000000602000000240000525341310004000001000100db5f59f098d27276c7833875a6263a3c" +
                     "c74ab17ba9a9df0b52aedbe7252745db7274d5271fd79c1f08f668ecfa8eaab5626fa76adc811d3c" +
                     "8fc55859b0d09d3bc0a84eecd0ba891f2b8a2fc55141cdcc37c2053d53491e650a479967c3622762" +
-                    "977900eddbf1252ed08a2413f00a28f3a0752a81203f03ccb7f684db373518b4\")]\r\n// Allows m" +
-                    "oq to access internals for testing\r\n[assembly: InternalsVisibleTo(\"DynamicProxyG" +
-                    "enAssembly2, PublicKey=002400000480000094000000060200000024000052534131000400000" +
-                    "1000100c547cac37abd99c8db225ef2f6c8a3602f3b3606cc9891605d02baa56104f4cfc0734aa39" +
-                    "b93bf7852f7d9266654753cc297e7d2edfe0bac1cdcf9f717241550e0a7b191195b7667bb4f64bcb" +
-                    "8e2121380fd1d9d46ad2d92d2d15605093924cceaf74c4861eff62abf69b9291ed0a340e113be11e" +
-                    "6a7d3113e92484cf7045cc7\")]\r\n[assembly: AssemblyConfiguration(\"\")]\r\n[assembly: As" +
-                    "semblyProduct(\"Amazon Web Services SDK for .NET\")]\r\n[assembly: AssemblyCompany(\"" +
-                    "Amazon.com, Inc\")]\r\n[assembly: AssemblyCopyright(\"Copyright Amazon.com, Inc. or " +
-                    "its affiliates. All Rights Reserved.\")]\r\n[assembly: AssemblyTrademark(\"\")]\r\n[ass" +
-                    "embly: AssemblyCulture(\"\")]\r\n\r\n// Setting ComVisible to false makes the types in" +
-                    " this assembly not visible \r\n// to COM components.  If you need to access a type" +
-                    " in this assembly from \r\n// COM, set the ComVisible attribute to true on that ty" +
-                    "pe.\r\n[assembly: ComVisible(false)]\r\n\r\n// Version information for an assembly con" +
-                    "sists of the following four values:\r\n//\r\n//      Major Version\r\n//      Minor Ve" +
-                    "rsion \r\n//      Build Number\r\n//      Revision\r\n//\r\n// You can specify all the v" +
-                    "alues or you can default the Build and Revision Numbers \r\n// by using the \'*\' as" +
-                    " shown below:\r\n// [assembly: AssemblyVersion(\"1.0.*\")]\r\n[assembly: AssemblyVersi" +
-                    "on(\"");
+                    "977900eddbf1252ed08a2413f00a28f3a0752a81203f03ccb7f684db373518b4\")]\r\n[assembly: " +
+                    "InternalsVisibleTo(\"UnitTests.NetStandard.CoreOnly, PublicKey=002400000480000094" +
+                    "0000000602000000240000525341310004000001000100db5f59f098d27276c7833875a6263a3cc7" +
+                    "4ab17ba9a9df0b52aedbe7252745db7274d5271fd79c1f08f668ecfa8eaab5626fa76adc811d3c8f" +
+                    "c55859b0d09d3bc0a84eecd0ba891f2b8a2fc55141cdcc37c2053d53491e650a479967c362276297" +
+                    "7900eddbf1252ed08a2413f00a28f3a0752a81203f03ccb7f684db373518b4\")]\r\n// Allows moq" +
+                    " to access internals for testing\r\n[assembly: InternalsVisibleTo(\"DynamicProxyGen" +
+                    "Assembly2, PublicKey=00240000048000009400000006020000002400005253413100040000010" +
+                    "00100c547cac37abd99c8db225ef2f6c8a3602f3b3606cc9891605d02baa56104f4cfc0734aa39b9" +
+                    "3bf7852f7d9266654753cc297e7d2edfe0bac1cdcf9f717241550e0a7b191195b7667bb4f64bcb8e" +
+                    "2121380fd1d9d46ad2d92d2d15605093924cceaf74c4861eff62abf69b9291ed0a340e113be11e6a" +
+                    "7d3113e92484cf7045cc7\")]\r\n[assembly: AssemblyConfiguration(\"\")]\r\n[assembly: Asse" +
+                    "mblyProduct(\"Amazon Web Services SDK for .NET\")]\r\n[assembly: AssemblyCompany(\"Am" +
+                    "azon.com, Inc\")]\r\n[assembly: AssemblyCopyright(\"Copyright Amazon.com, Inc. or it" +
+                    "s affiliates. All Rights Reserved.\")]\r\n[assembly: AssemblyTrademark(\"\")]\r\n[assem" +
+                    "bly: AssemblyCulture(\"\")]\r\n\r\n// Setting ComVisible to false makes the types in t" +
+                    "his assembly not visible \r\n// to COM components.  If you need to access a type i" +
+                    "n this assembly from \r\n// COM, set the ComVisible attribute to true on that type" +
+                    ".\r\n[assembly: ComVisible(false)]\r\n\r\n// Version information for an assembly consi" +
+                    "sts of the following four values:\r\n//\r\n//      Major Version\r\n//      Minor Vers" +
+                    "ion \r\n//      Build Number\r\n//      Revision\r\n//\r\n// You can specify all the val" +
+                    "ues or you can default the Build and Revision Numbers \r\n// by using the \'*\' as s" +
+                    "hown below:\r\n// [assembly: AssemblyVersion(\"1.0.*\")]\r\n[assembly: AssemblyVersion" +
+                    "(\"");
             
-            #line 55 "C:\Dev\Repos\aws-sdk-net-staging\generator\ServiceClientGeneratorLib\Generators\SourceFiles\CoreAssemblyInfo.tt"
+            #line 56 "C:\Dev\repos\aws-sdk-net-staging\generator\ServiceClientGeneratorLib\Generators\SourceFiles\CoreAssemblyInfo.tt"
             this.Write(this.ToStringHelper.ToStringWithCulture(this.Session["Version"]));
             
             #line default
             #line hidden
             this.Write("\")]\r\n[assembly: AssemblyFileVersion(\"");
             
-            #line 56 "C:\Dev\Repos\aws-sdk-net-staging\generator\ServiceClientGeneratorLib\Generators\SourceFiles\CoreAssemblyInfo.tt"
+            #line 57 "C:\Dev\repos\aws-sdk-net-staging\generator\ServiceClientGeneratorLib\Generators\SourceFiles\CoreAssemblyInfo.tt"
             this.Write(this.ToStringHelper.ToStringWithCulture(this.Session["FileVersion"]));
             
             #line default
