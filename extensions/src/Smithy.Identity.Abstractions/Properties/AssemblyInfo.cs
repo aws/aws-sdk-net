376df5bd4dc0ce9ad667ba9ce41c429adee383df
Daniel Pinheiro <dspin@amazon.com>
2024-10-08 16:26:06 -0400

    feat: Add abstraction project for identity and auth

---
+++ extensions/src/Smithy.Identity.Abstractions/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("Smithy.Identity.Abstractions")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("Smithy identity and authentication abstractions for the AWS SDK for .NET")]
+[assembly: AssemblyCompany("Amazon.com, Inc")]
+[assembly: AssemblyCopyright("Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.")]
+[assembly: AssemblyTrademark("")]
+
+#if NETFRAMEWORK
+[assembly: AssemblyVersion("0.1")]
+#else
+[assembly: AssemblyVersion("0.1")]
+#endif
+[assembly: AssemblyFileVersion("0.1")]
