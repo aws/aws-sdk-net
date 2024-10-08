65efa5ad4187ac6f7c3916a084830c4b6bb5bd20
Daniel Pinheiro <dspin@amazon.com>
2024-10-08 19:29:54 -0400

    chore: Add template file for modeled scheme resolvers

---
+++ generator/ServiceClientGeneratorLib/AuthenticationScheme.cs
@@ -0,0 +1,13 @@
+﻿namespace ServiceClientGenerator
+{
+    /// <summary>
+    /// Constants for the supported authentication schemes.
+    /// </summary>
+    public class AuthenticationScheme
+    {
+        public const string SigV4 = "aws.auth#sigv4";
+        public const string SigV4A = "aws.auth#sigv4a";
+        public const string Bearer = "smithy.api#httpBearerAuth"; 
+        public const string NoAuth = "smithy.api#noAuth";
+    }
+}
---
+++ generator/ServiceClientGeneratorLib/Generators/AuthResolvers/ModeledResolver.partial.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ServiceClientGenerator.Generators.AuthResolvers
+{
+    public partial class ModeledResolver
+    {
+        /// <summary>
+        /// Returns whether this service supports SigV4 (and therefore should include region in its auth scheme parameters).
+        /// </summary>
+        private bool IsSigV4Supported() => 
+            Config.ServiceModel.AuthSchemes != null && Config.ServiceModel.AuthSchemes.Contains(AuthenticationScheme.SigV4);
+
+        /// <summary>
+        /// Returns the list of auth schemes supported by the service.
+        /// </summary>
+        private IEnumerable<string> GetServiceAuthSchemes()
+        {
+            if (Config.ServiceModel.AuthSchemes == null || !Config.ServiceModel.AuthSchemes.Any())
+            {
+                return new List<string> { AuthenticationScheme.NoAuth };
+            }
+
+            return Config.ServiceModel.AuthSchemes.Select(s => s.ToString());
+        }
+
+        /// <summary>
+        /// Returns the operations which have auth schemes defined (they'll override the service-level auth schemes if
+        /// not null).
+        /// </summary>
+        private IEnumerable<Operation> GetOperationsWithAuthSchemes() => 
+            Config.ServiceModel.Operations.Where(o => o.AuthSchemes != null);
+    }
+}
