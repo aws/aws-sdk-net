b77a0f83dd80e84381573d74d8df6676ac61ca48
Malhar Khimsaria <96malhar@gmail.com>
2024-10-16 11:54:09 -0700

    Add Observability to DynamoDBContext operations is DynamoDB HLL (#3514)

--- sdk/src/Services/DynamoDBv2/Custom/DynamoDBTelemetry.cs
+++ sdk/src/Services/DynamoDBv2/Custom/DynamoDBTelemetry.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Amazon.DynamoDBv2.DataModel;
 using Amazon.Runtime.Telemetry;
 using Amazon.Runtime.Telemetry.Tracing;
 using Attributes = Amazon.Runtime.Telemetry.Attributes;
@@ -32,6 +33,27 @@ namespace Amazon.DynamoDBv2
 
             var tracer = tracerProvider.GetTracer(DynamoDBTracerScope);
             return tracer.CreateSpan(operationName, initialAttributes, spanKind, parentContext);
+        }
+
+        internal static TraceSpan CreateSpan(DynamoDBContext context, 
+            string methodName, 
+            Attributes initialAttributes = null,
+            SpanContext parentContext = null)
+        {
+            var operationName = ExtractOperationName(nameof(DynamoDBContext), methodName);
+
+            if (initialAttributes == null)
+                initialAttributes = new Attributes();
+
+            initialAttributes.Set(TelemetryConstants.MethodAttributeKey, operationName);
+            initialAttributes.Set(TelemetryConstants.SystemAttributeKey, TelemetryConstants.SystemAttributeValue);
+            initialAttributes.Set(TelemetryConstants.ServiceAttributeKey, DynamoDBTracerScope);
+
+            var tracerProvider = context?.Client?.Config?.TelemetryProvider?.TracerProvider
+                ?? AWSConfigs.TelemetryProvider.TracerProvider;
+
+            var tracer = tracerProvider.GetTracer(DynamoDBTracerScope);
+            return tracer.CreateSpan(operationName, initialAttributes, SpanKind.CLIENT, parentContext);
         }
     }
 }
