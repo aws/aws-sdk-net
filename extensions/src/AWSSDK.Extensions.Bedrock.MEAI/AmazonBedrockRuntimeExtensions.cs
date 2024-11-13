ac5b2511afd1a7c9d9308ac3af277df2c0c86d88
Stephen Toub <stoub@microsoft.com>
2024-11-13 12:19:38 -0500

    Move chat client implementation to new extension project / method

---
+++ extensions/src/AWSSDK.Extensions.Bedrock.MEAI/AmazonBedrockRuntimeExtensions.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ * 
+ *  http://aws.amazon.com/apache2.0
+ * 
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+using Microsoft.Extensions.AI;
+using System;
+
+namespace Amazon.BedrockRuntime;
+
+/// <summary>Provides extensions for working with <see cref="IAmazonBedrockRuntime"/> instances.</summary>
+public static class AmazonBedrockRuntimeExtensions
+{
+    /// <summary>Gets an <see cref="IChatClient"/> for the specified <see cref="IAmazonBedrockRuntime"/> instance.</summary>
+    /// <param name="runtime">The runtime instance to be represented as an <see cref="IChatClient"/>.</param>
+    /// <param name="modelId">
+    /// The default model ID to use when no model is specified in a request. If not specified,
+    /// a model must be provided in the <see cref="ChatOptions.ModelId"/> passed to <see cref="IChatClient.CompleteAsync"/>
+    /// or <see cref="IChatClient.CompleteStreamingAsync"/>.
+    /// </param>
+    /// <returns>A <see cref="IChatClient"/> instance representing the <see cref="IAmazonBedrockRuntime"/> instance.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="runtime"/> is <see langword="null"/>.</exception>
+    public static IChatClient AsChatClient(this IAmazonBedrockRuntime runtime, string? modelId = null) =>
+        runtime is not null ? new BedrockChatClient(runtime, modelId) :
+        throw new ArgumentNullException(nameof(runtime));
+}
---
+++ extensions/test/BedrockMEAITests/BedrockChatClientTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.AI;
+using System;
+using Xunit;
+
+namespace Amazon.BedrockRuntime;
+
+public class BedrockChatClientTests
+{
+    [Fact]
+    [Trait("UnitTest", "BedrockRuntime")]
+    public void AsChatClient_InvalidArguments_Throws()
+    {
+        Assert.Throws<ArgumentNullException>("runtime", () => AmazonBedrockRuntimeExtensions.AsChatClient(null));
+    }
+
+    [Theory]
+    [Trait("UnitTest", "BedrockRuntime")]
+    [InlineData(null)]
+    [InlineData("claude")]
+    public void AsChatClient_ReturnsInstance(string modelId)
+    {
+        IAmazonBedrockRuntime runtime = new AmazonBedrockRuntimeClient("awsAccessKeyId", "awsSecretAccessKey", RegionEndpoint.USEast1);
+        IChatClient client = runtime.AsChatClient(modelId);
+
+        Assert.NotNull(client);
+        Assert.Equal("Bedrock Runtime", client.Metadata.ProviderName);
+        Assert.Equal(modelId, client.Metadata.ModelId);
+    }
+
+    [Fact]
+    [Trait("UnitTest", "BedrockRuntime")]
+    public void AsChatClient_GetService()
+    {
+        IAmazonBedrockRuntime runtime = new AmazonBedrockRuntimeClient("awsAccessKeyId", "awsSecretAccessKey", RegionEndpoint.USEast1);
+        IChatClient client = runtime.AsChatClient();
+
+        Assert.Same(runtime, client.GetService<AmazonBedrockRuntimeClient>());
+        Assert.Same(runtime, client.GetService<IAmazonBedrockRuntime>());
+        Assert.Same(client, client.GetService<IChatClient>());
+
+        Assert.Null(client.GetService<string>());
+
+        Assert.Null(client.GetService<AmazonBedrockRuntimeClient>("key"));
+        Assert.Null(client.GetService<IAmazonBedrockRuntime>("key"));
+        Assert.Null(client.GetService<IChatClient>("key"));
+    }
+}
