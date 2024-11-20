3145b275afff2c4f3a37006ff337297e66fd6275
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2024-11-20 23:23:51 +0000

    This release adds the ability to reconfigure concurrent job settings for existing queues and create queues with custom concurrent job settings.

--- sdk/src/Services/MediaConvert/Generated/Model/Internal/MarshallTransformations/UpdateQueueRequestMarshaller.cs
+++ sdk/src/Services/MediaConvert/Generated/Model/Internal/MarshallTransformations/UpdateQueueRequestMarshaller.cs
@@ -70,6 +70,12 @@ namespace Amazon.MediaConvert.Model.Internal.MarshallTransformations
                 writer.Validate = false;
                 writer.WriteObjectStart();
                 var context = new JsonMarshallerContext(request, writer);
+                if(publicRequest.IsSetConcurrentJobs())
+                {
+                    context.Writer.WritePropertyName("concurrentJobs");
+                    context.Writer.Write(publicRequest.ConcurrentJobs);
+                }
+
                 if(publicRequest.IsSetDescription())
                 {
                     context.Writer.WritePropertyName("description");
