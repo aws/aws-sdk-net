280da5f17327b30225ae434e27592e246810b809
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2024-11-22 19:30:07 +0000

    Advanced Scaling in Amazon EMR Managed Scaling

--- sdk/src/Services/ElasticMapReduce/Generated/ServiceEnumerations.cs
+++ sdk/src/Services/ElasticMapReduce/Generated/ServiceEnumerations.cs
@@ -1973,6 +1973,56 @@ namespace Amazon.ElasticMapReduce
 
 
     /// <summary>
+    /// Constants used for properties of type ScalingStrategy.
+    /// </summary>
+    public class ScalingStrategy : ConstantClass
+    {
+
+        /// <summary>
+        /// Constant ADVANCED for ScalingStrategy
+        /// </summary>
+        public static readonly ScalingStrategy ADVANCED = new ScalingStrategy("ADVANCED");
+        /// <summary>
+        /// Constant DEFAULT for ScalingStrategy
+        /// </summary>
+        public static readonly ScalingStrategy DEFAULT = new ScalingStrategy("DEFAULT");
+
+        /// <summary>
+        /// This constant constructor does not need to be called if the constant
+        /// you are attempting to use is already defined as a static instance of 
+        /// this class.
+        /// This constructor should be used to construct constants that are not
+        /// defined as statics, for instance if attempting to use a feature that is
+        /// newer than the current version of the SDK.
+        /// </summary>
+        public ScalingStrategy(string value)
+            : base(value)
+        {
+        }
+
+        /// <summary>
+        /// Finds the constant for the unique value.
+        /// </summary>
+        /// <param name="value">The unique value for the constant</param>
+        /// <returns>The constant for the unique value</returns>
+        public static ScalingStrategy FindValue(string value)
+        {
+            return FindValue<ScalingStrategy>(value);
+        }
+
+        /// <summary>
+        /// Utility method to convert strings to the constant class.
+        /// </summary>
+        /// <param name="value">The string value to convert to the constant class.</param>
+        /// <returns></returns>
+        public static implicit operator ScalingStrategy(string value)
+        {
+            return FindValue(value);
+        }
+    }
+
+
+    /// <summary>
     /// Constants used for properties of type SpotProvisioningAllocationStrategy.
     /// </summary>
     public class SpotProvisioningAllocationStrategy : ConstantClass
