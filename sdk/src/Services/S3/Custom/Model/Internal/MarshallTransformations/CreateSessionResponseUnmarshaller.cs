a8eb989389f5186d5532f0da76d900f04726e7be
Peter Song <petesong@amazon.com>
2025-01-14 12:05:43 -0800

    Fix Merge Conflicts (#3605)

--- sdk/src/Services/S3/Custom/Model/Internal/MarshallTransformations/CreateSessionResponseUnmarshaller.cs
+++ sdk/src/Services/S3/Custom/Model/Internal/MarshallTransformations/CreateSessionResponseUnmarshaller.cs
@@ -97,7 +97,7 @@ namespace Amazon.S3.Model.Internal.MarshallTransformations
         /// <returns></returns>
         public override AmazonServiceException UnmarshallException(XmlUnmarshallerContext context, Exception innerException, HttpStatusCode statusCode)
         {
-            ErrorResponse errorResponse = XmlErrorResponseUnmarshaller.GetInstance().Unmarshall(context);
+            ErrorResponse errorResponse = S3ErrorResponseUnmarshaller.Instance.Unmarshall(context);
             errorResponse.InnerException = innerException;
             errorResponse.StatusCode = statusCode;
 
