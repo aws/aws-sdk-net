b6da1e2cd9e2613e1122e9a7178cc6015a090226
Peter Song <petesong@amazon.com>
2025-01-30 13:59:09 -0800

    fix merge conflict for dry run

--- sdk/src/Core/Amazon.Runtime/Internal/Transform/JsonUnmarshallerContext.cs
+++ sdk/src/Core/Amazon.Runtime/Internal/Transform/JsonUnmarshallerContext.cs
@@ -270,12 +270,10 @@ namespace Amazon.Runtime.Internal.Transform
                     text = reader.Reader.GetBoolean().ToString();
                     break;
                 case JsonTokenType.PropertyName:
-                    text = reader.Reader.GetString();
-                    break;
                 case JsonTokenType.String:
                     text = reader.Reader.GetString();
                     break;
-                   // For numbers we grab the raw value b/ c we don't want to lose precision.
+                 // For numbers we grab the raw value b/ c we don't want to lose precision.
                 case JsonTokenType.Number:
 #if NETSTANDARD2_0 || NETFRAMEWORK
                     // overload which accepts a ReadOnlySpan<byte> is only available in netstandard2.1 and netcore2.1+
@@ -290,7 +288,7 @@ namespace Amazon.Runtime.Internal.Transform
             return text;
         }
 
-#endregion
+        #endregion
 
         #region Public properties
 
