1548afdd38a76810e8d2393ef2376774e57d230a
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2025-02-13 19:15:06 +0000

    This release introduces the getFindingsStatistics API, enabling users to retrieve aggregated finding statistics for IAM Access Analyzer's external access and unused access analysis features. Updated service API and documentation.

--- sdk/src/Services/AccessAnalyzer/Generated/_bcl35/IAmazonAccessAnalyzer.cs
+++ sdk/src/Services/AccessAnalyzer/Generated/_bcl35/IAmazonAccessAnalyzer.cs
@@ -1064,6 +1064,62 @@ namespace Amazon.AccessAnalyzer
 
         #endregion
         
+        #region  GetFindingsStatistics
+
+
+        /// <summary>
+        /// Retrieves a list of aggregated finding statistics for an external access or unused
+        /// access analyzer.
+        /// </summary>
+        /// <param name="request">Container for the necessary parameters to execute the GetFindingsStatistics service method.</param>
+        /// 
+        /// <returns>The response from the GetFindingsStatistics service method, as returned by AccessAnalyzer.</returns>
+        /// <exception cref="Amazon.AccessAnalyzer.Model.AccessDeniedException">
+        /// You do not have sufficient access to perform this action.
+        /// </exception>
+        /// <exception cref="Amazon.AccessAnalyzer.Model.InternalServerException">
+        /// Internal server error.
+        /// </exception>
+        /// <exception cref="Amazon.AccessAnalyzer.Model.ResourceNotFoundException">
+        /// The specified resource could not be found.
+        /// </exception>
+        /// <exception cref="Amazon.AccessAnalyzer.Model.ThrottlingException">
+        /// Throttling limit exceeded error.
+        /// </exception>
+        /// <exception cref="Amazon.AccessAnalyzer.Model.ValidationException">
+        /// Validation exception error.
+        /// </exception>
+        /// <seealso href="http://docs.aws.amazon.com/goto/WebAPI/accessanalyzer-2019-11-01/GetFindingsStatistics">REST API Reference for GetFindingsStatistics Operation</seealso>
+        GetFindingsStatisticsResponse GetFindingsStatistics(GetFindingsStatisticsRequest request);
+
+        /// <summary>
+        /// Initiates the asynchronous execution of the GetFindingsStatistics operation.
+        /// </summary>
+        /// 
+        /// <param name="request">Container for the necessary parameters to execute the GetFindingsStatistics operation on AmazonAccessAnalyzerClient.</param>
+        /// <param name="callback">An AsyncCallback delegate that is invoked when the operation completes.</param>
+        /// <param name="state">A user-defined state object that is passed to the callback procedure. Retrieve this object from within the callback
+        ///          procedure using the AsyncState property.</param>
+        /// 
+        /// <returns>An IAsyncResult that can be used to poll or wait for results, or both; this value is also needed when invoking EndGetFindingsStatistics
+        ///         operation.</returns>
+        /// <seealso href="http://docs.aws.amazon.com/goto/WebAPI/accessanalyzer-2019-11-01/GetFindingsStatistics">REST API Reference for GetFindingsStatistics Operation</seealso>
+        IAsyncResult BeginGetFindingsStatistics(GetFindingsStatisticsRequest request, AsyncCallback callback, object state);
+
+
+
+        /// <summary>
+        /// Finishes the asynchronous execution of the  GetFindingsStatistics operation.
+        /// </summary>
+        /// 
+        /// <param name="asyncResult">The IAsyncResult returned by the call to BeginGetFindingsStatistics.</param>
+        /// 
+        /// <returns>Returns a  GetFindingsStatisticsResult from AccessAnalyzer.</returns>
+        /// <seealso href="http://docs.aws.amazon.com/goto/WebAPI/accessanalyzer-2019-11-01/GetFindingsStatistics">REST API Reference for GetFindingsStatistics Operation</seealso>
+        GetFindingsStatisticsResponse EndGetFindingsStatistics(IAsyncResult asyncResult);
+
+        #endregion
+        
         #region  GetFindingV2
 
 
--- sdk/src/Services/AccessAnalyzer/Generated/_netstandard/AmazonAccessAnalyzerClient.cs
+++ sdk/src/Services/AccessAnalyzer/Generated/_netstandard/AmazonAccessAnalyzerClient.cs
@@ -1185,6 +1185,56 @@ namespace Amazon.AccessAnalyzer
 
         #endregion
         
+        #region  GetFindingsStatistics
+
+        internal virtual GetFindingsStatisticsResponse GetFindingsStatistics(GetFindingsStatisticsRequest request)
+        {
+            var options = new InvokeOptions();
+            options.RequestMarshaller = GetFindingsStatisticsRequestMarshaller.Instance;
+            options.ResponseUnmarshaller = GetFindingsStatisticsResponseUnmarshaller.Instance;
+
+            return Invoke<GetFindingsStatisticsResponse>(request, options);
+        }
+
+
+
+        /// <summary>
+        /// Retrieves a list of aggregated finding statistics for an external access or unused
+        /// access analyzer.
+        /// </summary>
+        /// <param name="request">Container for the necessary parameters to execute the GetFindingsStatistics service method.</param>
+        /// <param name="cancellationToken">
+        ///     A cancellation token that can be used by other objects or threads to receive notice of cancellation.
+        /// </param>
+        /// 
+        /// <returns>The response from the GetFindingsStatistics service method, as returned by AccessAnalyzer.</returns>
+        /// <exception cref="Amazon.AccessAnalyzer.Model.AccessDeniedException">
+        /// You do not have sufficient access to perform this action.
+        /// </exception>
+        /// <exception cref="Amazon.AccessAnalyzer.Model.InternalServerException">
+        /// Internal server error.
+        /// </exception>
+        /// <exception cref="Amazon.AccessAnalyzer.Model.ResourceNotFoundException">
+        /// The specified resource could not be found.
+        /// </exception>
+        /// <exception cref="Amazon.AccessAnalyzer.Model.ThrottlingException">
+        /// Throttling limit exceeded error.
+        /// </exception>
+        /// <exception cref="Amazon.AccessAnalyzer.Model.ValidationException">
+        /// Validation exception error.
+        /// </exception>
+        /// <seealso href="http://docs.aws.amazon.com/goto/WebAPI/accessanalyzer-2019-11-01/GetFindingsStatistics">REST API Reference for GetFindingsStatistics Operation</seealso>
+        public virtual Task<GetFindingsStatisticsResponse> GetFindingsStatisticsAsync(GetFindingsStatisticsRequest request, System.Threading.CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var options = new InvokeOptions();
+            options.RequestMarshaller = GetFindingsStatisticsRequestMarshaller.Instance;
+            options.ResponseUnmarshaller = GetFindingsStatisticsResponseUnmarshaller.Instance;
+
+            return InvokeAsync<GetFindingsStatisticsResponse>(request, options, cancellationToken);
+        }
+
+        #endregion
+        
         #region  GetFindingV2
 
         internal virtual GetFindingV2Response GetFindingV2(GetFindingV2Request request)
