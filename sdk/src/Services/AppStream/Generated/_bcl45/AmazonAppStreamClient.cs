e9cc16f1ee1b47b197e4f589d82f437d1b72b413
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2025-02-21 19:23:05 +0000

    Added support for Certificate-Based Authentication on AppStream 2.0 multi-session fleets.

--- sdk/src/Services/AppStream/Generated/_bcl45/AmazonAppStreamClient.cs
+++ sdk/src/Services/AppStream/Generated/_bcl45/AmazonAppStreamClient.cs
@@ -5185,6 +5185,9 @@ namespace Amazon.AppStream
         /// <exception cref="Amazon.AppStream.Model.ConcurrentModificationException">
         /// An API error occurred. Wait a few minutes and try again.
         /// </exception>
+        /// <exception cref="Amazon.AppStream.Model.IncompatibleImageException">
+        /// The image can't be updated because it's not compatible for updates.
+        /// </exception>
         /// <exception cref="Amazon.AppStream.Model.InvalidRoleException">
         /// The specified role is invalid.
         /// </exception>
@@ -5221,6 +5224,9 @@ namespace Amazon.AppStream
         /// <returns>The response from the UpdateDirectoryConfig service method, as returned by AppStream.</returns>
         /// <exception cref="Amazon.AppStream.Model.ConcurrentModificationException">
         /// An API error occurred. Wait a few minutes and try again.
+        /// </exception>
+        /// <exception cref="Amazon.AppStream.Model.IncompatibleImageException">
+        /// The image can't be updated because it's not compatible for updates.
         /// </exception>
         /// <exception cref="Amazon.AppStream.Model.InvalidRoleException">
         /// The specified role is invalid.
