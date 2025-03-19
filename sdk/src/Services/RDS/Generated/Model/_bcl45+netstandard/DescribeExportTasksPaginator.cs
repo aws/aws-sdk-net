0e694ffc3aa1116d82703139417d7f7e90893395
Peter Song <petesong@amazon.com>
2025-03-19 05:55:27 -0700

    Update generator to use latest paginator model file

---
+++ sdk/src/Services/RDS/Generated/Model/_bcl45+netstandard/DescribeExportTasksPaginator.cs
@@ -0,0 +1,98 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ * 
+ *  http://aws.amazon.com/apache2.0
+ * 
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/*
+ * Do not modify this file. This file is generated from the rds-2014-10-31.normal.json service model.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+using System.Threading;
+using System.Threading.Tasks;
+using Amazon.Runtime;
+
+#pragma warning disable CS0612,CS0618
+namespace Amazon.RDS.Model
+{
+    /// <summary>
+    /// Base class for DescribeExportTasks paginators.
+    /// </summary>
+    internal sealed partial class DescribeExportTasksPaginator : IPaginator<DescribeExportTasksResponse>, IDescribeExportTasksPaginator
+    {
+        private readonly IAmazonRDS _client;
+        private readonly DescribeExportTasksRequest _request;
+        private int _isPaginatorInUse = 0;
+        
+        /// <summary>
+        /// Enumerable containing all full responses for the operation
+        /// </summary>
+        public IPaginatedEnumerable<DescribeExportTasksResponse> Responses => new PaginatedResponse<DescribeExportTasksResponse>(this);
+
+        /// <summary>
+        /// Enumerable containing all of the ExportTasks
+        /// </summary>
+        public IPaginatedEnumerable<ExportTask> ExportTasks => 
+            new PaginatedResultKeyResponse<DescribeExportTasksResponse, ExportTask>(this, (i) => i.ExportTasks ?? new List<ExportTask>());
+
+        internal DescribeExportTasksPaginator(IAmazonRDS client, DescribeExportTasksRequest request)
+        {
+            this._client = client;
+            this._request = request;
+        }
+#if BCL
+        IEnumerable<DescribeExportTasksResponse> IPaginator<DescribeExportTasksResponse>.Paginate()
+        {
+            if (Interlocked.Exchange(ref _isPaginatorInUse, 1) != 0)
+            {
+                throw new System.InvalidOperationException("Paginator has already been consumed and cannot be reused. Please create a new instance.");
+            }
+            PaginatorUtils.SetUserAgentAdditionOnRequest(_request);
+            var marker = _request.Marker;
+            DescribeExportTasksResponse response;
+            do
+            {
+                _request.Marker = marker;
+                response = _client.DescribeExportTasks(_request);
+                marker = response.Marker;
+                yield return response;
+            }
+            while (!string.IsNullOrEmpty(marker));
+        }
+#endif
+#if AWS_ASYNC_ENUMERABLES_API
+        async IAsyncEnumerable<DescribeExportTasksResponse> IPaginator<DescribeExportTasksResponse>.PaginateAsync([System.Runtime.CompilerServices.EnumeratorCancellation] CancellationToken cancellationToken)
+        {
+            if (Interlocked.Exchange(ref _isPaginatorInUse, 1) != 0)
+            {
+                throw new System.InvalidOperationException("Paginator has already been consumed and cannot be reused. Please create a new instance.");
+            }
+            PaginatorUtils.SetUserAgentAdditionOnRequest(_request);
+            var marker = _request.Marker;
+            DescribeExportTasksResponse response;
+            do
+            {
+                _request.Marker = marker;
+                response = await _client.DescribeExportTasksAsync(_request, cancellationToken).ConfigureAwait(false);
+                marker = response.Marker;
+                cancellationToken.ThrowIfCancellationRequested();
+                yield return response;
+            }
+            while (!string.IsNullOrEmpty(marker));
+        }
+#endif
+    }
+}
---
+++ sdk/src/Services/RDS/Generated/Model/_bcl45+netstandard/DescribePendingMaintenanceActionsPaginator.cs
@@ -0,0 +1,98 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ * 
+ *  http://aws.amazon.com/apache2.0
+ * 
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/*
+ * Do not modify this file. This file is generated from the rds-2014-10-31.normal.json service model.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+using System.Threading;
+using System.Threading.Tasks;
+using Amazon.Runtime;
+
+#pragma warning disable CS0612,CS0618
+namespace Amazon.RDS.Model
+{
+    /// <summary>
+    /// Base class for DescribePendingMaintenanceActions paginators.
+    /// </summary>
+    internal sealed partial class DescribePendingMaintenanceActionsPaginator : IPaginator<DescribePendingMaintenanceActionsResponse>, IDescribePendingMaintenanceActionsPaginator
+    {
+        private readonly IAmazonRDS _client;
+        private readonly DescribePendingMaintenanceActionsRequest _request;
+        private int _isPaginatorInUse = 0;
+        
+        /// <summary>
+        /// Enumerable containing all full responses for the operation
+        /// </summary>
+        public IPaginatedEnumerable<DescribePendingMaintenanceActionsResponse> Responses => new PaginatedResponse<DescribePendingMaintenanceActionsResponse>(this);
+
+        /// <summary>
+        /// Enumerable containing all of the PendingMaintenanceActions
+        /// </summary>
+        public IPaginatedEnumerable<ResourcePendingMaintenanceActions> PendingMaintenanceActions => 
+            new PaginatedResultKeyResponse<DescribePendingMaintenanceActionsResponse, ResourcePendingMaintenanceActions>(this, (i) => i.PendingMaintenanceActions ?? new List<ResourcePendingMaintenanceActions>());
+
+        internal DescribePendingMaintenanceActionsPaginator(IAmazonRDS client, DescribePendingMaintenanceActionsRequest request)
+        {
+            this._client = client;
+            this._request = request;
+        }
+#if BCL
+        IEnumerable<DescribePendingMaintenanceActionsResponse> IPaginator<DescribePendingMaintenanceActionsResponse>.Paginate()
+        {
+            if (Interlocked.Exchange(ref _isPaginatorInUse, 1) != 0)
+            {
+                throw new System.InvalidOperationException("Paginator has already been consumed and cannot be reused. Please create a new instance.");
+            }
+            PaginatorUtils.SetUserAgentAdditionOnRequest(_request);
+            var marker = _request.Marker;
+            DescribePendingMaintenanceActionsResponse response;
+            do
+            {
+                _request.Marker = marker;
+                response = _client.DescribePendingMaintenanceActions(_request);
+                marker = response.Marker;
+                yield return response;
+            }
+            while (!string.IsNullOrEmpty(marker));
+        }
+#endif
+#if AWS_ASYNC_ENUMERABLES_API
+        async IAsyncEnumerable<DescribePendingMaintenanceActionsResponse> IPaginator<DescribePendingMaintenanceActionsResponse>.PaginateAsync([System.Runtime.CompilerServices.EnumeratorCancellation] CancellationToken cancellationToken)
+        {
+            if (Interlocked.Exchange(ref _isPaginatorInUse, 1) != 0)
+            {
+                throw new System.InvalidOperationException("Paginator has already been consumed and cannot be reused. Please create a new instance.");
+            }
+            PaginatorUtils.SetUserAgentAdditionOnRequest(_request);
+            var marker = _request.Marker;
+            DescribePendingMaintenanceActionsResponse response;
+            do
+            {
+                _request.Marker = marker;
+                response = await _client.DescribePendingMaintenanceActionsAsync(_request, cancellationToken).ConfigureAwait(false);
+                marker = response.Marker;
+                cancellationToken.ThrowIfCancellationRequested();
+                yield return response;
+            }
+            while (!string.IsNullOrEmpty(marker));
+        }
+#endif
+    }
+}
