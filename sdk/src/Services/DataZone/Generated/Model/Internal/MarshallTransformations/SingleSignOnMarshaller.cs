3992c46352f7df3cef8af46372a742a617963d60
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2025-03-21 18:24:51 +0000

    Add support for overriding selection of default AWS IAM Identity Center instance as part of Amazon DataZone domain APIs.

--- sdk/src/Services/DataZone/Generated/Model/Internal/MarshallTransformations/SingleSignOnMarshaller.cs
+++ sdk/src/Services/DataZone/Generated/Model/Internal/MarshallTransformations/SingleSignOnMarshaller.cs
@@ -48,6 +48,12 @@ namespace Amazon.DataZone.Model.Internal.MarshallTransformations
         {
             if(requestObject == null)
                 return;
+            if(requestObject.IsSetIdcInstanceArn())
+            {
+                context.Writer.WritePropertyName("idcInstanceArn");
+                context.Writer.Write(requestObject.IdcInstanceArn);
+            }
+
             if(requestObject.IsSetType())
             {
                 context.Writer.WritePropertyName("type");
--- sdk/src/Services/DataZone/Generated/Model/Internal/MarshallTransformations/SingleSignOnUnmarshaller.cs
+++ sdk/src/Services/DataZone/Generated/Model/Internal/MarshallTransformations/SingleSignOnUnmarshaller.cs
@@ -66,6 +66,12 @@ namespace Amazon.DataZone.Model.Internal.MarshallTransformations
             int targetDepth = context.CurrentDepth;
             while (context.ReadAtDepth(targetDepth))
             {
+                if (context.TestExpression("idcInstanceArn", targetDepth))
+                {
+                    var unmarshaller = StringUnmarshaller.Instance;
+                    unmarshalledObject.IdcInstanceArn = unmarshaller.Unmarshall(context);
+                    continue;
+                }
                 if (context.TestExpression("type", targetDepth))
                 {
                     var unmarshaller = StringUnmarshaller.Instance;
