973ad979f166d4cad19deca6317e34c217718ccb
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2025-03-27 18:26:45 +0000

    Update IAM dual-stack endpoints for BJS, IAD and PDT partitions

--- sdk/src/Services/IdentityManagement/Generated/Internal/AmazonIdentityManagementServiceEndpointResolver.cs
+++ sdk/src/Services/IdentityManagement/Generated/Internal/AmazonIdentityManagementServiceEndpointResolver.cs
@@ -50,10 +50,10 @@ namespace Amazon.IdentityManagement.Internal
         {
             var config = (AmazonIdentityManagementServiceConfig)requestContext.ClientConfig;
             var result = new IdentityManagementServiceEndpointParameters();
-            result.Region = config.RegionEndpoint?.SystemName;
             result.UseDualStack = config.UseDualstackEndpoint;
             result.UseFIPS = config.UseFIPSEndpoint;
             result.Endpoint = config.ServiceURL;
+            result.Region = config.RegionEndpoint?.SystemName;
 
 
             // The region needs to be determined from the ServiceURL if not set.
