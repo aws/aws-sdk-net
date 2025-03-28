210a86fd759837c3992b858c2a91ea58d27ae6c0
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2025-03-28 18:23:29 +0000

    Add support for NetworkManager Dualstack endpoints.

--- sdk/src/Services/NetworkManager/Generated/Internal/AmazonNetworkManagerEndpointResolver.cs
+++ sdk/src/Services/NetworkManager/Generated/Internal/AmazonNetworkManagerEndpointResolver.cs
@@ -50,10 +50,10 @@ namespace Amazon.NetworkManager.Internal
         {
             var config = (AmazonNetworkManagerConfig)requestContext.ClientConfig;
             var result = new NetworkManagerEndpointParameters();
-            result.Region = config.RegionEndpoint?.SystemName;
             result.UseDualStack = config.UseDualstackEndpoint;
             result.UseFIPS = config.UseFIPSEndpoint;
             result.Endpoint = config.ServiceURL;
+            result.Region = config.RegionEndpoint?.SystemName;
 
 
             // The region needs to be determined from the ServiceURL if not set.
