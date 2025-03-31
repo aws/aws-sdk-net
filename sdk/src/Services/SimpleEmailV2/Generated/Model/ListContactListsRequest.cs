3167804d78ac27266ac17d9d1b768c168a1100b3
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2025-03-31 18:43:00 +0000

    Add dual-stack support to global endpoints.

--- sdk/src/Services/SimpleEmailV2/Generated/Model/ListContactListsRequest.cs
+++ sdk/src/Services/SimpleEmailV2/Generated/Model/ListContactListsRequest.cs
@@ -32,6 +32,13 @@ namespace Amazon.SimpleEmailV2.Model
     /// <summary>
     /// Container for the parameters to the ListContactLists operation.
     /// Lists all of the contact lists available.
+    /// 
+    ///  
+    /// <para>
+    /// If your output includes a "NextToken" field with a string value, this indicates there
+    /// may be additional contacts on the filtered list - regardless of the number of contacts
+    /// returned.
+    /// </para>
     /// </summary>
     public partial class ListContactListsRequest : AmazonSimpleEmailServiceV2Request
     {
