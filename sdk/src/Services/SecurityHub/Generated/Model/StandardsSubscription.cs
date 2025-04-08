7b78c40f4cf558fad83586c210ffad0aae454539
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2025-04-08 18:53:39 +0000

    Documentation updates for AWS Security Hub.

--- sdk/src/Services/SecurityHub/Generated/Model/StandardsSubscription.cs
+++ sdk/src/Services/SecurityHub/Generated/Model/StandardsSubscription.cs
@@ -44,7 +44,7 @@ namespace Amazon.SecurityHub.Model
         /// <summary>
         /// Gets and sets the property StandardsArn. 
         /// <para>
-        /// The ARN of a standard.
+        /// The ARN of the standard.
         /// </para>
         /// </summary>
         [AWSProperty(Required=true)]
@@ -63,23 +63,16 @@ namespace Amazon.SecurityHub.Model
         /// <summary>
         /// Gets and sets the property StandardsControlsUpdatable. 
         /// <para>
-        /// Indicates whether the controls associated with this standards subscription can be
-        /// viewed and updated.
-        /// </para>
-        ///  
-        /// <para>
-        /// The values are as follows:
+        /// Specifies whether you can retrieve information about and configure individual controls
+        /// that apply to the standard. Possible values are:
         /// </para>
         ///  <ul> <li> 
         /// <para>
-        ///  <c>READY_FOR_UPDATES</c> - Controls associated with this standards subscription can
-        /// be viewed and updated.
+        ///  <c>READY_FOR_UPDATES</c> - Controls in the standard can be retrieved and configured.
         /// </para>
         ///  </li> <li> 
         /// <para>
-        ///  <c>NOT_READY_FOR_UPDATES</c> - Controls associated with this standards subscription
-        /// cannot be retrieved or updated yet. Security Hub is still processing a request to
-        /// create the controls.
+        ///  <c>NOT_READY_FOR_UPDATES</c> - Controls in the standard cannot be retrieved or configured.
         /// </para>
         ///  </li> </ul>
         /// </summary>
@@ -117,32 +110,31 @@ namespace Amazon.SecurityHub.Model
         /// <summary>
         /// Gets and sets the property StandardsStatus. 
         /// <para>
-        /// The status of the standard subscription.
-        /// </para>
-        ///  
-        /// <para>
-        /// The status values are as follows:
+        /// The status of your subscription to the standard. Possible values are:
         /// </para>
         ///  <ul> <li> 
         /// <para>
-        ///  <c>PENDING</c> - Standard is in the process of being enabled.
+        ///  <c>PENDING</c> - The standard is in the process of being enabled. Or the standard
+        /// is already enabled and Security Hub is adding new controls to the standard.
         /// </para>
         ///  </li> <li> 
         /// <para>
-        ///  <c>READY</c> - Standard is enabled.
+        ///  <c>READY</c> - The standard is enabled.
         /// </para>
         ///  </li> <li> 
         /// <para>
-        ///  <c>INCOMPLETE</c> - Standard could not be enabled completely. Some controls may not
-        /// be available.
+        ///  <c>INCOMPLETE</c> - The standard could not be enabled completely. One or more errors
+        /// (<c>StandardsStatusReason</c>) occurred when Security Hub attempted to enable the
+        /// standard.
         /// </para>
         ///  </li> <li> 
         /// <para>
-        ///  <c>DELETING</c> - Standard is in the process of being disabled.
+        ///  <c>DELETING</c> - The standard is in the process of being disabled.
         /// </para>
         ///  </li> <li> 
         /// <para>
-        ///  <c>FAILED</c> - Standard could not be disabled.
+        ///  <c>FAILED</c> - The standard could not be disabled. One or more errors (<c>StandardsStatusReason</c>)
+        /// occurred when Security Hub attempted to disable the standard.
         /// </para>
         ///  </li> </ul>
         /// </summary>
@@ -180,7 +172,7 @@ namespace Amazon.SecurityHub.Model
         /// <summary>
         /// Gets and sets the property StandardsSubscriptionArn. 
         /// <para>
-        /// The ARN of a resource that represents your subscription to a supported standard.
+        /// The ARN of the resource that represents your subscription to the standard.
         /// </para>
         /// </summary>
         [AWSProperty(Required=true)]
