619d72e19d15a59eb6a12591784b66013aae7111
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2025-04-29 20:00:24 +0000

    This release adds API support for the connection recording GUI Connect feature of AWS Systems Manager

---
+++ sdk/src/Services/SSMGuiConnect/Generated/Model/Internal/MarshallTransformations/ConnectionRecordingPreferencesMarshaller.cs
@@ -0,0 +1,74 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ * 
+ *  http://aws.amazon.com/apache2.0
+ * 
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/*
+ * Do not modify this file. This file is generated from the ssm-guiconnect-2021-05-01.normal.json service model.
+ */
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+using Amazon.SSMGuiConnect.Model;
+using Amazon.Runtime;
+using Amazon.Runtime.Internal;
+using Amazon.Runtime.Internal.Transform;
+using Amazon.Runtime.Internal.Util;
+#pragma warning disable CS0612,CS0618
+namespace Amazon.SSMGuiConnect.Model.Internal.MarshallTransformations
+{
+    /// <summary>
+    /// ConnectionRecordingPreferences Marshaller
+    /// </summary>
+    public class ConnectionRecordingPreferencesMarshaller : IRequestMarshaller<ConnectionRecordingPreferences, JsonMarshallerContext> 
+    {
+        /// <summary>
+        /// Unmarshaller the response from the service to the response class.
+        /// </summary>  
+        /// <param name="requestObject"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public void Marshall(ConnectionRecordingPreferences requestObject, JsonMarshallerContext context)
+        {
+            if(requestObject == null)
+                return;
+            if(requestObject.IsSetKMSKeyArn())
+            {
+                context.Writer.WritePropertyName("KMSKeyArn");
+                context.Writer.WriteStringValue(requestObject.KMSKeyArn);
+            }
+
+            if(requestObject.IsSetRecordingDestinations())
+            {
+                context.Writer.WritePropertyName("RecordingDestinations");
+                context.Writer.WriteStartObject();
+
+                var marshaller = RecordingDestinationsMarshaller.Instance;
+                marshaller.Marshall(requestObject.RecordingDestinations, context);
+
+                context.Writer.WriteEndObject();
+            }
+
+        }
+
+        /// <summary>
+        /// Singleton Marshaller.
+        /// </summary>
+        public readonly static ConnectionRecordingPreferencesMarshaller Instance = new ConnectionRecordingPreferencesMarshaller();
+
+    }
+}
