39838a043655a410d83aea279ae8b9d341842a4f
Bo Blodgett <42787405+boblodgett@users.noreply.github.com>
2025-04-29 09:30:33 -0700

    Add check for H2Support == H2SupportDegree.Optional for specification compliance.

--- generator/ServiceClientGeneratorLib/ServiceModel.cs
+++ generator/ServiceClientGeneratorLib/ServiceModel.cs
@@ -453,7 +453,7 @@ namespace ServiceClientGenerator
                         ExcludedOperations.Add(operation.Name);
                     }
                     // H2 bi-directional (bidi) event streams are not yet supported
-                    else if (operation.IsEventStreamBidi)
+                    else if (H2Support == H2SupportDegree.Optional && operation.IsEventStreamBidi)
                     {
                         ExcludedOperations.Add(operation.Name);
                     }
