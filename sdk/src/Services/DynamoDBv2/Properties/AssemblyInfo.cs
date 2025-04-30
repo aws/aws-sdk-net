1207f969d83331cd9783544c6f0101c7ede781a0
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2025-04-30 18:25:11 +0000

    Updated generated files.

--- sdk/src/Services/DynamoDBv2/Properties/AssemblyInfo.cs
+++ sdk/src/Services/DynamoDBv2/Properties/AssemblyInfo.cs
@@ -41,9 +41,5 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-#if BCL
 [assembly: AssemblyVersion("4.0")]
-#else
-[assembly: AssemblyVersion("4.0.0.1")]
-#endif
 [assembly: AssemblyFileVersion("4.0.0.1")]
