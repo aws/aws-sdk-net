8d6806a25c3361ce30bf50c081d6b372247a8f90
Norm Johanson <normj@amazon.com>
2025-05-13 11:00:10 -0700

    Add tool for generating the DevConfig file

---
+++ buildtools/DevConfigGenerator/DevConfigGenerator/DevConfigFile.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+
+namespace DevConfigGenerator;
+
+public class DevConfigFile
+{
+    public CoreChange? Core { get; set; }
+
+    public List<Service> Services { get; } = new List<Service>();
+}
+
+public class CoreChange
+{
+    public List<string> Messages { get; } = new List<string>();
+
+    public bool UpdateServiceMinimum { get; set; }
+
+    public string Type { get; set; } = "patch";
+
+    public string FormattedNameColumn()
+    {
+        var sb = new StringBuilder();
+        sb.AppendLine("AWSSDK.Core");
+        sb.AppendLine($"Update service minimums: [green]{UpdateServiceMinimum}[/]");
+        return sb.ToString();
+    }
+
+    public string FormattedMessagesColumn()
+    {
+        var sb = new StringBuilder();
+        foreach (var message in Messages)
+        {
+            sb.AppendLine(message.ToString());
+        }
+        return sb.ToString();
+    }
+}
+
+public class Service
+{
+    public required string Name { get; init; }
+
+    public List<string> Messages { get; } = new List<string>();
+
+    public string Type { get; set; } = "patch";
+
+
+    public string FormattedNameColumn()
+    {
+        var sb = new StringBuilder();
+        sb.AppendLine($"AWSSDK.{Name}");
+        return sb.ToString();
+    }
+
+    public string FormattedMessagesColumn()
+    {
+        var sb = new StringBuilder();
+        foreach (var message in Messages)
+        {
+            sb.AppendLine(message.ToString());
+        }
+        return sb.ToString();
+    }
+}
---
+++ buildtools/DevConfigGenerator/DevConfigGenerator/DevConfigWriter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace DevConfigGenerator;
+
+public class DevConfigWriter
+{
+    public static void Write(DevConfigFile config, string filePath)
+    {
+        using var stream = File.OpenWrite(filePath);
+        using Utf8JsonWriter writer = new Utf8JsonWriter(stream, new JsonWriterOptions { Indented = true });
+
+        writer.WriteStartObject();
+
+        if (config.Core != null)
+        {
+            writer.WriteStartObject("core");
+
+            writer.WriteBoolean("updateMinimum", config.Core.UpdateServiceMinimum);
+            writer.WriteString("type", config.Core.Type);
+            writer.WriteStartArray("changeLogMessages");
+            foreach (var message in config.Core.Messages)
+            {
+                writer.WriteStringValue(message);
+            }
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+        }
+
+        if(config.Services.Any())
+        {
+            writer.WriteStartArray("services");
+
+            foreach (var service in config.Services)
+            {
+                writer.WriteStartObject();
+                writer.WriteString("serviceName", service.Name);
+                writer.WriteString("type", service.Type);
+
+                writer.WriteStartArray("changeLogMessages");
+                foreach(var message in service.Messages)
+                {
+                    writer.WriteStringValue(message);
+                }
+                writer.WriteEndArray();
+
+                writer.WriteEndObject();
+            }
+
+            writer.WriteEndArray();
+        }
+
+        writer.WriteEndObject();
+    }
+}
