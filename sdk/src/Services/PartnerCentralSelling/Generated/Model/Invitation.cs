ca9930b0b511cd3b3d0392061ee114b9556246cd
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2025-05-21 18:11:28 +0000

    Modified validation to allow expectedCustomerSpend array with zero elements in Partner Opportunity operations.

--- sdk/src/Services/PartnerCentralSelling/Generated/Model/Invitation.cs
+++ sdk/src/Services/PartnerCentralSelling/Generated/Model/Invitation.cs
@@ -46,7 +46,7 @@ namespace Amazon.PartnerCentralSelling.Model
         ///  A message accompanying the invitation. 
         /// </para>
         /// </summary>
-        [AWSProperty(Required=true, Min=1, Max=255)]
+        [AWSProperty(Required=true, Sensitive=true, Min=1, Max=255)]
         public string Message
         {
             get { return this._message; }
