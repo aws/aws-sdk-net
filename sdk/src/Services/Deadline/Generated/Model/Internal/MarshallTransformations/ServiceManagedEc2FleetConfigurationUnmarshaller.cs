4e6b63b1107339b5f77171eee29a224616fd5361
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2025-05-27 18:17:37 +0000

    AWS Deadline Cloud service-managed fleets now support storage profiles. With storage profiles, you can map file paths between a workstation and the worker hosts running the job.

--- sdk/src/Services/Deadline/Generated/Model/Internal/MarshallTransformations/ServiceManagedEc2FleetConfigurationUnmarshaller.cs
+++ sdk/src/Services/Deadline/Generated/Model/Internal/MarshallTransformations/ServiceManagedEc2FleetConfigurationUnmarshaller.cs
@@ -68,6 +68,12 @@ namespace Amazon.Deadline.Model.Internal.MarshallTransformations
                     unmarshalledObject.InstanceMarketOptions = unmarshaller.Unmarshall(context, ref reader);
                     continue;
                 }
+                if (context.TestExpression("storageProfileId", targetDepth))
+                {
+                    var unmarshaller = StringUnmarshaller.Instance;
+                    unmarshalledObject.StorageProfileId = unmarshaller.Unmarshall(context, ref reader);
+                    continue;
+                }
             }
             return unmarshalledObject;
         }
