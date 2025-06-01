52b859dc5c8e2a564437ee297e6405eb2a274a6a
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-06-01 10:15:32 +0200

    SQS: getAttributeValue does not account for attributes potentially being null

--- sdk/src/Services/SQS/Custom/Model/GetQueueAttributesResponse.extensions.cs
+++ sdk/src/Services/SQS/Custom/Model/GetQueueAttributesResponse.extensions.cs
@@ -207,7 +207,7 @@ namespace Amazon.SQS.Model
         private string getAttributeValue(string field)
         {
             string value = null;
-            this.Attributes.TryGetValue(field, out value);
+            this.Attributes?.TryGetValue(field, out value);
             return value;
         }
     }
