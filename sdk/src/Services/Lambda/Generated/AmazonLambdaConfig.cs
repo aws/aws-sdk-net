f90f16155251ef0509094bfe032a0fd83c9ae6aa
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2025-06-19 19:30:50 +0000

    Commit version number update changes

--- sdk/src/Services/Lambda/Generated/AmazonLambdaConfig.cs
+++ sdk/src/Services/Lambda/Generated/AmazonLambdaConfig.cs
@@ -33,7 +33,7 @@ namespace Amazon.Lambda
     public partial class AmazonLambdaConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("Lambda", "4.0.0.10");
+            InternalSDKUtils.BuildUserAgentString("Lambda", "4.0.1.0");
 
         private static readonly AmazonLambdaEndpointResolver EndpointResolver =
             new AmazonLambdaEndpointResolver();
--- sdk/src/Services/PaymentCryptographyData/Generated/AmazonPaymentCryptographyDataConfig.cs
+++ sdk/src/Services/PaymentCryptographyData/Generated/AmazonPaymentCryptographyDataConfig.cs
@@ -33,7 +33,7 @@ namespace Amazon.PaymentCryptographyData
     public partial class AmazonPaymentCryptographyDataConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("Payment Cryptography Data", "4.0.0.10");
+            InternalSDKUtils.BuildUserAgentString("Payment Cryptography Data", "4.0.1.0");
 
         private static readonly AmazonPaymentCryptographyDataEndpointResolver EndpointResolver =
             new AmazonPaymentCryptographyDataEndpointResolver();
