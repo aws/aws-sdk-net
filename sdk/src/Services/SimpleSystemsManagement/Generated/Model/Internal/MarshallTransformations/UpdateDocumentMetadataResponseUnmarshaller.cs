07f488dc6d54eb4063c6dc2c8e09c658718d3f01
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2025-06-30 18:22:30 +0000

    Introduces AccessType, a new filter value for the DescribeSessions API.

--- sdk/src/Services/SimpleSystemsManagement/Generated/Model/Internal/MarshallTransformations/UpdateDocumentMetadataResponseUnmarshaller.cs
+++ sdk/src/Services/SimpleSystemsManagement/Generated/Model/Internal/MarshallTransformations/UpdateDocumentMetadataResponseUnmarshaller.cs
@@ -87,6 +87,10 @@ namespace Amazon.SimpleSystemsManagement.Model.Internal.MarshallTransformations
                 {
                     return InvalidDocumentVersionExceptionUnmarshaller.Instance.Unmarshall(contextCopy, errorResponse, ref readerCopy);
                 }
+                if (errorResponse.Code != null && errorResponse.Code.Equals("TooManyUpdates"))
+                {
+                    return TooManyUpdatesExceptionUnmarshaller.Instance.Unmarshall(contextCopy, errorResponse, ref readerCopy);
+                }
             }
             return new AmazonSimpleSystemsManagementException(errorResponse.Message, errorResponse.InnerException, errorResponse.Type, errorResponse.Code, errorResponse.RequestId, errorResponse.StatusCode);
         }
