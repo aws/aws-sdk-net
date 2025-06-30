389e47dcc7e7422140c59aaa81de8db287c89b71
Garrett Beatty <gcbeatty@amazon.com>
2025-06-30 12:57:37 -0400

    move to non bcl folder

---
+++ sdk/src/Services/S3/Custom/Util/S3PostUploadSignedPolicy.cs
@@ -0,0 +1,298 @@
+﻿/*******************************************************************************
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not use
+ *  this file except in compliance with the License. A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ *  or in the "license" file accompanying this file.
+ *  This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ *  CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ *  specific language governing permissions and limitations under the License.
+ * *****************************************************************************
+ *    __  _    _  ___
+ *   (  )( \/\/ )/ __)
+ *   /__\ \    / \__ \
+ *  (_)(_) \/\/  (___/
+ *
+ *  AWS SDK for .NET
+ *
+ */
+
+using Amazon.Runtime;
+using Amazon.Util;
+using Amazon.Util.Internal;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+
+namespace Amazon.S3.Util
+{
+    /// <summary>
+    /// Utility class for managing and exchanging HTTP POST uploads of objects to Amazon S3.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// This object supports creating, marshalling, and unmarshalling of the information needed to build 
+    /// an authenticated HTTP POST request to S3 for uploading objects according to a policy. 
+    /// </para>
+    /// For more information, <see href="http://docs.aws.amazon.com/AmazonS3/latest/dev/UsingHTTPPOST.html"/>
+    /// </remarks>
+    public partial class S3PostUploadSignedPolicy
+    {
+        private static JsonDocumentOptions options = new JsonDocumentOptions { AllowTrailingCommas = true };
+
+        private static string
+           KEY_POLICY = "policy",
+           KEY_SIGNATURE = "signature",
+           KEY_ACCESSKEY = "access_key";
+
+        /// <summary>
+        /// The policy document which governs what uploads can be done.
+        /// </summary>
+        public string Policy { get; set; }
+
+        /// <summary>
+        /// The signature for the policy.
+        /// </summary>
+        public string Signature { get; set; }
+
+        /// <summary>
+        /// The AWS Access Key Id for the credential pair that produced the signature.
+        /// </summary>
+        public string AccessKeyId { get; set; }
+
+        /// <summary>
+        /// The security token from session or instance credentials.
+        /// </summary>
+        public string SecurityToken { get; set; }
+
+        /// <summary>
+        /// The signing algorithm used. Required as a field in the post Amazon
+        /// S3 can re-calculate the signature.
+        /// </summary>
+        public string Algorithm { get; set; }
+
+        /// <summary>
+        /// The date value in ISO8601 format. It is the same date used in
+        /// creating the signing key.
+        /// </summary>
+        public string Date { get; set; }
+
+        /// <summary>
+        /// In addition to the access key ID, this provides scope information
+        /// used in calculating the signing key for signature calculation. 
+        /// </summary>
+        public string Credential { get; set; }
+
+
+        /// <summary>
+        ///  Given a policy and AWS credentials, produce a S3PostUploadSignedPolicy.
+        /// </summary>
+        /// <param name="policy">JSON string representing the policy to sign</param>
+        /// <param name="credentials">Credentials to sign the policy with</param>
+        /// <param name="region">Service region endpoint.</param>
+        /// <returns>A signed policy object for use with an S3PostUploadRequest.</returns>
+        public static S3PostUploadSignedPolicy GetSignedPolicy(string policy, AWSCredentials credentials, RegionEndpoint region)
+        {
+            var signedAt = AWSSDKUtils.CorrectedUtcNow;
+
+            ImmutableCredentials iCreds = credentials.GetCredentials();
+            var algorithm = "AWS4-HMAC-SHA256";
+            var dateStamp = Runtime.Internal.Auth.AWS4Signer.FormatDateTime(signedAt, AWSSDKUtils.ISO8601BasicDateFormat);
+            var dateTimeStamp = Runtime.Internal.Auth.AWS4Signer.FormatDateTime(signedAt, AWSSDKUtils.ISO8601BasicDateTimeFormat);
+            var credentialString = string.Format(CultureInfo.InvariantCulture, "{0}/{1}/{2}/{3}/{4}/", iCreds.AccessKey, dateStamp, region.SystemName, "s3", Runtime.Internal.Auth.AWS4Signer.Terminator);
+
+            Dictionary<string, string> extraConditions = new Dictionary<string, string> {
+                { S3Constants.PostFormDataXAmzCredential, credentialString },
+                { S3Constants.PostFormDataXAmzAlgorithm, algorithm },
+                { S3Constants.PostFormDataXAmzDate, dateTimeStamp }
+            };
+            if (iCreds.UseToken) { extraConditions[S3Constants.PostFormDataSecurityToken] = iCreds.Token; }
+
+            var policyBytes = AddConditionsToPolicy(policy, extraConditions);
+
+            var base64Policy = Convert.ToBase64String(policyBytes);
+
+            var signingKey = Runtime.Internal.Auth.AWS4Signer.ComposeSigningKey(iCreds.SecretKey, region.SystemName, dateStamp, "s3");
+
+            var signature = AWSSDKUtils.ToHex(Runtime.Internal.Auth.AWS4Signer.ComputeKeyedHash(SigningAlgorithm.HmacSHA256, signingKey, base64Policy), true);
+
+            return new S3PostUploadSignedPolicy
+            {
+                Policy = base64Policy,
+                Signature = signature,
+                AccessKeyId = iCreds.AccessKey,
+                SecurityToken = iCreds.Token,
+                Algorithm = algorithm,
+                Date = dateTimeStamp,
+                Credential = credentialString
+            };
+        }
+
+        /// <summary>
+        /// Framework-agnostic implementation to add conditions to a policy document.
+        /// Uses JsonDocument/JsonElement instead of JsonNode.
+        /// </summary>
+        private static byte[] AddConditionsToPolicy(string policy, Dictionary<string, string> newConditions)
+        {
+            using (JsonDocument policyDoc = JsonDocument.Parse(policy, options))
+            {
+                using (var ms = new MemoryStream())
+                {
+                    using (var writer = new Utf8JsonWriter(ms))
+                    {
+                        writer.WriteStartObject();
+
+                        // Copy all properties except "conditions" from the original policy
+                        foreach (var property in policyDoc.RootElement.EnumerateObject())
+                        {
+                            if (property.Name != "conditions")
+                            {
+                                property.WriteTo(writer);
+                            }
+                        }
+
+                        // Write conditions array with modifications
+                        writer.WritePropertyName("conditions");
+                        writer.WriteStartArray();
+
+                        bool foundConditions = policyDoc.RootElement.TryGetProperty("conditions", out JsonElement conditionsElement);
+
+                        if (foundConditions && conditionsElement.ValueKind == JsonValueKind.Array)
+                        {
+                            // Process each existing condition
+                            foreach (JsonElement condition in conditionsElement.EnumerateArray())
+                            {
+                                bool shouldCopyCondition = true;
+                                string matchedKey = null;
+
+                                // Check if this condition is an object that contains any of our new condition keys
+                                if (condition.ValueKind == JsonValueKind.Object)
+                                {
+                                    foreach (var newCond in newConditions.Keys.ToList())
+                                    {
+                                        if (condition.TryGetProperty(newCond, out _))
+                                        {
+                                            matchedKey = newCond;
+                                            shouldCopyCondition = false;
+                                            break;
+                                        }
+                                    }
+                                }
+
+                                if (shouldCopyCondition)
+                                {
+                                    // Copy the original condition
+                                    condition.WriteTo(writer);
+                                }
+                                else if (matchedKey != null)
+                                {
+                                    // Write updated condition
+                                    writer.WriteStartObject();
+                                    foreach (var property in condition.EnumerateObject())
+                                    {
+                                        if (property.Name == matchedKey)
+                                        {
+                                            writer.WriteString(matchedKey, newConditions[matchedKey]);
+
+                                            // Remove this key from newConditions since we've handled it
+                                            newConditions.Remove(matchedKey);
+                                        }
+                                        else
+                                        {
+                                            property.WriteTo(writer);
+                                        }
+                                    }
+                                    writer.WriteEndObject();
+                                }
+                            }
+                        }
+
+                        // Add any remaining new conditions
+                        foreach (var newCond in newConditions)
+                        {
+                            writer.WriteStartObject();
+                            writer.WriteString(newCond.Key, newCond.Value);
+                            writer.WriteEndObject();
+                        }
+
+                        writer.WriteEndArray();
+                        writer.WriteEndObject();
+                        writer.Flush();
+
+                        return ms.ToArray();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get the policy document as a human readable string.
+        /// </summary>
+        /// <returns>Human readable policy document.</returns>
+        public string GetReadablePolicy()
+        {
+            return Encoding.UTF8.GetString(Convert.FromBase64String(this.Policy));
+        }
+
+     
+        /// <summary>
+        /// JSON representation of this object
+        /// </summary>
+        /// <returns>JSON string</returns>
+        public string ToJson()
+        {
+            var obj = new Dictionary<string, string>
+            {
+                [KEY_POLICY] = this.Policy,
+                [KEY_SIGNATURE] = this.Signature,
+                [KEY_ACCESSKEY] = this.AccessKeyId
+            };
+
+            return JsonSerializerHelper.Serialize<Dictionary<string, string>>(
+                obj,
+                DictionaryStringStringJsonSerializerContexts.Default
+            );
+        }
+
+        /// <summary>
+        /// Create an instance of this class from a JSON string.
+        /// </summary>
+        /// <param name="policyJson">JSON string</param>
+        /// <returns>Instance of S3PostUploadSignedPolicy</returns>
+        public static S3PostUploadSignedPolicy GetSignedPolicyFromJson(string policyJson)
+        {
+            try
+            {
+                using (var doc = JsonDocument.Parse(policyJson))
+                {
+                    var json = doc.RootElement;
+
+                    // Check if required fields exist and are of the correct type
+                    if (!json.TryGetProperty(KEY_POLICY, out var policyNode) || policyNode.ValueKind != JsonValueKind.String)
+                        throw new ArgumentException(string.Format(CultureInfo.InvariantCulture, "JSON document requires '{0}' field", KEY_POLICY), KEY_POLICY);
+                    if (!json.TryGetProperty(KEY_SIGNATURE, out var signatureNode) || signatureNode.ValueKind != JsonValueKind.String)
+                        throw new ArgumentException(string.Format(CultureInfo.InvariantCulture, "JSON document requires '{0}' field", KEY_SIGNATURE), KEY_SIGNATURE);
+                    if (!json.TryGetProperty(KEY_ACCESSKEY, out var accessKeyNode) || accessKeyNode.ValueKind != JsonValueKind.String)
+                        throw new ArgumentException(string.Format(CultureInfo.InvariantCulture, "JSON document requires '{0}' field", KEY_ACCESSKEY), KEY_ACCESSKEY);
+
+                    return new S3PostUploadSignedPolicy
+                    {
+                        Policy = policyNode.GetString(),
+                        Signature = signatureNode.GetString(),
+                        AccessKeyId = accessKeyNode.GetString()
+                    };
+                }
+            }
+            catch (JsonException e)
+            {
+                throw new ArgumentException("Invalid JSON document", e);
+            }
+        }
+    }
+}
