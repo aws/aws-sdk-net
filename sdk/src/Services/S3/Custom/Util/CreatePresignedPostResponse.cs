f53ba41ab07c3840d1c51c305340216bef0380fa
Garrett Beatty <gcbeatty@amazon.com>
2025-07-02 15:24:42 -0400

    presigned post

---
+++ sdk/src/Services/S3/Custom/Util/CreatePresignedPostResponse.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amazon.S3.Util
+{
+    /// <summary>
+    /// Response from creating a presigned POST request for S3.
+    /// Contains the URL and form fields needed for a browser-based file upload directly to S3.
+    /// </summary>
+    public class CreatePresignedPostResponse
+    {
+        /// <summary>
+        /// Gets the URL where the POST request should be submitted.
+        /// </summary>
+        public string Url { get; internal set; }
+        
+        /// <summary>
+        /// Gets the form fields that must be included in the POST request.
+        /// These fields contain the policy, signature, and other AWS-required parameters.
+        /// </summary>
+        public Dictionary<string, string> Fields { get; internal set; }
+        
+        /// <summary>
+        /// Initializes a new instance of the CreatePresignedPostResponse class.
+        /// </summary>
+        public CreatePresignedPostResponse()
+        {
+            Fields = new Dictionary<string, string>();
+        }
+    }
+
+}
