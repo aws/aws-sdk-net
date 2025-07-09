e02b8b036ed1bb585d239949333b35c05d2e15f8
Peter Song <petesong@amazon.com>
2025-07-09 15:23:39 -0700

    Other generated changes from moving PostMarshallCustomization

--- sdk/src/Services/S3/Generated/Model/Internal/MarshallTransformations/PutObjectTaggingRequestMarshaller.cs
+++ sdk/src/Services/S3/Generated/Model/Internal/MarshallTransformations/PutObjectTaggingRequestMarshaller.cs
@@ -115,6 +115,7 @@ namespace Amazon.S3.Model.Internal.MarshallTransformations
                     xmlWriter.WriteEndElement();
                 }
             }
+            PostMarshallCustomization(request, publicRequest);
             try 
             {
                 string content = stringWriter.ToString();
@@ -135,9 +136,7 @@ namespace Amazon.S3.Model.Internal.MarshallTransformations
             {
                 throw new AmazonServiceException("Unable to marshall request to XML", e);
             }
-
             request.UseQueryString = true;
-            PostMarshallCustomization(request, publicRequest);
             return request;
         }
         private static PutObjectTaggingRequestMarshaller _instance = new PutObjectTaggingRequestMarshaller();        
