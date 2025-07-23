522f858efb423e0f817f6d1177a8a64a070d32da
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2025-07-23 18:16:03 +0000

    Added the lastUserId parameter to the ListDevices and GetDevice API.

--- sdk/src/Services/WorkSpacesThinClient/Generated/Model/Internal/MarshallTransformations/DeviceSummaryUnmarshaller.cs
+++ sdk/src/Services/WorkSpacesThinClient/Generated/Model/Internal/MarshallTransformations/DeviceSummaryUnmarshaller.cs
@@ -104,6 +104,12 @@ namespace Amazon.WorkSpacesThinClient.Model.Internal.MarshallTransformations
                     unmarshalledObject.LastPostureAt = unmarshaller.Unmarshall(context, ref reader);
                     continue;
                 }
+                if (context.TestExpression("lastUserId", targetDepth))
+                {
+                    var unmarshaller = StringUnmarshaller.Instance;
+                    unmarshalledObject.LastUserId = unmarshaller.Unmarshall(context, ref reader);
+                    continue;
+                }
                 if (context.TestExpression("model", targetDepth))
                 {
                     var unmarshaller = StringUnmarshaller.Instance;
