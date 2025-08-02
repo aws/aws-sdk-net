25267dcee47d682eacb916ff677e80700b574373
Daniel Pinheiro <dspin@amazon.com>
2025-08-01 21:13:11 -0400

    fix: Update HTTP handler to handle scenario where content stream position is not zero

--- sdk/src/Core/Amazon.Runtime/Pipeline/HttpHandler/HttpHandler.cs
+++ sdk/src/Core/Amazon.Runtime/Pipeline/HttpHandler/HttpHandler.cs
@@ -308,7 +308,17 @@ namespace Amazon.Runtime.Internal
                 }
                 else
                 {
-                    originalStream = wrappedRequest.ContentStream;
+                    // If the current position for the ContentStream is not at the beginning, we need to handle it
+                    // before wrapping it during GetInputStream.
+                    if (wrappedRequest.ContentStream.CanSeek && wrappedRequest.ContentStream.Position != 0)
+                    {
+                        var size = wrappedRequest.ContentStream.Length - wrappedRequest.ContentStream.Position;
+                        originalStream = new PartialReadOnlyWrapperStream(wrappedRequest.ContentStream, size);
+                    }
+                    else
+                    {
+                        originalStream = wrappedRequest.ContentStream;
+                    }
                 }
 
                 var callback = ((Amazon.Runtime.Internal.IAmazonWebServiceRequest)wrappedRequest.OriginalRequest).StreamUploadProgressCallback;
@@ -355,7 +365,17 @@ namespace Amazon.Runtime.Internal
                 }
                 else
                 {
-                    originalStream = wrappedRequest.ContentStream;
+                    // If the current position for the ContentStream is not at the beginning, we need to handle it
+                    // before wrapping it during GetInputStream.
+                    if (wrappedRequest.ContentStream.CanSeek && wrappedRequest.ContentStream.Position != 0)
+                    {
+                        var size = wrappedRequest.ContentStream.Length - wrappedRequest.ContentStream.Position;
+                        originalStream = new PartialReadOnlyWrapperStream(wrappedRequest.ContentStream, size);
+                    }
+                    else
+                    {
+                        originalStream = wrappedRequest.ContentStream;
+                    }
                 }
 
                 var callback = ((Amazon.Runtime.Internal.IAmazonWebServiceRequest)wrappedRequest.OriginalRequest).StreamUploadProgressCallback;
