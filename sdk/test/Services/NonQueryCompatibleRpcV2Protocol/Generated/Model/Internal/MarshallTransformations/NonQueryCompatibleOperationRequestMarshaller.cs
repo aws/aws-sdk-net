eadaa345c2652e17819744fac523afceeded492c
Muhammad Othman <muhammmadothman@gmail.com>
2025-08-05 12:11:51 -0400

    Add QueryCompatible protocol tests for CBOR (#3929)

---
+++ sdk/test/Services/NonQueryCompatibleRpcV2Protocol/Generated/Model/Internal/MarshallTransformations/NonQueryCompatibleOperationRequestMarshaller.cs
@@ -0,0 +1,88 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ * 
+ *  http://aws.amazon.com/apache2.0
+ * 
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/*
+ * Do not modify this file. This file is generated from the non-query-compatible-rpcv2-protocol-2025-06-20.normal.json service model.
+ */
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+using Amazon.NonQueryCompatibleRpcV2Protocol.Model;
+using Amazon.Runtime;
+using Amazon.Runtime.Internal;
+using Amazon.Runtime.Internal.Transform;
+using Amazon.Runtime.Internal.Util;
+using Amazon.Extensions.CborProtocol;
+using Amazon.Extensions.CborProtocol.Internal;
+using Amazon.Extensions.CborProtocol.Internal.Transform;
+
+#pragma warning disable CS0612,CS0618
+namespace Amazon.NonQueryCompatibleRpcV2Protocol.Model.Internal.MarshallTransformations
+{
+    /// <summary>
+    /// NonQueryCompatibleOperation Request Marshaller
+    /// </summary>       
+    public class NonQueryCompatibleOperationRequestMarshaller : IMarshaller<IRequest, NonQueryCompatibleOperationRequest> , IMarshaller<IRequest,AmazonWebServiceRequest>
+    {
+        /// <summary>
+        /// Marshaller the request object to the HTTP request.
+        /// </summary>  
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public IRequest Marshall(AmazonWebServiceRequest input)
+        {
+            return this.Marshall((NonQueryCompatibleOperationRequest)input);
+        }
+
+        /// <summary>
+        /// Marshaller the request object to the HTTP request.
+        /// </summary>  
+        /// <param name="publicRequest"></param>
+        /// <returns></returns>
+        public IRequest Marshall(NonQueryCompatibleOperationRequest publicRequest)
+        {
+            IRequest request = new DefaultRequest(publicRequest, "Amazon.NonQueryCompatibleRpcV2Protocol");
+            request.Headers["smithy-protocol"] = "rpc-v2-cbor";
+            request.ResourcePath = "service/NonQueryCompatibleRpcV2Protocol/operation/NonQueryCompatibleOperation";
+            request.Headers["Accept"] = "application/cbor";
+            request.Headers[Amazon.Util.HeaderKeys.XAmzApiVersion] = "2025-06-20";
+            request.HttpMethod = "POST";
+
+            return request;
+        }
+        private static NonQueryCompatibleOperationRequestMarshaller _instance = new NonQueryCompatibleOperationRequestMarshaller();        
+
+        internal static NonQueryCompatibleOperationRequestMarshaller GetInstance()
+        {
+            return _instance;
+        }
+
+        /// <summary>
+        /// Gets the singleton.
+        /// </summary>  
+        public static NonQueryCompatibleOperationRequestMarshaller Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+    }
+}
