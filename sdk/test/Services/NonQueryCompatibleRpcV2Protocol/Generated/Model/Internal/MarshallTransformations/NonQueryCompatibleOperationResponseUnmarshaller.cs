014ddb42a1c65004bb628a6628314502a42ad979
Muhammad Othman <muhammmadothman@gmail.com>
2025-08-05 12:11:51 -0400

    Add QueryCompatible protocol tests for CBOR (#3929)

---
+++ sdk/test/Services/NonQueryCompatibleRpcV2Protocol/Generated/Model/Internal/MarshallTransformations/NonQueryCompatibleOperationResponseUnmarshaller.cs
@@ -0,0 +1,99 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ * 
+ *  http://aws.amazon.com/apache2.0
+ * 
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/*
+ * Do not modify this file. This file is generated from the non-query-compatible-rpcv2-protocol-2025-06-20.normal.json service model.
+ */
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Xml.Serialization;
+
+using Amazon.NonQueryCompatibleRpcV2Protocol.Model;
+using Amazon.Runtime;
+using Amazon.Runtime.Internal;
+using Amazon.Runtime.Internal.Transform;
+using Amazon.Runtime.Internal.Util;
+using Amazon.Util;
+using System.Formats.Cbor;
+using Amazon.Extensions.CborProtocol.Internal.Transform;
+
+#pragma warning disable CS0612,CS0618
+namespace Amazon.NonQueryCompatibleRpcV2Protocol.Model.Internal.MarshallTransformations
+{
+    /// <summary>
+    /// Response Unmarshaller for NonQueryCompatibleOperation operation
+    /// </summary>  
+    public class NonQueryCompatibleOperationResponseUnmarshaller : CborResponseUnmarshaller
+    {
+        /// <summary>
+        /// Unmarshaller the response from the service to the response class.
+        /// </summary>  
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override AmazonWebServiceResponse Unmarshall(CborUnmarshallerContext context)
+        {
+            NonQueryCompatibleOperationResponse response = new NonQueryCompatibleOperationResponse();
+            var reader = context.Reader;
+            context.AddPathSegment("NonQueryCompatibleOperation");
+
+            return response;
+        }
+
+        /// <summary>
+        /// Unmarshaller error response to exception.
+        /// </summary>  
+        /// <param name="context"></param>
+        /// <param name="innerException"></param>
+        /// <param name="statusCode"></param>
+        /// <returns></returns>
+        public override AmazonServiceException UnmarshallException(CborUnmarshallerContext context, Exception innerException, HttpStatusCode statusCode)
+        {
+            var errorResponse = CborErrorResponseUnmarshaller.GetInstance().Unmarshall(context);
+            errorResponse.InnerException = innerException;
+            errorResponse.StatusCode = statusCode;
+
+            var responseBodyBytes = context.GetResponseBodyBytes();
+
+            using (var streamCopy = new MemoryStream(responseBodyBytes))
+            using (var contextCopy = new CborUnmarshallerContext(streamCopy, false, context.ResponseData))
+            {
+            }
+            return new AmazonNonQueryCompatibleRpcV2ProtocolException(errorResponse.Message, errorResponse.InnerException, errorResponse.Type, errorResponse.Code, errorResponse.RequestId, errorResponse.StatusCode);
+        }
+
+        private static NonQueryCompatibleOperationResponseUnmarshaller _instance = new NonQueryCompatibleOperationResponseUnmarshaller();        
+
+        internal static NonQueryCompatibleOperationResponseUnmarshaller GetInstance()
+        {
+            return _instance;
+        }
+
+        /// <summary>
+        /// Gets the singleton.
+        /// </summary>  
+        public static NonQueryCompatibleOperationResponseUnmarshaller Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+    }
+}
