08be529cdd9506716c837787b1b207ed373d2a85
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2025-08-08 18:25:11 +0000

    Update documentation to use key ARN only in OutputEncryptionKMSKeyId request parameter

--- sdk/src/Services/TranscribeService/Generated/Model/StartMedicalScribeJobRequest.cs
+++ sdk/src/Services/TranscribeService/Generated/Model/StartMedicalScribeJobRequest.cs
@@ -250,51 +250,25 @@ namespace Amazon.TranscribeService.Model
         /// <summary>
         /// Gets and sets the property OutputEncryptionKMSKeyId. 
         /// <para>
-        /// The KMS key you want to use to encrypt your Medical Scribe output.
+        /// The Amazon Resource Name (ARN) of a KMS key that you want to use to encrypt your Medical
+        /// Scribe output.
         /// </para>
         ///  
         /// <para>
-        /// If using a key located in the <b>current</b> Amazon Web Services account, you can
-        /// specify your KMS key in one of four ways:
+        /// KMS key ARNs have the format <c>arn:partition:kms:region:account:key/key-id</c>. For
+        /// example: <c>arn:aws:kms:us-west-2:111122223333:key/1234abcd-12ab-34cd-56ef-1234567890ab</c>.
+        /// For more information, see <a href="https://docs.aws.amazon.com/kms/latest/developerguide/concepts.html#key-id-key-ARN">
+        /// KMS key ARNs</a>.
         /// </para>
-        ///  <ol> <li> 
-        /// <para>
-        /// Use the KMS key ID itself. For example, <c>1234abcd-12ab-34cd-56ef-1234567890ab</c>.
-        /// </para>
-        ///  </li> <li> 
-        /// <para>
-        /// Use an alias for the KMS key ID. For example, <c>alias/ExampleAlias</c>.
-        /// </para>
-        ///  </li> <li> 
-        /// <para>
-        /// Use the Amazon Resource Name (ARN) for the KMS key ID. For example, <c>arn:aws:kms:region:account-ID:key/1234abcd-12ab-34cd-56ef-1234567890ab</c>.
-        /// </para>
-        ///  </li> <li> 
-        /// <para>
-        /// Use the ARN for the KMS key alias. For example, <c>arn:aws:kms:region:account-ID:alias/ExampleAlias</c>.
-        /// </para>
-        ///  </li> </ol> 
-        /// <para>
-        /// If using a key located in a <b>different</b> Amazon Web Services account than the
-        /// current Amazon Web Services account, you can specify your KMS key in one of two ways:
-        /// </para>
-        ///  <ol> <li> 
-        /// <para>
-        /// Use the ARN for the KMS key ID. For example, <c>arn:aws:kms:region:account-ID:key/1234abcd-12ab-34cd-56ef-1234567890ab</c>.
-        /// </para>
-        ///  </li> <li> 
-        /// <para>
-        /// Use the ARN for the KMS key alias. For example, <c>arn:aws:kms:region:account-ID:alias/ExampleAlias</c>.
-        /// </para>
-        ///  </li> </ol> 
+        ///  
         /// <para>
         /// If you do not specify an encryption key, your output is encrypted with the default
         /// Amazon S3 key (SSE-S3).
         /// </para>
         ///  
         /// <para>
-        /// Note that the role specified in the <c>DataAccessRoleArn</c> request parameter must
-        /// have permission to use the specified KMS key.
+        /// Note that the role making the request and the role specified in the <c>DataAccessRoleArn</c>
+        /// request parameter (if present) must have permission to use the specified KMS key.
         /// </para>
         /// </summary>
         [AWSProperty(Min=1, Max=2048)]
