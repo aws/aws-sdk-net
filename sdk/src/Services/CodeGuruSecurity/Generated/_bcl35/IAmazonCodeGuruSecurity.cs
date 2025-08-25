9a387bd44cd2ca46d9e778ada4e10370bf0a4ff3
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2025-08-25 20:55:42 +0000

    Documentation update to notify users of the discontinuation of Amazon CodeGuru Security.

--- sdk/src/Services/CodeGuruSecurity/Generated/_bcl35/IAmazonCodeGuruSecurity.cs
+++ sdk/src/Services/CodeGuruSecurity/Generated/_bcl35/IAmazonCodeGuruSecurity.cs
@@ -32,8 +32,10 @@ namespace Amazon.CodeGuruSecurity
     ///
     /// <note> 
     /// <para>
-    /// Amazon CodeGuru Security is in preview release and is subject to change.
-    /// </para>
+    /// On November 20, 2025, AWS will discontinue support for Amazon CodeGuru Security. After
+    /// November 20, 2025, you will no longer be able to access the /codeguru/security console,
+    /// service resources, or documentation. For more information, see <a href="https://docs.aws.amazon.com/codeguru/latest/security-ug/end-of-support.html">https://docs.aws.amazon.com/codeguru/latest/security-ug/end-of-support.html</a>.
+    /// 
     ///  </note> 
     /// <para>
     /// This section provides documentation for the Amazon CodeGuru Security API operations.
@@ -47,6 +49,8 @@ namespace Amazon.CodeGuruSecurity
     /// CodeGuru Security improves the overall security of your application code. For more
     /// information about CodeGuru Security, see the <a href="https://docs.aws.amazon.com/codeguru/latest/security-ug/what-is-codeguru-security.html">Amazon
     /// CodeGuru Security User Guide</a>. 
+    /// </para>
+    /// 
     /// </para>
     /// </summary>
     public partial interface IAmazonCodeGuruSecurity : IAmazonService, IDisposable
--- sdk/src/Services/CodeGuruSecurity/Generated/_bcl45/AmazonCodeGuruSecurityClient.cs
+++ sdk/src/Services/CodeGuruSecurity/Generated/_bcl45/AmazonCodeGuruSecurityClient.cs
@@ -41,8 +41,10 @@ namespace Amazon.CodeGuruSecurity
     ///
     /// <note> 
     /// <para>
-    /// Amazon CodeGuru Security is in preview release and is subject to change.
-    /// </para>
+    /// On November 20, 2025, AWS will discontinue support for Amazon CodeGuru Security. After
+    /// November 20, 2025, you will no longer be able to access the /codeguru/security console,
+    /// service resources, or documentation. For more information, see <a href="https://docs.aws.amazon.com/codeguru/latest/security-ug/end-of-support.html">https://docs.aws.amazon.com/codeguru/latest/security-ug/end-of-support.html</a>.
+    /// 
     ///  </note> 
     /// <para>
     /// This section provides documentation for the Amazon CodeGuru Security API operations.
@@ -56,6 +58,8 @@ namespace Amazon.CodeGuruSecurity
     /// CodeGuru Security improves the overall security of your application code. For more
     /// information about CodeGuru Security, see the <a href="https://docs.aws.amazon.com/codeguru/latest/security-ug/what-is-codeguru-security.html">Amazon
     /// CodeGuru Security User Guide</a>. 
+    /// </para>
+    /// 
     /// </para>
     /// </summary>
     public partial class AmazonCodeGuruSecurityClient : AmazonServiceClient, IAmazonCodeGuruSecurity
