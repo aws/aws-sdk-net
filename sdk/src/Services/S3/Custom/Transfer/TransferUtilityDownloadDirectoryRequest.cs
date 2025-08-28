eb2edca49eb5a288665a7e750c7f14efc92e7849
Garrett Beatty <gcbeatty@amazon.com>
2025-08-28 14:09:07 -0400

    make directoryrequest a subclass of BaseDownloadRequest

--- sdk/src/Services/S3/Custom/Transfer/TransferUtilityDownloadDirectoryRequest.cs
+++ sdk/src/Services/S3/Custom/Transfer/TransferUtilityDownloadDirectoryRequest.cs
@@ -36,46 +36,12 @@ namespace Amazon.S3.Transfer
     /// <summary>
     /// Request object for downloading a directory with the TransferUtility.
     /// </summary>
-    public class TransferUtilityDownloadDirectoryRequest
+    public class TransferUtilityDownloadDirectoryRequest : BaseDownloadRequest
     {
-        private string bucketName;
         private string s3Directory;
         private string localDirectory;
         private bool downloadFilesConcurrently = false;
-        private DateTime? modifiedSinceDate;
-        private DateTime? unmodifiedSinceDate;
         private bool disableSlashCorrection = false;
-
-        private ServerSideEncryptionCustomerMethod serverSideCustomerEncryption;
-        private string serverSideEncryptionCustomerProvidedKey;
-        private string serverSideEncryptionCustomerProvidedKeyMD5;
-
-        private RequestPayer requestPayer;
-
-        /// <summary>
-        /// 	Gets or sets the name of the bucket.
-        /// </summary>
-        /// <value>
-        /// 	The name of the bucket.
-        /// </value>
-        public string BucketName
-        {
-            get { return this.bucketName; }
-            set { this.bucketName = value; }
-        }
-
-        /// <summary>
-        /// Gets whether or not the bucket name is set.
-        /// </summary>
-        /// <returns>
-        /// 	A value of <c>true</c> if the bucket name is set.
-        ///    Otherwise, returns <c>false</c>.
-        /// </returns>
-        internal bool IsSetBucketName()
-        {
-            return !System.String.IsNullOrEmpty(this.bucketName);
-        }
-
 
         /// <summary>
         /// 	Gets or sets the local directory where objects from Amazon S3 will be downloaded.  
@@ -132,51 +98,6 @@ namespace Amazon.S3.Transfer
             return !System.String.IsNullOrEmpty(this.s3Directory);
         }
 
-        /// <summary>
-        /// 	Gets or sets the <c>ModifiedSinceDate</c> property.  
-        /// 	Only objects that have been modified since this date will be
-        /// 	downloaded.
-        /// </summary>
-        /// <value>
-        /// 	The <c>ModifiedSinceDate</c> property. 
-        /// </value>
-        public DateTime ModifiedSinceDate
-        {
-            get { return this.modifiedSinceDate.GetValueOrDefault(); }
-            set { this.modifiedSinceDate = value; }
-        }
-
-        /// <summary>
-        /// Checks if ModifiedSinceDate property is set.
-        /// </summary>
-        /// <returns>A value of <c>true</c> if ModifiedSinceDate property is set.
-        /// 	Otherwise, returns <c>false</c>.</returns>
-        internal bool IsSetModifiedSinceDate()
-        {
-            return modifiedSinceDate.HasValue;
-        }
-
-        /// <summary>
-        /// 	Gets or sets the <c>UnmodifiedSinceDate</c> property.  
-        /// 	Only objects that have not been modified since this date will be downloaded.
-        /// </summary>
-        /// <value>
-        /// 	The <c>UnmodifiedSinceDate</c> property.
-        /// </value>
-        public DateTime UnmodifiedSinceDate
-        {
-            get { return this.unmodifiedSinceDate.GetValueOrDefault(); }
-            set { this.unmodifiedSinceDate = value; }
-        }
-
-        /// <summary>
-        /// Checks if UnmodifiedSinceDate property is set.
-        /// </summary>
-        /// <returns>true if UnmodifiedSinceDate property is set.</returns>
-        internal bool IsSetUnmodifiedSinceDate()
-        {
-            return unmodifiedSinceDate.HasValue;
-        }
 
         /// <summary>
         /// Gets or sets the DownloadFilesConcurrently property.
@@ -205,56 +126,6 @@ namespace Amazon.S3.Transfer
             set { this.disableSlashCorrection = value; }
         }
 
-        /// <summary>
-        /// The Server-side encryption algorithm to be used with the customer provided key.
-        /// </summary>
-        public ServerSideEncryptionCustomerMethod ServerSideEncryptionCustomerMethod
-        {
-            get { return this.serverSideCustomerEncryption; }
-            set { this.serverSideCustomerEncryption = value; }
-        }
-
-        /// <summary>
-        /// The base64-encoded encryption key for Amazon S3 to use to decrypt the object
-        /// <para>
-        /// Using the encryption key you provide as part of your request Amazon S3 manages both the encryption, as it writes 
-        /// to disks, and decryption, when you access your objects. Therefore, you don't need to maintain any data encryption code. The only 
-        /// thing you do is manage the encryption keys you provide.
-        /// </para>
-        /// <para>
-        /// When you retrieve an object, you must provide the same encryption key as part of your request. Amazon S3 first verifies 
-        /// the encryption key you provided matches, and then decrypts the object before returning the object data to you.
-        /// </para>
-        /// <para>
-        /// Important: Amazon S3 does not store the encryption key you provide.
-        /// </para>
-        /// </summary>
-        [AWSProperty(Sensitive = true)]
-        public string ServerSideEncryptionCustomerProvidedKey
-        {
-            get { return this.serverSideEncryptionCustomerProvidedKey; }
-            set { this.serverSideEncryptionCustomerProvidedKey = value; }
-        }
-
-        /// <summary>
-        /// The MD5 of the customer encryption key specified in the ServerSideEncryptionCustomerProvidedKey property. The MD5 is
-        /// base 64 encoded. This field is optional, the SDK will calculate the MD5 if this is not set.
-        /// </summary>
-        public string ServerSideEncryptionCustomerProvidedKeyMD5
-        {
-            get { return this.serverSideEncryptionCustomerProvidedKeyMD5; }
-            set { this.serverSideEncryptionCustomerProvidedKeyMD5 = value; }
-        }
-
-        /// <summary>
-        /// Confirms that the requester knows that they will be charged for the request. 
-        /// Bucket owners need not specify this parameter in their requests.
-        /// </summary>
-        public RequestPayer RequestPayer
-        {
-            get { return this.requestPayer; }
-            set { this.requestPayer = value; }
-        }
 
         /// <summary>
         /// The event for DownloadedDirectoryProgressEvent notifications. All
