a37467db7fef3272715c4549f190fc9d30b66239
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2025-08-28 19:43:41 +0000

    Added RDS HTTP Endpoint feature support flag to DescribeOrderableDBInstanceOptions API

--- sdk/src/Services/RDS/Generated/Model/GlobalCluster.cs
+++ sdk/src/Services/RDS/Generated/Model/GlobalCluster.cs
@@ -241,9 +241,10 @@ namespace Amazon.RDS.Model
         /// <summary>
         /// Gets and sets the property GlobalClusterResourceId. 
         /// <para>
-        /// The Amazon Web Services Region-unique, immutable identifier for the global database
-        /// cluster. This identifier is found in Amazon Web Services CloudTrail log entries whenever
-        /// the Amazon Web Services KMS key for the DB cluster is accessed.
+        /// The Amazon Web Services <a href="https://docs.aws.amazon.com/glossary/latest/reference/glos-chap.html?id=docs_gateway#partition">partition</a>-unique,
+        /// immutable identifier for the global database cluster. This identifier is found in
+        /// Amazon Web Services CloudTrail log entries whenever the Amazon Web Services KMS key
+        /// for the DB cluster is accessed.
         /// </para>
         /// </summary>
         public string GlobalClusterResourceId
