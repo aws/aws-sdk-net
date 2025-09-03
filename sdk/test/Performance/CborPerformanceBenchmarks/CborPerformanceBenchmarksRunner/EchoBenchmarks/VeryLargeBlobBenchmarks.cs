1a2ec6d88d4f5316005a6c4687eaf0c416cba99b
Muhammad Othman <muhammmadothman@gmail.com>
2025-09-03 13:26:31 -0400

    test

---
+++ sdk/test/Performance/CborPerformanceBenchmarks/CborPerformanceBenchmarksRunner/EchoBenchmarks/VeryLargeBlobBenchmarks.cs
@@ -0,0 +1,16 @@
+﻿using Amazon.Echo.Model;
+
+namespace CborPerformanceBenchmarksRunner.EchoBenchmarks;
+
+public class VeryLargeBlobBenchmarks : AllTypesBenchmarks
+{
+    public override string TestCase { get; } = "Very large blob";
+
+    public override void GenerateRequest()
+    {
+        request = new EchoOperationRequest
+        {
+            BlobMember = new MemoryStream(GenerateRandomBytes(262144)),
+        };
+    }
+}
---
+++ sdk/test/Performance/CborPerformanceBenchmarks/CborPerformanceBenchmarksRunner/Program.cs
@@ -0,0 +1,69 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Order;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Toolchains.InProcess.Emit;
+using CborPerformanceBenchmarksRunner.CloudWatchBenchmarks;
+using CborPerformanceBenchmarksRunner.EchoBenchmarks;
+using CborPerformanceBenchmarksRunner.SecretsManagerBenchmarks;
+using System.Collections.Immutable;
+using System.Reflection;
+using System.Text;
+
+
+var benchmarkTypes = new Type[]
+        {
+            typeof(AllTypesBenchmarks),
+            typeof(LongListOfStringsBenchmarks),
+            typeof(ComplexObjectBenchmarks),
+            typeof(ListOfComplexObjectBenchmarks),
+            typeof(VeryLargeBlobBenchmarks),
+            //typeof(PutAndGetMetricDataBenchmarks),
+            //typeof(ListMetricsBenchmarks),
+            //typeof(PutAndGetStringBenchmarks),
+            //typeof(PutAndGetBinaryBenchmarks),
+            //typeof(DescribeSecretBenchmarks),
+            //typeof(ListSecretsBenchmarks),
+        };
+
+
+//await Utils.CreateSecretsManagerResources();
+
+var config = ManualConfig.Create(DefaultConfig.Instance)
+    .AddColumn(StatisticColumn.P50)
+    .AddColumn(StatisticColumn.P90)
+    .AddColumn(StatisticColumn.Max)
+    .AddJob(Job.Default
+        .WithIterationCount(Utils.IterationsCount)
+        .WithEnvironmentVariable("ResultsPath", Utils.ResultsPath)
+        .WithEnvironmentVariable("RunStartTimestamp", Utils.RunStartTimestamp.ToString())
+        .WithId("RPCv2CborProtocol")
+        .WithArguments(new[] { new MsBuildArgument(@"-p:UseCbor=""true""") }))
+    .AddJob(Job.Default
+        .WithIterationCount(Utils.IterationsCount)
+        .WithEnvironmentVariable("ResultsPath", Utils.ResultsPath)
+        .WithEnvironmentVariable("RunStartTimestamp", Utils.RunStartTimestamp.ToString())
+        .WithId("OldProtocol")
+        .WithArguments(new[] { new MsBuildArgument(@"-p:UseCbor=""false""") })
+        .AsBaseline());
+
+
+#if DEBUG
+Console.WriteLine("Debug mode......");
+config = ManualConfig.Create(DebugInProcessConfigDry.Instance);
+#endif
+
+
+
+var summaries = BenchmarkSwitcher.FromTypes(benchmarkTypes).Run(args, config);
+
+
+Utils.StoreBenchmarkSummaries(summaries);
+
+//await Utils.CleanupSecretsManagerResources();
+
+
