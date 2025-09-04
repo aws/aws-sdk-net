5bf6ad05d3eed25663afbe6f3526b4dff461bd0f
Muhammad Othman <muhammmadothman@gmail.com>
2025-09-04 13:19:34 -0400

    Add CBOR performance benchmark runner with intial CloudWatch ListMetricsBenchmarks

---
+++ sdk/test/Performance/CborPerformanceBenchmarks/CborPerformanceBenchmarksRunner/Program.cs
@@ -0,0 +1,40 @@
+﻿using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using CborPerformanceBenchmarksRunner.CloudWatchBenchmarks;
+
+
+var benchmarkTypes = new Type[]
+        {
+            typeof(ListMetricsBenchmarks)
+        };
+
+var config = ManualConfig.Create(DefaultConfig.Instance)
+    .AddColumn(StatisticColumn.P50)
+    .AddColumn(StatisticColumn.P90)
+    .AddColumn(StatisticColumn.Max)
+    .AddJob(Job.Default
+        .WithIterationCount(Utils.IterationsCount)
+        .WithEnvironmentVariable("ResultsPath", Utils.ResultsPath)
+        .WithEnvironmentVariable("RunStartTimestamp", Utils.RunStartTimestamp.ToString())
+        .WithId("RPCv2CborProtocol")
+        .WithArguments(new[] { new MsBuildArgument(@"-p:UseCbor=""true""") }))
+    .AddJob(Job.Default
+        .WithIterationCount(Utils.IterationsCount)
+        .WithEnvironmentVariable("ResultsPath", Utils.ResultsPath)
+        .WithEnvironmentVariable("RunStartTimestamp", Utils.RunStartTimestamp.ToString())
+        .WithId("OldProtocol")
+        .WithArguments(new[] { new MsBuildArgument(@"-p:UseCbor=""false""") })
+        .AsBaseline());
+
+
+#if DEBUG
+Console.WriteLine("Debug mode......");
+config = ManualConfig.Create(DebugInProcessConfigDry.Instance);
+#endif
+
+var summaries = BenchmarkSwitcher.FromTypes(benchmarkTypes).Run(args, config);
+
+
+Utils.StoreBenchmarkSummaries(summaries);
