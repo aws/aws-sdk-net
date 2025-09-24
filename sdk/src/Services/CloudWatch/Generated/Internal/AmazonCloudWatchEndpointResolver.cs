d4108ffdb9c3937f56b2aaa1ddec6d9d7fc56fa1
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2025-09-24 18:22:28 +0000

    Fix default dualstack FIPS endpoints in AWS GovCloud(US) regions

--- sdk/src/Services/CloudWatch/Generated/Internal/AmazonCloudWatchEndpointResolver.cs
+++ sdk/src/Services/CloudWatch/Generated/Internal/AmazonCloudWatchEndpointResolver.cs
@@ -50,10 +50,10 @@ namespace Amazon.CloudWatch.Internal
         {
             var config = (AmazonCloudWatchConfig)requestContext.ClientConfig;
             var result = new CloudWatchEndpointParameters();
-            result.Region = config.RegionEndpoint?.SystemName;
             result.UseDualStack = config.UseDualstackEndpoint;
             result.UseFIPS = config.UseFIPSEndpoint;
             result.Endpoint = config.ServiceURL;
+            result.Region = config.RegionEndpoint?.SystemName;
 
 
             // The region needs to be determined from the ServiceURL if not set.
