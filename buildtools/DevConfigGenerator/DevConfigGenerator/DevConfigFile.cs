1af502e1e8031ede793073fed2f1c7f2d9a3537e
Norm Johanson <normj@amazon.com>
2025-09-25 09:37:35 -0700

    Update devconfig generator for extension support. (#4019)

--- buildtools/DevConfigGenerator/DevConfigGenerator/DevConfigFile.cs
+++ buildtools/DevConfigGenerator/DevConfigGenerator/DevConfigFile.cs
@@ -7,6 +7,8 @@ public class DevConfigFile
     public CoreChange? Core { get; set; }
 
     public List<Service> Services { get; } = new List<Service>();
+
+    public List<Extension> Extensions { get; } = new List<Extension>();
 }
 
 public class CoreChange
@@ -44,6 +46,32 @@ public class Service
 
     public string Type { get; set; } = "patch";
 
+
+    public string FormattedNameColumn()
+    {
+        var sb = new StringBuilder();
+        sb.AppendLine($"AWSSDK.{Name}");
+        return sb.ToString();
+    }
+
+    public string FormattedMessagesColumn()
+    {
+        var sb = new StringBuilder();
+        foreach (var message in Messages)
+        {
+            sb.AppendLine(message.ToString());
+        }
+        return sb.ToString();
+    }
+}
+
+public class Extension
+{
+    public required string Name { get; init; }
+
+    public List<string> Messages { get; } = new List<string>();
+
+    public string Type { get; set; } = "patch";
 
     public string FormattedNameColumn()
     {
