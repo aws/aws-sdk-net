995f175649587adffacc179eb87543a9dcfd242c
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2025-10-17 18:58:24 +0000

    Releasing minor endpoint updates.

--- sdk/src/Services/SimpleWorkflow/Generated/Internal/AmazonSimpleWorkflowEndpointResolver.cs
+++ sdk/src/Services/SimpleWorkflow/Generated/Internal/AmazonSimpleWorkflowEndpointResolver.cs
@@ -50,10 +50,10 @@ namespace Amazon.SimpleWorkflow.Internal
         {
             var config = (AmazonSimpleWorkflowConfig)requestContext.ClientConfig;
             var result = new SimpleWorkflowEndpointParameters();
-            result.Region = config.RegionEndpoint?.SystemName;
             result.UseDualStack = config.UseDualstackEndpoint;
             result.UseFIPS = config.UseFIPSEndpoint;
             result.Endpoint = config.ServiceURL;
+            result.Region = config.RegionEndpoint?.SystemName;
 
 
             // The region needs to be determined from the ServiceURL if not set.
