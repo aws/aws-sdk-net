a5c83705e7914a6a10fbd5b18f2789858f59e2ef
Peter Song <petesong@amazon.com>
2025-11-06 15:11:43 -0800

    Generate GetBucketAnalyticsConfiguration
    stack-info: PR: https://github.com/aws/aws-sdk-net/pull/4117, branch: peterrsongg/petesong/phase-3-pr-2/3

--- sdk/src/Services/S3/Custom/Model/Internal/MarshallTransformations/AnalyticsS3BucketDestinationUnmarshaller.cs
+++ sdk/src/Services/S3/Custom/Model/Internal/MarshallTransformations/AnalyticsS3BucketDestinationUnmarshaller.cs
@@ -1,98 +1,26 @@
-﻿/*
- * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- * 
- *  http://aws.amazon.com/apache2.0
- * 
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
-using System.Collections.Generic;
-
-using Amazon.S3.Model;
+﻿using Amazon.Runtime;
+using Amazon.Runtime.Internal;
 using Amazon.Runtime.Internal.Transform;
+using Amazon.Runtime.Internal.Util;
+using Amazon.S3.Model;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Xml.Serialization;
 
 namespace Amazon.S3.Model.Internal.MarshallTransformations
 {
     /// <summary>
-    ///   AnalyticsS3BucketDestination Unmarshaller
+    /// Custom unmarshaller for AnalyticsS3BucketDestination Object
     /// </summary>
-    public class AnalyticsS3BucketDestinationUnmarshaller : IXmlUnmarshaller<AnalyticsS3BucketDestination, XmlUnmarshallerContext>
+    public partial class AnalyticsS3BucketDestinationUnmarshaller : IXmlUnmarshaller<AnalyticsS3BucketDestination, XmlUnmarshallerContext>
     {
-        /// <summary>
-        /// Unmarshaller the response from the service to the response class.
-        /// </summary>
-        /// <param name="context"></param>
-        /// <returns></returns>
-        public AnalyticsS3BucketDestination Unmarshall(XmlUnmarshallerContext context)
+        void FormatCustomUnmarshall(XmlUnmarshallerContext context, AnalyticsS3BucketDestination unmarshalledObject)
         {
-            AnalyticsS3BucketDestination AnalyticsS3BucketDestination = new AnalyticsS3BucketDestination();
-            int originalDepth = context.CurrentDepth;
-            int targetDepth = originalDepth + 1;
-
-            if (context.IsStartOfDocument)
-                targetDepth += 2;
-
-            while (context.Read())
-            {
-                if (context.IsStartElement || context.IsAttribute)
-                {
-                    if (context.TestExpression("Format", targetDepth))
-                    {
-                        AnalyticsS3BucketDestination.Format = AnalyticsS3ExportFileFormat.FindValue(StringUnmarshaller.Instance.Unmarshall(context));
-
-                        continue;
-                    }
-                    if (context.TestExpression("BucketAccountId", targetDepth))
-                    {
-                        AnalyticsS3BucketDestination.BucketAccountId = StringUnmarshaller.Instance.Unmarshall(context);
-
-                        continue;
-                    }
-                    if (context.TestExpression("Bucket", targetDepth))
-                    {
-                        AnalyticsS3BucketDestination.BucketName = StringUnmarshaller.Instance.Unmarshall(context);
-
-                        continue;
-                    }
-                    if (context.TestExpression("Prefix", targetDepth))
-                    {
-                        AnalyticsS3BucketDestination.Prefix = StringUnmarshaller.Instance.Unmarshall(context);
-
-                        continue;
-                    }
-                }
-                else if (context.IsEndElement && context.CurrentDepth < originalDepth)
-                {
-                    return AnalyticsS3BucketDestination;
-                }
-            }
-
-
-
-            return AnalyticsS3BucketDestination;
-        }
-
-        private static AnalyticsS3BucketDestinationUnmarshaller _instance;
-
-        /// <summary>
-        /// Singleton for the unmarshaller
-        /// </summary>
-        public static AnalyticsS3BucketDestinationUnmarshaller Instance
-        {
-            get
-            {
-                if (_instance == null)
-                {
-                    _instance = new AnalyticsS3BucketDestinationUnmarshaller();
-                }
-                return _instance;
-            }
+            unmarshalledObject.Format = AnalyticsS3ExportFileFormat.FindValue(StringUnmarshaller.Instance.Unmarshall(context));
         }
     }
 }
--- sdk/src/Services/S3/Generated/Model/GetBucketAnalyticsConfigurationRequest.cs
+++ sdk/src/Services/S3/Generated/Model/GetBucketAnalyticsConfigurationRequest.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License").
@@ -12,22 +12,28 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
+
+/*
+ * Do not modify this file. This file is generated from the s3-2006-03-01.normal.json service model.
+ */
 using System;
 using System.Collections.Generic;
 using System.Xml.Serialization;
 using System.Text;
 using System.IO;
+using System.Net;
 
 using Amazon.Runtime;
 using Amazon.Runtime.Internal;
 
+#pragma warning disable CS0612,CS0618,CS1570
 namespace Amazon.S3.Model
 {
     /// <summary>
     /// Container for the parameters to the GetBucketAnalyticsConfiguration operation.
     /// <note> 
     /// <para>
-    /// This operation is not supported by directory buckets.
+    /// This operation is not supported for directory buckets.
     /// </para>
     ///  </note> 
     /// <para>
@@ -36,7 +42,7 @@ namespace Amazon.S3.Model
     /// </para>
     ///  
     /// <para>
-    /// To use this operation, you must have permissions to perform the <code>s3:GetAnalyticsConfiguration</code>
+    /// To use this operation, you must have permissions to perform the <c>s3:GetAnalyticsConfiguration</c>
     /// action. The bucket owner has this permission by default. The bucket owner can grant
     /// this permission to others. For more information about permissions, see <a href="https://docs.aws.amazon.com/AmazonS3/latest/userguide/using-with-s3-actions.html#using-with-s3-actions-related-to-bucket-subresources">
     /// Permissions Related to Bucket Subresource Operations</a> and <a href="https://docs.aws.amazon.com/AmazonS3/latest/userguide/s3-access-control.html">Managing
@@ -50,7 +56,7 @@ namespace Amazon.S3.Model
     /// </para>
     ///  
     /// <para>
-    /// The following operations are related to <code>GetBucketAnalyticsConfiguration</code>:
+    /// The following operations are related to <c>GetBucketAnalyticsConfiguration</c>:
     /// </para>
     ///  <ul> <li> 
     /// <para>
@@ -67,42 +73,56 @@ namespace Amazon.S3.Model
     ///  <a href="https://docs.aws.amazon.com/AmazonS3/latest/API/API_PutBucketAnalyticsConfiguration.html">PutBucketAnalyticsConfiguration</a>
     /// 
     /// </para>
-    ///  </li> </ul>
+    ///  </li> </ul> <important> 
+    /// <para>
+    /// You must URL encode any signed header values that contain spaces. For example, if
+    /// your header value is <c>my file.txt</c>, containing two spaces after <c>my</c>, you
+    /// must URL encode this value to <c>my%20%20file.txt</c>.
+    /// </para>
+    ///  </important>
     /// </summary>
     public partial class GetBucketAnalyticsConfigurationRequest : AmazonWebServiceRequest
     {
-        private string bucketName;
-        private string analyticsId;
-        private string expectedBucketOwner;
+        private string _analyticsId;
+        private string _bucketName;
+        private string _expectedBucketOwner;
 
         /// <summary>
-        /// The name of the bucket from which an analytics configuration is retrieved.
+        /// Gets and sets the property AnalyticsId. 
+        /// <para>
+        /// The ID that identifies the analytics configuration.
+        /// </para>
         /// </summary>
+        [AWSProperty(Required=true)]
+        public string AnalyticsId
+        {
+            get { return this._analyticsId; }
+            set { this._analyticsId = value; }
+        }
+
+        // Check to see if AnalyticsId property is set
+        internal bool IsSetAnalyticsId()
+        {
+            return this._analyticsId != null;
+        }
+
+        /// <summary>
+        /// Gets and sets the property BucketName. 
+        /// <para>
+        /// The name of the bucket from which an analytics configuration is retrieved.
+        /// </para>
+        /// </summary>
+        [AWSProperty(Required=true)]
         public string BucketName
         {
-            get { return this.bucketName; }
-            set { this.bucketName = value; }
+            get { return this._bucketName; }
+            set { this._bucketName = value; }
         }
 
         // Check to see if BucketName property is set
         internal bool IsSetBucketName()
         {
-            return !(string.IsNullOrEmpty(this.bucketName));
-        }
-
-        /// <summary>
-        /// The identifier used to represent an analytics configuration.
-        /// </summary>
-        public string AnalyticsId
-        {
-            get { return this.analyticsId; }
-            set { this.analyticsId = value; }
-        }
-
-        // Check to see if InventoryId property is set
-        internal bool IsSetAnalyticsId()
-        {
-            return !(string.IsNullOrEmpty(this.analyticsId));
+            return this._bucketName != null;
         }
 
         /// <summary>
@@ -110,22 +130,20 @@ namespace Amazon.S3.Model
         /// <para>
         /// The account ID of the expected bucket owner. If the account ID that you provide does
         /// not match the actual owner of the bucket, the request fails with the HTTP status code
-        /// <code>403 Forbidden</code> (access denied).
+        /// <c>403 Forbidden</c> (access denied).
         /// </para>
         /// </summary>
         public string ExpectedBucketOwner
         {
-            get { return this.expectedBucketOwner; }
-            set { this.expectedBucketOwner = value; }
+            get { return this._expectedBucketOwner; }
+            set { this._expectedBucketOwner = value; }
         }
 
-        /// <summary>
-        /// Checks to see if ExpectedBucketOwner is set.
-        /// </summary>
-        /// <returns>true, if ExpectedBucketOwner property is set.</returns>
+        // Check to see if ExpectedBucketOwner property is set
         internal bool IsSetExpectedBucketOwner()
         {
-            return !String.IsNullOrEmpty(this.expectedBucketOwner);
+            return this._expectedBucketOwner != null;
         }
+
     }
 }
