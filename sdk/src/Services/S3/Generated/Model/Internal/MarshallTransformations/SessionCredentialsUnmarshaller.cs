51859468873c91b1037c7fef5f3e4d2751634d5a
Peter Song <petesong@amazon.com>
2025-11-06 13:32:21 -0800

    generate create session

--- sdk/src/Services/S3/Generated/Model/Internal/MarshallTransformations/SessionCredentialsUnmarshaller.cs
+++ sdk/src/Services/S3/Generated/Model/Internal/MarshallTransformations/SessionCredentialsUnmarshaller.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License").
@@ -12,17 +12,31 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
+
+/*
+ * Do not modify this file. This file is generated from the s3-2006-03-01.normal.json service model.
+ */
+using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Xml.Serialization;
 
 using Amazon.S3.Model;
+using Amazon.Runtime;
+using Amazon.Runtime.Internal;
 using Amazon.Runtime.Internal.Transform;
+using Amazon.Runtime.Internal.Util;
 
+#pragma warning disable CS0612,CS0618
 namespace Amazon.S3.Model.Internal.MarshallTransformations
 {
     /// <summary>
     /// Response Unmarshaller for SessionCredentials Object
     /// </summary>  
-    public class SessionCredentialsUnmarshaller : IXmlUnmarshaller<SessionCredentials, XmlUnmarshallerContext>
+    public partial class SessionCredentialsUnmarshaller : IXmlUnmarshaller<SessionCredentials, XmlUnmarshallerContext>
     {
         /// <summary>
         /// Unmarshaller the response from the service to the response class.
@@ -34,10 +48,10 @@ namespace Amazon.S3.Model.Internal.MarshallTransformations
             SessionCredentials unmarshalledObject = new SessionCredentials();
             int originalDepth = context.CurrentDepth;
             int targetDepth = originalDepth + 1;
-
-            if (context.IsStartOfDocument)
-                targetDepth += 2;
-
+            
+            if (context.IsStartOfDocument) 
+               targetDepth += 2;
+            
             while (context.Read())
             {
                 if (context.IsStartElement || context.IsAttribute)
@@ -50,7 +64,7 @@ namespace Amazon.S3.Model.Internal.MarshallTransformations
                     }
                     if (context.TestExpression("Expiration", targetDepth))
                     {
-                        var unmarshaller = DateTimeUnmarshaller.Instance;
+                        var unmarshaller = NullableDateTimeUnmarshaller.Instance;
                         unmarshalledObject.Expiration = unmarshaller.Unmarshall(context);
                         continue;
                     }
@@ -66,16 +80,20 @@ namespace Amazon.S3.Model.Internal.MarshallTransformations
                         unmarshalledObject.SessionToken = unmarshaller.Unmarshall(context);
                         continue;
                     }
+
+                    XmlStructureUnmarshallCustomization(context, unmarshalledObject, targetDepth);
                 }
                 else if (context.IsEndElement && context.CurrentDepth < originalDepth)
                 {
                     return unmarshalledObject;
                 }
-            }
+            }          
             return unmarshalledObject;
         }
 
-        private static SessionCredentialsUnmarshaller _instance = new SessionCredentialsUnmarshaller();
+        partial void XmlStructureUnmarshallCustomization(XmlUnmarshallerContext context, SessionCredentials unmarshalledObject, int targetDepth);
+
+        private static SessionCredentialsUnmarshaller _instance = new SessionCredentialsUnmarshaller();        
 
         /// <summary>
         /// Gets the singleton.
@@ -84,10 +102,6 @@ namespace Amazon.S3.Model.Internal.MarshallTransformations
         {
             get
             {
-                if (_instance == null)
-                {
-                    _instance = new SessionCredentialsUnmarshaller();
-                }
                 return _instance;
             }
         }
