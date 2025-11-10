8142101053fac4f58645c8d084003e3cb3106183
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2025-11-10 19:33:18 +0000

    AWS Backup supports backups of Amazon EKS clusters, including Kubernetes cluster state and persistent storage attached to the EKS cluster via a persistent volume claim (EBS volumes, EFS file systems, and S3 buckets).

--- sdk/src/Services/Backup/Generated/Model/ListRestoreJobsRequest.cs
+++ sdk/src/Services/Backup/Generated/Model/ListRestoreJobsRequest.cs
@@ -41,6 +41,7 @@ namespace Amazon.Backup.Model
         private DateTime? _byCompleteBefore;
         private DateTime? _byCreatedAfter;
         private DateTime? _byCreatedBefore;
+        private string _byParentJobId;
         private string _byResourceType;
         private string _byRestoreTestingPlanArn;
         private RestoreJobStatus _byStatus;
@@ -138,6 +139,24 @@ namespace Amazon.Backup.Model
         internal bool IsSetByCreatedBefore()
         {
             return this._byCreatedBefore.HasValue; 
+        }
+
+        /// <summary>
+        /// Gets and sets the property ByParentJobId. 
+        /// <para>
+        /// This is a filter to list child (nested) restore jobs based on parent restore job ID.
+        /// </para>
+        /// </summary>
+        public string ByParentJobId
+        {
+            get { return this._byParentJobId; }
+            set { this._byParentJobId = value; }
+        }
+
+        // Check to see if ByParentJobId property is set
+        internal bool IsSetByParentJobId()
+        {
+            return this._byParentJobId != null;
         }
 
         /// <summary>
