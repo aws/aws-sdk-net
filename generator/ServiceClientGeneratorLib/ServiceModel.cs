2b579a06608bbe4fd481d619c559511a89cc584f
Peter Song <petesong@amazon.com>
2025-11-11 11:25:29 -0800

    generate deletebucketmetricsconfiguration (#4104)
    * Generate PutBucketMetricsConfiguration and generate check to see if required query string params are set. Remove visitor pattern and move custom logic for putlifecycleconfiguration marshaller to custom partial method
    
    * Generate ListBucketMetricsConfiguration
    
    * generate getbucketmetricsconfiguration
    
    * fix failing unit test for abortmultipartupload
    
    * fix failing unit test for abortmultipartupload
    
    * generate deletebucketmetricsconfiguration
    
    * update stringIsNullEmpty for string isSet methods
    
    * fix IsSet for string properties

--- generator/ServiceClientGeneratorLib/ServiceModel.cs
+++ generator/ServiceClientGeneratorLib/ServiceModel.cs
@@ -566,7 +566,7 @@ namespace ServiceClientGenerator
                         //new Operation(this, "PutBucketIntelligentTieringConfiguration", DocumentRoot[OperationsKey]["PutBucketIntelligentTieringConfiguration"]),
                         //new Operation(this, "PutBucketInventoryConfiguration", DocumentRoot[OperationsKey]["PutBucketInventoryConfiguration"]),
                         new Operation(this, "PutBucketMetricsConfiguration", DocumentRoot[OperationsKey]["PutBucketMetricsConfiguration"]),
-                        // new Operation(this, "DeleteBucketMetricsConfiguration", DocumentRoot[OperationsKey]["DeleteBucketMetricsConfiguration"]),
+                        new Operation(this, "DeleteBucketMetricsConfiguration", DocumentRoot[OperationsKey]["DeleteBucketMetricsConfiguration"]),
                         //new Operation(this, "DeleteBucketAnalyticsConfiguration", DocumentRoot[OperationsKey]["DeleteBucketAnalyticsConfiguration"]),
                         //new Operation(this, "DeleteBucketIntelligentTieringConfiguration", DocumentRoot[OperationsKey]["DeleteBucketIntelligentTieringConfiguration"]),
                         //new Operation(this, "DeleteBucketInventoryConfiguration", DocumentRoot[OperationsKey]["DeleteBucketInventoryConfiguration"]),
--- sdk/src/Services/S3/Generated/Model/DeleteBucketMetricsConfigurationResponse.cs
+++ sdk/src/Services/S3/Generated/Model/DeleteBucketMetricsConfigurationResponse.cs
@@ -12,20 +12,28 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
+
+/*
+ * Do not modify this file. This file is generated from the s3-2006-03-01.normal.json service model.
+ */
 using System;
 using System.Collections.Generic;
 using System.Xml.Serialization;
 using System.Text;
+using System.IO;
+using System.Net;
 
 using Amazon.Runtime;
+using Amazon.Runtime.Internal;
 
+#pragma warning disable CS0612,CS0618,CS1570
 namespace Amazon.S3.Model
 {
     /// <summary>
-    /// Returns information about the DeleteBucketMetricsConfiguration response metadata.
-    /// The DeleteBucketMetricsConfiguration operation has a void result type.
+    /// This is the response object from the DeleteBucketMetricsConfiguration operation.
     /// </summary>
     public partial class DeleteBucketMetricsConfigurationResponse : AmazonWebServiceResponse
     {
+
     }
 }
