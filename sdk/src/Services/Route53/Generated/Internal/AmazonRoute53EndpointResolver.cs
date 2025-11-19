b2761bce066532889502ef0c0e3231782f0fb057
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2025-11-19 22:05:45 +0000

    Add dual-stack endpoint support for Route53

--- sdk/src/Services/Route53/Generated/Internal/AmazonRoute53EndpointResolver.cs
+++ sdk/src/Services/Route53/Generated/Internal/AmazonRoute53EndpointResolver.cs
@@ -50,10 +50,10 @@ namespace Amazon.Route53.Internal
         {
             var config = (AmazonRoute53Config)requestContext.ClientConfig;
             var result = new Route53EndpointParameters();
-            result.Region = config.RegionEndpoint?.SystemName;
             result.UseDualStack = config.UseDualstackEndpoint;
             result.UseFIPS = config.UseFIPSEndpoint;
             result.Endpoint = config.ServiceURL;
+            result.Region = config.RegionEndpoint?.SystemName;
 
 
             // The region needs to be determined from the ServiceURL if not set.
