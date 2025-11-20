c08cdea813359ae8de4a0ce80ff8a057e72d14ee
Peter Song <petesong@amazon.com>
2025-11-19 23:51:31 -0800

    add shapes to S3NeedsCustomUpdate and add devconfig

--- generator/ServiceClientGeneratorLib/GeneratorDriver.cs
+++ generator/ServiceClientGeneratorLib/GeneratorDriver.cs
@@ -1719,8 +1719,13 @@ namespace ServiceClientGenerator
             // a dictionary of shape name to number of members that shape has
             var customUpdateShapes = new Dictionary<string, int>
             {
-                {"LifecycleFilter", 5 },
-                {"MetricsFilter", 4 }
+                { "LifecycleFilter", 5 },
+                { "MetricsFilter", 4 },
+                { "AnalyticsFilter", 3 },
+                { "IntelligentTieringFilter", 3 },
+                { "MetricsAndOperator", 3 },
+                { "AnalyticsAndOperator", 2 },
+                { "IntelligentTieringAndOperator", 2}
             };
             if (customUpdateShapes.TryGetValue(shape.Name, out int membersCount))
             {
