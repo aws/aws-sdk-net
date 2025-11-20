61fb4f09af253b0b84a3d7cc0429098c0b47c812
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2025-11-20 22:17:48 +0000

    Commit version number update changes

--- sdk/src/Services/DatabaseMigrationService/Generated/AmazonDatabaseMigrationServiceConfig.cs
+++ sdk/src/Services/DatabaseMigrationService/Generated/AmazonDatabaseMigrationServiceConfig.cs
@@ -33,7 +33,7 @@ namespace Amazon.DatabaseMigrationService
     public partial class AmazonDatabaseMigrationServiceConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("Database Migration Service", "4.0.6.1");
+            InternalSDKUtils.BuildUserAgentString("Database Migration Service", "4.0.7.0");
 
         private static readonly AmazonDatabaseMigrationServiceEndpointResolver EndpointResolver =
             new AmazonDatabaseMigrationServiceEndpointResolver();
--- sdk/src/Services/Imagebuilder/Generated/AmazonImagebuilderConfig.cs
+++ sdk/src/Services/Imagebuilder/Generated/AmazonImagebuilderConfig.cs
@@ -33,7 +33,7 @@ namespace Amazon.Imagebuilder
     public partial class AmazonImagebuilderConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("imagebuilder", "4.0.5.1");
+            InternalSDKUtils.BuildUserAgentString("imagebuilder", "4.0.6.0");
 
         private static readonly AmazonImagebuilderEndpointResolver EndpointResolver =
             new AmazonImagebuilderEndpointResolver();
