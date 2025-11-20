09fd480345108c0a97f38e4d1c267c824d5a6db8
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2025-11-20 21:15:19 +0000

    Added ServiceIntegrations as a request parameter for CreateLakeFormationIdentityCenterConfigurationRequest and UpdateLakeFormationIdentityCenterConfigurationRequest and response parameter for DescribeLakeFormationIdentityCenterConfigurationResponse

--- sdk/src/Services/LakeFormation/Generated/ServiceEnumerations.cs
+++ sdk/src/Services/LakeFormation/Generated/ServiceEnumerations.cs
@@ -723,6 +723,56 @@ namespace Amazon.LakeFormation
 
 
     /// <summary>
+    /// Constants used for properties of type ServiceAuthorization.
+    /// </summary>
+    public class ServiceAuthorization : ConstantClass
+    {
+
+        /// <summary>
+        /// Constant DISABLED for ServiceAuthorization
+        /// </summary>
+        public static readonly ServiceAuthorization DISABLED = new ServiceAuthorization("DISABLED");
+        /// <summary>
+        /// Constant ENABLED for ServiceAuthorization
+        /// </summary>
+        public static readonly ServiceAuthorization ENABLED = new ServiceAuthorization("ENABLED");
+
+        /// <summary>
+        /// This constant constructor does not need to be called if the constant
+        /// you are attempting to use is already defined as a static instance of 
+        /// this class.
+        /// This constructor should be used to construct constants that are not
+        /// defined as statics, for instance if attempting to use a feature that is
+        /// newer than the current version of the SDK.
+        /// </summary>
+        public ServiceAuthorization(string value)
+            : base(value)
+        {
+        }
+
+        /// <summary>
+        /// Finds the constant for the unique value.
+        /// </summary>
+        /// <param name="value">The unique value for the constant</param>
+        /// <returns>The constant for the unique value</returns>
+        public static ServiceAuthorization FindValue(string value)
+        {
+            return FindValue<ServiceAuthorization>(value);
+        }
+
+        /// <summary>
+        /// Utility method to convert strings to the constant class.
+        /// </summary>
+        /// <param name="value">The string value to convert to the constant class.</param>
+        /// <returns></returns>
+        public static implicit operator ServiceAuthorization(string value)
+        {
+            return FindValue(value);
+        }
+    }
+
+
+    /// <summary>
     /// Constants used for properties of type TransactionStatus.
     /// </summary>
     public class TransactionStatus : ConstantClass
