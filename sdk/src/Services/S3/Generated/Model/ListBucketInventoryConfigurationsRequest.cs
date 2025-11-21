524c8be8d3c58ff2b4b112afa96a8e1a283d6b4f
Peter Song <petesong@amazon.com>
2025-11-21 14:38:07 -0800

    Generate ListBucketInventoryConfigurations
    stack-info: PR: https://github.com/aws/aws-sdk-net/pull/4156, branch: peterrsongg/petesong/phase-3-pr4-3/3

--- sdk/src/Services/S3/Generated/Model/ListBucketInventoryConfigurationsRequest.cs
+++ sdk/src/Services/S3/Generated/Model/ListBucketInventoryConfigurationsRequest.cs
@@ -12,39 +12,47 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
+
+/*
+ * Do not modify this file. This file is generated from the s3-2006-03-01.normal.json service model.
+ */
 using System;
 using System.Collections.Generic;
 using System.Xml.Serialization;
 using System.Text;
+using System.IO;
+using System.Net;
 
 using Amazon.Runtime;
+using Amazon.Runtime.Internal;
 
+#pragma warning disable CS0612,CS0618,CS1570
 namespace Amazon.S3.Model
 {
     /// <summary>
     /// Container for the parameters to the ListBucketInventoryConfigurations operation.
     /// <note> 
     /// <para>
-    /// This operation is not supported by directory buckets.
+    /// This operation is not supported for directory buckets.
     /// </para>
     ///  </note> 
     /// <para>
-    /// Returns a list of inventory configurations for the bucket. You can have up to 1,000
-    /// analytics configurations per bucket.
+    /// Returns a list of S3 Inventory configurations for the bucket. You can have up to 1,000
+    /// inventory configurations per bucket.
     /// </para>
     ///  
     /// <para>
     /// This action supports list pagination and does not return more than 100 configurations
-    /// at a time. Always check the <code>IsTruncated</code> element in the response. If there
-    /// are no more configurations to list, <code>IsTruncated</code> is set to false. If there
-    /// are more configurations to list, <code>IsTruncated</code> is set to true, and there
-    /// is a value in <code>NextContinuationToken</code>. You use the <code>NextContinuationToken</code>
-    /// value to continue the pagination of the list by passing the value in continuation-token
-    /// in the request to <code>GET</code> the next page.
+    /// at a time. Always check the <c>IsTruncated</c> element in the response. If there are
+    /// no more configurations to list, <c>IsTruncated</c> is set to false. If there are more
+    /// configurations to list, <c>IsTruncated</c> is set to true, and there is a value in
+    /// <c>NextContinuationToken</c>. You use the <c>NextContinuationToken</c> value to continue
+    /// the pagination of the list by passing the value in continuation-token in the request
+    /// to <c>GET</c> the next page.
     /// </para>
     ///  
     /// <para>
-    ///  To use this operation, you must have permissions to perform the <code>s3:GetInventoryConfiguration</code>
+    ///  To use this operation, you must have permissions to perform the <c>s3:GetInventoryConfiguration</c>
     /// action. The bucket owner has this permission by default. The bucket owner can grant
     /// this permission to others. For more information about permissions, see <a href="https://docs.aws.amazon.com/AmazonS3/latest/userguide/using-with-s3-actions.html#using-with-s3-actions-related-to-bucket-subresources">Permissions
     /// Related to Bucket Subresource Operations</a> and <a href="https://docs.aws.amazon.com/AmazonS3/latest/userguide/s3-access-control.html">Managing
@@ -57,7 +65,7 @@ namespace Amazon.S3.Model
     /// </para>
     ///  
     /// <para>
-    /// The following operations are related to <code>ListBucketInventoryConfigurations</code>:
+    /// The following operations are related to <c>ListBucketInventoryConfigurations</c>:
     /// </para>
     ///  <ul> <li> 
     /// <para>
@@ -74,45 +82,57 @@ namespace Amazon.S3.Model
     ///  <a href="https://docs.aws.amazon.com/AmazonS3/latest/API/API_PutBucketInventoryConfiguration.html">PutBucketInventoryConfiguration</a>
     /// 
     /// </para>
-    ///  </li> </ul>
+    ///  </li> </ul> <important> 
+    /// <para>
+    /// You must URL encode any signed header values that contain spaces. For example, if
+    /// your header value is <c>my file.txt</c>, containing two spaces after <c>my</c>, you
+    /// must URL encode this value to <c>my%20%20file.txt</c>.
+    /// </para>
+    ///  </important>
     /// </summary>
     public partial class ListBucketInventoryConfigurationsRequest : AmazonWebServiceRequest
     {
-        private string bucketName;
-        private string token;
-        private string expectedBucketOwner;
+        private string _bucketName;
+        private string _continuationToken;
+        private string _expectedBucketOwner;
 
         /// <summary>
+        /// Gets and sets the property BucketName. 
+        /// <para>
         /// The name of the bucket containing the inventory configurations to retrieve.
+        /// </para>
         /// </summary>
+        [AWSProperty(Required=true)]
         public string BucketName
         {
-            get { return this.bucketName; }
-            set { this.bucketName = value; }
+            get { return this._bucketName; }
+            set { this._bucketName = value; }
         }
 
         // Check to see if BucketName property is set
         internal bool IsSetBucketName()
         {
-            return !(string.IsNullOrEmpty(this.bucketName));
+            return this._bucketName != null;
         }
 
         /// <summary>
-        /// The marker used to continue an inventory configuration listing that has been truncated. 
-        /// Use the <code>NextContinuationToken</code> from a previously truncated list response
-        /// to continue the listing. The continuation token is an opaque value that Amazon S3
-        /// understands.
+        /// Gets and sets the property ContinuationToken. 
+        /// <para>
+        /// The marker used to continue an inventory configuration listing that has been truncated.
+        /// Use the <c>NextContinuationToken</c> from a previously truncated list response to
+        /// continue the listing. The continuation token is an opaque value that Amazon S3 understands.
+        /// </para>
         /// </summary>
         public string ContinuationToken
         {
-            get { return this.token; }
-            set { this.token = value; }
+            get { return this._continuationToken; }
+            set { this._continuationToken = value; }
         }
 
-        // Check to see if Token property is set
+        // Check to see if ContinuationToken property is set
         internal bool IsSetContinuationToken()
         {
-            return !(string.IsNullOrEmpty(this.token));
+            return !String.IsNullOrEmpty(this._continuationToken);
         }
 
         /// <summary>
@@ -120,22 +140,20 @@ namespace Amazon.S3.Model
         /// <para>
         /// The account ID of the expected bucket owner. If the account ID that you provide does
         /// not match the actual owner of the bucket, the request fails with the HTTP status code
-        /// <code>403 Forbidden</code> (access denied).
+        /// <c>403 Forbidden</c> (access denied).
         /// </para>
         /// </summary>
         public string ExpectedBucketOwner
         {
-            get { return this.expectedBucketOwner; }
-            set { this.expectedBucketOwner = value; }
+            get { return this._expectedBucketOwner; }
+            set { this._expectedBucketOwner = value; }
         }
 
-        /// <summary>
-        /// Checks to see if ExpectedBucketOwner is set.
-        /// </summary>
-        /// <returns>true, if ExpectedBucketOwner property is set.</returns>
+        // Check to see if ExpectedBucketOwner property is set
         internal bool IsSetExpectedBucketOwner()
         {
-            return !String.IsNullOrEmpty(this.expectedBucketOwner);
+            return !String.IsNullOrEmpty(this._expectedBucketOwner);
         }
+
     }
 }
