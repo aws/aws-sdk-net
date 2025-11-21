4a8e571a82e258c26c3d93ce414189b6887fbbcf
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2025-11-21 20:44:50 +0000

    Support for agentcore gateway interceptor configurations and NONE authorizer type

--- sdk/src/Services/BedrockAgentCoreControl/Generated/ServiceEnumerations.cs
+++ sdk/src/Services/BedrockAgentCoreControl/Generated/ServiceEnumerations.cs
@@ -278,6 +278,10 @@ namespace Amazon.BedrockAgentCoreControl
         /// Constant CUSTOM_JWT for AuthorizerType
         /// </summary>
         public static readonly AuthorizerType CUSTOM_JWT = new AuthorizerType("CUSTOM_JWT");
+        /// <summary>
+        /// Constant NONE for AuthorizerType
+        /// </summary>
+        public static readonly AuthorizerType NONE = new AuthorizerType("NONE");
 
         /// <summary>
         /// This constant constructor does not need to be called if the constant
@@ -786,6 +790,56 @@ namespace Amazon.BedrockAgentCoreControl
         /// <param name="value">The string value to convert to the constant class.</param>
         /// <returns></returns>
         public static implicit operator ExceptionLevel(string value)
+        {
+            return FindValue(value);
+        }
+    }
+
+
+    /// <summary>
+    /// Constants used for properties of type GatewayInterceptionPoint.
+    /// </summary>
+    public class GatewayInterceptionPoint : ConstantClass
+    {
+
+        /// <summary>
+        /// Constant REQUEST for GatewayInterceptionPoint
+        /// </summary>
+        public static readonly GatewayInterceptionPoint REQUEST = new GatewayInterceptionPoint("REQUEST");
+        /// <summary>
+        /// Constant RESPONSE for GatewayInterceptionPoint
+        /// </summary>
+        public static readonly GatewayInterceptionPoint RESPONSE = new GatewayInterceptionPoint("RESPONSE");
+
+        /// <summary>
+        /// This constant constructor does not need to be called if the constant
+        /// you are attempting to use is already defined as a static instance of 
+        /// this class.
+        /// This constructor should be used to construct constants that are not
+        /// defined as statics, for instance if attempting to use a feature that is
+        /// newer than the current version of the SDK.
+        /// </summary>
+        public GatewayInterceptionPoint(string value)
+            : base(value)
+        {
+        }
+
+        /// <summary>
+        /// Finds the constant for the unique value.
+        /// </summary>
+        /// <param name="value">The unique value for the constant</param>
+        /// <returns>The constant for the unique value</returns>
+        public static GatewayInterceptionPoint FindValue(string value)
+        {
+            return FindValue<GatewayInterceptionPoint>(value);
+        }
+
+        /// <summary>
+        /// Utility method to convert strings to the constant class.
+        /// </summary>
+        /// <param name="value">The string value to convert to the constant class.</param>
+        /// <returns></returns>
+        public static implicit operator GatewayInterceptionPoint(string value)
         {
             return FindValue(value);
         }
