cdbd7d2bf4a702a90ec7afbb4ec86e4f4f30b279
Peter Song <petesong@amazon.com>
2025-11-25 13:34:41 -0800

    Generate ListObjects
    stack-info: PR: https://github.com/aws/aws-sdk-net/pull/4205, branch: peterrsongg/petesong/phase-3-pr5-rebased-2/2

--- sdk/src/Services/S3/Generated/Model/ListObjectsRequest.cs
+++ sdk/src/Services/S3/Generated/Model/ListObjectsRequest.cs
@@ -12,22 +12,28 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
+
+/*
+ * Do not modify this file. This file is generated from the s3-2006-03-01.normal.json service model.
+ */
 using System;
 using System.Collections.Generic;
 using System.Xml.Serialization;
 using System.Text;
 using System.IO;
+using System.Net;
 
 using Amazon.Runtime;
 using Amazon.Runtime.Internal;
 
+#pragma warning disable CS0612,CS0618,CS1570
 namespace Amazon.S3.Model
 {
     /// <summary>
     /// Container for the parameters to the ListObjects operation.
     /// <note> 
     /// <para>
-    /// This operation is not supported by directory buckets.
+    /// This operation is not supported for directory buckets.
     /// </para>
     ///  </note> 
     /// <para>
@@ -71,108 +77,118 @@ namespace Amazon.S3.Model
     ///  <a href="https://docs.aws.amazon.com/AmazonS3/latest/API/API_ListBuckets.html">ListBuckets</a>
     /// 
     /// </para>
-    ///  </li> </ul>
+    ///  </li> </ul> <important> 
+    /// <para>
+    /// You must URL encode any signed header values that contain spaces. For example, if
+    /// your header value is <c>my file.txt</c>, containing two spaces after <c>my</c>, you
+    /// must URL encode this value to <c>my%20%20file.txt</c>.
+    /// </para>
+    ///  </important>
     /// </summary>
     public partial class ListObjectsRequest : AmazonWebServiceRequest
     {
-        private string bucketName;
-        private string delimiter;
-        private EncodingType encoding;
-        private string expectedBucketOwner;
-        private string marker;
-        private int? maxKeys;
+        private string _bucketName;
+        private string _delimiter;
+        private EncodingType _encoding;
+        private string _expectedBucketOwner;
+        private string _marker;
+        private int? _maxKeys;
         private List<string> _optionalObjectAttributes = AWSConfigs.InitializeCollections ? new List<string>() : null;
-        private string prefix;
-        private RequestPayer requestPayer;
+        private string _prefix;
+        private RequestPayer _requestPayer;
 
         /// <summary>
         /// Gets and sets the property BucketName. 
         /// <para>
         /// The name of the bucket containing the objects.
-        /// </para> 
-        /// <para> 
-        /// <b>Directory buckets</b> - When you use this operation with a directory bucket, you must use virtual-hosted-style requests in the 
-        /// format <c> <i>Bucket-name</i>.s3express-<i>zone-id</i>.<i>region-code</i>.amazonaws.com</c>. Path-style requests are not 
-        /// supported. Directory bucket names must be unique in the chosen Zone (Availability Zone or Local Zone). Bucket names must follow 
-        /// the format <c> <i>bucket-base-name</i>--<i>zone-id</i>--x-s3</c> (for example, <c> <i>amzn-s3-demo-bucket</i>--<i>usw2-az1</i>--x-s3</c>). For 
-        /// information about bucket naming restrictions, see 
-        /// <a href="https://docs.aws.amazon.com/AmazonS3/latest/userguide/directory-bucket-naming-rules.html">Directory bucket naming rules</a> in 
-        /// the <i>Amazon S3 User Guide</i>.
-        /// </para> 
-        /// <para> 
-        /// <b>Access points</b> - When you use this action with an access point for general purpose buckets, you must provide the alias of the access 
-        /// point in place of the bucket name or specify the access point ARN. When you use this action with an access point for directory buckets, you 
-        /// must provide the access point name in place of the bucket name. When using the access point ARN, you must direct requests to the access point 
-        /// hostname. The access point hostname takes the form <i>AccessPointName</i>-<i>AccountId</i>.s3-accesspoint.<i>Region</i>.amazonaws.com. When 
-        /// using this action with an access point through the Amazon Web Services SDKs, you provide the access point ARN in place of the bucket name. For 
-        /// more information about access point ARNs, see 
-        /// <a href="https://docs.aws.amazon.com/AmazonS3/latest/userguide/using-access-points.html">Using access points</a> in the <i>Amazon S3 User Guide</i>.
-        /// </para> 
-        /// <note> 
+        /// </para>
+        ///  
+        /// <para>
+        ///  <b>Directory buckets</b> - When you use this operation with a directory bucket, you
+        /// must use virtual-hosted-style requests in the format <c> <i>Bucket-name</i>.s3express-<i>zone-id</i>.<i>region-code</i>.amazonaws.com</c>.
+        /// Path-style requests are not supported. Directory bucket names must be unique in the
+        /// chosen Zone (Availability Zone or Local Zone). Bucket names must follow the format
+        /// <c> <i>bucket-base-name</i>--<i>zone-id</i>--x-s3</c> (for example, <c> <i>amzn-s3-demo-bucket</i>--<i>usw2-az1</i>--x-s3</c>).
+        /// For information about bucket naming restrictions, see <a href="https://docs.aws.amazon.com/AmazonS3/latest/userguide/directory-bucket-naming-rules.html">Directory
+        /// bucket naming rules</a> in the <i>Amazon S3 User Guide</i>.
+        /// </para>
+        ///  
+        /// <para>
+        ///  <b>Access points</b> - When you use this action with an access point for general
+        /// purpose buckets, you must provide the alias of the access point in place of the bucket
+        /// name or specify the access point ARN. When you use this action with an access point
+        /// for directory buckets, you must provide the access point name in place of the bucket
+        /// name. When using the access point ARN, you must direct requests to the access point
+        /// hostname. The access point hostname takes the form <i>AccessPointName</i>-<i>AccountId</i>.s3-accesspoint.<i>Region</i>.amazonaws.com.
+        /// When using this action with an access point through the Amazon Web Services SDKs,
+        /// you provide the access point ARN in place of the bucket name. For more information
+        /// about access point ARNs, see <a href="https://docs.aws.amazon.com/AmazonS3/latest/userguide/using-access-points.html">Using
+        /// access points</a> in the <i>Amazon S3 User Guide</i>.
+        /// </para>
+        ///  <note> 
         /// <para>
         /// Object Lambda access points are not supported by directory buckets.
-        /// </para> 
-        /// </note> 
-        /// <para> 
-        /// <b>S3 on Outposts</b> - When you use this action with S3 on Outposts, you must direct requests to the S3 on Outposts hostname. The S3 on Outposts 
-        /// hostname takes the form <c> <i>AccessPointName</i>-<i>AccountId</i>.<i>outpostID</i>.s3-outposts.<i>Region</i>.amazonaws.com</c>. When you use 
-        /// this action with S3 on Outposts, the destination bucket must be the Outposts access point ARN or the access point alias. For more information 
-        /// about S3 on Outposts, see 
-        /// <a href="https://docs.aws.amazon.com/AmazonS3/latest/userguide/S3onOutposts.html">What is S3 on Outposts?</a> in the <i>Amazon S3 User Guide</i>.
+        /// </para>
+        ///  </note> 
+        /// <para>
+        ///  <b>S3 on Outposts</b> - When you use this action with S3 on Outposts, you must direct
+        /// requests to the S3 on Outposts hostname. The S3 on Outposts hostname takes the form
+        /// <c> <i>AccessPointName</i>-<i>AccountId</i>.<i>outpostID</i>.s3-outposts.<i>Region</i>.amazonaws.com</c>.
+        /// When you use this action with S3 on Outposts, the destination bucket must be the Outposts
+        /// access point ARN or the access point alias. For more information about S3 on Outposts,
+        /// see <a href="https://docs.aws.amazon.com/AmazonS3/latest/userguide/S3onOutposts.html">What
+        /// is S3 on Outposts?</a> in the <i>Amazon S3 User Guide</i>.
         /// </para>
         /// </summary>
+        [AWSProperty(Required=true)]
         public string BucketName
         {
-            get { return this.bucketName; }
-            set { this.bucketName = value; }
+            get { return this._bucketName; }
+            set { this._bucketName = value; }
         }
 
         // Check to see if BucketName property is set
         internal bool IsSetBucketName()
         {
-            return this.bucketName != null;
+            return this._bucketName != null;
         }
 
         /// <summary>
+        /// Gets and sets the property Delimiter. 
+        /// <para>
         /// A delimiter is a character that you use to group keys.
-        /// <para> 
-        /// <c>CommonPrefixes</c> is filtered out from results if it is not lexicographically greater than the key-marker.
+        /// </para>
+        ///  
+        /// <para>
+        ///  <c>CommonPrefixes</c> is filtered out from results if it is not lexicographically
+        /// greater than the key-marker.
         /// </para>
         /// </summary>
         public string Delimiter
         {
-            get { return this.delimiter; }
-            set { this.delimiter = value; }
+            get { return this._delimiter; }
+            set { this._delimiter = value; }
         }
 
         // Check to see if Delimiter property is set
         internal bool IsSetDelimiter()
         {
-            return this.delimiter != null;
+            return this._delimiter != null;
         }
 
         /// <summary>
-        /// <para>Encoding type used by Amazon S3 to encode the 
-        /// <a href="https://docs.aws.amazon.com/AmazonS3/latest/userguide/object-keys.html">object keys</a> 
-        /// in the response. Responses are encoded only in UTF-8. An object key can contain any Unicode 
-        /// character. However, the XML 1.0 parser can't parse certain characters, such as characters with an 
-        /// ASCII value from 0 to 10. For characters that aren't supported in XML 1.0, you can add this parameter 
-        /// to request that Amazon S3 encode the keys in the response. For more information about characters to avoid 
-        /// in object key names, see <a href="https://docs.aws.amazon.com/AmazonS3/latest/userguide/object-keys.html#object-key-guidelines">Object key naming guidelines</a>.
-        /// </para> <note> <para>When using the URL encoding type, non-ASCII characters that are used in an object's 
-        /// key name will be percent-encoded according to UTF-8 code values. For example, the object 
-        /// <code>test_file(3).png</code> will appear as <code>test_file%283%29.png</code>.</para> </note>
+        /// Gets and sets the property Encoding.
         /// </summary>
         public EncodingType Encoding
         {
-            get { return this.encoding; }
-            set { this.encoding = value; }
+            get { return this._encoding; }
+            set { this._encoding = value; }
         }
 
-        // Check to see if DeleteMarker property is set
+        // Check to see if Encoding property is set
         internal bool IsSetEncoding()
         {
-            return this.encoding != null;
+            return this._encoding != null;
         }
 
         /// <summary>
@@ -180,22 +196,19 @@ namespace Amazon.S3.Model
         /// <para>
         /// The account ID of the expected bucket owner. If the account ID that you provide does
         /// not match the actual owner of the bucket, the request fails with the HTTP status code
-        /// <code>403 Forbidden</code> (access denied).
+        /// <c>403 Forbidden</c> (access denied).
         /// </para>
         /// </summary>
         public string ExpectedBucketOwner
         {
-            get { return this.expectedBucketOwner; }
-            set { this.expectedBucketOwner = value; }
+            get { return this._expectedBucketOwner; }
+            set { this._expectedBucketOwner = value; }
         }
 
-        /// <summary>
-        /// Checks to see if ExpectedBucketOwner is set.
-        /// </summary>
-        /// <returns>true, if ExpectedBucketOwner property is set.</returns>
+        // Check to see if ExpectedBucketOwner property is set
         internal bool IsSetExpectedBucketOwner()
         {
-            return !String.IsNullOrEmpty(this.expectedBucketOwner);
+            return !String.IsNullOrEmpty(this._expectedBucketOwner);
         }
 
         /// <summary>
@@ -207,34 +220,34 @@ namespace Amazon.S3.Model
         /// </summary>
         public string Marker
         {
-            get { return this.marker; }
-            set { this.marker = value; }
+            get { return this._marker; }
+            set { this._marker = value; }
         }
 
         // Check to see if Marker property is set
         internal bool IsSetMarker()
         {
-            return this.marker != null;
+            return this._marker != null;
         }
 
         /// <summary>
         /// Gets and sets the property MaxKeys. 
         /// <para>
-        /// Sets the maximum number of keys returned in the response. By default the action returns
+        /// Sets the maximum number of keys returned in the response. By default, the action returns
         /// up to 1,000 key names. The response might contain fewer keys but will never contain
         /// more. 
         /// </para>
         /// </summary>
         public int? MaxKeys
         {
-            get { return this.maxKeys; }
-            set { this.maxKeys = value; }
+            get { return this._maxKeys; }
+            set { this._maxKeys = value; }
         }
 
         // Check to see if MaxKeys property is set
         internal bool IsSetMaxKeys()
         {
-            return this.maxKeys.HasValue;
+            return this._maxKeys.HasValue; 
         }
 
         /// <summary>
@@ -243,6 +256,11 @@ namespace Amazon.S3.Model
         /// Specifies the optional fields that you want returned in the response. Fields that
         /// you do not specify are not returned.
         /// </para>
+        /// <para />
+        /// Starting with version 4 of the SDK this property will default to null. If no data for this property is returned
+        /// from the service the property will also be null. This was changed to improve performance and allow the SDK and caller
+        /// to distinguish between a property not set or a property being empty to clear out a value. To retain the previous
+        /// SDK behavior set the AWSConfigs.InitializeCollections static property to true.
         /// </summary>
         public List<string> OptionalObjectAttributes
         {
@@ -257,18 +275,21 @@ namespace Amazon.S3.Model
         }
 
         /// <summary>
+        /// Gets and sets the property Prefix. 
+        /// <para>
         /// Limits the response to keys that begin with the specified prefix.
+        /// </para>
         /// </summary>
         public string Prefix
         {
-            get { return this.prefix; }
-            set { this.prefix = value; }
+            get { return this._prefix; }
+            set { this._prefix = value; }
         }
 
         // Check to see if Prefix property is set
         internal bool IsSetPrefix()
         {
-            return this.prefix != null;
+            return this._prefix != null;
         }
 
         /// <summary>
@@ -280,19 +301,15 @@ namespace Amazon.S3.Model
         /// </summary>
         public RequestPayer RequestPayer
         {
-            get { return this.requestPayer; }
-            set { this.requestPayer = value; }
+            get { return this._requestPayer; }
+            set { this._requestPayer = value; }
         }
 
-        /// <summary>
-        /// Checks to see if RequetsPayer is set.
-        /// </summary>
-        /// <returns>true, if RequestPayer property is set.</returns>
+        // Check to see if RequestPayer property is set
         internal bool IsSetRequestPayer()
         {
-            return requestPayer != null;
+            return this._requestPayer != null;
         }
 
     }
 }
-    
