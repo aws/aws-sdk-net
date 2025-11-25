2423fae04220ac942b8029104a9a34be4b2ca6aa
Peter Song <petesong@amazon.com>
2025-11-25 13:34:41 -0800

    Generate ListObjects

---
+++ sdk/src/Services/S3/Generated/Model/ListObjectsRequest.cs
@@ -0,0 +1,330 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ * 
+ *  http://aws.amazon.com/apache2.0
+ * 
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/*
+ * Do not modify this file. This file is generated from the s3-2006-03-01.normal.json service model.
+ */
+using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.Text;
+using System.IO;
+using System.Net;
+
+using Amazon.Runtime;
+using Amazon.Runtime.Internal;
+
+#pragma warning disable CS0612,CS0618,CS1570
+namespace Amazon.S3.Model
+{
+    /// <summary>
+    /// Container for the parameters to the ListObjects operation.
+    /// <important> 
+    /// <para>
+    /// End of support notice: Beginning November 21, 2025, Amazon S3 will stop returning
+    /// <c>DisplayName</c>. Update your applications to use canonical IDs (unique identifier
+    /// for Amazon Web Services accounts), Amazon Web Services account ID (12 digit identifier)
+    /// or IAM ARNs (full resource naming) as a direct replacement of <c>DisplayName</c>.
+    /// 
+    /// </para>
+    ///  
+    /// <para>
+    /// This change affects the following Amazon Web Services Regions: US East (N. Virginia)
+    /// Region, US West (N. California) Region, US West (Oregon) Region, Asia Pacific (Singapore)
+    /// Region, Asia Pacific (Sydney) Region, Asia Pacific (Tokyo) Region, Europe (Ireland)
+    /// Region, and South America (São Paulo) Region.
+    /// </para>
+    ///  </important> <note> 
+    /// <para>
+    /// This operation is not supported for directory buckets.
+    /// </para>
+    ///  </note> 
+    /// <para>
+    /// Returns some or all (up to 1,000) of the objects in a bucket. You can use the request
+    /// parameters as selection criteria to return a subset of the objects in a bucket. A
+    /// 200 OK response can contain valid or invalid XML. Be sure to design your application
+    /// to parse the contents of the response and handle it appropriately.
+    /// </para>
+    ///  <important> 
+    /// <para>
+    /// This action has been revised. We recommend that you use the newer version, <a href="https://docs.aws.amazon.com/AmazonS3/latest/API/API_ListObjectsV2.html">ListObjectsV2</a>,
+    /// when developing applications. For backward compatibility, Amazon S3 continues to support
+    /// <c>ListObjects</c>.
+    /// </para>
+    ///  </important> 
+    /// <para>
+    /// The following operations are related to <c>ListObjects</c>:
+    /// </para>
+    ///  <ul> <li> 
+    /// <para>
+    ///  <a href="https://docs.aws.amazon.com/AmazonS3/latest/API/API_ListObjectsV2.html">ListObjectsV2</a>
+    /// 
+    /// </para>
+    ///  </li> <li> 
+    /// <para>
+    ///  <a href="https://docs.aws.amazon.com/AmazonS3/latest/API/API_GetObject.html">GetObject</a>
+    /// 
+    /// </para>
+    ///  </li> <li> 
+    /// <para>
+    ///  <a href="https://docs.aws.amazon.com/AmazonS3/latest/API/API_PutObject.html">PutObject</a>
+    /// 
+    /// </para>
+    ///  </li> <li> 
+    /// <para>
+    ///  <a href="https://docs.aws.amazon.com/AmazonS3/latest/API/API_CreateBucket.html">CreateBucket</a>
+    /// 
+    /// </para>
+    ///  </li> <li> 
+    /// <para>
+    ///  <a href="https://docs.aws.amazon.com/AmazonS3/latest/API/API_ListBuckets.html">ListBuckets</a>
+    /// 
+    /// </para>
+    ///  </li> </ul> <important> 
+    /// <para>
+    /// You must URL encode any signed header values that contain spaces. For example, if
+    /// your header value is <c>my file.txt</c>, containing two spaces after <c>my</c>, you
+    /// must URL encode this value to <c>my%20%20file.txt</c>.
+    /// </para>
+    ///  </important>
+    /// </summary>
+    public partial class ListObjectsRequest : AmazonWebServiceRequest
+    {
+        private string _bucketName;
+        private string _delimiter;
+        private EncodingType _encoding;
+        private string _expectedBucketOwner;
+        private string _marker;
+        private int? _maxKeys;
+        private List<string> _optionalObjectAttributes = AWSConfigs.InitializeCollections ? new List<string>() : null;
+        private string _prefix;
+        private RequestPayer _requestPayer;
+
+        /// <summary>
+        /// Gets and sets the property BucketName. 
+        /// <para>
+        /// The name of the bucket containing the objects.
+        /// </para>
+        ///  
+        /// <para>
+        ///  <b>Directory buckets</b> - When you use this operation with a directory bucket, you
+        /// must use virtual-hosted-style requests in the format <c> <i>Bucket-name</i>.s3express-<i>zone-id</i>.<i>region-code</i>.amazonaws.com</c>.
+        /// Path-style requests are not supported. Directory bucket names must be unique in the
+        /// chosen Zone (Availability Zone or Local Zone). Bucket names must follow the format
+        /// <c> <i>bucket-base-name</i>--<i>zone-id</i>--x-s3</c> (for example, <c> <i>amzn-s3-demo-bucket</i>--<i>usw2-az1</i>--x-s3</c>).
+        /// For information about bucket naming restrictions, see <a href="https://docs.aws.amazon.com/AmazonS3/latest/userguide/directory-bucket-naming-rules.html">Directory
+        /// bucket naming rules</a> in the <i>Amazon S3 User Guide</i>.
+        /// </para>
+        ///  
+        /// <para>
+        ///  <b>Access points</b> - When you use this action with an access point for general
+        /// purpose buckets, you must provide the alias of the access point in place of the bucket
+        /// name or specify the access point ARN. When you use this action with an access point
+        /// for directory buckets, you must provide the access point name in place of the bucket
+        /// name. When using the access point ARN, you must direct requests to the access point
+        /// hostname. The access point hostname takes the form <i>AccessPointName</i>-<i>AccountId</i>.s3-accesspoint.<i>Region</i>.amazonaws.com.
+        /// When using this action with an access point through the Amazon Web Services SDKs,
+        /// you provide the access point ARN in place of the bucket name. For more information
+        /// about access point ARNs, see <a href="https://docs.aws.amazon.com/AmazonS3/latest/userguide/using-access-points.html">Using
+        /// access points</a> in the <i>Amazon S3 User Guide</i>.
+        /// </para>
+        ///  <note> 
+        /// <para>
+        /// Object Lambda access points are not supported by directory buckets.
+        /// </para>
+        ///  </note> 
+        /// <para>
+        ///  <b>S3 on Outposts</b> - When you use this action with S3 on Outposts, you must direct
+        /// requests to the S3 on Outposts hostname. The S3 on Outposts hostname takes the form
+        /// <c> <i>AccessPointName</i>-<i>AccountId</i>.<i>outpostID</i>.s3-outposts.<i>Region</i>.amazonaws.com</c>.
+        /// When you use this action with S3 on Outposts, the destination bucket must be the Outposts
+        /// access point ARN or the access point alias. For more information about S3 on Outposts,
+        /// see <a href="https://docs.aws.amazon.com/AmazonS3/latest/userguide/S3onOutposts.html">What
+        /// is S3 on Outposts?</a> in the <i>Amazon S3 User Guide</i>.
+        /// </para>
+        /// </summary>
+        [AWSProperty(Required=true)]
+        public string BucketName
+        {
+            get { return this._bucketName; }
+            set { this._bucketName = value; }
+        }
+
+        // Check to see if BucketName property is set
+        internal bool IsSetBucketName()
+        {
+            return this._bucketName != null;
+        }
+
+        /// <summary>
+        /// Gets and sets the property Delimiter. 
+        /// <para>
+        /// A delimiter is a character that you use to group keys.
+        /// </para>
+        ///  
+        /// <para>
+        ///  <c>CommonPrefixes</c> is filtered out from results if it is not lexicographically
+        /// greater than the key-marker.
+        /// </para>
+        /// </summary>
+        public string Delimiter
+        {
+            get { return this._delimiter; }
+            set { this._delimiter = value; }
+        }
+
+        // Check to see if Delimiter property is set
+        internal bool IsSetDelimiter()
+        {
+            return this._delimiter != null;
+        }
+
+        /// <summary>
+        /// Gets and sets the property Encoding.
+        /// </summary>
+        public EncodingType Encoding
+        {
+            get { return this._encoding; }
+            set { this._encoding = value; }
+        }
+
+        // Check to see if Encoding property is set
+        internal bool IsSetEncoding()
+        {
+            return this._encoding != null;
+        }
+
+        /// <summary>
+        /// Gets and sets the property ExpectedBucketOwner. 
+        /// <para>
+        /// The account ID of the expected bucket owner. If the account ID that you provide does
+        /// not match the actual owner of the bucket, the request fails with the HTTP status code
+        /// <c>403 Forbidden</c> (access denied).
+        /// </para>
+        /// </summary>
+        public string ExpectedBucketOwner
+        {
+            get { return this._expectedBucketOwner; }
+            set { this._expectedBucketOwner = value; }
+        }
+
+        // Check to see if ExpectedBucketOwner property is set
+        internal bool IsSetExpectedBucketOwner()
+        {
+            return !String.IsNullOrEmpty(this._expectedBucketOwner);
+        }
+
+        /// <summary>
+        /// Gets and sets the property Marker. 
+        /// <para>
+        /// Marker is where you want Amazon S3 to start listing from. Amazon S3 starts listing
+        /// after this specified key. Marker can be any key in the bucket.
+        /// </para>
+        /// </summary>
+        public string Marker
+        {
+            get { return this._marker; }
+            set { this._marker = value; }
+        }
+
+        // Check to see if Marker property is set
+        internal bool IsSetMarker()
+        {
+            return this._marker != null;
+        }
+
+        /// <summary>
+        /// Gets and sets the property MaxKeys. 
+        /// <para>
+        /// Sets the maximum number of keys returned in the response. By default, the action returns
+        /// up to 1,000 key names. The response might contain fewer keys but will never contain
+        /// more. 
+        /// </para>
+        /// </summary>
+        public int? MaxKeys
+        {
+            get { return this._maxKeys; }
+            set { this._maxKeys = value; }
+        }
+
+        // Check to see if MaxKeys property is set
+        internal bool IsSetMaxKeys()
+        {
+            return this._maxKeys.HasValue; 
+        }
+
+        /// <summary>
+        /// Gets and sets the property OptionalObjectAttributes. 
+        /// <para>
+        /// Specifies the optional fields that you want returned in the response. Fields that
+        /// you do not specify are not returned.
+        /// </para>
+        /// <para />
+        /// Starting with version 4 of the SDK this property will default to null. If no data for this property is returned
+        /// from the service the property will also be null. This was changed to improve performance and allow the SDK and caller
+        /// to distinguish between a property not set or a property being empty to clear out a value. To retain the previous
+        /// SDK behavior set the AWSConfigs.InitializeCollections static property to true.
+        /// </summary>
+        public List<string> OptionalObjectAttributes
+        {
+            get { return this._optionalObjectAttributes; }
+            set { this._optionalObjectAttributes = value; }
+        }
+
+        // Check to see if OptionalObjectAttributes property is set
+        internal bool IsSetOptionalObjectAttributes()
+        {
+            return this._optionalObjectAttributes != null && (this._optionalObjectAttributes.Count > 0 || !AWSConfigs.InitializeCollections);
+        }
+
+        /// <summary>
+        /// Gets and sets the property Prefix. 
+        /// <para>
+        /// Limits the response to keys that begin with the specified prefix.
+        /// </para>
+        /// </summary>
+        public string Prefix
+        {
+            get { return this._prefix; }
+            set { this._prefix = value; }
+        }
+
+        // Check to see if Prefix property is set
+        internal bool IsSetPrefix()
+        {
+            return this._prefix != null;
+        }
+
+        /// <summary>
+        /// Gets and sets the property RequestPayer. 
+        /// <para>
+        /// Confirms that the requester knows that she or he will be charged for the list objects
+        /// request. Bucket owners need not specify this parameter in their requests.
+        /// </para>
+        /// </summary>
+        public RequestPayer RequestPayer
+        {
+            get { return this._requestPayer; }
+            set { this._requestPayer = value; }
+        }
+
+        // Check to see if RequestPayer property is set
+        internal bool IsSetRequestPayer()
+        {
+            return this._requestPayer != null;
+        }
+
+    }
+}
