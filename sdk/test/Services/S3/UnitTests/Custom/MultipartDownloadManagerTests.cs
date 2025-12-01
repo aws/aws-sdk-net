d10e67b9dfa9de434db26003fe1936630c74b929
Garrett Beatty <gcbeatty@amazon.com>
2025-12-01 17:47:39 -0500

    Add error message for multipart download when using Encryption Client (#4171)

--- sdk/test/Services/S3/UnitTests/Custom/MultipartDownloadManagerTests.cs
+++ sdk/test/Services/S3/UnitTests/Custom/MultipartDownloadManagerTests.cs
@@ -151,6 +151,45 @@ namespace AWSSDK.UnitTests
             var coordinator = new MultipartDownloadManager(client, request, config, handler);
         }
 
+        [TestMethod]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void Constructor_WithEncryptionClient_ThrowsNotSupportedException()
+        {
+            // Arrange
+            var mockEncryptionClient = new Mock<IAmazonS3>();
+            mockEncryptionClient.As<Amazon.S3.Internal.IAmazonS3Encryption>();
+            
+            var request = MultipartDownloadTestHelpers.CreateOpenStreamRequest();
+            var config = MultipartDownloadTestHelpers.CreateBufferedDownloadConfiguration();
+            var mockDataHandler = CreateMockDataHandler();
+
+            // Act
+            var coordinator = new MultipartDownloadManager(mockEncryptionClient.Object, request, config, mockDataHandler.Object);
+        }
+
+        [TestMethod]
+        public void Constructor_WithEncryptionClient_ExceptionMessageIsDescriptive()
+        {
+            // Arrange
+            var mockEncryptionClient = new Mock<IAmazonS3>();
+            mockEncryptionClient.As<Amazon.S3.Internal.IAmazonS3Encryption>();
+            
+            var request = MultipartDownloadTestHelpers.CreateOpenStreamRequest();
+            var config = MultipartDownloadTestHelpers.CreateBufferedDownloadConfiguration();
+            var mockDataHandler = CreateMockDataHandler();
+
+            // Act & Assert
+            try
+            {
+                var coordinator = new MultipartDownloadManager(mockEncryptionClient.Object, request, config, mockDataHandler.Object);
+                Assert.Fail("Expected NotSupportedException was not thrown");
+            }
+            catch (NotSupportedException ex)
+            {
+                Assert.IsTrue(ex.Message.Contains("Multipart download is not supported when using Amazon.S3.Internal.IAmazonS3Encryption client. Please use the Amazon.S3.AmazonS3Client for multipart download."));
+            }
+        }
+
         #endregion
 
         #region Property Tests
