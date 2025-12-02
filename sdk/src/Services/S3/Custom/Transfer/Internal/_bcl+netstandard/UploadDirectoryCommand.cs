9816657a031cf173a34c218fbc369165bb9187f7
Phil Asmar <phil.asmar@gmail.com>
2025-12-02 17:08:33 -0500

    reply to garretts comments

--- sdk/src/Services/S3/Custom/Transfer/Internal/_bcl+netstandard/UploadDirectoryCommand.cs
+++ sdk/src/Services/S3/Custom/Transfer/Internal/_bcl+netstandard/UploadDirectoryCommand.cs
@@ -21,22 +21,28 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Amazon.S3.Transfer.Model;
+using Amazon.Runtime.Internal.Util;
 
 namespace Amazon.S3.Transfer.Internal
 {
     internal partial class UploadDirectoryCommand : BaseCommand<TransferUtilityUploadDirectoryResponse>
     {
         public bool UploadFilesConcurrently { get; set; }
+        private readonly Logger _logger = Logger.GetLogger(typeof(UploadDirectoryCommand));
 
         public override async Task<TransferUtilityUploadDirectoryResponse> ExecuteAsync(CancellationToken cancellationToken)
         {
             string prefix = GetKeyPrefix();
-
             string basePath = new DirectoryInfo(this._request.Directory).FullName;
 
+            _logger.DebugFormat("UploadDirectoryCommand starting. BasePath={0}, Prefix={1}, UploadFilesConcurrently={2}, ConcurrentServiceRequests={3}",
+                basePath, prefix, UploadFilesConcurrently, this._config.ConcurrentServiceRequests);
+
             string[] filePaths = await GetFiles(basePath, this._request.SearchPattern, this._request.SearchOption, cancellationToken)
-                .ConfigureAwait(continueOnCapturedContext: false);                
+                .ConfigureAwait(continueOnCapturedContext: false);
             this._totalNumberOfFiles = filePaths.Length;
+
+            _logger.DebugFormat("Discovered {0} file(s) to upload. TotalBytes={1}", _totalNumberOfFiles, _totalBytes);
 
             // Two-level throttling architecture:
             // 1. File-level throttler: Controls how many files are uploaded concurrently
@@ -55,11 +61,12 @@ namespace Amazon.S3.Transfer.Internal
             try
             {
                 var pendingTasks = new List<Task>();
-                
+
                 // File-level throttler: Controls concurrent file operations
-                fileOperationThrottler = UploadFilesConcurrently ? 
+                fileOperationThrottler = UploadFilesConcurrently ?
                     new SemaphoreSlim(this._config.ConcurrentServiceRequests) :
                     new SemaphoreSlim(1);
+                _logger.DebugFormat("Created fileOperationThrottler with initial count={0}", UploadFilesConcurrently ? this._config.ConcurrentServiceRequests : 1);
 
                 // HTTP-level throttler: Shared across all uploads to control total HTTP concurrency
                 sharedHttpRequestThrottler = this._utility.S3Client is Amazon.S3.Internal.IAmazonS3Encryption ?
@@ -70,12 +77,22 @@ namespace Amazon.S3.Transfer.Internal
                     // Use a throttler which will be shared between simple and multipart uploads
                     // to control total concurrent HTTP requests across all file operations.
                     new SemaphoreSlim(this._config.ConcurrentServiceRequests);
-
+                if (sharedHttpRequestThrottler == null)
+                {
+                    _logger.Debug(null, "sharedHttpRequestThrottler disabled due to encryption client. Multipart uploads will be serial per file.");
+                }
+                else
+                {
+                    _logger.DebugFormat("Created sharedHttpRequestThrottler with initial count={0}", this._config.ConcurrentServiceRequests);
+                }
 
                 internalCts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+
                 foreach (string filepath in filePaths)
                 {
+                    _logger.DebugFormat("Waiting for fileOperationThrottler to schedule file: {0}", filepath);
                     await fileOperationThrottler.WaitAsync(cancellationToken).ConfigureAwait(continueOnCapturedContext: false);
+                    _logger.DebugFormat("Acquired fileOperationThrottler for file: {0}. Currently scheduled: {1}", filepath, pendingTasks.Count + 1);
 
                     try
                     {
@@ -86,49 +103,59 @@ namespace Amazon.S3.Transfer.Internal
                             // don't schedule any more upload tasks. 
                             // Don't throw an OperationCanceledException here as we want to process the 
                             // responses and throw the original exception.
+                            _logger.Debug(null, "Internal cancellation requested; breaking out of scheduling loop.");
                             break;
                         }
-                        
+
                         var uploadRequest = ConstructRequest(basePath, filepath, prefix);
-                        
+                        _logger.DebugFormat("Constructed upload request for Key={0}, FilePath={1}", uploadRequest.Key, uploadRequest.FilePath);
+
                         Action<Exception> onFailure = (ex) =>
                         {
+                            _logger.Debug(ex, "Upload failed for Key={0}, FilePath={1}.", uploadRequest.Key, uploadRequest.FilePath);
                             this._request.OnRaiseObjectUploadFailedEvent(
                                 new ObjectUploadFailedEventArgs(
-                                    this._request, 
-                                    uploadRequest, 
+                                    this._request,
+                                    uploadRequest,
                                     ex));
                         };
-                        
+
                         var task = _failurePolicy.ExecuteAsync(
                             async () => {
+                                _logger.DebugFormat("Starting upload command for Key={0}", uploadRequest.Key);
                                 var command = _utility.GetUploadCommand(uploadRequest, sharedHttpRequestThrottler);
                                 await command.ExecuteAsync(internalCts.Token)
                                     .ConfigureAwait(false);
-                                Interlocked.Increment(ref _numberOfFilesSuccessfullyUploaded);
+                                var uploaded = Interlocked.Increment(ref _numberOfFilesSuccessfullyUploaded);
+                                _logger.DebugFormat("Completed upload for Key={0}. FilesSuccessfullyUploaded={1}", uploadRequest.Key, uploaded);
                             },
                             onFailure,
                             internalCts
                         );
-                        
+
                         pendingTasks.Add(task);
+                        _logger.DebugFormat("Scheduled upload task for Key={0}. PendingTasks={1}", uploadRequest.Key, pendingTasks.Count);
                     }
                     finally
                     {
                         fileOperationThrottler.Release();
+                        _logger.DebugFormat("Released fileOperationThrottler for file: {0}", filepath);
                     }
                 }
+
+                _logger.DebugFormat("Awaiting completion of {0} scheduled task(s)", pendingTasks.Count);
                 await TaskHelpers.WhenAllOrFirstExceptionAsync(pendingTasks, cancellationToken)
                     .ConfigureAwait(continueOnCapturedContext: false);
             }
             finally
-            {                
+            {
                 internalCts.Dispose();
                 fileOperationThrottler.Dispose();
                 sharedHttpRequestThrottler?.Dispose();
+                _logger.DebugFormat("UploadDirectoryCommand finished. FilesSuccessfullyUploaded={0}", _numberOfFilesSuccessfullyUploaded);
             }
 
-            return new TransferUtilityUploadDirectoryResponse
+            var response = new TransferUtilityUploadDirectoryResponse
             {
                 ObjectsUploaded = _numberOfFilesSuccessfullyUploaded,
                 ObjectsFailed = _errors.Count,
@@ -139,6 +166,9 @@ namespace Amazon.S3.Transfer.Internal
                         DirectoryResult.PartialSuccess :
                         DirectoryResult.Failure)
             };
+
+            _logger.DebugFormat("Response summary: Uploaded={0}, Failed={1}, Result={2}", response.ObjectsUploaded, response.ObjectsFailed, response.Result);
+            return response;
         }
 
         private Task<string[]> GetFiles(string path, string searchPattern, SearchOption searchOption, CancellationToken cancellationToken)
