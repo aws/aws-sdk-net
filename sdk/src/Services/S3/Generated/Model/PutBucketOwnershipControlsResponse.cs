67ca2a8fda6be5c81c9d5528798b90b83916487b
Muhammad Othman <muhammmadothman@gmail.com>
2025-12-09 10:41:09 -0500

    Generate PutBucketOwnershipControls S3 operation

--- sdk/src/Services/S3/Generated/Model/PutBucketOwnershipControlsResponse.cs
+++ sdk/src/Services/S3/Generated/Model/PutBucketOwnershipControlsResponse.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License").
@@ -12,19 +12,28 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
+
+/*
+ * Do not modify this file. This file is generated from the s3-2006-03-01.normal.json service model.
+ */
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Xml.Serialization;
 using System.Text;
-using Amazon.Runtime;
+using System.IO;
+using System.Net;
 
+using Amazon.Runtime;
+using Amazon.Runtime.Internal;
+
+#pragma warning disable CS0612,CS0618,CS1570
 namespace Amazon.S3.Model
 {
     /// <summary>
-    /// Returns information about the PutBucketOwnershipControls response metadata.
-    /// The PutBucketOwnershipControls operation has a void result type.
+    /// This is the response object from the PutBucketOwnershipControls operation.
     /// </summary>
     public partial class PutBucketOwnershipControlsResponse : AmazonWebServiceResponse
     {
+
     }
 }
