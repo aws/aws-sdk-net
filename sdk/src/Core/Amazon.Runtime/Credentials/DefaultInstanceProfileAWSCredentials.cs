78e88cbdb7a9147e4412af588bb687610ba22c09
Norm Johanson <normj@amazon.com>
2025-12-08 20:30:42 -0800

    Remove unnecessary using

--- sdk/src/Core/Amazon.Runtime/Credentials/DefaultInstanceProfileAWSCredentials.cs
+++ sdk/src/Core/Amazon.Runtime/Credentials/DefaultInstanceProfileAWSCredentials.cs
@@ -18,7 +18,6 @@ using Amazon.Util;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
-using static Amazon.Runtime.DefaultInstanceProfileAWSCredentials;
 
 namespace Amazon.Runtime
 {
