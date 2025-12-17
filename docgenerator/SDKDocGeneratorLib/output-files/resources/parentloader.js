d38cec63f36d6ba7c5e42f3c5a5f1b7459b21f6b
adaines <adaines@amazon.com>
2025-12-17 15:20:17 -0500

    modify parent loader to better handle backwards compatibility

--- docgenerator/SDKDocGeneratorLib/output-files/resources/parentloader.js
+++ docgenerator/SDKDocGeneratorLib/output-files/resources/parentloader.js
@@ -33,7 +33,8 @@
     else {
         //This content page is not in the expected frameset which contains the TOC so
         //reload the parent frame and cause it to reload this page in the frame.
-        //Read the tocid from the meta tag and build the frameset URL.
+
+        //First, try the new approach: read tocid from meta tag and build frameset URL
         var tocid = jQuery("meta[name='aws-tocid']").attr('content');
         var pathname = window.location.pathname;
         var itemsIndex = pathname.indexOf('/items/');
@@ -46,6 +47,32 @@
             //will run on reload to replace the address bar URL with the actual friendly content
             //frame page URL.
             window.location.href = basePath + '/index.html?page=' + filename + '&tocid=' + tocid;
+        }
+        else {
+            //Fallback: try the old approach using the canonical link for backward compatibility.
+            //This handles pages that may have been generated with older code or cached versions.
+            var pageLink = jQuery("link[rel='canonical']").attr('href');
+            if (pageLink && pageLink !== '') {
+                //Canonical links may look like:
+                //  http://docs.aws.amazon.com/sdkfornet/v4/apidocs/index.html?page=PAGE.html&tocid=TOCID (old format)
+                //  http://docs.aws.amazon.com/sdkfornet/v4/apidocs/items/S3/PAGE.html (new format)
+                //Attempt to find where the relative path starts after the hostname.
+                var seekFrom = 0;
+                var protocolEndIndex = pageLink.indexOf('://');
+                if (protocolEndIndex != -1) {
+                    seekFrom = protocolEndIndex + 3;
+                }
+
+                //Look for the start of the relative path.
+                var relPathIndex = pageLink.indexOf('/', seekFrom);
+                if (relPathIndex != -1) {
+                    //Check if this is the old format (contains index.html?page=)
+                    if (pageLink.indexOf('index.html?page=') !== -1) {
+                        //Old format: redirect to the canonical URL directly
+                        window.location.href = pageLink.substring(relPathIndex);
+                    }
+                }
+            }
         }
     }    
 })();
