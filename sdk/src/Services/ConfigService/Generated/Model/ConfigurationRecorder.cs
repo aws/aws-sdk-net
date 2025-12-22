f03905a8fe6242dfc7489bb4e216a31540bff6cf
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2025-12-22 19:20:12 +0000

    Added supported resourceTypes for Config from July to November 2025

--- sdk/src/Services/ConfigService/Generated/Model/ConfigurationRecorder.cs
+++ sdk/src/Services/ConfigService/Generated/Model/ConfigurationRecorder.cs
@@ -257,11 +257,11 @@ namespace Amazon.ConfigService.Model
         /// </para>
         ///  
         /// <para>
-        /// If you use an Amazon Web Services service that uses Config, such as Security Hub or
-        /// Control Tower, and an IAM role has already been created, make sure that the IAM role
-        /// that you use when setting up Config keeps the same minimum permissions as the pre-existing
-        /// IAM role. You must do this to ensure that the other Amazon Web Services service continues
-        /// to run as expected. 
+        /// If you use an Amazon Web Services service that uses Config, such as Security Hub CSPM
+        /// or Control Tower, and an IAM role has already been created, make sure that the IAM
+        /// role that you use when setting up Config keeps the same minimum permissions as the
+        /// pre-existing IAM role. You must do this to ensure that the other Amazon Web Services
+        /// service continues to run as expected. 
         /// </para>
         ///  
         /// <para>
