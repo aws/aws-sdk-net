001bd241311080e1dda1a573ed42a4726eda8d85
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2025-12-30 19:16:06 +0000

    This change sets the KafkaConnect GovCloud FIPS and FIPS DualStack endpoints to use kafkaconnect instead of kafkaconnect-fips as the service name. This is done to match the Kafka endpoints.

--- sdk/src/Services/KafkaConnect/Generated/Internal/AmazonKafkaConnectEndpointResolver.cs
+++ sdk/src/Services/KafkaConnect/Generated/Internal/AmazonKafkaConnectEndpointResolver.cs
@@ -50,10 +50,10 @@ namespace Amazon.KafkaConnect.Internal
         {
             var config = (AmazonKafkaConnectConfig)requestContext.ClientConfig;
             var result = new KafkaConnectEndpointParameters();
-            result.Region = requestContext.Request.AlternateEndpoint?.SystemName ?? config.RegionEndpoint?.SystemName;;
             result.UseDualStack = config.UseDualstackEndpoint;
             result.UseFIPS = config.UseFIPSEndpoint;
             result.Endpoint = config.ServiceURL;
+            result.Region = requestContext.Request.AlternateEndpoint?.SystemName ?? config.RegionEndpoint?.SystemName;;
 
 
             // The region needs to be determined from the ServiceURL if not set.
