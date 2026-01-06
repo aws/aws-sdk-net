2fc710fefe97838a37728f62ae2e82e8dac1cc5d
Peter Song <petesong@amazon.com>
2026-01-06 12:35:04 -0800

    Generate PutBucketLogging (#4270)
    * Generate PutBucketLogging
    
    * remove unused custom file

--- generator/ServiceClientGeneratorLib/ServiceModel.cs
+++ generator/ServiceClientGeneratorLib/ServiceModel.cs
@@ -588,6 +588,7 @@ namespace ServiceClientGenerator
                         new Operation(this, "DeleteObjects", DocumentRoot[OperationsKey]["DeleteObjects"]),
                         new Operation(this, "DeleteObjectTagging",  DocumentRoot[OperationsKey]["DeleteObjectTagging"]),
                         new Operation(this, "GetBucketLogging",  DocumentRoot[OperationsKey]["GetBucketLogging"]),
+                        new Operation(this, "PutBucketLogging", DocumentRoot[OperationsKey]["PutBucketLogging"])
 
                     };
                 }
