ccefd915731682445e3ed9199700b12679b29ee3
Peter Song <petesong@amazon.com>
2026-01-08 11:53:27 -0800

    Generate GetObjectTorrent

---
+++ sdk/src/Services/S3/Generated/Model/Internal/MarshallTransformations/GetObjectTorrentRequestMarshaller.cs
@@ -0,0 +1,103 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ * 
+ *  http://aws.amazon.com/apache2.0
+ * 
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/*
+ * Do not modify this file. This file is generated from the s3-2006-03-01.normal.json service model.
+ */
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+using Amazon.S3.Model;
+using Amazon.Runtime;
+using Amazon.Runtime.Internal;
+using Amazon.Runtime.Internal.Transform;
+using Amazon.Runtime.Internal.Util;
+using System.Xml;
+
+#pragma warning disable CS0612,CS0618
+namespace Amazon.S3.Model.Internal.MarshallTransformations
+{
+    /// <summary>
+    /// GetObjectTorrent Request Marshaller
+    /// </summary>       
+    public partial class GetObjectTorrentRequestMarshaller : IMarshaller<IRequest, GetObjectTorrentRequest> , IMarshaller<IRequest,AmazonWebServiceRequest>
+    {
+        /// <summary>
+        /// Marshaller the request object to the HTTP request.
+        /// </summary>  
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public IRequest Marshall(AmazonWebServiceRequest input)
+        {
+            return this.Marshall((GetObjectTorrentRequest)input);
+        }
+
+        /// <summary>
+        /// Marshaller the request object to the HTTP request.
+        /// </summary>  
+        /// <param name="publicRequest"></param>
+        /// <returns></returns>
+        public IRequest Marshall(GetObjectTorrentRequest publicRequest)
+        {
+            var request = new DefaultRequest(publicRequest, "Amazon.S3");
+            PreMarshallCustomization(request, publicRequest);
+            request.HttpMethod = "GET";
+            request.AddSubResource("torrent");
+        
+            if (publicRequest.IsSetExpectedBucketOwner()) 
+            {
+                request.Headers["x-amz-expected-bucket-owner"] = publicRequest.ExpectedBucketOwner;
+            }
+        
+            if (publicRequest.IsSetRequestPayer()) 
+            {
+                request.Headers["x-amz-request-payer"] = publicRequest.RequestPayer;
+            }
+            if (string.IsNullOrEmpty(publicRequest.BucketName))
+                throw new System.ArgumentException("BucketName is a required property and must be set before making this call.", "GetObjectTorrentRequest.BucketName");
+            if (string.IsNullOrEmpty(publicRequest.Key))
+                throw new System.ArgumentException("Key is a required property and must be set before making this call.", "GetObjectTorrentRequest.Key");
+            request.AddPathResource("{Key+}", StringUtils.FromString(publicRequest.Key));
+            request.ResourcePath = "/{Key+}";
+
+            PostMarshallCustomization(request, publicRequest);
+            return request;
+        }
+        private static GetObjectTorrentRequestMarshaller _instance = new GetObjectTorrentRequestMarshaller();        
+
+        internal static GetObjectTorrentRequestMarshaller GetInstance()
+        {
+            return _instance;
+        }
+
+        /// <summary>
+        /// Gets the singleton.
+        /// </summary>  
+        public static GetObjectTorrentRequestMarshaller Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        partial void PostMarshallCustomization(DefaultRequest defaultRequest, GetObjectTorrentRequest publicRequest);
+        partial void PreMarshallCustomization(DefaultRequest defaultRequest, GetObjectTorrentRequest publicRequest);
+    }    
+}
