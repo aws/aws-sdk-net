fa137a8aefebab3b9a79bd3e13bc8d87092b365d
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2026-01-09 19:15:51 +0000

    Updating endpoints.json file.

--- sdk/src/Core/RegionEndpoint/RegionEndpoint.generated.cs
+++ sdk/src/Core/RegionEndpoint/RegionEndpoint.generated.cs
@@ -258,9 +258,9 @@ namespace Amazon
         public static readonly RegionEndpoint USIsofSouth1 = GetEndpoint("us-isof-south-1", "US ISOF SOUTH");
 
         /// <summary>
-        /// The EU (Germany) endpoint.
+        /// The AWS European Sovereign Cloud (Germany) endpoint.
         /// </summary>
-        public static readonly RegionEndpoint EUSCDeEast1 = GetEndpoint("eusc-de-east-1", "EU (Germany)");
+        public static readonly RegionEndpoint EUSCDeEast1 = GetEndpoint("eusc-de-east-1", "AWS European Sovereign Cloud (Germany)");
 
     }
 }
