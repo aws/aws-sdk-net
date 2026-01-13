4fd4b365757ba4db114ec0ac1aa8c76ab3deb7a2
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2026-01-13 20:18:03 +0000

    Commit version number update changes

--- sdk/src/Services/DataZone/Generated/AmazonDataZoneConfig.cs
+++ sdk/src/Services/DataZone/Generated/AmazonDataZoneConfig.cs
@@ -32,7 +32,7 @@ namespace Amazon.DataZone
     public partial class AmazonDataZoneConfig : ClientConfig
     {
         private static readonly string UserAgentString =
-            InternalSDKUtils.BuildUserAgentString("DataZone", "3.7.513.15");
+            InternalSDKUtils.BuildUserAgentString("DataZone", "3.7.514.0");
 
         private string _userAgent = UserAgentString;
         ///<summary>
