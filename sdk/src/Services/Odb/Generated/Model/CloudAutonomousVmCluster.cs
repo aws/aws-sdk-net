67508bb92fb5268aa5c9087bb530c7dd36ab9476
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2026-01-20 19:53:16 +0000

    Adds support for associating and disassociating IAM roles with Autonomous VM cluster resources through the AssociateIamRoleToResource and DisassociateIamRoleFromResource APIs. The GetCloudAutonomousVmCluster and ListCloudAutonomousVmClusters API responses now include the iamRoles field.

--- sdk/src/Services/Odb/Generated/Model/CloudAutonomousVmCluster.cs
+++ sdk/src/Services/Odb/Generated/Model/CloudAutonomousVmCluster.cs
@@ -57,6 +57,7 @@ namespace Amazon.Odb.Model
         private string _domain;
         private double? _exadataStorageInTBsLowestScaledValue;
         private string _hostname;
+        private List<IamRole> _iamRoles = AWSConfigs.InitializeCollections ? new List<IamRole>() : null;
         private bool? _isMtlsEnabledVmCluster;
         private LicenseModel _licenseModel;
         private MaintenanceWindow _maintenanceWindow;
@@ -513,6 +514,30 @@ namespace Amazon.Odb.Model
         internal bool IsSetHostname()
         {
             return this._hostname != null;
+        }
+
+        /// <summary>
+        /// Gets and sets the property IamRoles. 
+        /// <para>
+        /// The Amazon Web Services Identity and Access Management (IAM) service roles associated
+        /// with the Autonomous VM cluster.
+        /// </para>
+        /// <para />
+        /// Starting with version 4 of the SDK this property will default to null. If no data for this property is returned
+        /// from the service the property will also be null. This was changed to improve performance and allow the SDK and caller
+        /// to distinguish between a property not set or a property being empty to clear out a value. To retain the previous
+        /// SDK behavior set the AWSConfigs.InitializeCollections static property to true.
+        /// </summary>
+        public List<IamRole> IamRoles
+        {
+            get { return this._iamRoles; }
+            set { this._iamRoles = value; }
+        }
+
+        // Check to see if IamRoles property is set
+        internal bool IsSetIamRoles()
+        {
+            return this._iamRoles != null && (this._iamRoles.Count > 0 || !AWSConfigs.InitializeCollections); 
         }
 
         /// <summary>
