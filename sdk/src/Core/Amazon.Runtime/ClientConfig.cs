f42defeb93b1ab43965e3ddedf72832a11465268
Daniel Pinheiro <3842788+dscpinheiro@users.noreply.github.com>
2026-02-10 16:35:33 -0500

    docs: Update documentation for `MaxErrorRetry` to call out different default for DynamoDB

--- sdk/src/Core/Amazon.Runtime/ClientConfig.cs
+++ sdk/src/Core/Amazon.Runtime/ClientConfig.cs
@@ -495,12 +495,16 @@ namespace Amazon.Runtime
             get { return this.serviceId; }
             set { this.serviceId = value; }
         }
+
         /// <summary>
         /// Returns the flag indicating how many retry HTTP requests an SDK should
         /// make for a single SDK operation invocation before giving up. This flag will 
         /// return 4 when the RetryMode is set to "Legacy" which is the default. For
-        /// RetryMode values of "Standard" or "Adaptive" this flag will return 2. In 
-        /// addition to the values returned that are dependent on the RetryMode, the
+        /// RetryMode values of "Standard" or "Adaptive" this flag will return 2.
+        /// <para />
+        /// <b>Note</b>: For the <c>DynamoDBv2</c> service package, the default value is 10.
+        /// <para />
+        /// In addition to the values returned that are dependent on the RetryMode, the
         /// value can be set to a specific value by using the AWS_MAX_ATTEMPTS environment
         /// variable, max_attempts in the shared configuration file, or by setting a
         /// value directly on this property. When using AWS_MAX_ATTEMPTS or max_attempts
