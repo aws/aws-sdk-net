c20bcd913e729fb86c85a9cbeaea98cd1e4d1510
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2026-02-20 20:48:23 +0000

    This release introduces AWS Signer Data Plane SDK client supporting GetRevocationStatus API. The new client enables AWS PrivateLink connectivity with both private DNS and VPC endpoint URLs.

---
+++ sdk/src/Services/SignerData/Generated/AmazonSignerDataConfig.cs
@@ -0,0 +1,119 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ * 
+ *  http://aws.amazon.com/apache2.0
+ * 
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/*
+ * Do not modify this file. This file is generated from the signer-data-2017-08-25.normal.json service model.
+ */
+
+using System;
+using Amazon.Runtime;
+using Amazon.Runtime.Internal;
+using Amazon.Util.Internal;
+using Amazon.Runtime.Internal.Auth;
+using Amazon.Runtime.Endpoints;
+using Amazon.SignerData.Internal;
+
+namespace Amazon.SignerData
+{
+    /// <summary>
+    /// Configuration for accessing Amazon SignerData service
+    /// </summary>
+    public partial class AmazonSignerDataConfig : ClientConfig
+    {
+        private static readonly string UserAgentString =
+            InternalSDKUtils.BuildUserAgentString("Signer Data", "4.0");
+
+        private static readonly AmazonSignerDataEndpointResolver EndpointResolver =
+            new AmazonSignerDataEndpointResolver();
+
+        private string _userAgent = UserAgentString;
+        ///<summary>
+        /// The ServiceId, which is the unique identifier for a service.
+        ///</summary>
+        public static new string ServiceId
+        {
+            get
+            {
+                return "Signer Data";
+            }
+        }
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public AmazonSignerDataConfig()
+            : base(new Amazon.Runtime.Internal.DefaultConfigurationProvider(AmazonSignerDataDefaultConfiguration.GetAllConfigurations()))
+        {
+            base.ServiceId = "Signer Data";
+            this.AuthenticationServiceName = "signer";
+            this.EndpointProvider = new AmazonSignerDataEndpointProvider();
+        }
+
+        /// <summary>
+        /// The constant used to lookup in the region hash the endpoint.
+        /// </summary>
+        public override string RegionEndpointServiceName
+        {
+            get
+            {
+                return "data-signer";
+            }
+        }
+
+        /// <summary>
+        /// Gets the ServiceVersion property.
+        /// </summary>
+        public override string ServiceVersion
+        {
+            get
+            {
+                return "2017-08-25";
+            }
+        }
+
+        /// <summary>
+        /// Gets the value of UserAgent property.
+        /// </summary>
+        public override string UserAgent
+        {
+            get
+            {
+                return _userAgent;
+            }
+        }
+
+        /// <summary>
+        /// Returns the endpoint that will be used for a particular request.
+        /// </summary>
+        /// <param name="parameters">A Container class for parameters used for endpoint resolution.</param>
+        /// <returns>The resolved endpoint for the given request.</returns>
+        public override Amazon.Runtime.Endpoints.Endpoint DetermineServiceOperationEndpoint(ServiceOperationEndpointParameters parameters)
+        {
+            var requestContext = new RequestContext(false)
+            {
+                ClientConfig = this,
+                OriginalRequest = parameters.Request,
+                Request = new DefaultRequest(parameters.Request, ServiceId)
+                {
+                    AlternateEndpoint = parameters.AlternateEndpoint
+                }
+            };
+
+            var executionContext = new Amazon.Runtime.Internal.ExecutionContext(requestContext, null);
+            return EndpointResolver.GetEndpoint(executionContext);
+        }
+
+
+    }
+}
