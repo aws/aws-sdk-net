c7caaf597cf31b44233edc6df4433669f03c3dfd
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2026-03-04 19:20:59 +0000

    As part of this release, Beanstalk introduce a new info type - analyze for request environment info and retrieve environment info operations. When customers request an Al analysis, Elastic Beanstalk runs a script on an instance in their environment and returns an analysis of events, health and logs.

--- sdk/src/Services/ElasticBeanstalk/Generated/_netstandard/AmazonElasticBeanstalkClient.cs
+++ sdk/src/Services/ElasticBeanstalk/Generated/_netstandard/AmazonElasticBeanstalkClient.cs
@@ -2037,6 +2037,12 @@ namespace Amazon.ElasticBeanstalk
         /// </para>
         ///  
         /// <para>
+        ///  Setting the <c>InfoType</c> to <c>analyze</c> collects recent events, instance health,
+        /// and logs from your environment and sends them to Amazon Bedrock in your account to
+        /// generate diagnostic insights and recommended next steps. 
+        /// </para>
+        ///  
+        /// <para>
         ///  Use <a>RetrieveEnvironmentInfo</a> to obtain the set of logs. 
         /// </para>
         ///  
