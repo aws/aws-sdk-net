acf77aa916da3df011962ea72c5327bc93006203
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2026-03-04 19:19:05 +0000

    Adds support for DeploymentStrategyOptions.

--- sdk/src/Services/Elasticsearch/Generated/ServiceEnumerations.cs
+++ sdk/src/Services/Elasticsearch/Generated/ServiceEnumerations.cs
@@ -335,6 +335,56 @@ namespace Amazon.Elasticsearch
 
 
     /// <summary>
+    /// Constants used for properties of type DeploymentStrategy.
+    /// </summary>
+    public class DeploymentStrategy : ConstantClass
+    {
+
+        /// <summary>
+        /// Constant CapacityOptimized for DeploymentStrategy
+        /// </summary>
+        public static readonly DeploymentStrategy CapacityOptimized = new DeploymentStrategy("CapacityOptimized");
+        /// <summary>
+        /// Constant Default for DeploymentStrategy
+        /// </summary>
+        public static readonly DeploymentStrategy Default = new DeploymentStrategy("Default");
+
+        /// <summary>
+        /// This constant constructor does not need to be called if the constant
+        /// you are attempting to use is already defined as a static instance of 
+        /// this class.
+        /// This constructor should be used to construct constants that are not
+        /// defined as statics, for instance if attempting to use a feature that is
+        /// newer than the current version of the SDK.
+        /// </summary>
+        public DeploymentStrategy(string value)
+            : base(value)
+        {
+        }
+
+        /// <summary>
+        /// Finds the constant for the unique value.
+        /// </summary>
+        /// <param name="value">The unique value for the constant</param>
+        /// <returns>The constant for the unique value</returns>
+        public static DeploymentStrategy FindValue(string value)
+        {
+            return FindValue<DeploymentStrategy>(value);
+        }
+
+        /// <summary>
+        /// Utility method to convert strings to the constant class.
+        /// </summary>
+        /// <param name="value">The string value to convert to the constant class.</param>
+        /// <returns></returns>
+        public static implicit operator DeploymentStrategy(string value)
+        {
+            return FindValue(value);
+        }
+    }
+
+
+    /// <summary>
     /// Constants used for properties of type DescribePackagesFilterName.
     /// </summary>
     public class DescribePackagesFilterName : ConstantClass
