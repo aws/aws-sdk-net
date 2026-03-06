a7edacb05611071f94a46ac648caa6513d084dc8
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2026-03-06 21:45:01 +0000

    Adds support for streaming memory records in AgentCore Memory

--- sdk/src/Services/BedrockAgentCoreControl/Generated/ServiceEnumerations.cs
+++ sdk/src/Services/BedrockAgentCoreControl/Generated/ServiceEnumerations.cs
@@ -671,6 +671,102 @@ namespace Amazon.BedrockAgentCoreControl
 
 
     /// <summary>
+    /// Constants used for properties of type ContentLevel.
+    /// </summary>
+    public class ContentLevel : ConstantClass
+    {
+
+        /// <summary>
+        /// Constant FULL_CONTENT for ContentLevel
+        /// </summary>
+        public static readonly ContentLevel FULL_CONTENT = new ContentLevel("FULL_CONTENT");
+        /// <summary>
+        /// Constant METADATA_ONLY for ContentLevel
+        /// </summary>
+        public static readonly ContentLevel METADATA_ONLY = new ContentLevel("METADATA_ONLY");
+
+        /// <summary>
+        /// This constant constructor does not need to be called if the constant
+        /// you are attempting to use is already defined as a static instance of 
+        /// this class.
+        /// This constructor should be used to construct constants that are not
+        /// defined as statics, for instance if attempting to use a feature that is
+        /// newer than the current version of the SDK.
+        /// </summary>
+        public ContentLevel(string value)
+            : base(value)
+        {
+        }
+
+        /// <summary>
+        /// Finds the constant for the unique value.
+        /// </summary>
+        /// <param name="value">The unique value for the constant</param>
+        /// <returns>The constant for the unique value</returns>
+        public static ContentLevel FindValue(string value)
+        {
+            return FindValue<ContentLevel>(value);
+        }
+
+        /// <summary>
+        /// Utility method to convert strings to the constant class.
+        /// </summary>
+        /// <param name="value">The string value to convert to the constant class.</param>
+        /// <returns></returns>
+        public static implicit operator ContentLevel(string value)
+        {
+            return FindValue(value);
+        }
+    }
+
+
+    /// <summary>
+    /// Constants used for properties of type ContentType.
+    /// </summary>
+    public class ContentType : ConstantClass
+    {
+
+        /// <summary>
+        /// Constant MEMORY_RECORDS for ContentType
+        /// </summary>
+        public static readonly ContentType MEMORY_RECORDS = new ContentType("MEMORY_RECORDS");
+
+        /// <summary>
+        /// This constant constructor does not need to be called if the constant
+        /// you are attempting to use is already defined as a static instance of 
+        /// this class.
+        /// This constructor should be used to construct constants that are not
+        /// defined as statics, for instance if attempting to use a feature that is
+        /// newer than the current version of the SDK.
+        /// </summary>
+        public ContentType(string value)
+            : base(value)
+        {
+        }
+
+        /// <summary>
+        /// Finds the constant for the unique value.
+        /// </summary>
+        /// <param name="value">The unique value for the constant</param>
+        /// <returns>The constant for the unique value</returns>
+        public static ContentType FindValue(string value)
+        {
+            return FindValue<ContentType>(value);
+        }
+
+        /// <summary>
+        /// Utility method to convert strings to the constant class.
+        /// </summary>
+        /// <param name="value">The string value to convert to the constant class.</param>
+        /// <returns></returns>
+        public static implicit operator ContentType(string value)
+        {
+            return FindValue(value);
+        }
+    }
+
+
+    /// <summary>
     /// Constants used for properties of type CredentialProviderType.
     /// </summary>
     public class CredentialProviderType : ConstantClass
