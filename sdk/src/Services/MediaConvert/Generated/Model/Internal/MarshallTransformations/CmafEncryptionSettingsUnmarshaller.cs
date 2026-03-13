1ec87bb795761ce24dc9d1d0fa6291a3d641e5f2
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2026-03-13 18:17:52 +0000

    This update adds support for Dolby AC-4 audio output, frame rate conversion between non-Dolby Vision inputs to Dolby Vision outputs, and clear lead CMAF HLS output.

--- sdk/src/Services/MediaConvert/Generated/Model/Internal/MarshallTransformations/CmafEncryptionSettingsUnmarshaller.cs
+++ sdk/src/Services/MediaConvert/Generated/Model/Internal/MarshallTransformations/CmafEncryptionSettingsUnmarshaller.cs
@@ -56,6 +56,12 @@ namespace Amazon.MediaConvert.Model.Internal.MarshallTransformations
             int targetDepth = context.CurrentDepth;
             while (context.ReadAtDepth(targetDepth, ref reader))
             {
+                if (context.TestExpression("clearLead", targetDepth))
+                {
+                    var unmarshaller = StringUnmarshaller.Instance;
+                    unmarshalledObject.ClearLead = unmarshaller.Unmarshall(context, ref reader);
+                    continue;
+                }
                 if (context.TestExpression("constantInitializationVector", targetDepth))
                 {
                     var unmarshaller = StringUnmarshaller.Instance;
