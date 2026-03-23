506165285fdc59ac3853866913758daad265ce37
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2026-03-23 18:18:29 +0000

    You can now use the UpdateRelatedItem API to update the content of comments and custom related items associated with a case.

---
+++ sdk/src/Services/ConnectCases/Generated/Model/Internal/MarshallTransformations/UpdateRelatedItemRequestMarshaller.cs
@@ -0,0 +1,139 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ * 
+ *  http://aws.amazon.com/apache2.0
+ * 
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/*
+ * Do not modify this file. This file is generated from the connectcases-2022-10-03.normal.json service model.
+ */
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+using Amazon.ConnectCases.Model;
+using Amazon.Runtime;
+using Amazon.Runtime.Internal;
+using Amazon.Runtime.Internal.Transform;
+using Amazon.Runtime.Internal.Util;
+using System.Text.Json;
+using System.Buffers;
+#if !NETFRAMEWORK
+using ThirdParty.RuntimeBackports;
+#endif
+#pragma warning disable CS0612,CS0618
+namespace Amazon.ConnectCases.Model.Internal.MarshallTransformations
+{
+    /// <summary>
+    /// UpdateRelatedItem Request Marshaller
+    /// </summary>       
+    public class UpdateRelatedItemRequestMarshaller : IMarshaller<IRequest, UpdateRelatedItemRequest> , IMarshaller<IRequest,AmazonWebServiceRequest>
+    {
+        /// <summary>
+        /// Marshaller the request object to the HTTP request.
+        /// </summary>  
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public IRequest Marshall(AmazonWebServiceRequest input)
+        {
+            return this.Marshall((UpdateRelatedItemRequest)input);
+        }
+
+        /// <summary>
+        /// Marshaller the request object to the HTTP request.
+        /// </summary>  
+        /// <param name="publicRequest"></param>
+        /// <returns></returns>
+        public IRequest Marshall(UpdateRelatedItemRequest publicRequest)
+        {
+            IRequest request = new DefaultRequest(publicRequest, "Amazon.ConnectCases");
+            request.Headers["Content-Type"] = "application/json";
+            request.Headers[Amazon.Util.HeaderKeys.XAmzApiVersion] = "2022-10-03";
+            request.HttpMethod = "PUT";
+
+            if (!publicRequest.IsSetCaseId())
+                throw new AmazonConnectCasesException("Request object does not have required field CaseId set");
+            request.AddPathResource("{caseId}", StringUtils.FromString(publicRequest.CaseId));
+            if (!publicRequest.IsSetDomainId())
+                throw new AmazonConnectCasesException("Request object does not have required field DomainId set");
+            request.AddPathResource("{domainId}", StringUtils.FromString(publicRequest.DomainId));
+            if (!publicRequest.IsSetRelatedItemId())
+                throw new AmazonConnectCasesException("Request object does not have required field RelatedItemId set");
+            request.AddPathResource("{relatedItemId}", StringUtils.FromString(publicRequest.RelatedItemId));
+            request.ResourcePath = "/domains/{domainId}/cases/{caseId}/related-items/{relatedItemId}";
+#if !NETFRAMEWORK
+            using ArrayPoolBufferWriter<byte> arrayPoolBufferWriter = new ArrayPoolBufferWriter<byte>();
+            using Utf8JsonWriter writer = new Utf8JsonWriter(arrayPoolBufferWriter);
+#else
+            using var memoryStream = new MemoryStream();
+            using Utf8JsonWriter writer = new Utf8JsonWriter(memoryStream);
+#endif
+            writer.WriteStartObject();
+            var context = new JsonMarshallerContext(request, writer);
+            if(publicRequest.IsSetContent())
+            {
+                context.Writer.WritePropertyName("content");
+                context.Writer.WriteStartObject();
+
+                var marshaller = RelatedItemUpdateContentMarshaller.Instance;
+                marshaller.Marshall(publicRequest.Content, context);
+
+                context.Writer.WriteEndObject();
+            }
+
+            if(publicRequest.IsSetPerformedBy())
+            {
+                context.Writer.WritePropertyName("performedBy");
+                context.Writer.WriteStartObject();
+
+                var marshaller = UserUnionMarshaller.Instance;
+                marshaller.Marshall(publicRequest.PerformedBy, context);
+
+                context.Writer.WriteEndObject();
+            }
+
+            writer.WriteEndObject();
+            writer.Flush();
+            // ToArray() must be called here because aspects of sigv4 signing require a byte array
+#if !NETFRAMEWORK
+            request.Content = arrayPoolBufferWriter.WrittenMemory.ToArray();
+#else
+            request.Content = memoryStream.ToArray();
+#endif
+            
+
+
+            return request;
+        }
+        private static UpdateRelatedItemRequestMarshaller _instance = new UpdateRelatedItemRequestMarshaller();        
+
+        internal static UpdateRelatedItemRequestMarshaller GetInstance()
+        {
+            return _instance;
+        }
+
+        /// <summary>
+        /// Gets the singleton.
+        /// </summary>  
+        public static UpdateRelatedItemRequestMarshaller Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+    }
+}
---
+++ sdk/src/Services/ConnectCases/Generated/Model/UpdateRelatedItemResponse.cs
@@ -0,0 +1,202 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ * 
+ *  http://aws.amazon.com/apache2.0
+ * 
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/*
+ * Do not modify this file. This file is generated from the connectcases-2022-10-03.normal.json service model.
+ */
+using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.Text;
+using System.IO;
+using System.Net;
+
+using Amazon.Runtime;
+using Amazon.Runtime.Internal;
+
+#pragma warning disable CS0612,CS0618,CS1570
+namespace Amazon.ConnectCases.Model
+{
+    /// <summary>
+    /// This is the response object from the UpdateRelatedItem operation.
+    /// </summary>
+    public partial class UpdateRelatedItemResponse : AmazonWebServiceResponse
+    {
+        private DateTime? _associationTime;
+        private RelatedItemContent _content;
+        private UserUnion _createdBy;
+        private UserUnion _lastUpdatedUser;
+        private string _relatedItemArn;
+        private string _relatedItemId;
+        private Dictionary<string, string> _tags = AWSConfigs.InitializeCollections ? new Dictionary<string, string>() : null;
+        private RelatedItemType _type;
+
+        /// <summary>
+        /// Gets and sets the property AssociationTime. 
+        /// <para>
+        /// Time at which the related item was associated with the case.
+        /// </para>
+        /// </summary>
+        [AWSProperty(Required=true)]
+        public DateTime? AssociationTime
+        {
+            get { return this._associationTime; }
+            set { this._associationTime = value; }
+        }
+
+        // Check to see if AssociationTime property is set
+        internal bool IsSetAssociationTime()
+        {
+            return this._associationTime.HasValue; 
+        }
+
+        /// <summary>
+        /// Gets and sets the property Content. 
+        /// <para>
+        /// Represents the content of the updated related item.
+        /// </para>
+        /// </summary>
+        [AWSProperty(Required=true)]
+        public RelatedItemContent Content
+        {
+            get { return this._content; }
+            set { this._content = value; }
+        }
+
+        // Check to see if Content property is set
+        internal bool IsSetContent()
+        {
+            return this._content != null;
+        }
+
+        /// <summary>
+        /// Gets and sets the property CreatedBy. 
+        /// <para>
+        /// Represents the creator of the related item.
+        /// </para>
+        /// </summary>
+        public UserUnion CreatedBy
+        {
+            get { return this._createdBy; }
+            set { this._createdBy = value; }
+        }
+
+        // Check to see if CreatedBy property is set
+        internal bool IsSetCreatedBy()
+        {
+            return this._createdBy != null;
+        }
+
+        /// <summary>
+        /// Gets and sets the property LastUpdatedUser. 
+        /// <para>
+        /// Represents the last user that updated the related item.
+        /// </para>
+        /// </summary>
+        public UserUnion LastUpdatedUser
+        {
+            get { return this._lastUpdatedUser; }
+            set { this._lastUpdatedUser = value; }
+        }
+
+        // Check to see if LastUpdatedUser property is set
+        internal bool IsSetLastUpdatedUser()
+        {
+            return this._lastUpdatedUser != null;
+        }
+
+        /// <summary>
+        /// Gets and sets the property RelatedItemArn. 
+        /// <para>
+        /// The Amazon Resource Name (ARN) of the updated related item.
+        /// </para>
+        /// </summary>
+        [AWSProperty(Required=true, Min=1, Max=500)]
+        public string RelatedItemArn
+        {
+            get { return this._relatedItemArn; }
+            set { this._relatedItemArn = value; }
+        }
+
+        // Check to see if RelatedItemArn property is set
+        internal bool IsSetRelatedItemArn()
+        {
+            return this._relatedItemArn != null;
+        }
+
+        /// <summary>
+        /// Gets and sets the property RelatedItemId. 
+        /// <para>
+        /// The unique identifier of the updated related item.
+        /// </para>
+        /// </summary>
+        [AWSProperty(Required=true, Min=1, Max=500)]
+        public string RelatedItemId
+        {
+            get { return this._relatedItemId; }
+            set { this._relatedItemId = value; }
+        }
+
+        // Check to see if RelatedItemId property is set
+        internal bool IsSetRelatedItemId()
+        {
+            return this._relatedItemId != null;
+        }
+
+        /// <summary>
+        /// Gets and sets the property Tags. 
+        /// <para>
+        /// A map of of key-value pairs that represent tags on a resource. Tags are used to organize,
+        /// track, or control access for this resource.
+        /// </para>
+        /// <para />
+        /// Starting with version 4 of the SDK this property will default to null. If no data for this property is returned
+        /// from the service the property will also be null. This was changed to improve performance and allow the SDK and caller
+        /// to distinguish between a property not set or a property being empty to clear out a value. To retain the previous
+        /// SDK behavior set the AWSConfigs.InitializeCollections static property to true.
+        /// </summary>
+        public Dictionary<string, string> Tags
+        {
+            get { return this._tags; }
+            set { this._tags = value; }
+        }
+
+        // Check to see if Tags property is set
+        internal bool IsSetTags()
+        {
+            return this._tags != null && (this._tags.Count > 0 || !AWSConfigs.InitializeCollections); 
+        }
+
+        /// <summary>
+        /// Gets and sets the property Type. 
+        /// <para>
+        /// Type of the updated related item.
+        /// </para>
+        /// </summary>
+        [AWSProperty(Required=true)]
+        public RelatedItemType Type
+        {
+            get { return this._type; }
+            set { this._type = value; }
+        }
+
+        // Check to see if Type property is set
+        internal bool IsSetType()
+        {
+            return this._type != null;
+        }
+
+    }
+}
