2c5e41101c9afcac7646517faf82fe30031a801c
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2026-04-01 18:20:00 +0000

    Updated SnapshotRetentionLimit documentation for ServerlessCache to correctly describe the parameter as number of days (max 35) instead of number of snapshots.

--- sdk/src/Services/ElastiCache/Generated/Model/CreateCacheParameterGroupRequest.cs
+++ sdk/src/Services/ElastiCache/Generated/Model/CreateCacheParameterGroupRequest.cs
@@ -68,7 +68,7 @@ namespace Amazon.ElastiCache.Model
         /// <summary>
         /// Instantiates CreateCacheParameterGroupRequest with the parameterized properties
         /// </summary>
-        /// <param name="cacheParameterGroupName">A user-specified name for the cache parameter group.</param>
+        /// <param name="cacheParameterGroupName">A user-specified name for the cache parameter group. This value is stored as a lowercase string.</param>
         /// <param name="cacheParameterGroupFamily">The name of the cache parameter group family that the cache parameter group can be used with. Valid values are: <c>valkey8</c> | <c>valkey7</c> | <c>memcached1.4</c> | <c>memcached1.5</c> | <c>memcached1.6</c> | <c>redis2.6</c> | <c>redis2.8</c> | <c>redis3.2</c> | <c>redis4.0</c> | <c>redis5.0</c> | <c>redis6.x</c> | <c>redis7</c> </param>
         /// <param name="description">A user-specified description for the cache parameter group.</param>
         public CreateCacheParameterGroupRequest(string cacheParameterGroupName, string cacheParameterGroupFamily, string description)
@@ -107,7 +107,8 @@ namespace Amazon.ElastiCache.Model
         /// <summary>
         /// Gets and sets the property CacheParameterGroupName. 
         /// <para>
-        /// A user-specified name for the cache parameter group.
+        /// A user-specified name for the cache parameter group. This value is stored as a lowercase
+        /// string.
         /// </para>
         /// </summary>
         [AWSProperty(Required=true)]
