0833b8833af592054e82cec0c25b33d9d6d4629b
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2026-04-01 18:20:03 +0000

    Added the ability to filter out empty sessions when listing sessions. Customers can now retrieve only sessions that still contain events, eliminating the need to check each session individually. No changes required for existing integrations.

--- sdk/src/Services/BedrockAgentCore/Generated/ServiceEnumerations.cs
+++ sdk/src/Services/BedrockAgentCore/Generated/ServiceEnumerations.cs
@@ -333,6 +333,52 @@ namespace Amazon.BedrockAgentCore
 
 
     /// <summary>
+    /// Constants used for properties of type EventFilterCondition.
+    /// </summary>
+    public class EventFilterCondition : ConstantClass
+    {
+
+        /// <summary>
+        /// Constant HAS_EVENTS for EventFilterCondition
+        /// </summary>
+        public static readonly EventFilterCondition HAS_EVENTS = new EventFilterCondition("HAS_EVENTS");
+
+        /// <summary>
+        /// This constant constructor does not need to be called if the constant
+        /// you are attempting to use is already defined as a static instance of 
+        /// this class.
+        /// This constructor should be used to construct constants that are not
+        /// defined as statics, for instance if attempting to use a feature that is
+        /// newer than the current version of the SDK.
+        /// </summary>
+        public EventFilterCondition(string value)
+            : base(value)
+        {
+        }
+
+        /// <summary>
+        /// Finds the constant for the unique value.
+        /// </summary>
+        /// <param name="value">The unique value for the constant</param>
+        /// <returns>The constant for the unique value</returns>
+        public static EventFilterCondition FindValue(string value)
+        {
+            return FindValue<EventFilterCondition>(value);
+        }
+
+        /// <summary>
+        /// Utility method to convert strings to the constant class.
+        /// </summary>
+        /// <param name="value">The string value to convert to the constant class.</param>
+        /// <returns></returns>
+        public static implicit operator EventFilterCondition(string value)
+        {
+            return FindValue(value);
+        }
+    }
+
+
+    /// <summary>
     /// Constants used for properties of type ExtractionJobStatus.
     /// </summary>
     public class ExtractionJobStatus : ConstantClass
