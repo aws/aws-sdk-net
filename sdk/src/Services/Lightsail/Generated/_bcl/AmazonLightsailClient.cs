e8d1fd84e2dfbe59e14fd360e662f79e5b005c3d
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2026-04-03 18:15:49 +0000

    Add support for tagging of Alarm resource type

--- sdk/src/Services/Lightsail/Generated/_bcl/AmazonLightsailClient.cs
+++ sdk/src/Services/Lightsail/Generated/_bcl/AmazonLightsailClient.cs
@@ -1842,6 +1842,12 @@ namespace Amazon.Lightsail
         /// For more information, see <a href="https://docs.aws.amazon.com/lightsail/latest/userguide/amazon-lightsail-notifications">Notifications
         /// in Amazon Lightsail</a>.
         /// </para>
+        ///  
+        /// <para>
+        /// The <c>create contact method</c> operation supports tag-based access control via request
+        /// tags. For more information, see the <a href="https://docs.aws.amazon.com/lightsail/latest/userguide/amazon-lightsail-controlling-access-using-tags">Lightsail
+        /// Developer Guide</a>.
+        /// </para>
         /// </summary>
         /// <param name="request">Container for the necessary parameters to execute the CreateContactMethod service method.</param>
         /// 
@@ -1900,6 +1906,12 @@ namespace Amazon.Lightsail
         /// Services Regions, and SMS text messages cannot be sent to some countries/regions.
         /// For more information, see <a href="https://docs.aws.amazon.com/lightsail/latest/userguide/amazon-lightsail-notifications">Notifications
         /// in Amazon Lightsail</a>.
+        /// </para>
+        ///  
+        /// <para>
+        /// The <c>create contact method</c> operation supports tag-based access control via request
+        /// tags. For more information, see the <a href="https://docs.aws.amazon.com/lightsail/latest/userguide/amazon-lightsail-controlling-access-using-tags">Lightsail
+        /// Developer Guide</a>.
         /// </para>
         /// </summary>
         /// <param name="request">Container for the necessary parameters to execute the CreateContactMethod service method.</param>
@@ -16126,6 +16138,12 @@ namespace Amazon.Lightsail
         /// overwrites the previous configuration of the alarm. The alarm is then evaluated with
         /// the updated configuration.
         /// </para>
+        ///  
+        /// <para>
+        /// The <c>put alarm</c> operation supports tag-based access control via request tags.
+        /// For more information, see the <a href="https://docs.aws.amazon.com/lightsail/latest/userguide/amazon-lightsail-controlling-access-using-tags">Lightsail
+        /// Developer Guide</a>.
+        /// </para>
         /// </summary>
         /// <param name="request">Container for the necessary parameters to execute the PutAlarm service method.</param>
         /// 
@@ -16194,6 +16212,12 @@ namespace Amazon.Lightsail
         /// When you update an existing alarm, its state is left unchanged, but the update completely
         /// overwrites the previous configuration of the alarm. The alarm is then evaluated with
         /// the updated configuration.
+        /// </para>
+        ///  
+        /// <para>
+        /// The <c>put alarm</c> operation supports tag-based access control via request tags.
+        /// For more information, see the <a href="https://docs.aws.amazon.com/lightsail/latest/userguide/amazon-lightsail-controlling-access-using-tags">Lightsail
+        /// Developer Guide</a>.
         /// </para>
         /// </summary>
         /// <param name="request">Container for the necessary parameters to execute the PutAlarm service method.</param>
