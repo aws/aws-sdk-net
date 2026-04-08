6bd4d3941e1cf525c82b91178b74982b5ee37da6
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2026-04-08 20:03:31 +0000

    Add UnableToListUpstreamImageReferrersException in ListImageReferrers

--- sdk/src/Services/ECR/Generated/Model/Internal/MarshallTransformations/ListImageReferrersResponseUnmarshaller.cs
+++ sdk/src/Services/ECR/Generated/Model/Internal/MarshallTransformations/ListImageReferrersResponseUnmarshaller.cs
@@ -101,6 +101,10 @@ namespace Amazon.ECR.Model.Internal.MarshallTransformations
                 {
                     return ServerExceptionUnmarshaller.Instance.Unmarshall(contextCopy, errorResponse, ref readerCopy);
                 }
+                if (errorResponse.Code != null && errorResponse.Code.Equals("UnableToListUpstreamImageReferrersException"))
+                {
+                    return UnableToListUpstreamImageReferrersExceptionUnmarshaller.Instance.Unmarshall(contextCopy, errorResponse, ref readerCopy);
+                }
                 if (errorResponse.Code != null && errorResponse.Code.Equals("ValidationException"))
                 {
                     return ValidationExceptionUnmarshaller.Instance.Unmarshall(contextCopy, errorResponse, ref readerCopy);
