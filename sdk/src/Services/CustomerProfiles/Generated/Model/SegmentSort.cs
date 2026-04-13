d9b0d3742f0acaa532185da1025ddf5671a250bd
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2026-04-13 18:21:31 +0000

    This release introduces changes to SegmentDefinition APIs to support sorting by attributes.

---
+++ sdk/src/Services/CustomerProfiles/Generated/Model/SegmentSort.cs
@@ -0,0 +1,64 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ * 
+ *  http://aws.amazon.com/apache2.0
+ * 
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/*
+ * Do not modify this file. This file is generated from the customer-profiles-2020-08-15.normal.json service model.
+ */
+using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.Text;
+using System.IO;
+using System.Net;
+
+using Amazon.Runtime;
+using Amazon.Runtime.Internal;
+
+#pragma warning disable CS0612,CS0618,CS1570
+namespace Amazon.CustomerProfiles.Model
+{
+    /// <summary>
+    /// Defines how segments should be sorted and ordered in the results.
+    /// </summary>
+    public partial class SegmentSort
+    {
+        private List<SortAttribute> _attributes = AWSConfigs.InitializeCollections ? new List<SortAttribute>() : null;
+
+        /// <summary>
+        /// Gets and sets the property Attributes. 
+        /// <para>
+        /// A list of attributes used to sort the segments and their ordering preferences.
+        /// </para>
+        /// <para />
+        /// Starting with version 4 of the SDK this property will default to null. If no data for this property is returned
+        /// from the service the property will also be null. This was changed to improve performance and allow the SDK and caller
+        /// to distinguish between a property not set or a property being empty to clear out a value. To retain the previous
+        /// SDK behavior set the AWSConfigs.InitializeCollections static property to true.
+        /// </summary>
+        [AWSProperty(Required=true, Min=1, Max=10)]
+        public List<SortAttribute> Attributes
+        {
+            get { return this._attributes; }
+            set { this._attributes = value; }
+        }
+
+        // Check to see if Attributes property is set
+        internal bool IsSetAttributes()
+        {
+            return this._attributes != null && (this._attributes.Count > 0 || !AWSConfigs.InitializeCollections); 
+        }
+
+    }
+}
--- sdk/src/Services/CustomerProfiles/Generated/ServiceEnumerations.cs
+++ sdk/src/Services/CustomerProfiles/Generated/ServiceEnumerations.cs
@@ -2799,6 +2799,110 @@ namespace Amazon.CustomerProfiles
 
 
     /// <summary>
+    /// Constants used for properties of type SegmentSortDataType.
+    /// </summary>
+    public class SegmentSortDataType : ConstantClass
+    {
+
+        /// <summary>
+        /// Constant DATE for SegmentSortDataType
+        /// </summary>
+        public static readonly SegmentSortDataType DATE = new SegmentSortDataType("DATE");
+        /// <summary>
+        /// Constant NUMBER for SegmentSortDataType
+        /// </summary>
+        public static readonly SegmentSortDataType NUMBER = new SegmentSortDataType("NUMBER");
+        /// <summary>
+        /// Constant STRING for SegmentSortDataType
+        /// </summary>
+        public static readonly SegmentSortDataType STRING = new SegmentSortDataType("STRING");
+
+        /// <summary>
+        /// This constant constructor does not need to be called if the constant
+        /// you are attempting to use is already defined as a static instance of 
+        /// this class.
+        /// This constructor should be used to construct constants that are not
+        /// defined as statics, for instance if attempting to use a feature that is
+        /// newer than the current version of the SDK.
+        /// </summary>
+        public SegmentSortDataType(string value)
+            : base(value)
+        {
+        }
+
+        /// <summary>
+        /// Finds the constant for the unique value.
+        /// </summary>
+        /// <param name="value">The unique value for the constant</param>
+        /// <returns>The constant for the unique value</returns>
+        public static SegmentSortDataType FindValue(string value)
+        {
+            return FindValue<SegmentSortDataType>(value);
+        }
+
+        /// <summary>
+        /// Utility method to convert strings to the constant class.
+        /// </summary>
+        /// <param name="value">The string value to convert to the constant class.</param>
+        /// <returns></returns>
+        public static implicit operator SegmentSortDataType(string value)
+        {
+            return FindValue(value);
+        }
+    }
+
+
+    /// <summary>
+    /// Constants used for properties of type SegmentSortOrder.
+    /// </summary>
+    public class SegmentSortOrder : ConstantClass
+    {
+
+        /// <summary>
+        /// Constant ASC for SegmentSortOrder
+        /// </summary>
+        public static readonly SegmentSortOrder ASC = new SegmentSortOrder("ASC");
+        /// <summary>
+        /// Constant DESC for SegmentSortOrder
+        /// </summary>
+        public static readonly SegmentSortOrder DESC = new SegmentSortOrder("DESC");
+
+        /// <summary>
+        /// This constant constructor does not need to be called if the constant
+        /// you are attempting to use is already defined as a static instance of 
+        /// this class.
+        /// This constructor should be used to construct constants that are not
+        /// defined as statics, for instance if attempting to use a feature that is
+        /// newer than the current version of the SDK.
+        /// </summary>
+        public SegmentSortOrder(string value)
+            : base(value)
+        {
+        }
+
+        /// <summary>
+        /// Finds the constant for the unique value.
+        /// </summary>
+        /// <param name="value">The unique value for the constant</param>
+        /// <returns>The constant for the unique value</returns>
+        public static SegmentSortOrder FindValue(string value)
+        {
+            return FindValue<SegmentSortOrder>(value);
+        }
+
+        /// <summary>
+        /// Utility method to convert strings to the constant class.
+        /// </summary>
+        /// <param name="value">The string value to convert to the constant class.</param>
+        /// <returns></returns>
+        public static implicit operator SegmentSortOrder(string value)
+        {
+            return FindValue(value);
+        }
+    }
+
+
+    /// <summary>
     /// Constants used for properties of type SegmentType.
     /// </summary>
     public class SegmentType : ConstantClass
@@ -2968,6 +3072,56 @@ namespace Amazon.CustomerProfiles
         /// <param name="value">The string value to convert to the constant class.</param>
         /// <returns></returns>
         public static implicit operator ServiceNowConnectorOperator(string value)
+        {
+            return FindValue(value);
+        }
+    }
+
+
+    /// <summary>
+    /// Constants used for properties of type SortAttributeType.
+    /// </summary>
+    public class SortAttributeType : ConstantClass
+    {
+
+        /// <summary>
+        /// Constant CALCULATED for SortAttributeType
+        /// </summary>
+        public static readonly SortAttributeType CALCULATED = new SortAttributeType("CALCULATED");
+        /// <summary>
+        /// Constant PROFILE for SortAttributeType
+        /// </summary>
+        public static readonly SortAttributeType PROFILE = new SortAttributeType("PROFILE");
+
+        /// <summary>
+        /// This constant constructor does not need to be called if the constant
+        /// you are attempting to use is already defined as a static instance of 
+        /// this class.
+        /// This constructor should be used to construct constants that are not
+        /// defined as statics, for instance if attempting to use a feature that is
+        /// newer than the current version of the SDK.
+        /// </summary>
+        public SortAttributeType(string value)
+            : base(value)
+        {
+        }
+
+        /// <summary>
+        /// Finds the constant for the unique value.
+        /// </summary>
+        /// <param name="value">The unique value for the constant</param>
+        /// <returns>The constant for the unique value</returns>
+        public static SortAttributeType FindValue(string value)
+        {
+            return FindValue<SortAttributeType>(value);
+        }
+
+        /// <summary>
+        /// Utility method to convert strings to the constant class.
+        /// </summary>
+        /// <param name="value">The string value to convert to the constant class.</param>
+        /// <returns></returns>
+        public static implicit operator SortAttributeType(string value)
         {
             return FindValue(value);
         }
