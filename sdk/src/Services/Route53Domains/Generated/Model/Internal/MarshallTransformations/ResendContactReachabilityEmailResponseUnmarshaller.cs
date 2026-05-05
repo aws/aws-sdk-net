14e8e1cef45bc7350e65bef3171d58e2438c2a06
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2026-05-05 18:16:22 +0000

    This release adds the TLDInMaintenance exception.

--- sdk/src/Services/Route53Domains/Generated/Model/Internal/MarshallTransformations/ResendContactReachabilityEmailResponseUnmarshaller.cs
+++ sdk/src/Services/Route53Domains/Generated/Model/Internal/MarshallTransformations/ResendContactReachabilityEmailResponseUnmarshaller.cs
@@ -103,6 +103,10 @@ namespace Amazon.Route53Domains.Model.Internal.MarshallTransformations
                 {
                     return OperationLimitExceededExceptionUnmarshaller.Instance.Unmarshall(contextCopy, errorResponse, ref readerCopy);
                 }
+                if (errorResponse.Code != null && errorResponse.Code.Equals("TLDInMaintenance"))
+                {
+                    return TLDInMaintenanceExceptionUnmarshaller.Instance.Unmarshall(contextCopy, errorResponse, ref readerCopy);
+                }
                 if (errorResponse.Code != null && errorResponse.Code.Equals("UnsupportedTLD"))
                 {
                     return UnsupportedTLDExceptionUnmarshaller.Instance.Unmarshall(contextCopy, errorResponse, ref readerCopy);
