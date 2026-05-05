9d867869836952f6cc1442ad20d63f2f2b5f1d2d
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2026-05-05 18:16:28 +0000

    Adds support for tagging CloudFront Functions and KeyValueStores resources.

--- sdk/src/Services/CloudFront/Generated/Model/CreateFunctionRequest.cs
+++ sdk/src/Services/CloudFront/Generated/Model/CreateFunctionRequest.cs
@@ -57,6 +57,7 @@ namespace Amazon.CloudFront.Model
         private MemoryStream _functionCode;
         private FunctionConfig _functionConfig;
         private string _name;
+        private Tags _tags;
 
         /// <summary>
         /// Gets and sets the property FunctionCode. 
@@ -116,6 +117,21 @@ namespace Amazon.CloudFront.Model
         internal bool IsSetName()
         {
             return this._name != null;
+        }
+
+        /// <summary>
+        /// Gets and sets the property Tags.
+        /// </summary>
+        public Tags Tags
+        {
+            get { return this._tags; }
+            set { this._tags = value; }
+        }
+
+        // Check to see if Tags property is set
+        internal bool IsSetTags()
+        {
+            return this._tags != null;
         }
 
     }
