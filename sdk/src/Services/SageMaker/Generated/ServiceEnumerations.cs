efed054889621dbf0d178785804e02685924ed21
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2026-05-21 20:23:01 +0000

    Add support for disabling home EFS file system creation on SageMaker domains.

--- sdk/src/Services/SageMaker/Generated/ServiceEnumerations.cs
+++ sdk/src/Services/SageMaker/Generated/ServiceEnumerations.cs
@@ -8687,6 +8687,56 @@ namespace Amazon.SageMaker
 
 
     /// <summary>
+    /// Constants used for properties of type HomeEfsFileSystemCreation.
+    /// </summary>
+    public class HomeEfsFileSystemCreation : ConstantClass
+    {
+
+        /// <summary>
+        /// Constant Disabled for HomeEfsFileSystemCreation
+        /// </summary>
+        public static readonly HomeEfsFileSystemCreation Disabled = new HomeEfsFileSystemCreation("Disabled");
+        /// <summary>
+        /// Constant Enabled for HomeEfsFileSystemCreation
+        /// </summary>
+        public static readonly HomeEfsFileSystemCreation Enabled = new HomeEfsFileSystemCreation("Enabled");
+
+        /// <summary>
+        /// This constant constructor does not need to be called if the constant
+        /// you are attempting to use is already defined as a static instance of 
+        /// this class.
+        /// This constructor should be used to construct constants that are not
+        /// defined as statics, for instance if attempting to use a feature that is
+        /// newer than the current version of the SDK.
+        /// </summary>
+        public HomeEfsFileSystemCreation(string value)
+            : base(value)
+        {
+        }
+
+        /// <summary>
+        /// Finds the constant for the unique value.
+        /// </summary>
+        /// <param name="value">The unique value for the constant</param>
+        /// <returns>The constant for the unique value</returns>
+        public static HomeEfsFileSystemCreation FindValue(string value)
+        {
+            return FindValue<HomeEfsFileSystemCreation>(value);
+        }
+
+        /// <summary>
+        /// Utility method to convert strings to the constant class.
+        /// </summary>
+        /// <param name="value">The string value to convert to the constant class.</param>
+        /// <returns></returns>
+        public static implicit operator HomeEfsFileSystemCreation(string value)
+        {
+            return FindValue(value);
+        }
+    }
+
+
+    /// <summary>
     /// Constants used for properties of type HubContentSortBy.
     /// </summary>
     public class HubContentSortBy : ConstantClass
