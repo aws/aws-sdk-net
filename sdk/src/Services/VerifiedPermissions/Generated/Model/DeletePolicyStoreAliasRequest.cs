71d1bf3b236608874bd51cf7431dc557f54c62a7
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2026-05-21 20:22:56 +0000

    Support hard deleting policy store aliases. Users can now delete an alias and immediately reassign it to a different policy store without waiting for the soft-delete retention period.

--- sdk/src/Services/VerifiedPermissions/Generated/Model/DeletePolicyStoreAliasRequest.cs
+++ sdk/src/Services/VerifiedPermissions/Generated/Model/DeletePolicyStoreAliasRequest.cs
@@ -40,16 +40,30 @@ namespace Amazon.VerifiedPermissions.Model
     /// </para>
     ///  
     /// <para>
-    /// When a policy store alias is deleted, it enters the <c>PendingDeletion</c> state.
-    /// When a policy store alias is in the <c>PendingDeletion</c> state, new policy store
-    /// aliases cannot be created with the same name. If the policy store alias is used in
-    /// an API that has a <c>policyStoreId</c> field, the operation will fail with a <c>ResourceNotFound</c>
+    /// By default, when a policy store alias is deleted, it enters the <c>PendingDeletion</c>
+    /// state. When a policy store alias is in the <c>PendingDeletion</c> state, new policy
+    /// store aliases cannot be created with the same name. If the policy store alias is used
+    /// in an API that has a <c>policyStoreId</c> field, the operation will fail with a <c>ResourceNotFound</c>
     /// exception.
     /// </para>
+    ///  
+    /// <para>
+    /// To immediately delete a policy store alias and bypass the <c>PendingDeletion</c> state,
+    /// set the <c>deletionMode</c> parameter to <c>HardDelete</c>.
+    /// </para>
+    ///  <important> 
+    /// <para>
+    /// Verified Permissions is eventually consistent. If you hard delete a policy store alias
+    /// and then immediately recreate it to be associated with a different policy store, requests
+    /// that reference this alias may continue to be evaluated against the previously associated
+    /// policy store for a short period of time.
+    /// </para>
+    ///  </important>
     /// </summary>
     public partial class DeletePolicyStoreAliasRequest : AmazonVerifiedPermissionsRequest
     {
         private string _aliasName;
+        private DeletionMode _deletionMode;
 
         /// <summary>
         /// Gets and sets the property AliasName. 
@@ -73,6 +87,35 @@ namespace Amazon.VerifiedPermissions.Model
         internal bool IsSetAliasName()
         {
             return this._aliasName != null;
+        }
+
+        /// <summary>
+        /// Gets and sets the property DeletionMode. 
+        /// <para>
+        /// Specifies the deletion mode for the policy store alias. The valid values are:
+        /// </para>
+        ///  <ul> <li> 
+        /// <para>
+        ///  <b>SoftDelete</b> – The policy store alias enters the <c>PendingDeletion</c> state.
+        /// This is the default behavior when no <c>deletionMode</c> is specified.
+        /// </para>
+        ///  </li> <li> 
+        /// <para>
+        ///  <b>HardDelete</b> – The policy store alias is immediately deleted, bypassing the
+        /// <c>PendingDeletion</c> state.
+        /// </para>
+        ///  </li> </ul>
+        /// </summary>
+        public DeletionMode DeletionMode
+        {
+            get { return this._deletionMode; }
+            set { this._deletionMode = value; }
+        }
+
+        // Check to see if DeletionMode property is set
+        internal bool IsSetDeletionMode()
+        {
+            return this._deletionMode != null;
         }
 
     }
