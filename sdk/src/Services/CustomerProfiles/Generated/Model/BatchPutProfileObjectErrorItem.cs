9d0e6fdec90df1c904d9fbb2c7830fcca3144057
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2026-05-28 18:28:22 +0000

    BatchPutProfileObject API adds multiple profile objects to a domain of a given ObjectType in a single API call.

---
+++ sdk/src/Services/CustomerProfiles/Generated/Model/BatchPutProfileObjectErrorItem.cs
@@ -0,0 +1,99 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ * 
+ *  http://aws.amazon.com/apache2.0
+ * 
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/*
+ * Do not modify this file. This file is generated from the customer-profiles-2020-08-15.normal.json service model.
+ */
+using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.Text;
+using System.IO;
+using System.Net;
+
+using Amazon.Runtime;
+using Amazon.Runtime.Internal;
+
+#pragma warning disable CS0612,CS0618,CS1570
+namespace Amazon.CustomerProfiles.Model
+{
+    /// <summary>
+    /// An item that failed to be added to the domain.
+    /// </summary>
+    public partial class BatchPutProfileObjectErrorItem
+    {
+        private int? _code;
+        private string _id;
+        private string _message;
+
+        /// <summary>
+        /// Gets and sets the property Code. 
+        /// <para>
+        /// The HTTP status code for the error.
+        /// </para>
+        /// </summary>
+        [AWSProperty(Required=true, Min=200, Max=599)]
+        public int? Code
+        {
+            get { return this._code; }
+            set { this._code = value; }
+        }
+
+        // Check to see if Code property is set
+        internal bool IsSetCode()
+        {
+            return this._code.HasValue; 
+        }
+
+        /// <summary>
+        /// Gets and sets the property Id. 
+        /// <para>
+        /// The unique identifier of the item in the batch request that failed.
+        /// </para>
+        /// </summary>
+        [AWSProperty(Required=true, Min=1, Max=64)]
+        public string Id
+        {
+            get { return this._id; }
+            set { this._id = value; }
+        }
+
+        // Check to see if Id property is set
+        internal bool IsSetId()
+        {
+            return this._id != null;
+        }
+
+        /// <summary>
+        /// Gets and sets the property Message. 
+        /// <para>
+        /// A message describing the error.
+        /// </para>
+        /// </summary>
+        [AWSProperty(Min=1, Max=1000)]
+        public string Message
+        {
+            get { return this._message; }
+            set { this._message = value; }
+        }
+
+        // Check to see if Message property is set
+        internal bool IsSetMessage()
+        {
+            return this._message != null;
+        }
+
+    }
+}
