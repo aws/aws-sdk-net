a73b157d63e790dd2703a2a85efeae3d275fd354
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2026-06-02 18:32:08 +0000

    Added iterator description to the GetRecords API response for Amazon Keyspaces Change Data Capture (CDC) streams, enabling consumers to track their current position within the stream.

--- sdk/src/Services/KeyspacesStreams/Generated/ServiceEnumerations.cs
+++ sdk/src/Services/KeyspacesStreams/Generated/ServiceEnumerations.cs
@@ -25,6 +25,56 @@ namespace Amazon.KeyspacesStreams
 {
 
     /// <summary>
+    /// Constants used for properties of type IteratorPosition.
+    /// </summary>
+    public class IteratorPosition : ConstantClass
+    {
+
+        /// <summary>
+        /// Constant AT_TIP for IteratorPosition
+        /// </summary>
+        public static readonly IteratorPosition AT_TIP = new IteratorPosition("AT_TIP");
+        /// <summary>
+        /// Constant BEHIND_TIP for IteratorPosition
+        /// </summary>
+        public static readonly IteratorPosition BEHIND_TIP = new IteratorPosition("BEHIND_TIP");
+
+        /// <summary>
+        /// This constant constructor does not need to be called if the constant
+        /// you are attempting to use is already defined as a static instance of 
+        /// this class.
+        /// This constructor should be used to construct constants that are not
+        /// defined as statics, for instance if attempting to use a feature that is
+        /// newer than the current version of the SDK.
+        /// </summary>
+        public IteratorPosition(string value)
+            : base(value)
+        {
+        }
+
+        /// <summary>
+        /// Finds the constant for the unique value.
+        /// </summary>
+        /// <param name="value">The unique value for the constant</param>
+        /// <returns>The constant for the unique value</returns>
+        public static IteratorPosition FindValue(string value)
+        {
+            return FindValue<IteratorPosition>(value);
+        }
+
+        /// <summary>
+        /// Utility method to convert strings to the constant class.
+        /// </summary>
+        /// <param name="value">The string value to convert to the constant class.</param>
+        /// <returns></returns>
+        public static implicit operator IteratorPosition(string value)
+        {
+            return FindValue(value);
+        }
+    }
+
+
+    /// <summary>
     /// Constants used for properties of type OriginType.
     /// </summary>
     public class OriginType : ConstantClass
