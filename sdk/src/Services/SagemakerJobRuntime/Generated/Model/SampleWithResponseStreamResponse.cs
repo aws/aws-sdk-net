047c664a1bc67502d92e37a9427d644abb63505d
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2026-06-02 18:32:04 +0000

    Amazon SageMaker Job Runtime is a new service for managing trajectory data during multi-turn customization jobs. It provides APIs to send inference requests to models during job execution, mark rollouts as complete, and submit reward values for training trajectories.

---
+++ sdk/src/Services/SagemakerJobRuntime/Generated/Model/SampleWithResponseStreamResponse.cs
@@ -0,0 +1,109 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ * 
+ *  http://aws.amazon.com/apache2.0
+ * 
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/*
+ * Do not modify this file. This file is generated from the sagemakerjobruntime-2026-02-01.normal.json service model.
+ */
+using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.Text;
+using System.IO;
+using System.Net;
+
+using Amazon.Runtime;
+using Amazon.Runtime.Internal;
+
+#pragma warning disable CS0612,CS0618,CS1570
+namespace Amazon.SagemakerJobRuntime.Model
+{
+    /// <summary>
+    /// This is the response object from the SampleWithResponseStream operation.
+    /// </summary>
+    public partial class SampleWithResponseStreamResponse : AmazonWebServiceResponse, IDisposable
+    {
+        private Stream _body;
+        private string _contentType;
+
+        /// <summary>
+        /// Gets and sets the property Body. 
+        /// <para>
+        /// The streaming response body, delivered as a series of PayloadPart events.
+        /// </para>
+        /// </summary>
+        [AWSProperty(Required=true, Sensitive=true)]
+        public Stream Body
+        {
+            get { return this._body; }
+            set { this._body = value; }
+        }
+
+        // Check to see if Body property is set
+        internal bool IsSetBody()
+        {
+            return this._body != null;
+        }
+
+        /// <summary>
+        /// Gets and sets the property ContentType. 
+        /// <para>
+        /// MIME type of the streaming inference result.
+        /// </para>
+        /// </summary>
+        public string ContentType
+        {
+            get { return this._contentType; }
+            set { this._contentType = value; }
+        }
+
+        // Check to see if ContentType property is set
+        internal bool IsSetContentType()
+        {
+            return this._contentType != null;
+        }
+
+        #region Dispose Pattern
+
+        private bool _disposed;
+
+        /// <summary>
+        /// Disposes of all managed and unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Disposes of all managed and unmanaged resources.
+        /// </summary>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+            {
+                this._body?.Dispose();
+                this._body = null;
+            }
+
+            this._disposed = true;
+         }
+
+         #endregion
+    }
+}
