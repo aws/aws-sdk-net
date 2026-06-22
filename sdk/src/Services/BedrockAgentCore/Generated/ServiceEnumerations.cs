9658eec93cbd1111c7d2c28bef8986ce910f3733
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2026-06-22 19:05:14 +0000

    Adds an optional extractionMode field to CreateEvent. SKIP retains the event in short-term memory but excludes it from long-term memory extraction.

--- sdk/src/Services/BedrockAgentCore/Generated/ServiceEnumerations.cs
+++ sdk/src/Services/BedrockAgentCore/Generated/ServiceEnumerations.cs
@@ -971,6 +971,52 @@ namespace Amazon.BedrockAgentCore
 
 
     /// <summary>
+    /// Constants used for properties of type ExtractionMode.
+    /// </summary>
+    public class ExtractionMode : ConstantClass
+    {
+
+        /// <summary>
+        /// Constant SKIP for ExtractionMode
+        /// </summary>
+        public static readonly ExtractionMode SKIP = new ExtractionMode("SKIP");
+
+        /// <summary>
+        /// This constant constructor does not need to be called if the constant
+        /// you are attempting to use is already defined as a static instance of 
+        /// this class.
+        /// This constructor should be used to construct constants that are not
+        /// defined as statics, for instance if attempting to use a feature that is
+        /// newer than the current version of the SDK.
+        /// </summary>
+        public ExtractionMode(string value)
+            : base(value)
+        {
+        }
+
+        /// <summary>
+        /// Finds the constant for the unique value.
+        /// </summary>
+        /// <param name="value">The unique value for the constant</param>
+        /// <returns>The constant for the unique value</returns>
+        public static ExtractionMode FindValue(string value)
+        {
+            return FindValue<ExtractionMode>(value);
+        }
+
+        /// <summary>
+        /// Utility method to convert strings to the constant class.
+        /// </summary>
+        /// <param name="value">The string value to convert to the constant class.</param>
+        /// <returns></returns>
+        public static implicit operator ExtractionMode(string value)
+        {
+            return FindValue(value);
+        }
+    }
+
+
+    /// <summary>
     /// Constants used for properties of type HarnessBedrockApiFormat.
     /// </summary>
     public class HarnessBedrockApiFormat : ConstantClass
