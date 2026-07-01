1915c2970551ed4e75a9ecdc066b048e7dca8851
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2026-07-01 18:16:17 +0000

    Adds support for integer-second duration normalization and the option to disable explicit weighted prediction.

--- sdk/src/Services/MediaConvert/Generated/Model/Container.cs
+++ sdk/src/Services/MediaConvert/Generated/Model/Container.cs
@@ -57,9 +57,9 @@ namespace Amazon.MediaConvert.Model
 
         /// <summary>
         /// Gets and sets the property Format. The format of your media file. For example: MP4,
-        /// QuickTime (MOV), Matroska (MKV), WebM, MXF, Wave, AVI, MPEG-TS, or MPEG-PS. Note that
-        /// this will be blank if your media file has a format that the MediaConvert Probe operation
-        /// does not recognize.
+        /// QuickTime (MOV), Matroska (MKV), WebM, MXF, Wave, AVI, MPEG-TS, MPEG-PS, or MP3. Note
+        /// that this will be blank if your media file has a format that the MediaConvert Probe
+        /// operation does not recognize.
         /// </summary>
         public Format Format
         {
