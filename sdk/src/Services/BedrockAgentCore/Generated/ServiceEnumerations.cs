058c8273836f8c3f6086010481f5e3716deb09e9
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2026-08-06 18:32:52 +0000

    Add support for capacity provider sessions in Amazon Bedrock AgentCore. Customers can now delete an active session running on a runtime instance launched through their capacity provider.

--- sdk/src/Services/BedrockAgentCore/Generated/ServiceEnumerations.cs
+++ sdk/src/Services/BedrockAgentCore/Generated/ServiceEnumerations.cs
@@ -493,6 +493,72 @@ namespace Amazon.BedrockAgentCore
 
 
     /// <summary>
+    /// Constants used for properties of type CapacityProviderSessionStatus.
+    /// </summary>
+    public class CapacityProviderSessionStatus : ConstantClass
+    {
+
+        /// <summary>
+        /// Constant Active for CapacityProviderSessionStatus
+        /// </summary>
+        public static readonly CapacityProviderSessionStatus Active = new CapacityProviderSessionStatus("Active");
+        /// <summary>
+        /// Constant Deleted for CapacityProviderSessionStatus
+        /// </summary>
+        public static readonly CapacityProviderSessionStatus Deleted = new CapacityProviderSessionStatus("Deleted");
+        /// <summary>
+        /// Constant Deleting for CapacityProviderSessionStatus
+        /// </summary>
+        public static readonly CapacityProviderSessionStatus Deleting = new CapacityProviderSessionStatus("Deleting");
+        /// <summary>
+        /// Constant Deprovisioning for CapacityProviderSessionStatus
+        /// </summary>
+        public static readonly CapacityProviderSessionStatus Deprovisioning = new CapacityProviderSessionStatus("Deprovisioning");
+        /// <summary>
+        /// Constant Provisioning for CapacityProviderSessionStatus
+        /// </summary>
+        public static readonly CapacityProviderSessionStatus Provisioning = new CapacityProviderSessionStatus("Provisioning");
+        /// <summary>
+        /// Constant Stopped for CapacityProviderSessionStatus
+        /// </summary>
+        public static readonly CapacityProviderSessionStatus Stopped = new CapacityProviderSessionStatus("Stopped");
+
+        /// <summary>
+        /// This constant constructor does not need to be called if the constant
+        /// you are attempting to use is already defined as a static instance of 
+        /// this class.
+        /// This constructor should be used to construct constants that are not
+        /// defined as statics, for instance if attempting to use a feature that is
+        /// newer than the current version of the SDK.
+        /// </summary>
+        public CapacityProviderSessionStatus(string value)
+            : base(value)
+        {
+        }
+
+        /// <summary>
+        /// Finds the constant for the unique value.
+        /// </summary>
+        /// <param name="value">The unique value for the constant</param>
+        /// <returns>The constant for the unique value</returns>
+        public static CapacityProviderSessionStatus FindValue(string value)
+        {
+            return FindValue<CapacityProviderSessionStatus>(value);
+        }
+
+        /// <summary>
+        /// Utility method to convert strings to the constant class.
+        /// </summary>
+        /// <param name="value">The string value to convert to the constant class.</param>
+        /// <returns></returns>
+        public static implicit operator CapacityProviderSessionStatus(string value)
+        {
+            return FindValue(value);
+        }
+    }
+
+
+    /// <summary>
     /// Constants used for properties of type CloudWatchLogsFilterOperator.
     /// </summary>
     public class CloudWatchLogsFilterOperator : ConstantClass
