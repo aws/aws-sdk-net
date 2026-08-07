4868dd80b93078fa189e8130927e8b3615b627d0
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2026-08-07 18:19:08 +0000

    Increased the maximum allowed length of the oauthToken parameter in the CreateApp and UpdateApp APIs to support longer OAuth tokens issued by third-party Git providers.

--- sdk/src/Services/Amplify/Generated/Model/CreateAppRequest.cs
+++ sdk/src/Services/Amplify/Generated/Model/CreateAppRequest.cs
@@ -464,7 +464,7 @@ namespace Amazon.Amplify.Model
         /// .
         /// </para>
         /// </summary>
-        [AWSProperty(Sensitive=true, Max=1000)]
+        [AWSProperty(Sensitive=true, Max=4096)]
         public string OauthToken
         {
             get { return this._oauthToken; }
