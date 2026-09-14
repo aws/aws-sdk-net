8b7830942e5fde1220c1a1b1fed57a4e19a57702
aws-sdk-dotnet-automation <github-aws-sdk-dotnet-automation@amazon.com>
2026-09-14 19:01:44 +0000

    Increased the maximum number of services returned in the supportEligibleSpendByService field of ListEnterpriseSupportLinkedAccountCharges

--- sdk/src/Services/Billing/Generated/Model/LinkedAccountCharge.cs
+++ sdk/src/Services/Billing/Generated/Model/LinkedAccountCharge.cs
@@ -203,7 +203,7 @@ namespace Amazon.Billing.Model
         /// to distinguish between a property not set or a property being empty to clear out a value. To retain the previous
         /// SDK behavior set the AWSConfigs.InitializeCollections static property to true.
         /// </summary>
-        [AWSProperty(Min=0, Max=50)]
+        [AWSProperty(Min=0, Max=200)]
         public List<ServiceLevelAccountUsage> SupportEligibleSpendByService
         {
             get { return this._supportEligibleSpendByService; }
